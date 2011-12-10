c6e4c46b148590650869a632079a5a7d617c72d4
Geoff Schmidt <geoff@geoffschmidt.com>
2011-12-09 16:38:31 -0800

    liveui requires jquery

--- packages/liveui/package.js
+++ packages/liveui/package.js
@@ -6,4 +6,11 @@ Package.describe({
 Package.require('underscore');
 Package.require('livedata');
 Package.require('session');
+
+// XXX Depends on jquery because we need a selector engine to resolve
+// event maps. What would be nice is, if you've included jquery or
+// zepto, use one of those; if not, ship our own copy of sizzle (but,
+// you still want the event object normalization that jquery provides?)
+Package.require('jquery');
+
 Package.client_file('liveui.js');
