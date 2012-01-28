7f133d37b5e8f9ea27110d6be036f3ede5160d1c
Nick Martin <nim@meteor.com>
2012-01-27 16:53:38 -0800

    Move htmljs test. This was missed as it happened at the same time as the test migration.

--- packages/htmljs/package.js
+++ packages/htmljs/package.js
@@ -6,3 +6,9 @@ Package.on_use(function (api) {
   // Note: html.js will optionally use jquery if it's available
   api.add_files('html.js', 'client');
 });
+
+Package.on_test(function (api) {
+  api.use('htmljs', 'client');
+  api.use('tinytest');
+  api.add_files('htmljs_test.js', 'client');
+});
