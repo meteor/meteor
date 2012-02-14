42a29bd1777725445c031d1e96922ec3db9ba10c
Nick Martin <nim@meteor.com>
2012-02-13 19:53:16 -0800

    Remove JSON from sockjs. We already are shipping a JSON, we don't need another.

--- packages/stream/package.js
+++ packages/stream/package.js
@@ -4,7 +4,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.use(['underscore', 'logging', 'uuid'], ['client', 'server']);
+  api.use(['underscore', 'logging', 'uuid', 'json'], ['client', 'server']);
   api.use('reload', 'client');
 
   api.add_files('sockjs-0.2.1.js', 'client');
