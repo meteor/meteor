6037802e53b9f3b4488fc52431a98530286c0efb
Nick Martin <nim@meteor.com>
2012-02-21 14:48:20 -0800

    Remove all example files.
    None of them are needed for now. Future self: check here if you have an emacs mode you want to install.

--- admin/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(meteor):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="meteor" command="/usr/bin/meteor"
--- admin/debian/meteor.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the meteor package
-#
-0 4	* * *	root	[ -x /usr/bin/meteor_maintenance ] && /usr/bin/meteor_maintenance
