ba34c25de18ea6e2531c5e3ea74aa1d5e36d8fdf
Nick Martin <nim@meteor.com>
2012-02-21 14:55:00 -0800

    Build and install meteor.

--- admin/build-release.sh
+++ admin/build-release.sh
@@ -5,6 +5,7 @@ set -e
 # cd to top level dir
 cd `dirname $0`
 cd ..
+TOPDIR=$(pwd)
 
 UNAME=$(uname)
 ARCH=$(uname -m)
@@ -45,3 +46,20 @@ TARBALL=~/meteor-package-${UNAME}-${ARCH}-${VERSION}.tar.gz
 echo "Tarring to: $TARBALL"
 
 tar -C /usr/local --exclude .meteor/local -czf "$TARBALL" meteor
+
+
+if [ "$UNAME" == "Linux" ] ; then
+    echo "Building debian package"
+    DEBDIR=$(mktemp -d)
+    cd "$DEBDIR"
+    cp "$TARBALL" "meteor_${VERSION}.orig.tar.gz"
+    mkdir "meteor-${VERSION}"
+    cd "meteor-${VERSION}"
+    cp -r "$TOPDIR/admin/debian" .
+    export TARBALL
+    dpkg-buildpackage
+
+    # XXX!
+    cp ../*.deb ~
+
+fi
