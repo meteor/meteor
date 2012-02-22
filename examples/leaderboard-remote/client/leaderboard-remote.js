cb14d116e090e783b8eac63f6c908d826f806698
matt debergalis <deberg@gmail.com>
2012-02-21 17:03:49 -0800

    autopublish in leaderboard

--- examples/leaderboard-remote/client/leaderboard-remote.js
+++ examples/leaderboard-remote/client/leaderboard-remote.js
@@ -3,8 +3,6 @@ Leaderboard = Meteor.connect("http://leader2.meteor.com/sockjs");
 // XXX I'd rather this be Leaderboard.Players.. can this API be easier?
 Players = new Meteor.Collection("players", Leaderboard);
 
-Leaderboard.subscribe("top10");
-
 Template.main.events = {
   'keydown': function () {
     Session.set("error", null);
