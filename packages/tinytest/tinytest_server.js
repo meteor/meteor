3f1ff26e432c57d828dd8502ebf53c5b47c02071
matt debergalis <deberg@gmail.com>
2012-02-23 14:36:36 -0800

    Convert to new publish API.

--- packages/tinytest/tinytest_server.js
+++ packages/tinytest/tinytest_server.js
@@ -2,9 +2,8 @@ Meteor.startup(function () {
   Meteor._ServerTestResults.remove();
 });
 
-App.publish('tinytest/results', {
-  collection: Meteor._ServerTestResults,
-  selector: function (params) { return {run_id: params.run_id} }
+App.publish('tinytest/results', function (sub, params) {
+  return Meteor._ServerTestResults.find({run_id: params.run_id});
 });
 
 App.methods({
