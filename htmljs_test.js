9f10e45687b3d7790002a979ca9ba1c026d8b5b6
Geoff Schmidt <geoff@geoffschmidt.com>
2012-03-02 05:22:40 -0800

    rework public API for defining tests

--- htmljs_test.js
+++ htmljs_test.js
@@ -1,5 +1,5 @@
 
-test("htmljs", function (test) {
+Tinytest.add("htmljs", function (test) {
 
   // Make sure "style" works, which has to be special-cased for IE.
   test.equal(DIV({style:"display:none"}).style.display, "none");
