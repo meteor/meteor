21adb18ec62a61248e42c8a8fe7abc1ed922c378
Geoff Schmidt <geoff@geoffschmidt.com>
2012-03-06 21:23:48 -0800

    helpers for multilevel arrays

---
+++ packages/meteor/helpers.js
@@ -0,0 +1,55 @@
+// XXX namespacing -- find a better home for these?
+
+_.extend(Meteor, {
+  // _get(a,b,c,d) returns a[b][c][d], or else undefined if a[b] or
+  // a[b][c] doesn't exist.
+  _get: function (obj /*, arguments */) {
+    for (var i = 1; i < arguments.length; i++) {
+      if (!(arguments[i] in obj))
+        return undefined;
+      obj = obj[arguments[i]];
+    }
+    return obj;
+  },
+
+  // _ensure(a,b,c,d) ensures that a[b][c][d] exists. If it does not,
+  // it is created and set to {}. Either way, it is returned.
+  _ensure: function (obj /*, arguments */) {
+    for (var i = 1; i < arguments.length; i++) {
+      var key = arguments[i];
+      if (!(key in obj))
+        obj[key] = {};
+      obj = obj[key];
+    }
+
+    return obj;
+  },
+
+  // _delete(a, b, c, d) deletes a[b][c][d], then a[b][c] unless it
+  // isn't empty, then a[b] unless it isn't empty.
+  _delete: function (obj /*, arguments */) {
+    var stack = [obj];
+    var leaf = true;
+    for (var i = 1; i < arguments.length - 1; i++) {
+      var key = arguments[i];
+      if (!(key in obj)) {
+        leaf = false;
+        break;
+      }
+      obj = obj[key];
+      stack.push(obj);
+    }
+
+    for (var i = stack.length - 1; i >= 0; i--) {
+      var key = arguments[i+1];
+
+      if (leaf)
+        leaf = false;
+      else
+        for (var other in stack[i][key])
+          return; // not empty -- we're done
+
+      delete stack[i][key];
+    }
+  }
+});
