7377ce2041145cf1cd7b728f47ade3d5da8168aa
matt debergalis <deberg@gmail.com>
2012-03-11 17:06:05 -0700

    Major formatting and content update to docs.

--- docs/client/docs.js
+++ docs/client/docs.js
@@ -6,8 +6,17 @@ Meteor.startup(function () {
   // later.
   // prettyPrint();
 
-  var sections = $('#main h1, #main h2, #main h3');
+  var sections = [];
+  _.each($('#main h1, #main h2, #main h3'), function (elt) {
+    var classes = (elt.getAttribute('class') || '').split(/\s+/);
+    if (_.indexOf(classes, "nosection") === -1)
+      sections.push(elt);
+  });
+
   for (var i = 0; i < sections.length; i++) {
+    var classes = (sections[i].getAttribute('class') || '').split(/\s+/);
+    if (_.indexOf(classes, "nosection") !== -1)
+      continue;
     sections[i].prev = sections[i-1] || sections[i];
     sections[i].next = sections[i+1] || sections[i];
     $(sections[i]).waypoint({context: '#main', offset: 30});
@@ -52,27 +61,54 @@ var toc = [
     "Reactivity",
     "Templates",
     "Smart Packages",
-    "Deploying",
+    "Deploying"
   ],
+
   "API", [
     "Meteor", [
+      "is_client",
+      "is_server",
+      "startup",
+      "setTimeout",
+      "setInterval",
+      "clearTimeout",
+      "clearInterval",
+
       "publish",
+      "subscription.set",
+      "subscription.unset",
+      "subscription.complete",
+      "subscription.flush",
+      "subscription.onStop",
+      "subscription.stop",
       "subscribe",
       "autosubscribe",
-      "is_client, is_server",
-      "startup",
-      "flush",
+
+      "methods",
+      "Error",
+      "invocation.is_simulation",
+      "invocation.unblock",
+      "call",
+      "apply",
+
       "status",
-      "reconnect"
+      "reconnect",
+      "connect",
+
+      "flush",
+
+      "random",
+      "uuid"
     ],
-    "Collection", [
-      {name: "Meteor.Collection", id: "create_collection"},
+
+    {name: "Meteor.Collection", id: "meteorcollection_header"}, [
+      "Meteor.Collection",
       "find",
       "findOne",
-      "cursor.count",
       "cursor.foreach",
       "cursor.map",
       "cursor.fetch",
+      "cursor.count",
       "cursor.rewind",
       "cursor.observe",
       "insert",
@@ -83,25 +119,49 @@ var toc = [
       {name: "Modifiers", style: "noncode"},
       {name: "Sort specifiers", style: "noncode"}
     ],
+
     "Session", [
       "set",
       "get",
       "equals"
     ],
+
     "Meteor.ui", [
       "render",
       "renderList",
       {type: "spacer"},
       {name: "Event maps", style: "noncode"}
     ],
+
+    {type: "spacer"},
+
     "Meteor.deps", [
       {name: "Meteor.deps.Context", id: "context"},
       {name: "Meteor.deps.Context.current", id: "current"},
-      {type: "spacer"},
       "run",
       {name: "on_invalidate", id: "on_invalidate"},
-      "invalidate",
+      "invalidate"
     ],
+
+    {name: "Meteor.EnvironmentVariable", id: "meteor_environmentvariable"}, [
+      "Meteor.EnvironmentVariable",
+      "environment_variable.get",
+      "environment_variable.withValue",
+      "environment_variable.bindEnvironment"
+    ],
+
+    // "EnvironmentVariable",
+
+    "Packages", [
+      "amplify",
+      "backbone",
+      "coffeescript",
+      "jquery",
+      "less",
+      "showdown",
+      "underscore"
+    ],
+
     "Command line", [
       "meteor help",
       "meteor run",
@@ -116,20 +176,11 @@ var toc = [
       "meteor reset",
       "meteor bundle"
     ]
-  ],
-  {name: "Packages", id: "packagelist"}, [
-    "amplify",
-    "backbone",
-    "coffeescript",
-    "jquery",
-    "less",
-    "showdown",
-    "underscore"
   ]
 ];
 
 var name_to_id = function (name) {
-  var x = name.toLowerCase().replace(/[^a-z0-9_]/g, '');
+  var x = name.toLowerCase().replace(/[^a-z0-9_,]/g, '').replace(/,/g, '_');
   return x;
 };
 
@@ -162,4 +213,137 @@ Template.nav.type = function (what) {
 
 Template.nav.maybe_current = function () {
   return Session.equals("section", this.id) ? "current" : "";
+};
+
+Handlebars.registerHelper('warning', function(fn) {
+  return Template.warning_helper(fn(this), true);
+});
+
+Handlebars.registerHelper('dtdd', function(name, optType, fn) {
+  var type = null;
+  // handle optional positional argument (messy)
+  if (! fn)
+    fn = optType; // two arguments
+  else
+    type = optType; // three arguments
+
+  return Template.dtdd_helper(
+    {descr: fn(this), name:name, type:type}, true);
+});
+
+Handlebars.registerHelper('better_markdown', function(fn) {
+  var converter = new Showdown.converter();
+  var input = fn(this);
+
+  ///////
+  // Make Markdown *actually* skip over block-level elements when
+  // processing a string.
+  //
+  // Official Markdown doesn't descend into
+  // block elements written out as HTML (divs, tables, etc.), BUT
+  // it doesn't skip them properly either.  It assumes they are
+  // either pretty-printed with their contents indented, or, failing
+  // that, it just scans for a close tag with the same name, and takes
+  // it regardless of whether it is the right one.  As a hack to work
+  // around Markdown's hacks, we find the block-level elements
+  // using a proper recursive method and rewrite them to be indented
+  // with the final close tag on its own line.
+  ///////
+
+  // Open-block tag should be at beginning of line,
+  // and not, say, in a string literal in example code, or in a pre block.
+  // Tag must be followed by a non-word-char so that we match whole tag, not
+  // eg P for PRE.  All regexes we wish to use when scanning must have
+  // 'g' flag so that they respect (and set) lastIndex.
+  // Assume all tags are lowercase.
+  var rOpenBlockTag = /^\s{0,2}<(p|div|h[1-6]|blockquote|pre|table|dl|ol|ul|script|noscript|form|fieldset|iframe|math|ins|del)(?=\W)/mg;
+  var rTag = /<(\/?\w+)/g;
+  var idx = 0;
+  var newParts = [];
+  var blockBuf = [];
+  // helper function to execute regex `r` starting at idx and putting
+  // the end index back into idx; accumulate the intervening string
+  // into an array; and return the regex's first capturing group.
+  var rcall = function(r, inBlock) {
+    var lastIndex = idx;
+    r.lastIndex = lastIndex;
+    var match = r.exec(input);
+    var result = null;
+    if (! match) {
+      idx = input.length;
+    } else {
+      idx = r.lastIndex;
+      result = match[1];
+    }
+    (inBlock ? blockBuf : newParts).push(input.substring(lastIndex, idx));
+    return result;
+  };
+
+  var nestedTags = [];
+  while (idx < input.length) {
+    var blockTag = rcall(rOpenBlockTag, false);
+    if (blockTag) {
+      nestedTags.push(blockTag);
+      while (nestedTags.length) {
+        var tag = rcall(rTag, true);
+        if (! tag) {
+          throw new Error("Expected </"+nestedTags[nestedTags.length-1]+
+                          "> but found end of string");
+        } else if (tag.charAt(0) === '/') {
+          // close tag
+          var tagToPop = tag.substring(1);
+          var tagPopped = nestedTags.pop();
+          if (tagPopped !== tagToPop)
+            throw new Error(("Mismatched close tag, expected </"+tagPopped+
+                             "> but found </"+tagToPop+">: "+
+                             input.substr(idx-50,50)+"{HERE}"+
+                             input.substr(idx,50)).replace(/\n/g,'\\n'));
+        } else {
+          // open tag
+          nestedTags.push(tag);
+        }
+      }
+      var newBlock = blockBuf.join('');
+      var closeTagLoc = newBlock.lastIndexOf('<');
+      var firstMatchingClose = newBlock.indexOf('</'+blockTag+'>');
+      var shouldIndent =
+            (firstMatchingClose >= 0 && firstMatchingClose < closeTagLoc);
+      // Put final close tag at beginning of line, indent other lines if necessary.
+      // Not indenting unless necessary saves us from indenting in a <pre> tag.
+      var part1 = newBlock.substring(0, closeTagLoc);
+      var part2 = newBlock.substring(closeTagLoc);
+      if (shouldIndent)
+        part1 = part1.replace(/\n/g, '\n  ');
+      newBlock = part1 + '\n' + part2;
+      newParts.push(newBlock);
+      blockBuf.length = 0;
+    }
+  }
+
+  var newInput = newParts.join('');
+  var output = converter.makeHtml(newInput);
+  return output;
+});
+
+Handlebars.registerHelper('dstache', function() {
+  return '{{';
+});
+
+Handlebars.registerHelper('tstache', function() {
+  return '{{{';
+});
+
+Handlebars.registerHelper('api_section', function(id, nameFn) {
+  return Template.api_section_helper(
+    {name: nameFn(this), id:id}, true);
+});
+
+Handlebars.registerHelper('api_box_inline', function(box, fn) {
+  return Template.api_box(_.extend(box, {body: fn(this)}), true);
+});
+
+Template.api_box.bare = function() {
+  return ((this.descr && this.descr.length) ||
+          (this.args && this.args.length) ||
+          (this.options && this.options.length)) ? "" : "bareapi";
 };
