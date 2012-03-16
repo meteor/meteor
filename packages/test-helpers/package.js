413c0778b52fce4f87e6ad3b3935e1ca40f8d4f0
Geoff Schmidt <geoff@geoffschmidt.com>
2012-03-16 02:22:47 -0700

    move testAsyncMulti into test-helpers

--- packages/test-helpers/package.js
+++ packages/test-helpers/package.js
@@ -6,6 +6,7 @@ Package.on_use(function (api, where) {
   where = where || ["client", "server"];
 
   api.add_files('try_all_permutations.js', where);
+  api.add_files('async_multi.js', where);
 });
 
 Package.on_test(function (api) {
