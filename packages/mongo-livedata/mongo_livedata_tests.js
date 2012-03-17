a00ee6357492756ecd435c7746d8d24d5f0064fc
Geoff Schmidt <geoff@geoffschmidt.com>
2012-03-16 17:30:50 -0700

    some basic tests for mongo-livedata

--- packages/mongo-livedata/mongo_livedata_tests.js
+++ packages/mongo-livedata/mongo_livedata_tests.js
@@ -32,3 +32,114 @@ testAsyncMulti("mongo-livedata - database failure reporting", [
     });
   }
 ]);
+
+// XXX namespacing
+Meteor._LivedataTestCollection =
+  new Meteor.Collection("livedata_test_collection");
+
+Tinytest.addAsync("mongo-livedata - basics", function (test, onComplete) {
+  var coll = Meteor._LivedataTestCollection;
+  var run = test.runId();
+
+  var log = '';
+  var obs = coll.find({run: run}, {sort: ["x"]}).observe({
+    added: function (doc, before_index) {
+      log += 'a(' + doc.x + ',' + before_index + ')';
+    },
+    changed: function (new_doc, at_index, old_doc) {
+      log += 'c(' + new_doc.x + ',' + at_index + ',' + old_doc.x + ')';
+    },
+    moved: function (doc, old_index, new_index) {
+      log += 'm(' + doc.x + ',' + old_index + ',' + new_index + ')';
+    },
+    removed: function (doc, at_index) {
+      log += 'r(' + doc.x + ',' + at_index + ')';
+    }
+  });
+
+  var expectObserve = function (expected, f) {
+    if (Meteor.is_client) {
+      f();
+    } else {
+      var fence = new Meteor._WriteFence;
+      Meteor._CurrentWriteFence.withValue(fence, f);
+      var future = new Future;
+      fence.onAllCommitted(function () {
+        future['return']();
+      });
+      fence.arm();
+      future.wait();
+    }
+
+    if (!(expected instanceof Array))
+      expected = [expected];
+
+    test.include(expected, log);
+    log = '';
+  };
+
+  test.equal(coll.find({run: run}).count(), 0);
+  test.equal(coll.findOne("abc"), undefined);
+  test.equal(coll.findOne({run: run}), undefined);
+
+  expectObserve('a(1,0)', function () {
+    var id = coll.insert({run: run, x: 1});
+    test.equal(id.length, 36);
+    test.equal(coll.find({run: run}).count(), 1);
+    test.equal(coll.findOne(id).x, 1);
+    test.equal(coll.findOne({run: run}).x, 1);
+  });
+
+  expectObserve('a(4,1)', function () {
+    var id2 = coll.insert({run: run, x: 4});
+    test.equal(coll.find({run: run}).count(), 2);
+    test.equal(coll.find({_id: id2}).count(), 1);
+    test.equal(coll.findOne(id2).x, 4);
+  });
+
+  test.equal(coll.findOne({run: run}, {sort: ["x"], skip: 0}).x, 1);
+  test.equal(coll.findOne({run: run}, {sort: ["x"], skip: 1}).x, 4);
+  test.equal(coll.findOne({run: run}, {sort: {x: -1}, skip: 0}).x, 4);
+  test.equal(coll.findOne({run: run}, {sort: {x: -1}, skip: 1}).x, 1);
+
+  var cur = coll.find({run: run}, {sort: ["x"]});
+  var total = 0;
+  cur.forEach(function (doc) {
+    total *= 10;
+    total += doc.x;
+  })
+  test.equal(total, 14);
+
+  cur.rewind();
+  test.equal(cur.map(function (doc) {
+    return doc.x * 2;
+  }), [2, 8]);
+
+  test.equal(_.pluck(coll.find({run: run}, {sort: {x: -1}}).fetch(), "x"),
+             [4, 1]);
+
+  expectObserve('c(3,0,1)c(6,1,4)', function () {
+    coll.update({run: run}, {$inc: {x: 2}}, {multi: true});
+    test.equal(_.pluck(coll.find({run: run}, {sort: {x: -1}}).fetch(), "x"),
+               [6, 3]);
+  });
+
+  expectObserve(['c(13,0,3)m(13,0,1)', 'm(6,1,0)c(13,1,3)'], function () {
+    coll.update({run: run, x: 3}, {$inc: {x: 10}}, {multi: true});
+    test.equal(_.pluck(coll.find({run: run}, {sort: {x: -1}}).fetch(), "x"),
+               [13, 6]);
+  });
+
+  expectObserve('r(13,1)', function () {
+    coll.remove({run: run, x: {$gt: 10}});
+    test.equal(coll.find({run: run}).count(), 1);
+  });
+
+  expectObserve('r(6,0)', function () {
+    coll.remove({run: run});
+    test.equal(coll.find({run: run}).count(), 0);
+  });
+
+  obs.stop();
+  onComplete();
+});
