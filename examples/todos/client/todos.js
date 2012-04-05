117e9e04675447eed977820f88621b09a0c72858
David Greenspan <dgreenspan@alum.mit.edu>
2012-04-05 16:47:04 -0700

    additional documentation etc in todos

--- examples/todos/client/todos.js
+++ examples/todos/client/todos.js
@@ -1,11 +1,28 @@
+// Client-side JavaScript, bundled and sent to client.
+
+// Define Minimongo collections to match server/publish.js.
+Lists = new Meteor.Collection("lists");
+Todos = new Meteor.Collection("todos");
+
+// ID of currently selected list
 Session.set('list_id', null);
+
+// Name of currently selected tag for filtering
 Session.set('tag_filter', null);
+
+// When adding tag to a todo, ID of the todo
 Session.set('editing_addtag', null);
+
+// When editing a list name, ID of the list
 Session.set('editing_listname', null);
+
+// When editing todo text, ID of the todo
 Session.set('editing_itemname', null);
 
+
+// Subscribe to 'lists' collection on startup.
+// Select a list once data has arrived.
 Meteor.subscribe('lists', function () {
-  // Once the lists have loaded, select the first one.
   if (!Session.get('list_id')) {
     var list = Lists.findOne({}, {sort: {name: 1}});
     if (list)
@@ -13,23 +30,29 @@ Meteor.subscribe('lists', function () {
   }
 });
 
+// Always be subscribed to the todos for the selected list.
 Meteor.autosubscribe(function () {
   var list_id = Session.get('list_id');
   if (list_id)
     Meteor.subscribe('todos', list_id);
 });
 
+
 ////////// Helpers for in-place editing //////////
 
-var okcancel_events = function(selector) {
-  return 'keyup %, keydown %, focusout %'.replace(/%/g, selector);
+// Returns an event_map key for attaching "ok/cancel" events to
+// a text input (given by selector)
+var okcancel_events = function (selector) {
+  return 'keyup '+selector+', keydown '+selector+', focusout '+selector;
 };
 
-var make_okcancel_handler = function(options) {
-  var ok = options.ok || function() {};
-  var cancel = options.cancel || function() {};
+// Creates an event handler for interpreting "escape", "return", and "blur"
+// on a text field and calling "ok" or "cancel" callbacks.
+var make_okcancel_handler = function (options) {
+  var ok = options.ok || function () {};
+  var cancel = options.cancel || function () {};
 
-  return function(evt) {
+  return function (evt) {
     if (evt.type === "keydown" && evt.which === 27) {
       // escape = cancel
       cancel.call(this, evt);
@@ -46,41 +69,12 @@ var make_okcancel_handler = function(options) {
   };
 };
 
-var focus_field_by_id = function(id) {
+// Finds a text input in the DOM by id and focuses it.
+var focus_field_by_id = function (id) {
   var input = document.getElementById(id);
   if (input) {
     input.focus();
     input.select();
-  }
-};
-
-////////// Tag Filter //////////
-
-Template.tag_filter.tags = function () {
-  // Pick out the unique tags from all tasks.
-  var tags = _(Todos.find({list_id: Session.get('list_id')}).fetch())
-    .chain().pluck('tags').compact().flatten().sort().uniq(true).value();
-  // for some reason, .map can't be chained on IE8. underscore bug?
-  tags = _.map(tags, function (tag) { return {tag: tag} });
-
-  tags.unshift({tag: null}); // "show all" button
-  return tags;
-};
-
-Template.tag_item.selected = function () {
-  return Session.equals('tag_filter', this.tag) ? 'selected' : '';
-};
-
-Template.tag_item.tag_text = function () {
-  return this.tag || "All items";
-};
-
-Template.tag_item.events = {
-  'mousedown': function () {
-    if (Session.equals('tag_filter', this.tag))
-      Session.set('tag_filter', null);
-    else
-      Session.set('tag_filter', this.tag);
   }
 };
 
@@ -89,6 +83,18 @@ Template.tag_item.events = {
 Template.lists.lists = function () {
   return Lists.find({}, {sort: {name: 1}});
 };
+
+Template.lists.events = {};
+
+// Attach events to keydown, keyup, and blur on "New list" input box.
+Template.lists.events[ okcancel_events('#new-list') ] =
+  make_okcancel_handler({
+    ok: function (text, evt) {
+      var id = Lists.insert({name: text});
+      Router.setList(id);
+      evt.target.value = "";
+    }
+  });
 
 Template.list_item.selected = function () {
   return Session.equals('list_id', this._id) ? 'selected' : '';
@@ -108,30 +114,19 @@ Template.list_item.events = {
   },
   'dblclick': function (evt) { // start editing list name
     Session.set('editing_listname', this._id);
-    Meteor.flush();
+    Meteor.flush(); // force DOM redraw, so we can focus the edit field
     focus_field_by_id("list-name-input");
   }
 };
 
 Template.list_item.events[ okcancel_events('#list-name-input') ] =
   make_okcancel_handler({
-    ok: function(value) {
+    ok: function (value) {
       Lists.update(this._id, {$set: {name: value}});
       Session.set('editing_listname', null);
     },
-    cancel: function() {
+    cancel: function () {
       Session.set('editing_listname', null);
-    }
-  });
-
-Template.create_list.events = {};
-
-Template.create_list.events[ okcancel_events('#new-list') ] =
-  make_okcancel_handler({
-    ok: function(text, evt) {
-      var id = Lists.insert({name: text});
-      Router.setList(id);
-      evt.target.value = "";
     }
   });
 
@@ -145,7 +140,7 @@ Template.todos.events = {};
 
 Template.todos.events[ okcancel_events('#new-todo') ] =
   make_okcancel_handler({
-    ok: function(text, evt) {
+    ok: function (text, evt) {
       var tag = Session.get('tag_filter');
       Todos.insert({
         text: text,
@@ -158,8 +153,10 @@ Template.todos.events[ okcancel_events('#new-todo') ] =
     }
   });
 
+Template.todos.todos = function () {
+  // Determine which todos to display in main pane,
+  // selected based on list_id and tag_filter.
 
-Template.todo_list.todos = function () {
   var list_id = Session.get('list_id');
   if (!list_id)
     return {};
@@ -206,41 +203,38 @@ Template.todo_item.events = {
 
   'click .addtag': function (evt) {
     Session.set('editing_addtag', this._id);
-    Meteor.flush();
+    Meteor.flush(); // update DOM before focus
     focus_field_by_id("edittag-input");
   },
 
   'dblclick .display .todo-text': function (evt) {
     Session.set('editing_itemname', this._id);
-    Meteor.flush();
+    Meteor.flush(); // update DOM before focus
     focus_field_by_id("todo-input");
   }
-
 };
 
 Template.todo_item.events[ okcancel_events('#todo-input') ] =
   make_okcancel_handler({
-    ok: function(value) {
+    ok: function (value) {
       Todos.update(this._id, {$set: {text: value}});
       Session.set('editing_itemname', null);
     },
-    cancel: function() {
+    cancel: function () {
       Session.set('editing_itemname', null);
     }
   });
 
-
 Template.todo_item.events[ okcancel_events('#edittag-input') ] =
   make_okcancel_handler({
-    ok: function(value) {
+    ok: function (value) {
       Todos.update(this._id, {$addToSet: {tags: value}});
       Session.set('editing_addtag', null);
     },
-    cancel: function() {
+    cancel: function () {
       Session.set('editing_addtag', null);
     }
   });
-
 
 Template.todo_tag.events = {
   'click .remove': function (evt) {
@@ -249,9 +243,47 @@ Template.todo_tag.events = {
 
     evt.target.parentNode.style.opacity = 0;
     // wait for CSS animation to finish
-    Meteor.setTimeout(function() {
+    Meteor.setTimeout(function () {
       Todos.update({_id: id}, {$pull: {tags: tag}});
     }, 300);
+  }
+};
+
+////////// Tag Filter //////////
+
+// Pick out the unique tags from all todos in current list.
+Template.tag_filter.tags = function () {
+  var tags = [];
+
+  Todos.find({list_id: Session.get('list_id')}).forEach(function (todo) {
+    _.each(todo.tags, function (tag) {
+      tags.push(tag);
+    });
+  });
+
+  tags.sort();
+  tags = _.uniq(tags, true);
+  tags.unshift(null);
+
+  return _.map(tags, function (tag) {
+    return {tag: tag}; // create objects for template
+  });
+};
+
+Template.tag_item.tag_text = function () {
+  return this.tag || "All items";
+};
+
+Template.tag_item.selected = function () {
+  return Session.equals('tag_filter', this.tag) ? 'selected' : '';
+};
+
+Template.tag_item.events = {
+  'mousedown': function () {
+    if (Session.equals('tag_filter', this.tag))
+      Session.set('tag_filter', null);
+    else
+      Session.set('tag_filter', this.tag);
   }
 };
 
@@ -271,8 +303,6 @@ var TodosRouter = Backbone.Router.extend({
 });
 
 Router = new TodosRouter;
-
-////////// Startup //////////
 
 Meteor.startup(function () {
   Backbone.history.start({pushState: true});
