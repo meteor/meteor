1a503d1abf42abf39e7dd056f9f6497a3d15d3c4
matt debergalis <deberg@gmail.com>
2012-04-06 16:36:39 -0700

    fix DB writes in callbacks in server methods.
    interval callbacks scheduled inside a server method are not part of the method invocation, and should clear out the CurrentInvocation and CurrentWriteFence environment variables.

--- packages/meteor/timers.js
+++ packages/meteor/timers.js
@@ -1,5 +1,23 @@
 _.extend(Meteor, {
+  // Meteor.setTimeout and Meteor.setInterval callbacks scheduled
+  // inside a server method are not part of the method invocation, and
+  // should clear out the CurrentInvocation and CurrentWriteFence
+  // environment variables.
+
   setTimeout: function (f, duration) {
+    if (Meteor._CurrentInvocation) {
+      if (Meteor._CurrentInvocation.get() && Meteor._CurrentInvocation.get().is_simulation)
+        throw new Error("Can't set timers inside simulations");
+
+      var f_with_ci = f;
+      f = function () { Meteor._CurrentInvocation.withValue(null, f_with_ci); };
+    }
+
+    if (Meteor._CurrentWriteFence) {
+      var f_with_cwf = f;
+      f = function () { Meteor._CurrentWriteFence.withValue(null, f_with_cwf); };
+    }
+
     return setTimeout(Meteor.bindEnvironment(f, function (e) {
       // XXX report nicely (or, should we catch it at all?)
       Meteor._debug("Exception from setTimeout callback:", e.stack);
@@ -7,9 +25,22 @@ _.extend(Meteor, {
   },
 
   setInterval: function (f, duration) {
+    if (Meteor._CurrentInvocation) {
+      if (Meteor._CurrentInvocation.get() && Meteor._CurrentInvocation.get().is_simulation)
+        throw new Error("Can't set timers inside simulations");
+
+      var f_with_ci = f;
+      f = function () { Meteor._CurrentInvocation.withValue(null, f_with_ci); };
+    }
+
+    if (Meteor._CurrentWriteFence) {
+      var f_with_cwf = f;
+      f = function () { Meteor._CurrentWriteFence.withValue(null, f_with_cwf); };
+    }
+
     return setInterval(Meteor.bindEnvironment(f, function (e) {
       // XXX report nicely (or, should we catch it at all?)
-      Meteor._debug("Exception from setInterval callback:", e.stack);
+      Meteor._debug("Exception from setInterval callback:", e);
     }), duration);
   },
 
