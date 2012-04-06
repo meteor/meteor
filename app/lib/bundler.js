6bed8754d49f98d929edce3de280b1e7c65e69b0
Nick Martin <nim@meteor.com>
2012-04-06 16:04:50 -0700

    Wrap bundler in a try to catch unexpected bundle time error. This fixes template syntax errors killing the whole app.

--- app/lib/bundler.js
+++ app/lib/bundler.js
@@ -613,29 +613,33 @@ _.extend(Bundle.prototype, {
 exports.bundle = function (project_dir, output_path, options) {
   options = options || {};
 
-  // Create a bundle, add the project
-  packages.flush();
-  var bundle = new Bundle;
-  var project = packages.get_for_dir(project_dir, ignore_files);
-  bundle.use(project);
+  try {
+    // Create a bundle, add the project
+    packages.flush();
+    var bundle = new Bundle;
+    var project = packages.get_for_dir(project_dir, ignore_files);
+    bundle.use(project);
 
-  // Include tests if requested
-  if (options.include_tests) {
-    // in the future, let use specify the driver, instead of hardcoding?
-    bundle.use(packages.get('test-in-browser'));
-    bundle.include_tests(project);
+    // Include tests if requested
+    if (options.include_tests) {
+      // in the future, let use specify the driver, instead of hardcoding?
+      bundle.use(packages.get('test-in-browser'));
+      bundle.include_tests(project);
+    }
+
+    // Minify, if requested
+    if (!options.no_minify)
+      bundle.minify();
+
+    // Write to disk
+    var dev_bundle_mode =
+          options.skip_dev_bundle ? "skip" : (
+            options.symlink_dev_bundle ? "symlink" : "copy");
+    bundle.write_to_directory(output_path, project_dir, dev_bundle_mode);
+
+    if (bundle.errors.length)
+      return bundle.errors;
+  } catch (err) {
+    return ["Exception while bundling application:\n" + (err.stack || err)];
   }
-
-  // Minify, if requested
-  if (!options.no_minify)
-    bundle.minify();
-
-  // Write to disk
-  var dev_bundle_mode =
-    options.skip_dev_bundle ? "skip" : (
-      options.symlink_dev_bundle ? "symlink" : "copy");
-  bundle.write_to_directory(output_path, project_dir, dev_bundle_mode);
-
-  if (bundle.errors.length)
-    return bundle.errors;
 };
