b5cf5d904ef2be10525c6bfb7392b80d9eee19a5
Heikki Ylönen <heikki74@gmail.com>
2012-04-14 20:59:00 +0300

    Add stylus package.

---
+++ packages/stylus/package.js
@@ -0,0 +1,27 @@
+Package.describe({
+  summary: 'Expressive, dynamic, robust CSS.'
+});
+
+var stylus = require('stylus');
+var fs = require('fs');
+
+Package.register_extension(
+  'styl', function(bundle, source_path, serve_path, where) {
+    serve_path = serve_path + '.css';
+
+    var contents = fs.readFileSync(source_path);
+
+    stylus.render(contents.toString('utf8'), { filename: serve_path }, function(err, css) {
+      if (err) {
+        bundle.error(source_path + ': Stylus compiler error: ' + err.message);
+        return;
+      }
+      bundle.add_resource({
+        type: 'css',
+        path: serve_path,
+        data: new Buffer(css),
+        where: where
+      });
+    });
+  }
+);
