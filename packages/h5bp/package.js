a224ee51cd356691c5cbdc82f98419ee55bd833b
Andre Dublin <81dublin@gmail.com>
2012-04-15 00:16:00 -0400

    html5 boilerplate

--- packages/h5bp/package.js
+++ packages/h5bp/package.js
@@ -1,7 +1,11 @@
 Package.describe({
-  summary: "The HTML5 Boilerplate"
+  summary: "The HTML5 Boilerplate",
 });
 
 Package.on_use(function (api) {
-  api.add_files('h5bp.js', 'client');
+  api.add_files('style.css', 'client');
+  api.add_files('humans.txt', 'client');
+  api.add_files('.htaccess', 'client');
+  api.add_files('crossdomain.xml', 'client');
+  api.add_files('robots.txt', 'client');
 });
