02abc5711e00ad8a4cac7f71d9e6ce058cd88f1f
fivethirty <up@fivethirty.am>
2012-04-25 12:04:07 -0700

    remove png handler from bootstrap smart package.

--- packages/bootstrap/package.js
+++ packages/bootstrap/package.js
@@ -2,8 +2,6 @@ Package.describe({
   summary: "UX/UI framework from Twitter"
 })
 
-var fs = require('fs');
-
 Package.on_use(function (api) {
   api.add_files('css/bootstrap.css', 'client');
   api.add_files('css/bootstrap-responsive.css', 'client');
@@ -11,18 +9,3 @@ Package.on_use(function (api) {
   api.add_files('img/glyphicons-halflings.png', 'client');
   api.add_files('img/glyphicons-halflings-white.png', 'client');
 });
-
-// handle adding PNG sprites for icons
-Package.register_extension(
-  "png", function(bundle, source_path, serve_path, where) {
-
-    var png = fs.readFileSync(source_path)
-
-    bundle.add_resource({
-      type: "img",
-      path: serve_path,
-      data : png,
-      where: where
-    });
-  }
-);
