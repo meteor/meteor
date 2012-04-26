c24f891f8559efd31b6d17d533f4cfae89d7bc07
fivethirty <up@fivethirty.am>
2012-04-25 19:39:21 -0700

    adding bootstrap-override.css to make paths to icons absolute.  this is needed because the bundler minifies css and serves it from the "/" directory.

--- packages/bootstrap/package.js
+++ packages/bootstrap/package.js
@@ -8,4 +8,9 @@ Package.on_use(function (api) {
   api.add_files('js/bootstrap.js', 'client');
   api.add_files('img/glyphicons-halflings.png', 'client');
   api.add_files('img/glyphicons-halflings-white.png', 'client');
+
+  // XXX this makes the paths to the icon sets absolute. it needs
+  // to be included _after_ the standard bootstrap css so
+  // that it's styles take precedence.
+  api.add_files('css/bootstrap-override.css', 'client');
 });
