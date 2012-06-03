e406bda3f60c9ab5e2a7798146b36ef86ef5eb2f
Blake Miner <miner.blake@gmail.com>
2012-06-03 03:43:00 +0000

    Changes made in Blade 1.3 See https://github.com/bminer/node-blade/commit/b4497e3edbc31290deadbe098b5e764a66aec46c

--- packages/blade/package.js
+++ packages/blade/package.js
@@ -8,7 +8,8 @@ Package.describe({
 Package.register_extension("blade", function(bundle, srcPath, servePath, where) {
 	if(where !== "client") return; //get outta here, yo!
 	//The template name does not contain ".blade" file extension or a beginning "/"
-	var templateName = path.dirname(servePath).substr(1) + "/" + path.basename(servePath, ".blade");
+	var templateName = path.dirname(servePath).substr(1);
+	templateName += (templateName.length > 0 ? "/" : "") + path.basename(servePath, ".blade");
 	//Templates are assumed to be stored in "views/", so remove this from the name, if needed
 	if(templateName.substr(0, 6) == "views/")
 		templateName = templateName.substr(6);
@@ -22,34 +23,52 @@ Package.register_extension("blade", function(bundle, srcPath, servePath, where) 
 		'minify': false, //for debugging
 		'includeSource': true //for debugging
 	}, function(err, tmpl) {
+		if(err) throw err;
+		if(templateName == "head")
+			tmpl({}, function(err, html) {
+				//This should happen synchronously due to compile options set above
+				bundle.add_resource({
+					type: 'head',
+					data: html,
+					where: where
+				});
+			});
+		else if(templateName == "body")
+			tmpl({}, function(err, html) {
+				//This should happen synchronously due to compile options set above
+				bundle.add_resource({
+					type: 'body',
+					data: html,
+					where: where
+				});
+			});
 		bundle.add_resource({
 			type: 'js',
 			path: "/views/" + templateName + ".js", //This can be changed to whatever
 			data: new Buffer("Meteor.startup(function(){blade.cachedViews[" +
 				//just put the template itself in blade.cachedViews
 				JSON.stringify(templateName + ".blade") + "]=" + tmpl.toString() + ";" +
-				//special support for index.blade
-				(templateName == "index" ? "document.body.appendChild(Meteor.ui.render(": "") +
 				//define a template with the proper name
 				"Meteor._def_template(" + JSON.stringify(templateName) +
 					//when the template is called...
 					", function(data, obj) {data = data || {};" +
 						//helpers work...
-						"for(var i in obj.helpers) if(typeof obj.helpers[i] == 'function')" +
-							"data[i]=obj.helpers[i]();" +
+						"for(var i in obj.helpers)\n" +
+							"if(typeof obj.helpers[i] == 'function') " +
+								"data[i]=obj.helpers[i]();" +
 						/*call the actual Blade template here, passing in data
 							`ret` is used to capture async results.
 							Note that since we are using caching for file includes,
 							there is no async. All code is ran synchronously. */
 						"var ret = ''; blade.cachedViews[" + JSON.stringify(templateName + ".blade") +
-						"](data,function(err,html) {" +
+						"](data, function(err,html) {" +
 							"if(err) throw err; ret = html;" +
 						//so... by here, we can just return `ret`, and everything works okay
-						"}); return ret;" +
-						"}" +
-					")" +
-				(templateName == "index" ? "))": "") +
-				"});"),
+						"});\n" +
+						"return ret;" +
+					"}" +
+				")" +
+			"});"),
 			where: where
 		});
 	});
