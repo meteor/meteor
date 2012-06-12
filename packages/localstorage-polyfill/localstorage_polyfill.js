340c1ba8225593e3d70dc5b567f95ee751954712
Avital Oliver <avital@thewe.net>
2012-06-12 15:08:11 -0700

    New oauth login flow
    Also, some code reorganization and bugfix for IE7

--- packages/localstorage-polyfill/localstorage_polyfill.js
+++ packages/localstorage-polyfill/localstorage_polyfill.js
@@ -1,12 +1,11 @@
 Meteor.startup(function() { // Since we need document.body to be defined
   if (!window.localStorage) {
     window.localStorage = (function () {
-      var userdata = document.createElement('span'); // could be anything
-
-      if (userdata.load) { // If we are on IE, which support userData
+      if ($.browser.msie) { // If we are on IE, which support userData
+        var userdata = document.createElement('span'); // could be anything
+        userdata.style.behavior = 'url("#default#userData")';
         userdata.id = 'localstorage-polyfill-helper';
         userdata.style.display = 'none';
-        userdata.style.behavior = 'url("#default#userData")';
         document.body.appendChild(userdata);
 
         var userdataKey = 'localStorage';
@@ -24,14 +23,20 @@ Meteor.startup(function() { // Since we need document.body to be defined
           },
 
           getItem: function (key) {
+            userdata.load(userdataKey);
             return userdata.getAttribute(key);
           }
         };
       } else {
+        Meteor._debug(
+          "You are running a browser with no localStorage or userData "
+            + "support (presumable Opera Mini). Logging in from one tab "
+            + "will not cause another tab to be logged in.");
+
         return {
-          setItem: function() {},
-          removeItem: function() {},
-          getItem: function() {}
+          setItem: function () {},
+          removeItem: function () {},
+          getItem: function () {}
         };
       };
     })();
