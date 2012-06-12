d73b72395a7f3e262f1cb267c689af7bd1731cac
Avital Oliver <avital@thewe.net>
2012-06-12 15:08:11 -0700

    New oauth login flow
    Also, some code reorganization and bugfix for IE7

--- packages/accounts/accounts_client.js
+++ packages/accounts/accounts_client.js
@@ -1,99 +1,87 @@
-Meteor.user = function () {
-  if (Meteor.default_connection.userId()) {
-    // XXX full identity?
-    return {_id: Meteor.default_connection.userId()};
-  } else {
-    return null;
-  }
-};
-
-if (Handlebars) {
-  Handlebars.registerHelper('user', function () {
-    return Meteor.user();
-  });
-}
-
-Meteor.loginWithFacebook = function (callback) {
-  var openCenteredPopup = function(url, width, height) {
-    var screenX = typeof window.screenX !== 'undefined'
-          ? window.screenX : window.screenLeft;
-    var screenY = typeof window.screenY !== 'undefined'
-          ? window.screenY : window.screenTop;
-    var outerWidth = typeof window.outerWidth !== 'undefined'
-          ? window.outerWidth : document.body.clientWidth;
-    var outerHeight = typeof window.outerHeight !== 'undefined'
-          ? window.outerHeight : (document.body.clientHeight - 22);
-
-    // Use `outerWidth - width` and `outerHeight - height` for help in
-    // positioning the popup centered relative to the current window
-    var left = screenX + (outerWidth - width) / 2;
-    var top = screenY + (outerHeight - height) / 2;
-    var features = ('width=' + width + ',height=' + height +
-                    ',left=' + left + ',top=' + top);
-
-    var newwindow = window.open(url, 'Login', features);
-    if (newwindow.focus)
-      newwindow.focus();
-    return newwindow;
+(function() {
+  Meteor.user = function () {
+    if (Meteor.default_connection.userId()) {
+      // XXX full identity?
+      return {_id: Meteor.default_connection.userId()};
+    } else {
+      return null;
+    }
   };
 
-  if (!Meteor.accounts.facebook._appId || !Meteor.accounts.facebook._appUrl)
-    throw new Meteor.accounts.facebook.SetupError("Need to call Meteor.accounts.facebook.setup first");
+  if (Handlebars) {
+    Handlebars.registerHelper('user', function () {
+      return Meteor.user();
+    });
+  }
 
-  var oauthState = Meteor.uuid();
+  Meteor.loginWithFacebook = function () {
+    var openCenteredPopup = function(url, width, height) {
+      var screenX = typeof window.screenX !== 'undefined'
+            ? window.screenX : window.screenLeft;
+      var screenY = typeof window.screenY !== 'undefined'
+            ? window.screenY : window.screenTop;
+      var outerWidth = typeof window.outerWidth !== 'undefined'
+            ? window.outerWidth : document.body.clientWidth;
+      var outerHeight = typeof window.outerHeight !== 'undefined'
+            ? window.outerHeight : (document.body.clientHeight - 22);
 
-  var popup = openCenteredPopup(
-    'https://www.facebook.com/dialog/oauth?client_id=' + Meteor.accounts.facebook._appId +
-      '&redirect_uri=' + Meteor.accounts.facebook._appUrl + '/_oauth/facebook' +
-      '&scope=email&state=' + oauthState,
-    1000, 600); // XXX should we use different dimensions, e.g. on mobile?
+      // Use `outerWidth - width` and `outerHeight - height` for help in
+      // positioning the popup centered relative to the current window
+      var left = screenX + (outerWidth - width) / 2;
+      var top = screenY + (outerHeight - height) / 2;
+      var features = ('width=' + width + ',height=' + height +
+                      ',left=' + left + ',top=' + top);
 
-  var checkPopupOpen = setInterval(function() {
-    if (popup.closed) {
-      clearInterval(checkPopupOpen);
-
-      // Hit our OAuth endpoint to cancel this login request so that
-      // the app doesn't hang
-      Meteor.http.get(Meteor.accounts.facebook._appUrl +
-                      '/_oauth/facebook/?error=error&state=' + oauthState,
-                      function() {});
-    }
-  }, 1000);
-
-  Meteor.apply('login', [
-    {oauth: {version: 2, provider: 'facebook', state: oauthState}}
-  ], {wait: true}, function(error, result) {
-    if (error) {
-      Meteor._debug("Server error on login", error);
-      return;
-    }
-
-    localStorage.setItem("Meteor.loginToken", result.token);
-    Meteor.default_connection.setUserId(result.id);
-    Meteor.default_connection.onReconnect = function() {
-      Meteor.apply('login', [{resume: result.token}], {wait: true}, function(error, result) {
-        if (error) {
-          Meteor.default_connection.setUserId(null);
-          localStorage.setItem("Meteor.loginToken", "");
-          Meteor._debug("Server error on login", error);
-          return;
-        }
-      });
+      var newwindow = window.open(url, 'Login', features);
+      if (newwindow.focus)
+        newwindow.focus();
+      return newwindow;
     };
-    if (typeof callback === 'function')
-      callback();
-  });
-};
 
-Meteor.logout = function () {
-  Meteor.apply('logout', [], {wait: true}, function(error, result) {
-    if (error) {
-      Meteor._debug("Server error on logout", error);
-      return;
-    }
+    if (!Meteor.accounts.facebook._appId || !Meteor.accounts.facebook._appUrl)
+      throw new Meteor.accounts.facebook.SetupError("Need to call Meteor.accounts.facebook.setup first");
 
-    localStorage.setItem("Meteor.loginToken", "");
-    Meteor.default_connection.setUserId(null);
-    Meteor.default_connection.onReconnect = null;
-  });
-};
+    var oauthState = Meteor.uuid();
+
+    var popup = openCenteredPopup(
+      'https://www.facebook.com/dialog/oauth?client_id=' + Meteor.accounts.facebook._appId +
+        '&redirect_uri=' + Meteor.accounts.facebook._appUrl + '/_oauth/facebook?close' +
+        '&scope=email&state=' + oauthState,
+      1000, 600); // XXX should we use different dimensions, e.g. on mobile?
+
+    var checkPopupOpen = setInterval(function() {
+      if (popup.closed) {
+        clearInterval(checkPopupOpen);
+        tryLoginAfterPopupClosed(oauthState);
+      }
+    }, 100);
+  };
+
+  // Send an OAuth login method to the server. If the user authorized
+  // access in the popup this should log the user in, otherwise
+  // nothing should happen.
+  var tryLoginAfterPopupClosed = function(oauthState) {
+    Meteor.apply('login', [
+      {oauth: {version: 2, provider: 'facebook', state: oauthState}}
+    ], {wait: true}, function(error, result) {
+      if (error) {
+        Meteor._debug("Server error on login", error);
+        return;
+      }
+
+      Meteor.accounts.loginAndStoreToken(result.token);
+      callback && callback();
+    });
+  };
+
+  Meteor.logout = function () {
+    Meteor.apply('logout', [], {wait: true}, function(error, result) {
+      if (error) {
+        Meteor._debug("Server error on logout", error);
+        return;
+      } else {
+        Meteor.accounts.forceClientLoggedOut();
+      }
+    });
+  };
+})();
