8044da412bbfafc46ef6875351b7b7cbb1d496b8
Avital Oliver <avital@thewe.net>
2012-06-15 13:45:36 -0700

    Better interface for OAuth2 login services + more cleanup

--- packages/accounts-google/google_server.js
+++ packages/accounts-google/google_server.js
@@ -1,45 +1,56 @@
 (function () {
+
   Meteor.accounts.google.setSecret = function (secret) {
     Meteor.accounts.google._secret = secret;
   };
 
-  Meteor.accounts.oauth2.providers.google = {
-    userIdForOauthReq: function(req) {
-      var accessToken = getAccessToken(req);
-
-      // XXX can we generalize this flow into the oauth abstraction?
-      if (!accessToken)
-        return null;
-
-      var identity = Meteor.http.get(
-        "https://www.googleapis.com/oauth2/v1/userinfo",
-          {params: {access_token: accessToken}}).data;
-
-      return Meteor.accounts.updateOrCreateUser(
-        identity.email, {name: identity.name},
-        'google', identity.id, {accessToken: accessToken});
-    }
-  };
-
-  var getAccessToken = function (req) {
-    if (req.query.error) {
+  Meteor.accounts.oauth2.registerService('google', function(query) {
+    if (query.error) {
       // The user didn't authorize access
-      // XXX can we generalize this into the oauth abstration?
+      // XXX can/should we generalize this into the oauth abstration?
       return null;
     }
 
-    var response = Meteor.http.post(
+    if (!Meteor.accounts.google._clientId || !Meteor.accounts.google._appUrl)
+      throw new Meteor.accounts.ConfigError("Need to call Meteor.accounts.google.config first");
+    if (!Meteor.accounts.google._secret)
+      throw new Meteor.accounts.ConfigError("Need to call Meteor.accounts.google.setSecret first");
+
+    var accessToken = getAccessToken(query);
+    var identity = getIdentity(accessToken);
+
+    return {
+      email: identity.email,
+      userData: {name: identity.name},
+      serviceUserId: identity.id,
+      serviceData: {accessToken: accessToken}
+    };
+  });
+
+  var getAccessToken = function (query) {
+    var result = Meteor.http.post(
       "https://accounts.google.com/o/oauth2/token", {params: {
-        code: req.query.code,
+        code: query.code,
         client_id: Meteor.accounts.google._clientId,
         client_secret: Meteor.accounts.google._secret,
         redirect_uri: Meteor.accounts.google._appUrl + "/_oauth/google?close",
         grant_type: 'authorization_code'
-      }}).data;
+      }});
 
-    if (response.error)
-      throw response;
+    if (result.error) // if the http response was an error
+      throw result.error;
+    if (result.data.error) // if the http response was a json object with an error attribute
+      throw result.data;
+    return result.data.access_token;
+  };
 
-    return response.access_token;
+  var getIdentity = function (accessToken) {
+    var result = Meteor.http.get(
+      "https://www.googleapis.com/oauth2/v1/userinfo",
+      {params: {access_token: accessToken}});
+
+    if (result.error)
+      throw result.error;
+    return result.data;
   };
 })();
