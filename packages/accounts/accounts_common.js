44751cebc520702719cdd4adccc9e0306734b2ee
Nick Martin <nim@meteor.com>
2012-06-15 13:11:02 -0700

    Clean up users autopublish.
    Users table suppresses private fields when autopublished. Current user published turned into automatic publish, so the warning about the autopublish package is suppressed.

--- packages/accounts/accounts_common.js
+++ packages/accounts/accounts_common.js
@@ -1,11 +1,18 @@
-Meteor.users = new Meteor.Collection("users");
-
 if (!Meteor.accounts) {
   Meteor.accounts = {};
 }
 
+// internal login tokens collection. Never published.
 Meteor.accounts._loginTokens = new Meteor.Collection(
   "accounts._loginTokens",
+  null /*manager*/,
+  null /*driver*/,
+  true /*preventAutopublish*/);
+
+// Users table. Don't use the normal autopublish, since we want to hide
+// some fields. Code to autopublish this is in accounts_server.js.
+Meteor.users = new Meteor.Collection(
+  "users",
   null /*manager*/,
   null /*driver*/,
   true /*preventAutopublish*/);
