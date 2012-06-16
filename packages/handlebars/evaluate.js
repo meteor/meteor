2a9538e04cb8e9ac10747ac9869ea2f1062bfca7
David Greenspan <dgreenspan@alum.mit.edu>
2012-06-16 14:46:45 -0700

    Handlebars: allow dots on helpers, methods, and undefineds

--- packages/handlebars/evaluate.js
+++ packages/handlebars/evaluate.js
@@ -95,40 +95,69 @@ Handlebars.evaluate = function (ast, data, options) {
   var eval_value = function (stack, id) {
     if (typeof(id) !== "object")
       return id;
-    if (id.length === 2 && id[0] === 0 && (id[1] in helpers))
-      return helpers[id[1]]; // found helper
+
+    var helperThis = stack.data;
+
+    // follow '..' in {{../../foo.bar}}
     for (var i = 0; i < id[0]; i++) {
       if (!stack.parent)
         throw new Error("Too many '..' segments");
       else
         stack = stack.parent;
     }
-    var ret = stack.data;
-    if (id.length > 1 && typeof ret !== 'object') {
-      // Fail with better error than "can't read property of undefined".
-      // Looking up id[1] as a property will fail, because
-      // there is no data context object.  Probably the developer
-      // intended to use a helper that doesn't exist.
-      if (typeof (function() {})[id[1]] !== 'undefined') {
-        // An even more specific case for a helpful error.
-        // The developer probably tried to name a helper 'name',
-        // 'length', or some other built-in function property.
-        // Assignments to these properties are no-ops, so the
-        // helper declaration is undetectable.
-        // We can't always catch this mistake, because if there is any
-        // object as data context, it's legal for the developer to
-        // ask for {{name}} as a property of the object, perhaps an
-        // optional one.  But if there is no data context, we get
-        // to be helpful.
+
+    if (id.length === 1)
+      // no name: {{this}}, {{..}}, {{../..}}
+      return stack.data;
+
+    var data;
+    if (id[0] === 0 && (id[1] in helpers)) {
+      // first path segment is a helper
+      data = helpers[id[1]];
+    } else {
+      if ((! data instanceof Object) &&
+          (typeof (function() {})[id[1]] !== 'undefined')) {
+        // Give a helpful error message if the user tried to name
+        // a helper 'name', 'length', or some other built-in property
+        // of function objects.  Unfortunately, this case is very
+        // hard to detect, as Template.foo.name = ... will fail silently,
+        // and {{name}} will be silently empty if the property doesn't
+        // exist (per Handlebars rules).
+        // However, if there is no data context at all, we jump in.
         throw new Error("Can't call a helper '"+id[1]+"' because "+
                         "it is a built-in function property in JavaScript");
       }
-      throw new Error("Unknown helper '"+id[1]+"'");
+      // first path segment is property of data context
+      data = (stack.data && stack.data[id[1]]);
     }
-    for (var i = 1; i < id.length; i++)
-      // XXX error (and/or unknown key) handling
-      ret = ret[id[i]];
-    return ret;
+
+    // handle dots, as in {{foo.bar}}
+    for (var i = 2; i < id.length; i++) {
+      // Call functions when taking the dot, to support
+      // for example currentUser.name.
+      //
+      // In the case of {{foo.bar}}, we end up returning one of:
+      // - helpers.foo.bar
+      // - helpers.foo().bar
+      // - stack.data.foo.bar
+      // - stack.data.foo().bar.
+      //
+      // The caller does the final application with any
+      // arguments, as in {{foo.bar arg1 arg2}}, and passes
+      // the current data context in `this`.  Therefore,
+      // we use the current data context (`helperThis`)
+      // for all function calls.
+      if (typeof data === 'function')
+        data = data.call(helperThis);
+      else if (data === undefined || data === null)
+        // Handlebars fails silently and returns "" if
+        // we start to access properties that don't exist.
+        data = '';
+
+      data = data[id[i]];
+    }
+
+    return data;
   };
 
   // 'extra' will be clobbered, but not 'params'
