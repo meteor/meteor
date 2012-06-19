49918e58ca7f5cfae467aac7b9c502d82767ad59
Tom Coleman <tom@thesnail.org>
2012-06-19 11:16:20 +1000

    Unsuccessfully tried to add reloading test.

--- packages/session/session_client_tests.js
+++ packages/session/session_client_tests.js
@@ -27,8 +27,24 @@ Tinytest.add("session get/equals reactiveness", function(test) {
   test.equal(context_called, 4);
 });
 
-// don't really know how to run this test
+// XXX : can't override native window.location.reload
+//  need to find another way to test this.
+//
+// // XXX: don't really know how to run this test, this feels a bit hacky, 
+// //   but at least tests something
 // Tinytest.add("session state stored", function(test) {
 //   Session.set('permanent', 'value');
+//   test.isTrue(Session.equals('permanent', 'value'));
+//   
+//   var old_reload = window.location.reload;
+//   window.location.reload = function() {
+//     // clear the Session out
+//     for (var key in Session.keys) {
+//       Session.set(key, undefined);
+//     }
+//   };
+//   // Meteor._reload.reload();
+//   window.location.reload = old_reload;
+//   
 //   test.isTrue(Session.equals('permanent', 'value'));
 // });
