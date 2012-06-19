2e2ea18522398762d849d697d1289ba413f0f51f
David Greenspan <dgreenspan@alum.mit.edu>
2012-06-18 18:34:01 -0700

    handlebars: set `this` appropriately when calling methods

--- packages/templating/templating_tests.js
+++ packages/templating/templating_tests.js
@@ -150,8 +150,9 @@ Tinytest.add("templating - helpers and dots", function(test) {
       },
       currentCountry: function() {
         return {name: 'Iceland',
+                _pop: 321007,
                 population: function() {
-                  return 321007;
+                  return this._pop;
                 },
                 unicorns: 0, // falsy value
                 daisyGetter: function() {
@@ -206,8 +207,13 @@ Tinytest.add("templating - helpers and dots", function(test) {
   test.equal(Template.test_helpers_d(dataObj).match(/\S+/g), [
     // helpers should get current data context in `this`
     'daisygetter=petal',
-    // object methods should get current data context in `this`
-    'thisTest=leaf'
+    // object methods should get object in `this`
+    'thisTest=leaf',
+    // nesting inside {{#with fancy}} shouldn't affect
+    // method
+    '../thisTest=leaf',
+    // combine .. and .
+    '../fancy.currentFruit=guava'
   ]);
 
   test.equal(Template.test_helpers_e(dataObj).match(/\S+/g), [
@@ -216,10 +222,7 @@ Tinytest.add("templating - helpers and dots", function(test) {
     'fancy.currentFruit=guava',
     'fancy.currentCountry.name=Iceland',
     'fancy.currentCountry.population=321007',
-    'fancy.currentCountry.unicorns=0',
-    // all functions receive the current data context in `this`,
-    // for consistency
-    'fancy.currentCountry.daisyGetter=petal'
+    'fancy.currentCountry.unicorns=0'
   ]);
 
   test.equal(Template.test_helpers_f(dataObj).match(/\S+/g), [
@@ -228,10 +231,7 @@ Tinytest.add("templating - helpers and dots", function(test) {
     'fancyhelper.currentFruit=guava',
     'fancyhelper.currentCountry.name=Iceland',
     'fancyhelper.currentCountry.population=321007',
-    'fancyhelper.currentCountry.unicorns=0',
-    // all functions receive the current data context in `this`,
-    // for consistency
-    'fancyhelper.currentCountry.daisyGetter=petal'
+    'fancyhelper.currentCountry.unicorns=0'
   ]);
 
   // test significance of 'this', which prevents helper from
@@ -240,4 +240,5 @@ Tinytest.add("templating - helpers and dots", function(test) {
     'platypus=eggs',
     'this.platypus=weird'
   ]);
+
 });
