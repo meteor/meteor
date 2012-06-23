a248f0ff2869eea8d0cb7ee30f747169460a71a2
Nick Martin <nim@meteor.com>
2012-06-22 21:01:42 -0700

    Allowing passing in a default DDP endpoint as an environment variable.

--- packages/livedata/client_convenience.js
+++ packages/livedata/client_convenience.js
@@ -1,14 +1,24 @@
-_.extend(Meteor, {
-  default_connection: Meteor.connect('/', true /* restart_on_update */),
+(function () {
+  // By default, try to connect back to the same endpoint as the page
+  // was served from.
+  var ddp_endpoint = '/';
+  if (__meteor_runtime_config__.DEFAULT_DDP_ENDPOINT)
+    ddp_endpoint = __meteor_runtime_config__.DEFAULT_DDP_ENDPOINT;
 
-  refresh: function (notification) {
-  }
-});
+  _.extend(Meteor, {
+    default_connection: Meteor.connect(ddp_endpoint,
+                                       true /* restart_on_update */),
 
-// Proxy the public methods of Meteor.default_connection so they can
-// be called directly on Meteor.
-_.each(['subscribe', 'methods', 'call', 'apply', 'status', 'reconnect'],
-       function (name) {
-         Meteor[name] = _.bind(Meteor.default_connection[name],
-                               Meteor.default_connection);
-       });
+    refresh: function (notification) {
+    }
+  });
+
+  // Proxy the public methods of Meteor.default_connection so they can
+  // be called directly on Meteor.
+  _.each(['subscribe', 'methods', 'call', 'apply', 'status', 'reconnect'],
+         function (name) {
+           Meteor[name] = _.bind(Meteor.default_connection[name],
+                                 Meteor.default_connection);
+         });
+
+})();
