6ea8a1f30cc753e8b622b86704e1940dff5b67a1
ZHANG Cheng <czhang.oss@gmail.com>
2012-06-29 00:31:36 +0800

    New smart package for Weibo (short for sina.com's microblogging service in China)

---
+++ packages/accounts-weibo/weibo_client.js
@@ -0,0 +1,18 @@
+(function () {
+  Meteor.loginWithWeibo = function () {
+    if (!Meteor.accounts.weibo._clientId || !Meteor.accounts.weibo._appUrl)
+      throw new Meteor.accounts.ConfigError("Need to call Meteor.accounts.weibo.config first");
+
+    var state = Meteor.uuid();
+    // XXX need to support configuring access_type and scope
+    var loginUrl =
+          'https://api.weibo.com/oauth2/authorize' +
+          '?response_type=code' +
+          '&client_id=' + Meteor.accounts.weibo._clientId +
+          '&redirect_uri=' + Meteor.accounts.weibo._appUrl + '/_oauth/weibo?close' +
+          '&state=' + state;
+
+    Meteor.accounts.oauth2.initiateLogin(state, loginUrl);
+  };
+
+}) ();
