0e383bd8be41e693eb995c6a331fc97898455226
Nick Martin <nim@meteor.com>
2012-07-10 18:04:53 -0700

    Add an option to configure scope for google and facebook login.

--- packages/accounts-facebook/facebook_common.js
+++ packages/accounts-facebook/facebook_common.js
@@ -2,9 +2,10 @@ if (!Meteor.accounts.facebook) {
   Meteor.accounts.facebook = {};
 }
 
-Meteor.accounts.facebook.config = function(appId, appUrl) {
+Meteor.accounts.facebook.config = function(appId, appUrl, options) {
   Meteor.accounts.facebook._appId = appId;
   Meteor.accounts.facebook._appUrl = appUrl;
+  Meteor.accounts.facebook._options = options;
 };
 
 
