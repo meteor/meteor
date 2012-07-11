2fc244417b0329832f80976de0fcf25626396726
David Greenspan <dgreenspan@alum.mit.edu>
2012-07-11 13:55:51 -0700

    Handlebars: allow getters/helpers in args, fix hashes

--- templating_tests.js
+++ templating_tests.js
@@ -141,6 +141,11 @@ Tinytest.add("templating - helpers and dots", function(test) {
     return this.daisy;
   });
 
+  // XXX for debugging
+  Handlebars.registerHelper("debugger", function() {
+    debugger;
+  });
+
   var getFancyObject = function() {
     return {
       foo: 'bar',
@@ -170,6 +175,13 @@ Tinytest.add("templating - helpers and dots", function(test) {
     return 'snout';
   };
 
+  var listFour = function(a, b, c, d, options) {
+    var keywordArgs = _.map(_.keys(options.hash), function(k) {
+      return k+':'+options.hash[k];
+    });
+    return [a, b, c, d].concat(keywordArgs).join(' ');
+  };
+
   var dataObj = {
     zero: 0,
     platypus: 'weird',
@@ -177,7 +189,8 @@ Tinytest.add("templating - helpers and dots", function(test) {
     daisy: 'petal',
     tree: function() { return 'leaf'; },
     thisTest: function() { return this.tree(); },
-    fancy: getFancyObject()
+    fancy: getFancyObject(),
+    methodListFour: listFour
   };
 
   test.equal(Template.test_helpers_a(dataObj).match(/\S+/g), [
@@ -241,4 +254,56 @@ Tinytest.add("templating - helpers and dots", function(test) {
     'this.platypus=weird'
   ]);
 
+  // test interpretation of arguments
+
+  Template.test_helpers_h.helperListFour = listFour;
+
+  var trials =
+        Template.test_helpers_h(dataObj).match(/\(.*?\)/g);
+  test.equal(trials[0],
+             '(methodListFour 6 7 8 9=6 7 8 9)');
+  test.equal(trials[1],
+             '(methodListFour platypus thisTest fancyhelper.currentFruit fancyhelper.currentCountry.unicorns=eggs leaf guava 0)');
+  test.equal(trials[2],
+             '(methodListFour platypus thisTest fancyhelper.currentFruit fancyhelper.currentCountry.unicorns a=platypus b=thisTest c=fancyhelper.currentFruit d=fancyhelper.currentCountry.unicorns=eggs leaf guava 0 a:eggs b:leaf c:guava d:0)');
+  test.equal(trials[3],
+             '(helperListFour platypus thisTest fancyhelper.currentFruit fancyhelper.currentCountry.unicorns=eggs leaf guava 0)');
+  test.equal(trials[4],
+             '(helperListFour platypus thisTest fancyhelper.currentFruit fancyhelper.currentCountry.unicorns a=platypus b=thisTest c=fancyhelper.currentFruit d=fancyhelper.currentCountry.unicorns=eggs leaf guava 0 a:eggs b:leaf c:guava d:0)');
+  test.equal(trials.length, 5);
+
+  // test interpretation of block helper invocation
+
+  Template.test_helpers_i.uppercase = function(fn) {
+    return fn().toUpperCase();
+  };
+  Template.test_helpers_i.tr = function(options) {
+    var str = options.fn();
+    _.each(options.hash, function(v,k) {
+      str = str.replace(new RegExp(k, 'g'), v);
+    });
+    return str;
+  };
+  Template.test_helpers_i.arg_and_dict = function(arg, options) {
+    if (typeof options.hash !== "object")
+      throw new Error();
+    return _.keys(options.hash).length;
+  };
+  Template.test_helpers_i.get_arg = function(arg) {
+    return arg;
+  };
+  Template.test_helpers_i.helperListFour = listFour;
+
+  trials =
+        Template.test_helpers_i(dataObj).match(/\(.*?\)/g);
+  test.equal(trials[0], "(uppercase apple=APPLE)");
+  test.equal(trials[1], "(altered banana=bododo)");
+  // presence of arg should prevent keyword arguments from
+  // being passed to block helper, whether or not the arg
+  // is a function.
+  test.equal(trials[2], "(nokeys=0)");
+  test.equal(trials[3], "(nokeys=0)");
+  test.equal(trials[4],
+             '(biggie=eggs leaf guava 0 a:eggs b:leaf c:guava d:0)');
+  test.equal(trials.length, 5);
 });
