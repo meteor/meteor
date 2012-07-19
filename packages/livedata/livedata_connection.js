9693ac4be3d2e007d3eb92fed91c8ba1b43eeb5f
Avital Oliver <avital@thewe.net>
2012-07-19 14:00:54 -0700

    Fix an ordering bug with method responses related to wait methods

--- packages/livedata/livedata_connection.js
+++ packages/livedata/livedata_connection.js
@@ -112,7 +112,6 @@ Meteor._LivedataConnection = function (url, restart_on_update) {
   });
 
   self.stream.on('reset', function () {
-
     // Send a connect message at the beginning of the stream.
     // NOTE: reset is called even on the first connection, so this is
     // the only place we send this message.
@@ -605,8 +604,7 @@ _.extend(Meteor._LivedataConnection.prototype, {
     }
 
     if (!m) {
-      // XXX write a better error
-      Meteor._debug("Can't interpret method response message");
+      Meteor._debug("Can't match method response to original method call", msg);
       return;
     }
 
@@ -614,20 +612,16 @@ _.extend(Meteor._LivedataConnection.prototype, {
       // Wait until we have completed all outstanding methods.
       if (self.outstanding_methods.length === 0 &&
          self.outstanding_wait_method_response) {
-        // Fire necessary outstanding method callbacks, making sure we
-        // only fire the outstanding wait method after all other outstanding
-        // methods' callbacks were fired
-        if (m === self.outstanding_wait_method) {
-          self._deliverMethodResponse(self.outstanding_wait_method,
-                       self.outstanding_wait_method_response /*(=== msg)*/);
-        } else {
-          self._deliverMethodResponse(m, msg);
-          self._deliverMethodResponse(self.outstanding_wait_method,
-                       self.outstanding_wait_method_response /*(!== msg)*/);
-        }
 
-        self.outstanding_wait_method = null;
+        // Start by saving the outstanding wait method details, since
+        // we're going to reshift the blocked ones and try to send
+        // them *before* calling the method callback. It is necessary
+        // to call method callbacks last since they might themselves
+        // call other methods
+        var savedOutstandingWaitMethod = self.outstanding_wait_method;
+        var savedOutstandingWaitMethodResponse = self.outstanding_wait_method_response;
         self.outstanding_wait_method_response = null;
+        self.outstanding_wait_method = null;
 
         // Find first blocked method with wait: true
         var i;
@@ -635,15 +629,31 @@ _.extend(Meteor._LivedataConnection.prototype, {
           if (self.blocked_methods[i].wait)
             break;
 
-        // Move as many blocked methods as we can into outstanding_methods
-        // and outstanding_wait_method if needed
+        // Move as many blocked methods as we can into
+        // outstanding_methods and outstanding_wait_method if needed
         self.outstanding_methods = _.first(self.blocked_methods, i);
         if (i !== self.blocked_methods.length) {
           self.outstanding_wait_method = self.blocked_methods[i];
           self.blocked_methods = _.rest(self.blocked_methods, i+1);
         }
 
+        // Send any new outstanding methods after we reshift the
+        // blocked methods.  Intentionally do this before calling the
+        // method response because they might call additional methods
+        // that shouldn't be sent twice.
         self._sendOutstandingMethods();
+
+        // Fire necessary outstanding method callbacks, making sure we
+        // only fire the outstanding wait method after all other outstanding
+        // methods' callbacks were fired
+        if (m === savedOutstandingWaitMethod) {
+          self._deliverMethodResponse(savedOutstandingWaitMethod,
+                       savedOutstandingWaitMethodResponse /*(=== msg)*/);
+        } else {
+          self._deliverMethodResponse(m, msg);
+          self._deliverMethodResponse(savedOutstandingWaitMethod,
+                       savedOutstandingWaitMethodResponse /*(!== msg)*/);
+        }
       } else {
         if (m !== self.outstanding_wait_method)
           self._deliverMethodResponse(m, msg);
