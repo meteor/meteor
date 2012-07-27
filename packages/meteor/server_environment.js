f3b2e930eec7fa5b3d404a4f839039f5f609b1c0
Corban Brook <corbanbrook@gmail.com>
2012-07-26 21:32:31 -0400

    Allows you to clearly set an environment mode to run the project in (development, staging, or production) and reveals is_development, is_staging, and is_production properties on the Meteor class in both server and client.

--- packages/meteor/server_environment.js
+++ packages/meteor/server_environment.js
@@ -1,4 +1,7 @@
 Meteor = {
   is_client: false,
   is_server: true
+  is_development: process.env.METEOR_ENV === 'development',
+  is_staging: process.env.METEOR_ENV === 'staging',
+  is_production: process.env.METEOR_ENV === 'production'
 };
