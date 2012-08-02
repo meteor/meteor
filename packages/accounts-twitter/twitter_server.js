8a8692b517ccefaae9a6d598495f62dac176ff5c
Mike Bannister <notimpossiblemike@gmail.com>
2012-08-02 00:31:57 -0400

    better munging twitter data for updateOrCreateUser

--- packages/accounts-twitter/twitter_server.js
+++ packages/accounts-twitter/twitter_server.js
@@ -10,12 +10,17 @@
 
     return {
       options: {
-        // XXX Figure out what to do here
-        email: identity.screen_name + '@OAUTH1_TWITTER',
-        // XXX Do we want to keep the accessTokenSecret also?
-        services: {twitter: {id: identity.id, accessToken: oauth.accessToken}}
+        services: {
+          twitter: {
+            id: identity.id,
+            screenName: identity.screen_name,
+            accessToken: oauth.accessToken
+          }
+        }
       },
-      extra: {name: identity.name}
+      extra: {
+        name: identity.name
+      }
     };
   });
 }) ();
