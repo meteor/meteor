3e75ed625ab7b127e508da4748894f52b27f6249
Geoff Schmidt <geoff@geoffschmidt.com>
2012-08-09 17:57:32 -0700

    comment changes

--- packages/spark/spark.js
+++ packages/spark/spark.js
@@ -18,6 +18,12 @@
 
 // XXX should functions with an htmlFunc use try/finally inside?
 
+// XXX test that lists are cleaned up if not materialized
+
+// XXX test that non-Spark.render case works for each function (eg,
+// list() returns the expected HTML, Spark.createLandmark creates and
+// then destroys a landmark -- may already be tested?)
+
 (function() {
 
 Spark = {};
