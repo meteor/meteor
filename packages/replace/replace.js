ca3fbe5c78d84a7785dfb805fdaed0d8ee7dd619
Jens Ulrich Hjuler Pedersen <multiply@juhp.net>
2012-08-19 05:54:54 +0200

    Plain replaces are now global

--- packages/replace/replace.js
+++ packages/replace/replace.js
@@ -1,6 +1,10 @@
 Handlebars.registerHelper('replace', function (map, content) {
   str = content.fn(this);
   _.each(map, function (value) {
+    if (! (value.from instanceof RegExp)) {
+      fromSafe = String(value.from).replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+      value.from = new RegExp(fromSafe, 'g');
+    }
     str = str.replace(value.from, value.to);
   })
   return str;
