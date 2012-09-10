d51d060f83c21a5581d3676df008529249895fc8
Lloyd Brookes <lloyd.brookes@gmail.com>
2012-09-10 21:26:16 +0100

    Fix OAuth login on Android (fixes #328).

--- packages/accounts-oauth-helper/oauth_client.js
+++ packages/accounts-oauth-helper/oauth_client.js
@@ -10,7 +10,10 @@
     var popup = openCenteredPopup(url, 650, 331);
 
     var checkPopupOpen = setInterval(function() {
-      if (popup.closed) {
+      // Fix for #328 - added a second test criteria (popup.closed === undefined) 
+      // to humour this Android quirk: 
+      // http://code.google.com/p/android/issues/detail?id=21061
+      if (popup.closed || popup.closed === undefined) {
         clearInterval(checkPopupOpen);
         tryLoginAfterPopupClosed(state);
       }
