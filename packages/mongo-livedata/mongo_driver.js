afcafe1eb571241871eda698b7ba6b71774828aa
David Glasser <glasser@meteor.com>
2012-09-28 17:49:08 -0700

    Refactor Mongo cursor reading so that all reads go through _nextObject.

--- packages/mongo-livedata/mongo_driver.js
+++ packages/mongo-livedata/mongo_driver.js
@@ -290,8 +290,21 @@ _Mongo.Cursor = function (mongo, collection_name, selector, options, cursor) {
   self.selector = selector;
   self.options = options;
   self.cursor = cursor;
+  self._synchronousNextObject = Future.wrap(cursor.nextObject.bind(cursor));
+  self._synchronousCount = Future.wrap(cursor.count.bind(cursor));
 
   self.visited_ids = {};
+};
+
+_Mongo.Cursor.prototype._nextObject = function () {
+  var self = this;
+  while (true) {
+    var doc = self._synchronousNextObject().wait();
+    if (!doc || !doc._id) return null;
+    if (self.visited_ids[doc._id]) continue;
+    self.visited_ids[doc._id] = true;
+    return doc;
+  }
 };
 
 // XXX Make more like ECMA forEach:
@@ -299,19 +312,12 @@ _Mongo.Cursor = function (mongo, collection_name, selector, options, cursor) {
 _Mongo.Cursor.prototype.forEach = function (callback) {
   var self = this;
 
-  var wrappedNextObject = Future.wrap(self.cursor.nextObject.bind(self.cursor));
-
   // We implement the loop ourself instead of using self.cursor.each, because
   // "each" will call its callback outside of a fiber which makes it much more
   // complex to make this function synchronous.
   while (true) {
-    var doc = wrappedNextObject().wait();
-    if (!doc || !doc._id)
-      return;
-    // Have we already seen this doc (Mongo cursors can return duplicates)?
-    if (self.visited_ids[doc._id])
-      continue;
-    self.visited_ids[doc._id] = true;
+    var doc = self._nextObject();
+    if (!doc) return;
     callback(doc);
   }
 };
@@ -339,32 +345,12 @@ _Mongo.Cursor.prototype.rewind = function () {
 
 _Mongo.Cursor.prototype.fetch = function () {
   var self = this;
-  var future = new Future;
-
-  self.cursor.toArray(function (err, res) {
-    future.ret([err, res]);
-  });
-
-  var result = future.wait();
-  if (result[0])
-    throw result[0];
-  // dedup
-  return _.uniq(result[1], false, function(doc) {
-    return doc._id; });
+  return self.map(_.identity);
 };
 
 _Mongo.Cursor.prototype.count = function () {
   var self = this;
-  var future = new Future;
-
-  self.cursor.count(function (err, res) {
-    future.ret([err, res]);
-  });
-
-  var result = future.wait();
-  if (result[0])
-    throw result[0];
-  return result[1];
+  return self._synchronousCount().wait();
 };
 
 // options to contain:
