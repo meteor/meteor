7d368a3f6ed27f5664c3703c4f204fba5f90140b
Avital Oliver <avital@thewe.net>
2012-10-09 15:35:54 -0700

    accounts-ui: configure scope for external services

--- packages/accounts-ui-unstyled/accounts_ui.js
+++ packages/accounts-ui-unstyled/accounts_ui.js
@@ -1,7 +1,11 @@
 if (!Accounts.ui)
   Accounts.ui = {};
-if (!Accounts.ui._options)
-  Accounts.ui._options = {};
+
+if (!Accounts.ui._options) {
+  Accounts.ui._options = {
+    requestScope: {}
+  };
+}
 
 Accounts.ui.config = function(options) {
   if (options.newUserWithPasswordHas) {
@@ -13,12 +17,21 @@ Accounts.ui.config = function(options) {
     ], options.newUserWithPasswordHas)) {
       if (Accounts.ui._options.newUserWithPasswordHas)
         throw new Error("Can't set `newUserWithPasswordHas` more than once");
+      else
+        Accounts.ui._options.newUserWithPasswordHas = options.newUserWithPasswordHas;
     } else {
       throw new Error("Invalid option for `newUserWithPasswordHas`: " + newUserWithPasswordHas);
     }
   }
 
-  _.extend(Accounts.ui._options, options);
+  if (options.requestScope) {
+    _.each(options.requestScope, function (scope, service) {
+      if (Accounts.ui._options.requestScope[service])
+        throw new Error("Can't set `requestScope` more than once for " + service);
+      else
+        Accounts.ui._options.requestScope[service] = scope;
+    });
+  }
 };
 
 Accounts.ui._newUserWithPasswordHas = function () {
--- packages/accounts-ui-unstyled/login_buttons_single.js
+++ packages/accounts-ui-unstyled/login_buttons_single.js
@@ -6,7 +6,7 @@
     'click .login-button': function () {
       var serviceName = this.name;
       loginButtonsSession.resetMessages();
-      Meteor["loginWith" + capitalize(serviceName)](function (err) {
+      var callback = function (err) {
         if (!err) {
           loginButtonsSession.closeDropdown();
         } else if (err instanceof Accounts.LoginCancelledError) {
@@ -16,7 +16,15 @@
         } else {
           loginButtonsSession.set('errorMessage', err.reason || "Unknown error");
         }
-      });
+      };
+
+      var loginWithService = Meteor["loginWith" + capitalize(serviceName)];
+
+      var options = {}; // use default scope unless specified
+      if (Accounts.ui._options.requestScope[serviceName])
+        options.scope = Accounts.ui._options.requestScope[serviceName];
+
+      loginWithService(options, callback);
     }
   });
 
