f119348d9cda7d764a35165984775cc6e0fb06d7
David Glasser <glasser@meteor.com>
2012-10-15 10:52:18 -0700

    Remove addressed XXX comment: verification tokens are now separate.

--- examples/parties/server/server.js
+++ examples/parties/server/server.js
@@ -1,8 +1,7 @@
 // XXX autopublish warning is printed on each restart. super spammy!
 
 Meteor.publish("directory", function () {
-  // XXX too easy to accidentally publish the list of validation tokens
-  return Meteor.users.find({}, {fields: {"emails.address": 1}});
+  return Meteor.users.find({}, {fields: {emails: 1}});
 });
 
 Meteor.publish("parties", function () {
