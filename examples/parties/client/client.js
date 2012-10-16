0ca1386c106a9f6f0469d63c7734b42486a8e2ef
Geoff Schmidt <geoff@geoffschmidt.com>
2012-10-15 23:49:29 -0700

    parties: style d3 portion of app

--- examples/parties/client/client.js
+++ examples/parties/client/client.js
@@ -119,7 +119,7 @@ function relMouseCoords (element, event) {
   var canvasY = 0;
   var currentElement = element;
 
-  do{
+  do {
     totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
     totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
   }
@@ -155,82 +155,62 @@ Template.map.rendered = function () {
   if (! self.handle) {
     self.handle = Meteor.autorun(function () {
       var selected = Session.get('selected');
+      var selectedParty = selected && Parties.findOne(selected);
 
-      var marker = d3.select(self.node).select(".circles").selectAll("circle")
-        .data(Parties.find().fetch(),
-              function (party) { return party._id; });
+      var radius = function (party) {
+        return 10 + Math.sqrt(attending(party)) * 10;
+      };
 
-      var labels = d3.select(self.node).select(".labels").selectAll("text")
-        .data(Parties.find().fetch(),
-              function (party) { return party._id; });
+      // Draw a circle for each party
+      var circles = d3.select(self.node).select(".circles").selectAll("circle")
+        .data(Parties.find().fetch(), function (party) { return party._id; });
 
-      marker.enter().append("circle")
-        .attr("id", function (party) {
-          return party._id;
-        })
-        .attr("cx", function (party) {
-          return party.x * 500;
-        })
-        .attr("cy", function (party) {
-          return party.y * 500;
-        })
-        // XXX match area
-        // (duplicated below)
-        .attr("r", function (party) {
-          return 10 + Math.sqrt(attending(party)) * 10;
-        })
-        .style("fill", function (party) {
-          return party.public ? 'red' : 'blue';
+      var updateCircles = function (group) {
+        group.attr("id", function (party) { return party._id; })
+        .attr("cx", function (party) { return party.x * 500; })
+        .attr("cy", function (party) { return party.y * 500; })
+        .attr("r", radius)
+        .attr("class", function (party) {
+          return party.public ? "public" : "private";
         })
         .style('opacity', function (party) {
-          return selected === party._id ? 1 : 0.4;
+          return selected === party._id ? 1 : 0.6;
         });
+      };
 
-      marker.transition()
-        .duration(250)
-        .attr("cx", function (party) {
-          return party.x * 500;
-        })
-        .attr("cy", function (party) {
-          return party.y * 500;
-        })
-        .attr("r", function (party) {
-          return 10 + Math.sqrt(attending(party)) * 8;
-        })
-        .style("fill", function (party) {
-          return party.public ? 'red' : 'blue';
-        })
-        .style('opacity', function (party) {
-          return selected === party._id ? 1 : 0.4;
-        })
-        .ease("cubic-out");
+      updateCircles(circles.enter().append("circle"));
+      updateCircles(circles.transition().duration(250).ease("cubic-out"));
+      circles.exit().remove();
 
-      marker.exit().remove();
+      // Label each with the current attendance count
+      var labels = d3.select(self.node).select(".labels").selectAll("text")
+        .data(Parties.find().fetch(), function (party) { return party._id; });
 
-      // XXX it'd be nice to stroke the text out in white
-      labels.enter().append("text")
-        .attr("id", function (party) {
-          return party._id;
-        })
-        .attr("x", function (party) {
-          return party.x * 500;
-        })
-        .attr("y", function (party) {
-          return party.y * 500;
-        })
-        .text(function (party) {return attending(party);});
+      var updateLabels = function (group) {
+        group.attr("id", function (party) { return party._id; })
+        .text(function (party) {return attending(party) || '';})
+        .attr("x", function (party) { return party.x * 500; })
+        .attr("y", function (party) { return party.y * 500 + radius(party)/2 })
+        .style('font-size', function (party) {
+          return radius(party) * 1.25 + "px";
+        });
+      };
 
-      labels.transition().duration(250)
-        .attr("x", function (party) {
-          return party.x * 500;
-        })
-        .attr("y", function (party) {
-          return party.y * 500;
-        })
-        .text(function (party) {return attending(party);});
-
+      updateLabels(labels.enter().append("text"));
+      updateLabels(labels.transition().duration(250).ease("cubic-out"));
       labels.exit().remove();
 
+      // Draw a dashed circle around the currently selected party, if any
+      var callout = d3.select(self.node).select("circle.callout")
+        .transition().duration(250).ease("cubic-out");
+      if (selectedParty)
+        callout.attr("cx", selectedParty.x * 500)
+        .attr("cy", selectedParty.y * 500)
+        .attr("r", radius(selectedParty) + 10)
+        .attr("class", "callout")
+        .attr("display", '');
+      else
+        callout.attr("display", 'none');
     });
   }
 };
