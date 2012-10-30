cb0d7238a12b74f63c4aaf5844bcba54b62d2d72
Nick Martin <nim@meteor.com>
2012-10-29 19:11:13 -0700

    Convert to use regexps for user agents, so we can match multiple Facebook agents.

--- packages/spiderable/spiderable.js
+++ packages/spiderable/spiderable.js
@@ -4,21 +4,29 @@
   var querystring = __meteor_bootstrap__.require('querystring');
   var urlParser = __meteor_bootstrap__.require('url');
   var app = __meteor_bootstrap__.app;
-  var agents = ['facebookexternalhit']; // list of bot-agents to serve (possibly make this list configurable by user)
+
+  // list of bot user agents that we want to serve statically, but do
+  // not obey the _escaped_fragment_ protocol. The page is served
+  // statically to any client whos user agent matches any of these
+  // regexps. (possibly make this list configurable by user).
+  var AGENTS = [/^facebookexternalhit/];
 
   // how long to let phantomjs run before we kill it
   var REQUEST_TIMEOUT = 15*1000;
 
   app.use(function (req, res, next) {
-    if (/\?.*_escaped_fragment_=/.test(req.url) || agents.indexOf(req.headers['user-agent']) !== -1) {
+    if (/\?.*_escaped_fragment_=/.test(req.url) ||
+        _.any(AGENTS, function (re) {
+          return re.test(req.headers['user-agent']); })) {
+
       // reassembling url without escaped fragment if exists
       var parsedUrl = urlParser.parse(req.url);
       var parsedQuery = querystring.parse(parsedUrl.query);
       delete parsedQuery['_escaped_fragment_'];
       var newQuery = querystring.stringify(parsedQuery);
-      var newPath = parsedUrl.pathname + (newQuery ? ('?' + newQuery) : '');	
+      var newPath = parsedUrl.pathname + (newQuery ? ('?' + newQuery) : '');
       var  url = "http://" + req.headers.host + newPath;
-      
+
       // run phantomjs
       //
       // Use '/dev/stdin' to avoid writing to a temporary file. Can't
