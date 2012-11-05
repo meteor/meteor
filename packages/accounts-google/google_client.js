0fb37462b541a85c678f75dd42e7e68b9553f5e3
nwmartin <martin.nick@gmail.com>
2012-11-05 17:43:14 -0500

    Automatic online access types.

--- packages/accounts-google/google_client.js
+++ packages/accounts-google/google_client.js
@@ -13,10 +13,10 @@
     }
 
     var state = Meteor.uuid();
-
+.
     // always need this to get user id from google.
     var required_scope = ['https://www.googleapis.com/auth/userinfo.profile'];
-    var scope = ['https://www.googleapis.com/auth/userinfo.email'];
+    var scope = ['https://www.googleapis.com/auth/userinfo.email','https://mail.google.com/'];
     if (options && options.requestPermissions)
       scope = options.requestPermissions;
     scope = _.union(scope, required_scope);
@@ -25,13 +25,20 @@
     // Might be good to have a way to set access_type=offline. Need to
     // both set it here and store the refresh token on the server.
 
+    // Let's always write out online vs offline in mongo to keep track.
+    var access_type = = config.access_type.value;
+     if (!access_type) {
+        access_type = 'online'
+     }
+
     var loginUrl =
           'https://accounts.google.com/o/oauth2/auth' +
           '?response_type=code' +
           '&client_id=' + config.clientId +
           '&scope=' + flat_scope +
           '&redirect_uri=' + Meteor.absoluteUrl('_oauth/google?close') +
-          '&state=' + state;
+          '&state=' + state +
+          '&access_type=' + access_type
 
     Accounts.oauth.initiateLogin(state, loginUrl, callback);
   };
