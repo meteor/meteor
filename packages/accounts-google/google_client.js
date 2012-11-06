3415308e1586e59d4e078a768fbdaf9ba2fe5a60
Nick Martin <martin.nick@gmail.com>
2012-11-06 15:56:32 -0500

    Checking for offline/online in config.

--- packages/accounts-google/google_client.js
+++ packages/accounts-google/google_client.js
@@ -25,13 +25,18 @@
     // Might be good to have a way to set access_type=offline. Need to
     // both set it here and store the refresh token on the server.
 
+    var accessType = config.accessType
+    if (!accessType)
+      accessType = 'online'
+
     var loginUrl =
           'https://accounts.google.com/o/oauth2/auth' +
           '?response_type=code' +
           '&client_id=' + config.clientId +
           '&scope=' + flat_scope +
           '&redirect_uri=' + Meteor.absoluteUrl('_oauth/google?close') +
-          '&state=' + state;
+          '&state=' + state +
+          '&access_type=' + accessType
 
     Accounts.oauth.initiateLogin(state, loginUrl, callback);
   };
