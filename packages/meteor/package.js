c7710ccd3ec7a0b2100892e5af6460af616fa886
David Glasser <glasser@meteor.com>
2012-11-10 17:17:41 -0800

    New class Meteor._SynchronousQueue, which runs tasks serially.
    Will be used for Meteor._Mongo._LiveResultsSet de-duping.

--- packages/meteor/package.js
+++ packages/meteor/package.js
@@ -57,5 +57,7 @@ Package.on_test(function (api) {
   api.add_files('helpers_test.js', ['client', 'server']);
   api.add_files('dynamics_test.js', ['client', 'server']);
 
+  api.add_files('fiber_helpers_test.js', ['server']);
+
   api.add_files('url_tests.js', ['client', 'server']);
 });
