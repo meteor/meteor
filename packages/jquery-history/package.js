2a68982e87e3d3f32263792b66fbd34381834177
Nick Martin <nim@meteor.com>
2012-11-12 16:17:35 -0800

    Make jquery-history package use the json package, not ship its own copy of json2.js

--- packages/jquery-history/package.js
+++ packages/jquery-history/package.js
@@ -3,9 +3,9 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.use('jquery');
-  api.add_files(['json2.js',
-                 'history.adapter.jquery.js',
+  api.use('json', 'client');
+  api.use('jquery', 'client');
+  api.add_files(['history.adapter.jquery.js',
                  'history.html4.js',
                  'history.js'],
                 'client');
