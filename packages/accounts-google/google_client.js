8d9420d25584c4c125ca596dd5faa8700c8f461a
Avital Oliver <avital@thewe.net>
2012-11-20 15:16:47 -0800

    Rename local variables (to camel case)

--- packages/accounts-google/google_client.js
+++ packages/accounts-google/google_client.js
@@ -15,21 +15,20 @@
     var state = Meteor.uuid();
 
     // always need this to get user id from google.
-    var required_scope = ['https://www.googleapis.com/auth/userinfo.profile'];
+    var requiredScope = ['https://www.googleapis.com/auth/userinfo.profile'];
     var scope = ['https://www.googleapis.com/auth/userinfo.email'];
     if (options && options.requestPermissions)
       scope = options.requestPermissions;
-    scope = _.union(scope, required_scope);
-    var flat_scope = _.map(scope, encodeURIComponent).join('+');
-
     // Might be good to have a way to set access_type=offline. Need to
     // both set it here and store the refresh token on the server.
+    scope = _.union(scope, requiredScope);
+    var flatScope = _.map(scope, encodeURIComponent).join('+');
 
     var loginUrl =
           'https://accounts.google.com/o/oauth2/auth' +
           '?response_type=code' +
           '&client_id=' + config.clientId +
-          '&scope=' + flat_scope +
+          '&scope=' + flatScope +
           '&redirect_uri=' + Meteor.absoluteUrl('_oauth/google?close') +
           '&state=' + state;
 
