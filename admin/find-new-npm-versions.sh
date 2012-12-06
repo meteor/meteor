ab0eb2c090576bbe47ac36a7388f8dc13c69d928
Avital Oliver <avital@thewe.net>
2012-12-05 19:45:01 -0800

    Add script to detect new versions of npm modules

---
+++ admin/find-new-npm-versions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cat generate-dev-bundle.sh | grep "npm install" | sed "s/npm install //" | sed "s/@.*//" | while read PACKAGE
+do
+  CURRENT_VERSION=`cat generate-dev-bundle.sh | grep "npm install $PACKAGE" | sed "s/npm install //" | sed "s/.*@//"`
+  LATEST_VERSION=`~/meteor/dev_bundle/bin/npm info $PACKAGE version 2> /dev/null`
+  if [ "$CURRENT_VERSION" != "$LATEST_VERSION" ]
+  then
+    echo "$PACKAGE -- current version: $CURRENT_VERSION, latest version: $LATEST_VERSION"
+  fi
+done
