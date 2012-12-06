6902c2a2caa40fd7330da93f71b545d561fb8346
Avital Oliver <avital@thewe.net>
2012-12-05 19:46:49 -0800

    Don't hard-code meteor install directory in find-new-npm-versions.sh

--- admin/find-new-npm-versions.sh
+++ admin/find-new-npm-versions.sh
@@ -2,7 +2,7 @@
 cat generate-dev-bundle.sh | grep "npm install" | sed "s/npm install //" | sed "s/@.*//" | while read PACKAGE
 do
   CURRENT_VERSION=`cat generate-dev-bundle.sh | grep "npm install $PACKAGE" | sed "s/npm install //" | sed "s/.*@//"`
-  LATEST_VERSION=`~/meteor/dev_bundle/bin/npm info $PACKAGE version 2> /dev/null`
+  LATEST_VERSION=`../dev_bundle/bin/npm info $PACKAGE version 2> /dev/null`
   if [ "$CURRENT_VERSION" != "$LATEST_VERSION" ]
   then
     echo "$PACKAGE -- current version: $CURRENT_VERSION, latest version: $LATEST_VERSION"
