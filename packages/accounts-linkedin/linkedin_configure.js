348bfe084c6ee8a0e603f3ab06bd7c481b56483d
Alex Notov <anotov@gmail.com>
2012-12-07 22:14:49 -0800

    Set consumerKey not apiKey

--- packages/accounts-linkedin/linkedin_configure.js
+++ packages/accounts-linkedin/linkedin_configure.js
@@ -1,12 +1,11 @@
-/*
 Template.configureLoginServiceDialogForLinkedin.siteUrl = function () {
-  // Linkedi doesn't recognize localhost as a domain name
+  // Linkedin doesn't recognize localhost as a domain name
   return Meteor.absoluteUrl({replaceLocalhost: true});
 };
-*/
+
 Template.configureLoginServiceDialogForLinkedin.fields = function () {
   return [
-    {property: 'apiKey', label: 'API Key'},
+    {property: 'consumerKey', label: 'Consumer Key'},
     {property: 'secret', label: 'Secret Key'}
   ];
 };
