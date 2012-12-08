b098944d442e152a3eed4648a6a3b4eaa47d8383
Alex Notov <anotov@gmail.com>
2012-12-08 00:27:45 -0800

    Authorize url should be authorize, and authenticatee should be authenticate

--- packages/accounts-linkedin/linkedin_common.js
+++ packages/accounts-linkedin/linkedin_common.js
@@ -4,7 +4,7 @@ if (!Accounts.linkedin) {
 
 Accounts.linkedin._urls = {
   requestToken: "https://api.linkedin.com/uas/oauth/requestToken",
-  authorize: "https://api.linkedin.com/uas/oauth/authenticate",
+  authorize: "https://api.linkedin.com/uas/oauth/authorize",
   accessToken: "https://api.linkedin.com/uas/oauth/accessToken",
   authenticate: "https://api.linkedin.com/uas/oauth/authenticate"
 };
