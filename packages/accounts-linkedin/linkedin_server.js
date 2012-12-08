65cbe3176fc7d46433f3001dc012fc90a174a693
Alex Notov <anotov@gmail.com>
2012-12-08 00:15:20 -0800

    Use json and don't get fields that are not supported

--- packages/accounts-linkedin/linkedin_server.js
+++ packages/accounts-linkedin/linkedin_server.js
@@ -1,18 +1,18 @@
 (function () {
 
   Accounts.oauth.registerService('linkedin', 1, function(oauthBinding) {
-    var identity = oauthBinding.get('http://api.linkedin.com/v1/people/~');
+    var identity = oauthBinding.get('http://api.linkedin.com/v1/people/~?format=json');
 
     return {
       serviceData: {
         id: identity.id,
-        screenName: identity.screen_name,
+        screenName: identity.id,
         accessToken: oauthBinding.accessToken,
         accessTokenSecret: oauthBinding.accessTokenSecret
       },
       options: {
         profile: {
-          name: identity.name
+          name: formatted-name
         }
       }
     };
