67b41508687e484cb72deccdce931407a1c96853
Martin Naumann <martin.naumann@centralway.com>
2012-12-14 22:41:06 +0100

    Adding expiresAt property to Google login data for accounts-google. fixes #525

--- packages/accounts-google/google_server.js
+++ packages/accounts-google/google_server.js
@@ -2,20 +2,21 @@
 
   Accounts.oauth.registerService('google', 2, function(query) {
 
-    var accessToken = getAccessToken(query);
-    var identity = getIdentity(accessToken);
+    var accessObject = getAccessObject(query);
+    var identity = getIdentity(accessObject.accessToken);
 
     return {
       serviceData: {
         id: identity.id,
-        accessToken: accessToken,
+        accessToken: accessObject.accessToken,
+        expiresAt: new Date().getTime() + (1000 * parseInt(accessObject.expiresIn, 10)),
         email: identity.email
       },
       options: {profile: {name: identity.name}}
     };
   });
 
-  var getAccessToken = function (query) {
+  var getAccessObject = function (query) {
     var config = Accounts.loginServiceConfiguration.findOne({service: 'google'});
     if (!config)
       throw new Accounts.ConfigError("Service not configured");
@@ -33,7 +34,10 @@
       throw result.error;
     if (result.data.error) // if the http response was a json object with an error attribute
       throw result.data;
-    return result.data.access_token;
+    return {
+      accessToken: result.data.access_token,
+      expiresIn: result.data.expires_in
+    };
   };
 
   var getIdentity = function (accessToken) {
