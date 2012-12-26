3746d38842bb5e4208eb83a0a2ce55db5f98e92d
Alex Notov <anotov@gmail.com>
2012-12-26 13:10:03 -0800

    Remove uniqueness check. Devs shouldn't be idiots :)

--- packages/accounts-ui-unstyled/accounts_ui_unstyled_tests.js
+++ packages/accounts-ui-unstyled/accounts_ui_unstyled_tests.js
@@ -9,17 +9,3 @@ Tinytest.add(
     test.equal(_.first(services), {name: "password"});
   }
 );
-
-
-Tinytest.add(
-  'accounts-ui - getLoginServices retuns unique services', 
-  function (test) {
-    // setup
-    var services;
-    Accounts._loginButtons.loginServices.push('password');
-    Accounts._loginButtons.loginServices.push('password');
-
-    services = Accounts._loginButtons.getLoginServices();
-    test.length(services, 1);
-  }
-);
