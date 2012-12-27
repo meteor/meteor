e4befac16ffa5bf09c8a8ec75a19fab8f1d3e16c
David Glasser <glasser@meteor.com>
2012-12-27 14:00:49 -0800

    copy-release-from-jenkins now takes care of setting URLs correctly in s3://com.meteor.static/test/update/

--- admin/copy-release-from-jenkins.sh
+++ admin/copy-release-from-jenkins.sh
@@ -8,10 +8,15 @@
 set -e
 set -u
 
+cd `dirname $0`
+
 TARGET="s3://com.meteor.static/test/"
+TEST=no
 if [ $# -ge 1 -a $1 = '--prod' ]; then
     shift
     TARGET="s3://com.meteor.static/"
+else
+    TEST=yes
 fi
 
 if [ $# -ne 1 ]; then
@@ -34,3 +39,15 @@ s3cmd ls s3://com.meteor.jenkins/$DIRNAME/ | \
 
 echo Copying to $TARGET
 s3cmd -P cp -r s3://com.meteor.jenkins/$DIRNAME/ $TARGET
+
+if [ $TEST = 'yes' ]; then
+    echo Uploading modified install-s3.sh and manifest.json
+
+    OUTDIR=$(mktemp -dt meteor-crfj)
+    perl -pe 's!https://d3sqy0vbqsdhku.cloudfront.net!https://s3.amazonaws.com/com.meteor.static/test!g' install-s3.sh >$OUTDIR/install-s3.sh
+    perl -pe 's!https://d3sqy0vbqsdhku.cloudfront.net!https://s3.amazonaws.com/com.meteor.static/test!g' manifest.json >$OUTDIR/manifest.json
+
+    cd $OUTDIR
+    s3cmd -P put install-s3.sh s3://com.meteor.static/test/update/
+    s3cmd -P put manifest.json s3://com.meteor.static/test/update/
+fi
