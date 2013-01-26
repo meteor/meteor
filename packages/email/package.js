b35d7ff03acb77fb6a98bf55fb69377f1d2eb5e3
Avital Oliver <avital@thewe.net>
2013-01-25 22:03:03 -0800

    minor text change

--- packages/email/package.js
+++ packages/email/package.js
@@ -2,6 +2,8 @@ Package.describe({
   summary: "Send email messages"
 });
 
+// Pinned at older version. 0.1.16+ uses mimelib, not mimelib-noiconv
+// which is much bigger. We need a better solution.
 Npm.depends({mailcomposer: "0.1.15", simplesmtp: "0.1.25", "stream-buffers": "0.2.3"});
 
 Package.on_use(function (api) {
