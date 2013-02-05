3bdf41add159bf4b71b79c9fa7f29642731ba09f
David Glasser <glasser@meteor.com>
2013-02-04 16:42:18 -0800

    Rewrite sort compiler to avoid eval, too!

--- packages/minimongo/selector.js
+++ packages/minimongo/selector.js
@@ -543,4 +543,62 @@ LocalCollection._selectorIsId = function (selector) {
   return (typeof selector === "string") || (typeof selector === "number");
 };
 
+// Give a sort spec, which can be in any of these forms:
+//   {"key1": 1, "key2": -1}
+//   [["key1", "asc"], ["key2", "desc"]]
+//   ["key1", ["key2", "desc"]]
+//
+// (.. with the first form being dependent on the key enumeration
+// behavior of your javascript VM, which usually does what you mean in
+// this case if the key names don't look like integers ..)
+//
+// return a function that takes two objects, and returns -1 if the
+// first object comes first in order, 1 if the second object comes
+// first, or 0 if neither object comes before the other.
+
+LocalCollection._compileSort = function (spec) {
+  var sortSpecParts = [];
+
+  if (spec instanceof Array) {
+    for (var i = 0; i < spec.length; i++) {
+      if (typeof spec[i] === "string") {
+        sortSpecParts.push({
+          lookup: makeLookupFunction(spec[i]),
+          ascending: true
+        });
+      } else {
+        sortSpecParts.push({
+          lookup: makeLookupFunction(spec[i][0]),
+          ascending: spec[i][1] !== "desc"
+        });
+      }
+    }
+  } else if (typeof spec === "object") {
+    for (var key in spec) {
+      sortSpecParts.push({
+        lookup: makeLookupFunction(key),
+        ascending: spec[key] >= 0
+      });
+    }
+  } else {
+    throw Error("Bad sort specification: ", JSON.stringify(spec));
+  }
+
+  if (sortSpecParts.length === 0)
+    return function () {return 0;};
+
+  return function (a, b) {
+    for (var i = 0; i < sortSpecParts.length; ++i) {
+      var specPart = sortSpecParts[i];
+      var aValue = specPart.lookup(a);
+      var bValue = specPart.lookup(b);
+      var compare = LocalCollection._f._cmp(aValue, bValue);
+      if (compare !== 0)
+        return specPart.ascending ? compare : -compare;
+    };
+    return 0;
+  };
+};
+
+
 })();
