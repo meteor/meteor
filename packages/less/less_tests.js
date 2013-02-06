de6a08d09ef3e4156cdf93a6cea6b2241e3647d3
David Glasser <glasser@meteor.com>
2013-02-06 14:08:39 -0800

    Choose an even more portable CSS property to test @imports.
    Colors were showing up as rgb(a, b, c) in some browsers and as #aabbcc in
    others.

--- packages/less/less_tests.js
+++ packages/less/less_tests.js
@@ -6,12 +6,10 @@ Tinytest.add("less - presence", function(test) {
   d.node().style.display = 'block';
 
   var p = d.node().firstChild;
-  var leftBorder = getStyleProperty(p, 'border-left-style');
-  test.equal(leftBorder, "dashed");
+  test.equal(getStyleProperty(p, 'border-left-style'), "dashed");
 
   // test @import
-  var leftBorderColor = getStyleProperty(p, 'border-left-color');
-  test.equal(leftBorderColor, "rgb(50, 20, 86)");
+  test.equal(getStyleProperty(p, 'border-right-style'), "dotted");
 
   d.kill();
 });
