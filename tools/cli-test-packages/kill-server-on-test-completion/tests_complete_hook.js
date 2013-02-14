00c539196175471acc2bbd53f479317aa0b0199b
Avital Oliver <avital@thewe.net>
2013-02-13 16:06:05 -0800

    Simplify code in response to Glasser's code review

--- tools/cli-test-packages/kill-server-on-test-completion/tests_complete_hook.js
+++ tools/cli-test-packages/kill-server-on-test-completion/tests_complete_hook.js
@@ -14,44 +14,9 @@ if (Meteor.isServer) {
   });
 } else { // isClient
 
-  // Copied from test-in-browser/driver.js
-  expandFailures = function() {
-    // walk whole tree to look for failed tests
-    var walk = function (groups) {
-      var ret = true;
-
-      _.each(groups || [], function (group) {
-        if (!ret)
-          return;
-
-        _.each(group.tests || [], function (t) {
-          if (!ret)
-            return;
-          if (_testStatus(t) === "failed")
-            t.expanded = true;
-        });
-
-        if (!walk(group.groups))
-          ret = false;
-      });
-
-      return ret;
-    };
-
-    walk(resultTree);
-    _resultsChanged();
-  };
-
   Meteor.onTestsComplete = function() {
-    // XXX I couldn't get this to work without setTimeout
-    // (it saw "running..." in the DOM); even with a call to
-    // Meteor.flush()
-    setTimeout(function() {
-      expandFailures();
-      setTimeout(function() {
-        var results = document.body.innerText;
-        Meteor.call('packageTestsComplete', results);
-      }, 1000);
-    }, 1000);
+    Meteor.flush();
+    var results = document.body.innerText;
+    Meteor.call('packageTestsComplete', results);
   };
 }
