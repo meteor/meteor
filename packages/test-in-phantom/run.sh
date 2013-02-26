250ab97561182de5af14e1a2baadf8adc4c4ec42
Naomi Seyfer <naomi@meteor.com>
2013-02-25 17:19:37 -0800

    Added a simple shell script to run tests

---
+++ packages/test-in-phantom/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#eventually this should be the new engine way to run tests
+cd $METEOR_HOME/packages
+meteor --tests=test-in-phantom &
+METEOR_PID=$!
+
+sleep 1
+
+phantomjs ./test-in-phantom/runner.js
+
+kill $METEOR_PID
