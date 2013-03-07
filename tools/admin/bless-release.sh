1e56a0dfe657041732f10486a62937a5fd1c6c42
David Glasser <glasser@meteor.com>
2013-03-06 23:57:23 -0800

    fix bless-release.sh for *.release.json

--- tools/admin/bless-release.sh
+++ tools/admin/bless-release.sh
@@ -8,5 +8,5 @@ echo "Releasing from $1 to $2"
 
 s3cmd -P cp "s3://com.meteor.warehouse/unpublished/$1/meteor-engine-bootstrap-*" \
             "s3://com.meteor.static/test/"
-s3cmd -P cp "s3://com.meteor.warehouse/releases/$1.json" \
-            "s3://com.meteor.warehouse/releases/$2.json"
+s3cmd -P cp "s3://com.meteor.warehouse/releases/$1.release.json" \
+            "s3://com.meteor.warehouse/releases/$2.release.json"
