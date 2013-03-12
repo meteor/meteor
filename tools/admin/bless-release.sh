62a8dd2e38263bae6516c26862f0758abeb8174d
David Glasser <glasser@meteor.com>
2013-03-12 16:49:42 -0700

    install-bootstrap.txt (goes in s3://com.meteor.static/update/)

--- tools/admin/bless-release.sh
+++ tools/admin/bless-release.sh
@@ -6,8 +6,6 @@ set -u
 
 echo "Releasing from $1 to $2"
 
-s3cmd -P cp "s3://com.meteor.warehouse/unpublished/$1/meteor-engine-bootstrap-*" \
-            "s3://com.meteor.static/test/"
 s3cmd -P cp "s3://com.meteor.warehouse/releases/$1.release.json" \
             "s3://com.meteor.warehouse/releases/$2.release.json"
 s3cmd -P put changelog.json "s3://com.meteor.warehouse/releases/$2.changelog.json"
