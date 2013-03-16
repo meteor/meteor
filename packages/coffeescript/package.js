c67ffa7150e5443209c370648ec91442c101a24a
mquandalle <mquandalle@novaways.com>
2013-03-16 03:59:40 +0100

    Add support for Source Map in the coffeescript package

--- packages/coffeescript/package.js
+++ packages/coffeescript/package.js
@@ -7,21 +7,50 @@ var fs = require('fs');
 var path = require('path');
 
 var coffeescript_handler = function(bundle, source_path, serve_path, where) {
-  serve_path = serve_path + '.js';
+  var options = {
+    bare: true,
+    sourceMap: true,
+    filename: source_path,
+    literate: path.extname(source_path) === '.litcoffee'
+  };  
 
-  var contents = fs.readFileSync(source_path);
-  var options = {bare: true, filename: source_path, literate: path.extname(source_path) === '.litcoffee'};
+  // Compile from CoffeeScript to JavaScript
+  var coffeeContent = fs.readFileSync(source_path).toString('utf8');
   try {
-    contents = coffee.compile(contents.toString('utf8'), options);
+    var contents = coffee.compile(coffeeContent, options);
+    var jsContent = new Buffer(contents.js);
   } catch (e) {
-    return bundle.error(e.message);
+    return console.log(bundle.error(e.message));
   }
 
-  contents = new Buffer(contents);
+  if (options.sourceMap) {
+    // Bundle the non-compiled CoffeeScript file
+    bundle.add_resource({
+      type: "static", // WARNING : Seems to doesn't be updated on client reloading
+      path: serve_path,
+      data: coffeeContent,
+      where: where
+    });
+
+    // Add the Source Map link
+    var mapPath = serve_path+".map";
+    jsContent += "//@ sourceMappingURL="+mapPath;
+
+    // Bundle the Source Map
+    var srcMapContent = contents.v3SourceMap.replace(".js", ".coffee.js");
+    bundle.add_resource({
+      type: "static", // WARNING : Seems to doesn't be updated on client reloading
+      path: mapPath,
+      data: srcMapContent,
+      where: where
+    });
+  }
+
+  // Bundle the compiled JavaScript file
   bundle.add_resource({
     type: "js",
     path: serve_path,
-    data: contents,
+    data: jsContent,
     where: where
   });
 }
