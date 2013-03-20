a62451aebdd8c70532bc46ab9c437800ee8576fd
mquandalle <mquandalle@novaways.com>
2013-03-20 23:40:24 +0100

    Add support for '.coffee.md' new extension see issue #830

--- packages/coffeescript/package.js
+++ packages/coffeescript/package.js
@@ -4,7 +4,7 @@ Package.describe({
 
 Npm.depends({"coffee-script": "1.6.1"});
 
-var coffeescript_handler = function(bundle, source_path, serve_path, where) {
+var coffeescript_handler = function(bundle, source_path, serve_path, where, literate) {
   var fs = Npm.require('fs');
   var path = Npm.require('path');
   var coffee = Npm.require('coffee-script');
@@ -12,7 +12,7 @@ var coffeescript_handler = function(bundle, source_path, serve_path, where) {
     bare: true,
     sourceMap: true,
     filename: source_path,
-    literate: path.extname(source_path) === '.litcoffee'
+    literate: literate
   };  
 
   // Compile from CoffeeScript to JavaScript
@@ -29,13 +29,13 @@ var coffeescript_handler = function(bundle, source_path, serve_path, where) {
     bundle.add_resource({
       type: "static", // WARNING : Seems to doesn't be updated on client reloading
       path: serve_path,
-      data: coffeeContent+"#bonjour",
+      data: coffeeContent,
       where: where
     });
 
     // Add the Source Map link
-    var mapPath = serve_path+".map";
-    jsContent += "//@ sourceMappingURL="+mapPath;
+    var mapPath = serve_path + ".map";
+    jsContent += "//@ sourceMappingURL=" + mapPath;
 
     // Bundle the Source Map
     var srcMapContent = contents.v3SourceMap.replace(".js", ".coffee.js");
@@ -50,16 +50,23 @@ var coffeescript_handler = function(bundle, source_path, serve_path, where) {
   // Bundle the compiled JavaScript file
   bundle.add_resource({
     type: "js",
-    path: serve_path+".js",
+    path: serve_path + ".js",
     data: jsContent,
     where: where
   });
 }
 
-Package.register_extension("coffee", coffeescript_handler);
-Package.register_extension("litcoffee", coffeescript_handler);
+Package.register_extension("coffee", function(bundle, source_path, serve_path, where) {
+  coffeescript_handler(bundle, source_path, serve_path, where, false);
+});
+Package.register_extension("litcoffee", function(bundle, source_path, serve_path, where) {
+  coffeescript_handler(bundle, source_path, serve_path, where, false);
+});
+Package.register_extension("coffee.md", function(bundle, source_path, serve_path, where) {
+  coffeescript_handler(bundle, source_path, serve_path, where, false);
+});
 
 Package.on_test(function (api) {
-  api.add_files(['coffeescript_tests.coffee', 'litcoffeescript_tests.litcoffee', 'coffeescript_tests.js'],
+  api.add_files(['coffeescript_tests.coffee', 'litcoffeescript_tests.litcoffee', 'litcoffeescript_tests.coffee.md', 'coffeescript_tests.js'],
                 ['client', 'server']);
 });
