6daa432e5e8ab607dfb37baf1a4098de624c2916
Ian Serlin <ian@ianserlin.com>
2013-04-05 13:32:11 -0700

    updating to code conventions

--- packages/handlebars/evaluate.js
+++ packages/handlebars/evaluate.js
@@ -34,20 +34,20 @@ Handlebars._default_helpers = {
       return options.fn(data);
   },
   'each': function (data, options) {
-    var parentData = this
-      , hasValues = false
-      , key = _.isArray(data) ? '_index' : '_key'
-      , branches = _.map(data, function(x, i) {
-          hasValues = true;
-          // enable eachWithIndex functional parity with handlebars
-          if(x){ x[key] = i; }
-          // infer a branch key from the data
-          var branch = ((x && x._id) || (typeof x === 'string' ? x : null) ||
-                        Spark.UNIQUE_LABEL);
-          return Spark.labelBranch(branch, function() {
-            return options.fn(x);
-          });
-        }).join('');
+    var parentData = this,
+      hasValues = false,
+      key = _.isArray(data) ? '_index' : '_key',
+      branches = _.map(data, function(x, i) {
+        hasValues = true;
+        // enable eachWithIndex functional parity with handlebars
+        if(x){ x[key] = i; }
+        // infer a branch key from the data
+        var branch = ((x && x._id) || (typeof x === 'string' ? x : null) ||
+                      Spark.UNIQUE_LABEL);
+        return Spark.labelBranch(branch, function() {
+          return options.fn(x);
+        });
+      }).join('');
     
     return hasValues && branches || Spark.labelBranch('else', function () {
       return options.inverse(parentData);
