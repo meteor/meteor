a15d1ecdee1fd951fdee3d628fd1322938d69b4b
Andrew Wilcox <andrew.wilcox@gmail.com>
2013-04-23 14:27:44 -0400

    Unpolyfill localstorage.
    Fixes #634.
    
    The unit test now passes in Firefox when dom.storage.enabled is set to
    false (if we care :)
    
    Tested in Chrome, Firefox, and IE 6 and 7.

---
+++ packages/localstorage/package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  summary: "Simulates local storage on IE 6,7 using userData",
+});
+
+Package.on_use(function (api) {
+  api.use('jquery', 'client'); // XXX only used for browser detection. remove.
+
+  api.add_files('localstorage.js', 'client');
+});
+
+Package.on_test(function (api) {
+  api.use('localstorage', 'client');
+  api.use('tinytest');
+
+  api.add_files('localstorage_tests.js', 'client');
+});
