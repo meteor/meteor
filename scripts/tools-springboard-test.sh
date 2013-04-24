c6c24dffb58e16657017aa936ca6f87de8de5e83
Avital Oliver <avital@thewe.net>
2013-04-24 15:34:58 -0700

    Correct comment

--- scripts/tools-springboard-test.sh
+++ scripts/tools-springboard-test.sh
@@ -9,6 +9,7 @@ fi
 
 METEOR="$METEOR_TOOLS_TREE_DIR/bin/meteor"
 
-# This release was built from the 'release-for-testing-springboarding'
-# tag in GitHub. All it does is print this string and exit.
-$METEOR --release release-used-to-test-springboarding | grep "THIS IS A FAKE RELEASE ONLY USED TO TEST ENGINE SPRINGBOARDING"
+# This release was built from the
+# 'release/release-used-to-test-springboarding' tag in GitHub. All it
+# does is print this string and exit.
+$METEOR --release release/used-to-test-springboarding | grep "THIS IS A FAKE RELEASE ONLY USED TO TEST ENGINE SPRINGBOARDING"
