9e0e7e1138776b0452f2b2ad946dcdc6a6b0045c
Nick Martin <nim@meteor.com>
2013-05-03 17:35:38 -0700

    Fix typo in springboard test script that caused test to fail

--- scripts/tools-springboard-test.sh
+++ scripts/tools-springboard-test.sh
@@ -12,4 +12,4 @@ METEOR="$METEOR_TOOLS_TREE_DIR/bin/meteor"
 # This release was built from the
 # 'release/release-used-to-test-springboarding' tag in GitHub. All it
 # does is print this string and exit.
-$METEOR --release release/used-to-test-springboarding | grep "THIS IS A FAKE RELEASE ONLY USED TO TEST ENGINE SPRINGBOARDING"
+$METEOR --release release-used-to-test-springboarding | grep "THIS IS A FAKE RELEASE ONLY USED TO TEST ENGINE SPRINGBOARDING"
