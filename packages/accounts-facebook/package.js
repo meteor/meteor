e2030ec7ca8788c4385d57491e874c612cf9d6f7
Tim Haines <tmhaines@gmail.com>
2013-05-06 16:28:58 -0700

    Extract facebook package from accounts-facebook

--- packages/accounts-facebook/package.js
+++ packages/accounts-facebook/package.js
@@ -1,16 +1,12 @@
 Package.describe({
-  summary: "Login service for Facebook accounts"
+  summary: "Accounts service for Facebook accounts"
 });
 
 Package.on_use(function(api) {
   api.use('accounts-base', ['client', 'server']);
-  api.use('oauth2', ['client', 'server']);
-  api.use('http', ['client', 'server']);
-  api.use('templating', 'client');
+  api.use('facebook', ['client', 'server']);
 
-  api.add_files(
-    ['facebook_login_button.css', 'facebook_configure.html', 'facebook_configure.js'],
-    'client');
+  api.add_files('facebook_login_button.css', 'client');
 
   api.add_files('facebook_common.js', ['client', 'server']);
   api.add_files('facebook_server.js', 'server');
