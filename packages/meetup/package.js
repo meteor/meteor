8f2eb9b48195f5bc393f46b18ba2e7fb00725381
Tim Haines <tmhaines@gmail.com>
2013-05-06 17:09:43 -0700

    Extract meetup package from accounts-meetup

---
+++ packages/meetup/package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  summary: "Login service for Meetup accounts"
+});
+
+Package.on_use(function(api) {
+  api.use('oauth2', ['client', 'server']);
+  api.use('http', ['client', 'server']);
+  api.use('templating', 'client');
+
+  api.add_files(
+    ['meetup_configure.html', 'meetup_configure.js'],
+    'client');
+
+  api.add_files('meetup_common.js', ['client', 'server']);
+  api.add_files('meetup_server.js', 'server');
+  api.add_files('meetup_client.js', 'client');
+});
