708eafe725a5776d3c454612ea4b0711ae266de7
Tim Haines <tmhaines@gmail.com>
2013-05-06 17:09:43 -0700

    Extract meetup package from accounts-meetup

--- packages/accounts-meetup/meetup_client.js
+++ packages/accounts-meetup/meetup_client.js
@@ -1,33 +1,3 @@
-Meteor.loginWithMeetup = function (options, callback) {
-  // support both (options, callback) and (callback).
-  if (!callback && typeof options === 'function') {
-    callback = options;
-    options = {};
-  }
-
-  var config = ServiceConfiguration.configurations.findOne({service: 'meetup'});
-  if (!config) {
-    callback && callback(new ServiceConfiguration.ConfigError("Service not configured"));
-    return;
-  }
-  var state = Random.id();
-
-  var scope = (options && options.requestPermissions) || [];
-  var flatScope = _.map(scope, encodeURIComponent).join('+');
-
-  var loginUrl =
-        'https://secure.meetup.com/oauth2/authorize' +
-        '?client_id=' + config.clientId +
-        '&response_type=code' +
-        '&scope=' + flatScope +
-        '&redirect_uri=' + Meteor.absoluteUrl('_oauth/meetup?close') +
-        '&state=' + state;
-
-  // meetup box gets taller when permissions requested.
-  var height = 620;
-  if (_.without(scope, 'basic').length)
-    height += 130;
-
-  Accounts.oauth.initiateLogin(state, loginUrl, callback,
-                               {width: 900, height: height});
+Meteor.loginWithMeetup = function(options, callback) {
+  Meetup.requestCredential(options, callback, Accounts.oauth.tryLoginAfterPopupClosed);
 };
--- packages/accounts-meetup/package.js
+++ packages/accounts-meetup/package.js
@@ -1,16 +1,12 @@
 Package.describe({
-  summary: "Login service for Meetup accounts"
+  summary: "Accounts service for Meetup accounts"
 });
 
 Package.on_use(function(api) {
   api.use('accounts-base', ['client', 'server']);
-  api.use('oauth2', ['client', 'server']);
-  api.use('http', ['client', 'server']);
-  api.use('templating', 'client');
+  api.use('meetup', ['client', 'server']);
 
-  api.add_files(
-    ['meetup_login_button.css', 'meetup_configure.html', 'meetup_configure.js'],
-    'client');
+  api.add_files('meetup_login_button.css', 'client');
 
   api.add_files('meetup_common.js', ['client', 'server']);
   api.add_files('meetup_server.js', 'server');
