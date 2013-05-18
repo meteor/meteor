372a16e6ce83176451146f980e737f15a60c531f
estark37 <emily@meteor.com>
2013-05-17 18:21:32 -0700

    Factor out most of ctl into a ctl-helper package
    We use ctl-helper on galaxy in the proxy control program.

--- packages/ctl/ctl.js
+++ packages/ctl/ctl.js
@@ -1,132 +1,17 @@
-var optimist = Npm.require('optimist');
-var Future = Npm.require('fibers/future');
-
-var Commands = [];
-
-var findCommand = function (name) {
-  var cmd = _.where(Commands, { name: name })[0];
-  if (! cmd) {
-    console.log("'" + name + "' is not a ctl command. See 'ctl --help'.");
-    process.exit(1);
-  }
-
-  return cmd;
-};
-
-var findGalaxy = _.once(function () {
-  if (!('GALAXY' in process.env)) {
-    console.log(
-      "GALAXY environment variable must be set. See 'galaxy --help'.");
-    process.exit(1);
-  }
-
-  return Meteor.connect(process.env['GALAXY']);
-});
-
-var jobsCollection = _.once(function () {
-  return new Meteor.Collection("jobs", {manager: findGalaxy()});
-});
-
-// use _.memoize so that this is called only once per app.
-var subscribeToAppJobs = _.memoize(function (appName) {
-  var f = new Future();
-  findGalaxy().subscribe("jobsByApp", appName, {
-    onReady: function () {f.return();},
-    onError: function (e) {f.throw(e);}
-  });
-  f.wait();
-});
-
-// XXX this never unsubs...
-var getJobsByApp = function (appName, restOfSelector) {
-  var galaxy = findGalaxy();
-  subscribeToAppJobs(appName);
-  var selector = {app: appName};
-  if (restOfSelector)
-    _.extend(selector, restOfSelector);
-  return jobsCollection().find(selector);
-};
-
-var myAppName = _.once(function () {
-  if (!('GALAXY_APP' in process.env)) {
-    console.log("GALAXY_APP environment variable must be set.");
-    process.exit(1);
-  }
-  return process.env.GALAXY_APP;
-});
-
-var myJobId = _.once(function () {
-  if (!('GALAXY_JOB' in process.env)) {
-    console.log("GALAXY_JOB environment variable must be set.");
-    process.exit(1);
-  }
-  return process.env.GALAXY_JOB;
-});
-
-var usage = function() {
-  process.stdout.write(
-"Usage: ctl [--help] <command> [<args>]\n" +
-"\n" +
-"For now, the GALAXY environment variable must be set to the location of\n" +
-"your Galaxy management server (Ultraworld.) This string is in the same\n" +
-"format as the argument to Meteor.connect().\n" +
-"\n" +
-"Commands:\n");
-  _.each(Commands, function (cmd) {
-    if (cmd.help && ! cmd.hidden) {
-      var name = cmd.name + "                ".substr(cmd.name.length);
-      process.stdout.write("   " + name + cmd.help + "\n");
-    }
-  });
-  process.stdout.write("\n");
-  process.stdout.write(
-    "See 'ctl help <command>' for details on a command.\n");
-  process.exit(1);
-};
-
-
-// XXX copied to meteor/tools/deploy-galaxy.js
-var exitWithError = function (error, messages) {
-  messages = messages || {};
-
-  if (! (error instanceof Meteor.Error))
-    throw error; // get a stack
-
-  var msg = messages[error.error];
-  if (msg)
-    process.stderr.write(msg + "\n");
-  else if (error instanceof Meteor.Error)
-    process.stderr.write("Denied: " + error.message + "\n");
-
-  process.exit(1);
-};
-
-
-// XXX copied to meteor/tools/deploy-galaxy.js
-var prettyCall = function (galaxy, name, args, messages) {
-  try {
-    var ret = galaxy.apply(name, args);
-  } catch (e) {
-    exitWithError(e, messages);
-  }
-  return ret;
-};
-
-
-Commands.push({
+Ctl.Commands.push({
   name: "help",
   func: function (argv) {
     if (!argv._.length || argv.help)
-      usage();
+      Ctl.usage();
     var cmd = argv._.splice(0,1)[0];
     argv.help = true;
 
-    findCommand(cmd).func(argv);
+    Ctl.findCommand(cmd).func(argv);
   }
 });
 
 
-Commands.push({
+Ctl.Commands.push({
   name: "start",
   help: "Start this app",
   func: function (argv) {
@@ -140,11 +25,11 @@ Commands.push({
       process.exit(1);
     }
 
-    var numServers = getJobsByApp(
-      myAppName(), {program: 'server', done: false}).count();
+    var numServers = Ctl.getJobsByApp(
+      Ctl.myAppName(), {program: 'server', done: false}).count();
     if (numServers === 0) {
-      var appConfig = prettyCall(
-        findGalaxy(), 'getAppConfiguration', [myAppName()]);
+      var appConfig = Ctl.prettyCall(
+        Ctl.findGalaxy(), 'getAppConfiguration', [Ctl.myAppName()]);
 
       var deployConfig = {
         boot: {
@@ -167,7 +52,7 @@ Commands.push({
       };
 
       // XXX args? env?
-      prettyCall(findGalaxy(), 'run', [myAppName(), 'server', {
+      Ctl.prettyCall(Ctl.findGalaxy(), 'run', [Ctl.myAppName(), 'server', {
         exitPolicy: 'restart',
         env: {
           METEOR_DEPLOY_CONFIG: JSON.stringify(deployConfig)
@@ -186,13 +71,7 @@ Commands.push({
   }
 });
 
-
-var kill = function (programName, jobId) {
-  console.log("Killing %s (%s)", programName, jobId);
-  prettyCall(findGalaxy(), 'kill', [jobId]);
-};
-
-Commands.push({
+Ctl.Commands.push({
   name: "stop",
   help: "Stop this app",
   func: function (argv) {
@@ -208,23 +87,23 @@ Commands.push({
 
     // Get all jobs (other than this job: don't commit suicide!) that are not
     // already killed.
-    var jobs = getJobsByApp(
-      myAppName(), {_id: {$ne: myJobId()}, done: false});
+    var jobs = Ctl.getJobsByApp(
+      Ctl.myAppName(), {_id: {$ne: Ctl.myJobId()}, done: false});
     jobs.forEach(function (job) {
       // Don't commit suicide.
-      if (job._id === myJobId())
+      if (job._id === Ctl.myJobId())
         return;
       // It's dead, Jim.
       if (job.done)
         return;
-      kill(job.program, job._id);
+      Ctl.kill(job.program, job._id);
     });
     console.log("Server stopped.");
   }
 });
 
 
-Commands.push({
+Ctl.Commands.push({
   name: "scale",
   help: "Scale jobs",
   func: function (argv) {
@@ -248,14 +127,14 @@ Commands.push({
     });
 
     _.each(scales, function (s) {
-      var jobs = getJobsByApp(
-        myAppName(), {program: s.program, done: false});
+      var jobs = Ctl.getJobsByApp(
+        Ctl.myAppName(), {program: s.program, done: false});
       jobs.forEach(function (job) {
         --s.scale;
         // Is this an extraneous job, more than the number that we need? Kill
         // it!
         if (s.scale < 0) {
-          kill(s.program, job._id);
+          Ctl.kill(s.program, job._id);
         }
       });
       // Now start any jobs that are necessary.
@@ -264,7 +143,7 @@ Commands.push({
       console.log("Starting %d jobs for %s", s.scale, s.program);
       _.times(s.scale, function () {
         // XXX args? env?
-        prettyCall(findGalaxy(), 'run', [myAppName(), s.program, {
+        Ctl.prettyCall(Ctl.findGalaxy(), 'run', [Ctl.myAppName(), s.program, {
           exitPolicy: 'restart'
         }]);
       });
@@ -272,24 +151,7 @@ Commands.push({
   }
 });
 
-
-
 // @export main
 main = function (argv) {
-  var opt = optimist(argv)
-    .alias('h', 'help')
-    .boolean('help');
-  argv = opt.argv;
-
-  if (argv.help) {
-    argv._.splice(0, 0, "help");
-    delete argv.help;
-  }
-
-  var cmdName = 'help';
-  if (argv._.length)
-    cmdName = argv._.splice(0,1)[0];
-
-  findCommand(cmdName).func(argv);
-  return 0;
+  return Ctl.main(argv);
 };
--- packages/ctl/package.js
+++ packages/ctl/package.js
@@ -2,10 +2,8 @@ Package.describe({
   summary: "Default control program for an application"
 });
 
-Npm.depends({optimist: '0.4.0'});
-
 Package.on_use(function (api) {
-  api.use(['underscore', 'livedata', 'mongo-livedata'], 'server');
+  api.use(['underscore', 'livedata', 'mongo-livedata', 'ctl-helper'], 'server');
 
   api.add_files('ctl.js', 'server');
 });
