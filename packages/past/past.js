7aebc95f50868b570a4dc9d891c6d4c2106e1197
Nick Martin <nim@meteor.com>
2013-05-22 22:42:00 -0700

    Add missing word.

--- packages/past/past.js
+++ packages/past/past.js
@@ -1,7 +1,8 @@
-// This file is used to set up aliases and methods to preserve backwards on some 
-// deprecated methods.  Care should be taken when adding aliases and methods
-// that the target will not be undefined, as the past package is loaded early.
-// In some cases, it may be best to define the alias in the package it refers to.
+// This file is used to set up aliases and methods to preserve backwards
+// compatibility on some deprecated methods.  Care should be taken when
+// adding aliases and methods that the target will not be undefined, as
+// the past package is loaded early.  In some cases, it may be best to
+// define the alias in the package it refers to.
 
 // Old under_score version of camelCase public API names.
 Meteor.is_client = Meteor.isClient;
