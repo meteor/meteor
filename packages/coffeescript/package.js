a93ee948fa0c8d62fb583ce54bc570ec88f9e0f1
mquandalle <mquandalle@novaways.com>
2013-06-01 20:21:53 +0200

    Add support for the  extension

--- packages/coffeescript/package.js
+++ packages/coffeescript/package.js
@@ -4,14 +4,18 @@ Package.describe({
 
 Npm.depends({"coffee-script": "1.6.2"});
 
-var coffeescript_handler = function(bundle, source_path, serve_path, where) {
+var coffeescript_handler = function(bundle, source_path, serve_path, where, literate) {
   var fs = Npm.require('fs');
   var path = Npm.require('path');
   var coffee = Npm.require('coffee-script');
   serve_path = serve_path + '.js';
 
   var contents = fs.readFileSync(source_path);
-  var options = {bare: true, filename: source_path, literate: path.extname(source_path) === '.litcoffee'};
+  var options = {
+    bare: true, 
+    filename: source_path, 
+    literate: literate
+  };
   try {
     contents = coffee.compile(contents.toString('utf8'), options);
   } catch (e) {
@@ -31,8 +35,17 @@ var coffeescript_handler = function(bundle, source_path, serve_path, where) {
   });
 }
 
-Package.register_extension("coffee", coffeescript_handler);
-Package.register_extension("litcoffee", coffeescript_handler);
+var coffeescript_handler_literate = function(bundle, source_path, serve_path, where) {
+  coffeescript_handler(bundle, source_path, serve_path, where, true);
+}
+
+var coffeescript_handler_no_literate = function(bundle, source_path, serve_path, where) {
+  coffeescript_handler(bundle, source_path, serve_path, where, false);
+}
+
+Package.register_extension("coffee", coffeescript_handler_no_literate);
+Package.register_extension("litcoffee", coffeescript_handler_literate);
+Package.register_extension("coffee.md", coffeescript_handler_literate);
 
 Package.on_test(function (api) {
   api.add_files([
