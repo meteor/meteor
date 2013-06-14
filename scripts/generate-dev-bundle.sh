2a0f7afa7d92055b827a875ca8ca7b32a31bab56
Geoff Schmidt <geoff@geoffschmidt.com>
2013-06-13 20:48:27 -0700

    Add 'source-map' to dev_bundle and bump dev_bundle version. (Consider this a preliminary version number, subject to change, until this branch is actually merged.)

--- scripts/generate-dev-bundle.sh
+++ scripts/generate-dev-bundle.sh
@@ -109,6 +109,7 @@ npm install tar@0.1.14
 npm install kexec@0.1.1
 npm install shell-quote@0.0.1
 npm install byline@2.0.3
+npm install source-map@0.1.22
 
 # uglify-js has a bug which drops 'undefined' in arrays:
 # https://github.com/mishoo/UglifyJS2/pull/97
