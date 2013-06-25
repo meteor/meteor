65c288bdb830537d99940e9c941866c6bb7df8a4
Tim Haines <tmhaines@gmail.com>
2013-06-24 23:05:24 -0700

    Handle Meteor.loginWithX called with callback but not options

--- packages/accounts-google/google_client.js
+++ packages/accounts-google/google_client.js
@@ -1,4 +1,10 @@
 Meteor.loginWithGoogle = function(options, callback) {
+  // support a callback without options
+  if (! callback && typeof options === "function") {
+    callback = options;
+    options = null;
+  }
+
   var credentialRequestCompleteCallback = Accounts.oauth.credentialRequestCompleteHandler(callback);
   Google.requestCredential(options, credentialRequestCompleteCallback);
 };
--- packages/accounts-twitter/twitter_client.js
+++ packages/accounts-twitter/twitter_client.js
@@ -1,4 +1,10 @@
 Meteor.loginWithTwitter = function(options, callback) {
+  // support a callback without options
+  if (! callback && typeof options === "function") {
+    callback = options;
+    options = null;
+  }
+
   var credentialRequestCompleteCallback = Accounts.oauth.credentialRequestCompleteHandler(callback);
   Twitter.requestCredential(options, credentialRequestCompleteCallback);
 };
