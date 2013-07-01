317fc8abeed619a2058282015fd5b0dc46053f8e
David Greenspan <dgreenspan@alum.mit.edu>
2013-07-01 15:31:44 -0700

    example stuff from demo

--- examples/unfinished/shark/client/shark.js
+++ examples/unfinished/shark/client/shark.js
@@ -1,20 +1,24 @@
-
-
-
 Items = new Meteor.Collection(null);
 Items.insert({ text: 'Foo' });
 Items.insert({ text: 'Bar' });
 Items.insert({ text: 'Baz' });
 
-Body({
-  items: function () {
-    return Items.find({}, { sort: { text: 1 }});
-  },
-  name: 'David'
-});
-
 Meteor.startup(function () {
   Items.insert({ text: 'Qux' });
   Items.remove({ text: 'Foo' });
   Items.update({ text: 'Bar' }, { text: 'Car' });
+});
+
+Body({
+  items: function () {
+    return Items.find({}, { sort: { text: 1 }});
+  },
+  name: 'David',
+  containerClass: function () { return Session.get('containerClass'); }
+});
+
+Template.item({
+  foo: function () { return Session.get('foo'); },
+  rand: function () { return Math.random(); },
+  built: function () { console.log('built'); }
 });
