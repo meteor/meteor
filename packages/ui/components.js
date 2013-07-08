f894d150531ba6347e9f6649741e788380d3eebf
David Greenspan <dgreenspan@alum.mit.edu>
2013-07-08 12:04:00 -0700

    beginning of reactive attrs in new API

--- packages/ui/components.js
+++ packages/ui/components.js
@@ -1,32 +1,7 @@
 var Component = UIComponent;
 
-var ESCAPED_CHARS_UNQUOTED_REGEX = /[&<>]/g;
-var ESCAPED_CHARS_QUOTED_REGEX = /[&<>"]/g;
-
-var escapeMap = {
-  "<": "&lt;",
-  ">": "&gt;",
-  "&": "&amp;",
-  '"': "&quot;"
-};
-var escapeOne = function(c) {
-  return escapeMap[c];
-};
-
-var encodeEntities = function (text, isQuoted) {
-  // Encode Unicode characters to HTML entities.
-  //
-  // This implementation just encodes the characters that otherwise
-  // wouldn't parse (like `<`) and passes the rest through.  You'd
-  // need to do something different if you care about HTML entities as
-  // a way to embed special characters in ASCII.
-  return text.replace(isQuoted ? ESCAPED_CHARS_QUOTED_REGEX :
-                      ESCAPED_CHARS_UNQUOTED_REGEX, escapeOne);
-};
-
-
 _UI.Text = Component.extend({
-  _encodeEntities: encodeEntities,
+  _encodeEntities: _UI.encodeSpecialEntities,
   _stringify: function (x) {
     return String(x == null ? '' : x);
   },
--- packages/ui/render.js
+++ packages/ui/render.js
@@ -1,5 +1,30 @@
 var Component = UIComponent;
 
+var ESCAPED_CHARS_UNQUOTED_REGEX = /[&<>]/g;
+var ESCAPED_CHARS_QUOTED_REGEX = /[&<>"]/g;
+
+var escapeMap = {
+  "<": "&lt;",
+  ">": "&gt;",
+  "&": "&amp;",
+  '"': "&quot;"
+};
+var escapeOne = function(c) {
+  return escapeMap[c];
+};
+
+_UI.encodeSpecialEntities = function (text, isQuoted) {
+  // Encode Unicode characters to HTML entities.
+  //
+  // This implementation just encodes the characters that otherwise
+  // wouldn't parse (like `<`) and passes the rest through.  You'd
+  // need to do something different if you care about HTML entities as
+  // a way to embed special characters in ASCII.
+  return text.replace(isQuoted ? ESCAPED_CHARS_QUOTED_REGEX :
+                      ESCAPED_CHARS_UNQUOTED_REGEX, escapeOne);
+};
+
+var ATTRIBUTE_NAME_REGEX = /^[^\s"'>/=/]+$/;
 
 makeRenderBuffer = function (component, options) {
   var isPreview = !! options && options.preview;
@@ -11,8 +36,10 @@ makeRenderBuffer = function (component, options) {
 
   var handle = function (arg) {
     if (typeof arg === 'string') {
+      // "HTML"
       strs.push(arg);
     } else if (arg instanceof Component) {
+      // Component
       var commentString = randomString + '_' + (commentUid++);
       strs.push('<!--', commentString, '-->');
       component.add(arg);
@@ -24,7 +51,7 @@ makeRenderBuffer = function (component, options) {
       } else if (typeof arg.type === 'function') {
         var curType;
         component.autorun(function (c) {
-          // capture reactivity:
+          // capture dependencies on this line:
           var type = arg.type();
           if (c.firstRun) {
             curType = type;
@@ -34,6 +61,7 @@ makeRenderBuffer = function (component, options) {
           } else if (type !== curType) {
             var oldChild = curChild;
             curType = type;
+            // don't capture any dependencies here
             Deps.nonreactive(function () {
               curChild = curType.create(arg.args);
               component.replaceChild(oldChild, curChild);
@@ -45,8 +73,21 @@ makeRenderBuffer = function (component, options) {
       } else {
         throw new Error("Expected 'type' to be Component or function");
       }
+    } else if (arg.attrs) {
+      // attrs object inserts zero or more `name="value"` items
+      // into the HTML, and can reactively update them later.
+      // You can have multiple attrs objects in a tag, but they
+      // can't specify any of the same attributes (i.e. the right
+      // thing won't happen).
+      for (var attrName in arg.attrs) {
+        if (! ATTRIBUTE_NAME_REGEX.test(attrName))
+          throw new Error("Illegal HTML attribute name: " + attrName);
+        // XXX push initial HTML into strs
+        // XXX set up an autorun
+        // XXX make attr update hookable
+      }
     } else {
-      throw new Error("Expected string or Component");
+      throw new Error("Expected HTML string, Component, component spec or attrs spec");
     }
   };
 
