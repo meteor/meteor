7617d8931aec153fd4148c16af5f98262ededaa4
Geoff Schmidt <geoff@geoffschmidt.com>
2013-07-12 17:36:40 -0700

    Update docs, query, and showdown for namespacing

--- packages/jquery/package.js
+++ packages/jquery/package.js
@@ -2,6 +2,9 @@ Package.describe({
   summary: "Manipulate the DOM using CSS selectors"
 });
 
-Package.on_use(function (api) {
+Package.on_use(function (api, where) {
   api.add_files('jquery.js', 'client');
+
+  api.exportSymbol('$', where);
+  api.exportSymbol('jQuery', where);
 });
