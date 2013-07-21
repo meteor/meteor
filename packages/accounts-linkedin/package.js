0c92e3d08388776514d75ef596c0d92e873bfce7
Tim Brandin <tim.brandin@gmail.com>
2013-07-21 16:07:12 +0200

    Implement api.imply. Make all accounts packages imply accounts-base.

--- packages/accounts-linkedin/package.js
+++ packages/accounts-linkedin/package.js
@@ -4,6 +4,8 @@ Package.describe({
 
 Package.on_use(function(api) {
   api.use('accounts-base', ['client', 'server']);
+  // Export Accounts (etc) to packages using this one.
+  api.imply('accounts-base', ['client', 'server']);
   api.use('linkedin', ['client', 'server']);
 
   api.add_files(['linkedin_login_button.css'], 'client');
