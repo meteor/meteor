29f2adee7064669522867a807af7ab6007968bfe
David Greenspan <dgreenspan@alum.mit.edu>
2013-07-23 16:29:48 -0700

    port "skel"

--- tools/skel/~name~.js
+++ tools/skel/~name~.js
@@ -1,13 +1,38 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to ~name~.";
-  };
+  // Plan is for this to change to simply
+  // `Template.hello({ ...`
+  Template.hello.include({
+    greeting: function () {
+      return "Welcome to ~name~.";
+    },
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+    // we'll probably change `built` to `rendered`
+    // (the intention was to emphasize that `built`
+    // is when you have DOM nodes and it never
+    // gets called on the server, but I don't like
+    // it enough)
+    built: function () {
+      var self = this;
+      // There will be event maps that do delegation
+      // on the DOM range of the component for you.
+      //
+      // If there's some unconditional node to hang
+      // event handlers on, it's perfectly fine to use
+      // jQuery event handlers manually.
+      self.$("input").on('click', function () {
+        // No way to get the current event data at the
+        // moment except:
+        // `var data = self.findByElement(evt.currentTarget).data();`
+        //
+        // You have full access to `self`, so it's only sucky
+        // if you want data that's down inside the body of
+        // an `#each` or `#if` or another component.
+        //
+        // Event maps will dig out the current data for you.
+
+        if (typeof console !== 'undefined')
+          console.log("You pressed the button");
+      });
     }
   });
 }
