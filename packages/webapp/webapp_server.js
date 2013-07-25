ae965f1aa15cc6befb2ba07c116f9844d679d0ce
Nick Martin <nim@meteor.com>
2013-07-25 14:54:15 -0700

    Make an environment variable to control where we bind.
    This only affects the bundled app (eg, what you'd run in production). The dev mode runner has a bunch of '127.0.0.1' addresses hardcoded, and will need more work.

--- packages/webapp/webapp_server.js
+++ packages/webapp/webapp_server.js
@@ -197,6 +197,9 @@ var runWebAppServer = function () {
   if (process.env.PORT && !_.has(deployConfig.boot.bind, 'localPort')) {
     deployConfig.boot.bind.localPort = parseInt(process.env.PORT);
   }
+  if (process.env.BIND_IP && !_.has(deployConfig.boot.bind, 'localIp')) {
+    deployConfig.boot.bind.localIp = process.env.BIND_IP;
+  }
   copyEnvVarToDeployConfig(deployConfig, "MONGO_URL", "mongo-livedata", "url");
 
   // webserver
@@ -437,7 +440,9 @@ var runWebAppServer = function () {
 
     // only start listening after all the startup code has run.
     var bind = deployConfig.boot.bind;
-    httpServer.listen(bind.localPort || 0, Meteor.bindEnvironment(function() {
+    var localPort = bind.localPort || 0;
+    var localIp = bind.localIp || '0.0.0.0';
+    httpServer.listen(localPort, localIp, Meteor.bindEnvironment(function() {
       if (argv.keepalive || true)
         console.log("LISTENING"); // must match run.js
       var port = httpServer.address().port;
