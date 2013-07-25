8094ac7d0d7e6d5a252c422c73e5b96e2e3a916e
David Glasser <glasser@meteor.com>
2013-07-24 22:46:40 -0700

    oops, remove debugging code

--- htmljs_test.js
+++ htmljs_test.js
@@ -1,7 +1,5 @@
 
 Tinytest.add("htmljs", function (test) {
-  console.log("HI")
-  console.log(DIV)
   // Make sure "style" works, which has to be special-cased for IE.
   test.equal(DIV({style:"display:none"}).style.display, "none");
 });
