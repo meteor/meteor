96d19efa67d72468a0fa5e6b038db9c661f7aef3
David Glasser <glasser@meteor.com>
2013-07-24 22:05:10 -0700

    Merge exports list with package variables in slice JSON.
    This implies that all exports are package variables, which made underscore,
    jquery, and htmljs (which explicitly assigned to fields on the global variable)
    break. We now properly encapsulate these packages (except for window.jQuery,
    which we let sneak out because bootstrap wants it).  This means that packages
    that want _ need to use underscore, and packages that want $ need to use jquery.
    Also, you can't use _ in minimongo $where any more (matching mongod).

--- htmljs_test.js
+++ htmljs_test.js
@@ -1,6 +1,7 @@
 
 Tinytest.add("htmljs", function (test) {
-
+  console.log("HI")
+  console.log(DIV)
   // Make sure "style" works, which has to be special-cased for IE.
   test.equal(DIV({style:"display:none"}).style.display, "none");
 });
