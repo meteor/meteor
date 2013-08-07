5bb8c6d908bfef9952e2474d5ecb09611fb681c4
Naomi Seyfer <naomi@meteor.com>
2013-08-07 14:09:36 -0700

    New way of interacting with galaxy now actually binds to proxy

--- packages/galaxy/package.js
+++ packages/galaxy/package.js
@@ -3,7 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.use(['underscore', 'livedata']);
+  api.use(['underscore', 'livedata', 'ejson']);
   api.use(['mongo-livedata'], {
     unordered: true
   });
