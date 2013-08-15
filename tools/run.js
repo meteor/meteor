00cfdaaafb4771d456f17249ba2bfa2cd894e0ef
David Glasser <glasser@meteor.com>
2013-08-14 19:08:47 -0700

    In "meteor run", run mongo in repl-set mode, so that oplog is available.
    Note: this is pretty slow. Before merging this branch, should cache the fact
    that replset has been initiated on port N so that on the next run, if the port
    hasn't changed, we don't need to re-initiate.

--- tools/run.js
+++ tools/run.js
@@ -20,6 +20,7 @@ var unipackage = require('./unipackage.js');
 var _ = require('underscore');
 var inFiber = require('./fiber-helpers.js').inFiber;
 var Future = require('fibers/future');
+var Fiber = require('fibers');
 
 ////////// Globals //////////
 //XXX: Refactor to not have globals anymore?
@@ -598,55 +599,54 @@ exports.run = function (context, options) {
   var mongoErrorTimer;
   var mongoStartupPrintTimer;
   var launch = function () {
-    Status.mongoHandle = mongo_runner.launch_mongo(
-      context.appDir,
-      mongoPort,
-      function () { // On Mongo startup complete
-        // don't print mongo startup is slow warning.
-        if (mongoStartupPrintTimer) {
-          clearTimeout(mongoStartupPrintTimer);
-          mongoStartupPrintTimer = null;
+    Fiber(function () {
+      Status.mongoHandle = mongo_runner.launchMongo({
+        context: context,
+        port: mongoPort,
+        onListen: function () { // On Mongo startup complete
+          // don't print mongo startup is slow warning.
+          if (mongoStartupPrintTimer) {
+            clearTimeout(mongoStartupPrintTimer);
+            mongoStartupPrintTimer = null;
+          }
+          restartServer();
+        },
+        onExit: function (code, signal, stderr) { // On Mongo dead
+          if (Status.shuttingDown) {
+            return;
+          }
+
+          // Print only last 20 lines of stderr.
+          stderr = stderr.split('\n').slice(-20).join('\n');
+
+          console.log(
+            stderr + "Unexpected mongo exit code " + code + ". Restarting.\n");
+
+          // if mongo dies 3 times with less than 5 seconds between each,
+          // declare it failed and die.
+          mongoErrorCount += 1;
+          if (mongoErrorCount >= 3) {
+            var explanation = mongoExitCodes.Codes[code];
+            console.log("Can't start mongod\n");
+            if (explanation)
+              console.log(explanation.longText);
+            if (explanation === mongoExitCodes.EXIT_NET_ERROR) {
+              console.log(
+                "\nCheck for other processes listening on port " + mongoPort +
+                  "\nor other meteors running in the same project.");
+            }
+            if (!explanation && /GLIBC/i.test(stderr)) {
+              console.log(
+                "\nLooks like you are trying to run Meteor on an old Linux " +
+                  "distribution. Meteor on Linux requires glibc version 2.9 " +
+                  "or above. Try upgrading your distribution to the latest " +
+                  "version.");
+            }
+            process.exit(1);
+          }
         }
-        restartServer();
-      },
-      function (code, signal, stderr) { // On Mongo dead
-        if (Status.shuttingDown) {
-          return;
-        }
-
-        // Print only last 20 lines of stderr.
-        stderr = stderr.split('\n').slice(-20).join('\n');
-
-        console.log(stderr + "Unexpected mongo exit code " + code + ". Restarting.\n");
-
-        // if mongo dies 3 times with less than 5 seconds between each,
-        // declare it failed and die.
-        mongoErrorCount += 1;
-        if (mongoErrorCount >= 3) {
-          var explanation = mongoExitCodes.Codes[code];
-          console.log("Can't start mongod\n");
-          if (explanation)
-            console.log(explanation.longText);
-          if (explanation === mongoExitCodes.EXIT_NET_ERROR)
-            console.log("\nCheck for other processes listening on port " + mongoPort +
-                        "\nor other meteors running in the same project.");
-          if (!explanation && /GLIBC/i.test(stderr))
-            console.log("\nLooks like you are trying to run Meteor on an old Linux " +
-                        "distribution. Meteor on Linux only supports Linux with glibc " +
-                        "version 2.9 and above. Try upgrading your distribution " +
-                        "to the latest version.");
-          process.exit(1);
-        }
-        if (mongoErrorTimer)
-          clearTimeout(mongoErrorTimer);
-        mongoErrorTimer = setTimeout(function () {
-          mongoErrorCount = 0;
-          mongoErrorTimer = null;
-        }, 5000);
-
-        // Wait a sec to restart.
-        setTimeout(launch, 1000);
       });
+    }).run();
   };
 
   startProxy(outerPort, innerPort, function () {
