6dc55c03a17a498dfc124b29cf2d918a90bf536c
David Glasser <glasser@meteor.com>
2013-08-18 11:51:45 -0700

    oplog checkpoint from friday

--- tools/run.js
+++ tools/run.js
@@ -243,6 +243,7 @@ var startServer = function (options) {
 
   env.PORT = options.innerPort;
   env.MONGO_URL = options.mongoUrl;
+  env.XXX_OPLOG_URL = options.oplogUrl;
   env.ROOT_URL = options.rootUrl;
   if (options.settings)
     env.METEOR_SETTINGS = options.settings;
@@ -413,6 +414,8 @@ exports.run = function (context, options) {
   // Allow override and use of external mongo. Matches code in launch_mongo.
   var mongoUrl = process.env.MONGO_URL ||
         ("mongodb://127.0.0.1:" + mongoPort + "/meteor");
+  var oplogUrl = process.env.MONGO_URL ? undefined
+        : "mongodb://127.0.01:" + mongoPort + "/local";
   var firstRun = true;
 
   var serverHandle;
@@ -565,6 +568,7 @@ exports.run = function (context, options) {
       outerPort: outerPort,
       innerPort: innerPort,
       mongoUrl: mongoUrl,
+      oplogUrl: oplogUrl,
       rootUrl: rootUrl,
       library: context.library,
       rawLogs: options.rawLogs,
