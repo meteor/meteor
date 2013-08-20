30b08fc01286fd92f5570e5eb54849d46c8f1726
Nick Martin <nim@meteor.com>
2013-08-19 19:51:01 -0700

    Export Spiderable symbol, so people can override user agents. Also add twitterbot to agents. Fixes #1300.

--- packages/spiderable/package.js
+++ packages/spiderable/package.js
@@ -7,6 +7,8 @@ Package.on_use(function (api) {
   api.use(['templating'], 'client');
   api.use(['underscore'], ['client', 'server']);
 
+  api.export('Spiderable', 'server');
+
   api.add_files('spiderable.html', 'client');
   api.add_files('spiderable.js', 'server');
 });
