8a8640010d117db6c9109ab6cd0883fc711bb82b
Avital Oliver <avital@thewe.net>
2013-09-12 11:53:52 -0700

    small update

--- examples/unfinished/shark/client/shark.js
+++ examples/unfinished/shark/client/shark.js
@@ -1,6 +1,6 @@
 UI.body.items = Items.find({}, { sort: { rank: 1 } });
 
-ReallyBasicRanks = {
+SimpleRationalRanks = {
   beforeFirst: function (firstRank) { return firstRank - 1; },
   between: function (beforeRank, afterRank) { return (beforeRank + afterRank) / 2; },
   afterLast: function (lastRank) { return lastRank + 1; }
@@ -8,16 +8,16 @@ ReallyBasicRanks = {
 
 UI.body.rendered = function () {
   this.$('#list').sortable({ // uses the 'sortable' interaction from jquery ui
-    stop: function (event, ui) {
+    stop: function (event, ui) { // fired when an item is dropped
       var el = ui.item.get(0), before = ui.item.prev().get(0), after = ui.item.next().get(0);
 
       var newRank;
       if (!before) { // moving to the top of the list
-        newRank = ReallyBasicRanks.beforeFirst(after.$ui.data().rank);
+        newRank = SimpleRationalRanks.beforeFirst(after.$ui.data().rank);
       } else if (!after) { // moving to the bottom of the list
-        newRank = ReallyBasicRanks.afterLast(before.$ui.data().rank);
+        newRank = SimpleRationalRanks.afterLast(before.$ui.data().rank);
       } else {
-        newRank = ReallyBasicRanks.between(before.$ui.data().rank, after.$ui.data().rank);
+        newRank = SimpleRationalRanks.between(before.$ui.data().rank, after.$ui.data().rank);
       }
       Items.update(el.$ui.data()._id, {$set: {rank: newRank}});
     }
