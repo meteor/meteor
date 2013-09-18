ba4737c8768c2136bb1209dd38e183cd4f2e9a5c
David Glasser <glasser@meteor.com>
2013-09-17 18:34:57 -0700

    doc fetcher!

--- packages/mongo-livedata/id_map.js
+++ packages/mongo-livedata/id_map.js
@@ -1,36 +1,36 @@
 IdMap = function () {
   var self = this;
-  self.map = {};
+  self._map = {};
 };
 
 _.extend(IdMap.prototype, {
   get: function (id) {
     var self = this;
     var key = LocalCollection._idStringify(id);
-    return self.map[key];
+    return self._map[key];
   },
   set: function (id, value) {
     var self = this;
     var key = LocalCollection._idStringify(id);
-    self.map[key] = value;
+    self._map[key] = value;
   },
   remove: function (id) {
     var self = this;
     var key = LocalCollection._idStringify(id);
-    delete self.map[key];
+    delete self._map[key];
   },
   has: function (id) {
     var self = this;
     var key = LocalCollection._idStringify(id);
-    return _.has(self.map, key);
+    return _.has(self._map, key);
   },
   // XXX used?
   setDefault: function (id, def) {
     var self = this;
     var key = LocalCollection._idStringify(id);
-    if (_.has(self.map, key))
-      return self.map[key];
-    self.map[key] = def;
+    if (_.has(self._map, key))
+      return self._map[key];
+    self._map[key] = def;
     return def;
   }
 });
