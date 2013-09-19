3a0af0745e4c89e91837f34605a68f701029015d
David Glasser <glasser@meteor.com>
2013-09-19 10:23:43 -0700

    only one mongo-livedata test fails

--- packages/mongo-livedata/id_map.js
+++ packages/mongo-livedata/id_map.js
@@ -28,6 +28,17 @@ _.extend(IdMap.prototype, {
     var self = this;
     return _.isEmpty(self._map);
   },
+  clear: function () {
+    var self = this;
+    self._map = {};
+  },
+  each: function (iterator) {
+    var self = this;
+    _.each(self._map, function (value, key, obj) {
+      var context = this;
+      iterator.call(context, value, LocalCollection._idParse(key), obj);
+    });
+  },
   // XXX used?
   setDefault: function (id, def) {
     var self = this;
