e5b30daf8c885701c22b589446327b41cc6d074b
Avital Oliver <avital@thewe.net>
2013-09-18 18:15:03 -0700

    support {{else}} in {{#each}}

--- packages/ui/each.js
+++ packages/ui/each.js
@@ -14,13 +14,39 @@ UI.Each = Component.extend({
     var content = self.content;
     if (typeof content === 'function')
       content = _.bind(content, self);
+    var elseContent = self.elseContent;
+    if (typeof elseContent === 'function')
+      elseContent = _.bind(elseContent, self);
 
     var range = self.dom;
+
+    // if there is an else clause, keep track of the number of
+    // rendered items.  use this to display the else clause when count
+    // becomes zero, and remove it when count becomes positive.
+    var count = 0;
+    var addToCount = function(diff) {
+      if (!elseContent) // if no else, no need to keep track of count
+        return;
+
+      if (count + diff < 0)
+        throw new Error("count should never become negative");
+
+      if (count === 0) {
+        // remove else clause
+        range.removeAll();
+      }
+      count += diff;
+      if (count === 0) {
+        // display else clause
+        range.add(null, UI.render(elseContent, {}, self), null);
+      }
+    };
 
     ObserveSequence.observe(function () {
       return self.get();
     }, {
       addedAt: function (id, item, i, beforeId) {
+        addToCount(1);
         id = LocalCollection._idStringify(id);
 
         var data = item;
@@ -48,6 +74,7 @@ UI.Each = Component.extend({
         range.add(id, comp, beforeId);
       },
       removed: function (id, item) {
+        addToCount(-1);
         range.remove(LocalCollection._idStringify(id));
       },
       movedTo: function (id, item, i, j, beforeId) {
@@ -59,5 +86,7 @@ UI.Each = Component.extend({
         range.get(LocalCollection._idStringify(id)).data.$set(newItem);
       }
     });
+
+    addToCount(0); // display the else clause if no displayed items
   }
 });
