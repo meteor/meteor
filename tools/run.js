43570ac538dc7492947bad1bb6b805467621cb7c
Nick Martin <nim@meteor.com>
2013-09-21 17:40:59 -0700

    typos

--- tools/run.js
+++ tools/run.js
@@ -403,8 +403,8 @@ exports.run = function (context, options) {
   // Allow override and use of external mongo. Matches code in launch_mongo.
   var mongoUrl = process.env.MONGO_URL ||
         ("mongodb://127.0.0.1:" + mongoPort + "/meteor");
-  var oplogUrl = process.env.MONGO_URL ? undefined
-        : "mongodb://127.0.01:" + mongoPort + "/local";
+  var oplogUrl = process.env.XXX_OPLOG_URL ? undefined
+        : "mongodb://127.0.0.1:" + mongoPort + "/local";
   var firstRun = true;
 
   var serverHandle;
