a3d539ed0bab7b8fa0f72cb7daf6467ba0aec17c
Matt DeBergalis <matt@meteor.com>
2013-09-25 11:39:24 -0700

    Disallow words added after game clock expires.
    Fixes #541.

--- examples/wordplay/model.js
+++ examples/wordplay/model.js
@@ -102,9 +102,11 @@ Meteor.methods({
     var word = Words.findOne(word_id);
     var game = Games.findOne(word.game_id);
 
-    // client and server can both check: must be at least three chars
-    // long, not already used, and possible to make on the board.
-    if (word.length < 3
+    // client and server can both check that the game has time remaining, and
+    // that the word is at least three chars, isn't already used, and is
+    // possible to make on the board.
+    if (game.clock === 0
+        || word.length < 3
         || Words.find({game_id: word.game_id, word: word.word}).count() > 1
         || paths_for_word(game.board, word.word).length === 0) {
       Words.update(word._id, {$set: {score: 0, state: 'bad'}});
