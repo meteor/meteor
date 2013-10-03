3b3a51f6e565118c44a763b6ad48ab30ed7c7a6b
Avital Oliver <avital@thewe.net>
2013-10-03 14:55:59 -0700

    bring back Handlebars._escape

--- packages/ui/handlebars_backcompat.js
+++ packages/ui/handlebars_backcompat.js
@@ -5,3 +5,22 @@ Handlebars = {
     this._globalHelpers[name] = func;
   }
 };
+
+// Utility to HTML-escape a string.
+Handlebars._escape = (function() {
+  var escape_map = {
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&#x27;",
+    "`": "&#x60;", /* IE allows backtick-delimited attributes?? */
+    "&": "&amp;"
+  };
+  var escape_one = function(c) {
+    return escape_map[c];
+  };
+
+  return function (x) {
+    return x.replace(/[&<>"'`]/g, escape_one);
+  };
+})();
