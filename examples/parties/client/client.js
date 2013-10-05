77d50e4f3d79ec86c85a0e9be2ab81ea72b891b1
Geoff Schmidt <geoff@geoffschmidt.com>
2013-10-04 18:19:57 -0700

    parties example: force selection to an actually-existing party. This handles the case where the selected party is deleted by another user, and also the case where the server rejects the party creation request.

--- examples/parties/client/client.js
+++ examples/parties/client/client.js
@@ -3,13 +3,16 @@
 Meteor.subscribe("directory");
 Meteor.subscribe("parties");
 
-// If no party selected, select one.
+// If no party selected, or if the selected party was deleted, select one.
 Meteor.startup(function () {
   Deps.autorun(function () {
-    if (! Session.get("selected")) {
+    var selected = Session.get("selected");
+    if (! selected || ! Parties.findOne(selected)) {
       var party = Parties.findOne();
       if (party)
         Session.set("selected", party._id);
+      else
+        Session.set("selected", null);
     }
   });
 });
