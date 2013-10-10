df657e0af6c8b110f54d62e876fc4a3be43bc187
Avital Oliver <avital@thewe.net>
2013-10-10 12:24:51 -0700

    Deprecate preserve-inputs

--- packages/preserve-inputs/package.js
+++ packages/preserve-inputs/package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  summary: "Automatically preserve all form fields with a unique id"
+  summary: "Deprecated package (now empty)",
+  internal: true
 });
 
 Package.on_use(function (api) {
-  api.use(['underscore', 'spark']);
-  api.add_files("preserve-inputs.js", "client");
+  // Deprecated -- Meteor UI makes preserve directives unnecessary
 });
