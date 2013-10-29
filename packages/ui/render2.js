03312eb0a0e724b462b50e1a3168cd45e0e733ed
David Greenspan <dgreenspan@alum.mit.edu>
2013-10-28 19:32:32 -0700

    CharRef and Comment; toCode

--- packages/ui/render2.js
+++ packages/ui/render2.js
@@ -52,26 +52,46 @@ UI.Tag.defineTag = function (name) {
   UI.Tag[name] = makeTagFunc(name);
 };
 
+// e.g. `CharRef({html: '&mdash;', str: '\u2014'})`
+UI.Tag.CharRef = makeTagFunc('CharRef');
+// e.g. `Comment("foo")`
+UI.Tag.Comment = makeTagFunc('Comment');
+
 _.each(allElementNames, UI.Tag.defineTag);
 
 ////////////////////////////////////////
 
+var sanitizeComment = function (content) {
+  return content.replace(/--+/g, '').replace(/-$/, '');
+};
+
+// XXX should do more error-checking for the case where user is supplying the tags.
+// For example, check that CharRef has `html` and `str` properties and no content.
+// Check that Comment has a single string child and no attributes.  Etc.
+
 var materialize = function (node, parentNode, beforeNode) {
+  beforeNode = beforeNode || null; // for IE
+
   if (node && (typeof node === 'object') &&
       (typeof node.splice === 'function')) {
     // Tag or array
     if (node.tagName) {
-      // Tag
-      var elem = document.createElement(node.tagName);
-      if (node.attrs) {
-        _.each(node.attrs, function (v, k) {
-          elem.setAttribute(k, v);
+      if (node.tagName === 'CharRef') {
+        parentNode.insertBefore(document.createTextNode(node.attrs.str), beforeNode);
+      } else if (node.tagName === 'Comment') {
+        parentNode.insertBefore(document.createComment(sanitizeComment(node[0])));
+      } else {
+        var elem = document.createElement(node.tagName);
+        if (node.attrs) {
+          _.each(node.attrs, function (v, k) {
+            elem.setAttribute(k, attributeValueToString(v));
+          });
+        }
+        _.each(node, function (child) {
+          materialize(child, elem);
         });
+        parentNode.insertBefore(elem, beforeNode);
       }
-      _.each(node, function (child) {
-        materialize(child, elem);
-      });
-      parentNode.insertBefore(elem, beforeNode);
     } else {
       // array
       _.each(node, function (child) {
@@ -93,9 +113,57 @@ var properCaseTagName = function (name) {
   return name.toLowerCase();
 };
 
-var stringifyAttributeValue = function (v) {
-  // XXX todo
-  return '"' + v.replace(/"/g, '&quot;').replace(/&/g, '&amp;') + '"';
+var attributeValuePartToQuotedStringPart = function (v) {
+  if (typeof v === 'string') {
+    return v.replace(/"/g, '&quot;').replace(/&/g, '&amp;');
+  } else if (v.tagName === 'CharRef') {
+    return v.attrs.html;
+  }
+};
+
+var attributeValueToQuotedString = function (v) {
+  var result = '"';
+  if (typeof v === 'object' && (typeof v.length === 'number')) {
+    // array
+    for (var i = 0; i < v.length; i++)
+      result += attributeValuePartToQuotedStringPart(v[i]);
+  } else {
+    result += attributeValuePartToQuotedStringPart(v);
+  }
+  result += '"';
+  return result;
+};
+
+var attributeValuePartToString = function (v) {
+  if (typeof v === 'string') {
+    return v;
+  } else if (v.tagName === 'CharRef') {
+    return v.attrs.str;
+  }
+};
+
+var attributeValueToString = function (v) {
+  if (typeof v === 'object' && (typeof v.length === 'number')) {
+    // array
+    var result = '';
+    for (var i = 0; i < v.length; i++)
+      result += attributeValuePartToString(v[i]);
+    return result;
+  } else {
+    return attributeValuePartToString(v);
+  }
+};
+
+var attributeValueToCode = function (v) {
+  if (typeof v === 'object' && (typeof v.length === 'number')) {
+    // array
+    var partStrs = [];
+    for (var i = 0; i < v.length; i++)
+      partStrs.push(toCode(v[i]));
+    return '[' + partStrs.join(', ') + ']';
+  } else {
+    return toCode(v);
+  }
 };
 
 var toHTML = function (node) {
@@ -106,17 +174,24 @@ var toHTML = function (node) {
     // Tag or array
     if (node.tagName) {
       // Tag
-      result += '<' + properCaseTagName(node.tagName);
-      if (node.attrs) {
-        _.each(node.attrs, function (v, k) {
-          result += ' ' + k + '=' + stringifyAttributeValue(v);
+      if (node.tagName === 'CharRef') {
+        result += node.attrs.html;
+      } else if (node.tagName === 'Comment') {
+        result += '<!--' + sanitizeComment(node[0]) + '-->';
+      } else {
+        // XXX handle void elements, like BR
+        result += '<' + properCaseTagName(node.tagName);
+        if (node.attrs) {
+          _.each(node.attrs, function (v, k) {
+            result += ' ' + k + '=' + attributeValueToQuotedString(v);
+          });
+        }
+        result += '>';
+        _.each(node, function (child) {
+          result += toHTML(child);
         });
+        result += '</' + properCaseTagName(node.tagName) + '>';
       }
-      result += '>';
-      _.each(node, function (child) {
-        result += toHTML(child);
-      });
-      result += '</' + properCaseTagName(node.tagName) + '>';
     } else {
       // array
       _.each(node, function (child) {
@@ -135,5 +210,56 @@ var toHTML = function (node) {
   return result;
 };
 
+var toJSLiteral = function (obj) {
+  // http://timelessrepo.com/json-isnt-a-javascript-subset
+  return (JSON.stringify(obj)
+          .replace(/\u2028/g, '\\u2028')
+          .replace(/\u2029/g, '\\u2029'));
+};
+
+var toCode = function (node) {
+  var result = "";
+
+  if (node && (typeof node === 'object') &&
+      (typeof node.splice === 'function')) {
+    // Tag or array
+    if (node.tagName) {
+      // Tag
+      result += 'UI.Tag.' + node.tagName + '(';
+      var argStrs = [];
+      if (node.attrs) {
+        var kvStrs = [];
+        _.each(node.attrs, function (v, k) {
+          kvStrs.push((/^[a-zA-Z]+$/.test(k) ? k : toJSLiteral(k)) + ': ' +
+                      attributeValueToCode(v));
+        });
+        argStrs.push('{' + kvStrs.join(', ') + '}');
+      }
+
+      _.each(node, function (child) {
+        argStrs.push(toCode(child));
+      });
+
+      result += argStrs.join(', ') + ')';
+    } else {
+      // array
+      result += '[';
+      result += _.map(node, toCode).join(', ');
+      result += ']';
+      return result;
+    }
+  } else if (typeof node === 'string') {
+    result += toJSLiteral(node);
+  } else if (node == null) {
+    // null or undefined.
+    // do nothing.
+  } else {
+    throw new Error("Unexpected item in HTML tree: " + node);
+  }
+
+  return result;
+};
+
 UI.materialize = materialize;
 UI.toHTML = toHTML;
+UI.toCode = toCode;
