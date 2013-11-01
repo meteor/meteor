8be473df8995d07317129a8afdfcc53f75bce01b
Avital Oliver <avital@thewe.net>
2013-11-01 16:50:09 -0700

    Unbreak `meteor test-packages`

--- packages/handlebars/package.js
+++ packages/handlebars/package.js
@@ -4,7 +4,11 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  throw new Error(
-    "The 'handlebars' package is deprecated. "
-      + "`Handlebars.registerHelper` is now defined in the 'ui' package.");
+// XXX we unfortunately we can't do this since `meteor test-packages`
+// tries to load all packages.
+//
+//  throw new Error(
+//    "The 'handlebars' package is deprecated. "
+//      + "`Handlebars.registerHelper` is now defined in the 'ui' package.");
 });
+
