1d0ce3ff2d46670e5e00c169a4c8da865748c2d9
Andrew Wilcox <andrew.wilcox@gmail.com>
2013-11-13 16:42:33 -0500

    livedata doesn't need to use localstorage because it's now using reload migration instead.

--- packages/livedata/package.js
+++ packages/livedata/package.js
@@ -25,8 +25,6 @@ Package.on_use(function (api) {
   // If the facts package is loaded, publish some statistics.
   api.use('facts', 'server', {weak: true});
 
-  api.use('localstorage', 'client');
-
   api.export('DDP');
   api.export('DDPServer', 'server');
 
