f3acb1e0ef049abf59311eb0086f8c1ccb9f64a0
David Greenspan <dgreenspan@alum.mit.edu>
2013-12-06 00:01:07 -0800

    fix odd test failure (typo?)

--- templating_tests.js
+++ templating_tests.js
@@ -397,7 +397,7 @@ Tinytest.add("templating - template arg", function (test) {
   var div = renderToDiv(Template.test_template_arg_a.withData({food: "pie"}));
   var cleanupDiv = addToBody(div);
   test.equal($(div).text(), "Greetings 1-bold Line");
-  clickElement(DomUtils.find(div, 'i')[0]);
+  clickElement(DomUtils.find(div, 'i'));
   test.equal($(div).text(), "Hello 3-element World (the secret is strawberry pie)");
 
   cleanupDiv();
