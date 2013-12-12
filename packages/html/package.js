734fecc5467c5bdf33fdbee3f0cd5bda61dbe05b
David Greenspan <dgreenspan@alum.mit.edu>
2013-12-11 20:10:05 -0800

    Make package dependencies slightly more accurate

--- packages/html/package.js
+++ packages/html/package.js
@@ -4,6 +4,8 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
+  // we attach stuff to the global symbol `HTML`, exported
+  // by `htmljs`, so we both use and effectively imply it.
   api.use('htmljs');
   api.imply('htmljs');
 
@@ -18,7 +20,7 @@ Package.on_test(function (api) {
   api.use('tinytest');
   api.use('html');
   api.use('underscore');
-  api.use('ui'); // for `toCode`
+  api.use('spacebars'); // for `toJS`
   api.add_files(['charref_tests.js',
                  'tokenize_tests.js',
                  'parse_tests.js']);
