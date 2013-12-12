22306dbc11289b36df8b906e011ca7a60531dd58
David Greenspan <dgreenspan@alum.mit.edu>
2013-12-11 23:03:26 -0800

    Make markdown work(?)

--- packages/showdown/template-integration.js
+++ packages/showdown/template-integration.js
@@ -1,11 +1,12 @@
 if (Package.ui) {
   var UI = Package.ui.UI;
+  var HTML = Package.htmljs.HTML; // implied by `ui`
   Package.ui.Handlebars.registerHelper('markdown', UI.block(function () {
-    var self;
+    var self = this;
     return function () {
       var text = UI.toRawText(self.__content);
       var converter = new Showdown.converter();
-      return converter.makeHtml(text);
+      return HTML.Raw(converter.makeHtml(text));
     };
   }));
 }
