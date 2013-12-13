ae2404d1f34c5ef6c7e12a83920ec539d80ebed6
Emily Stark <emily@meteor.com>
2013-12-12 21:52:36 -0800

    Remove trailing slashes on pathnames for proxy bindings

--- packages/webapp/webapp_server.js
+++ packages/webapp/webapp_server.js
@@ -661,7 +661,7 @@ WebAppInternals.bindToProxy = function (proxyConfig) {
         throw err;
       outstanding--;
       checkComplete();
-    }
+    };
   };
 
   // for now, have our (temporary) requiresAuth flag apply to all
@@ -701,7 +701,8 @@ WebAppInternals.bindToProxy = function (proxyConfig) {
         parsedUrl.pathname = "";
       if (! parsedUrl.pathname.indexOf("/") !== 0) {
         // Relative path
-        parsedUrl.pathname = bindPathPrefix + "/" + parsedUrl.pathname;
+        parsedUrl.pathname = bindPathPrefix +
+          (parsedUrl.pathname !== "" ? "/" + parsedUrl.pathname : "");
       }
     }
 
