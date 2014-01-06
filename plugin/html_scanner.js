17dd43bd8ed3820ff4352dc6c577c737f15a0704
David Greenspan <dgreenspan@alum.mit.edu>
2014-01-06 13:48:31 -0800

    Route Spacebars errors properly
    Now Spacebars syntax errors (as well as html_scanner errors) go through proper error reporting channels rather than throwing exceptions.  For simple cases, we are back to showing nice errors like Handlebars did on devel, with context.
    
    Next:  Comb the spacebars compiler for errors that are thrown or lack good line numbers.  Go through our own list of bad error messages.

--- plugin/html_scanner.js
+++ plugin/html_scanner.js
@@ -19,11 +19,12 @@ html_scanner = {
       index += amount;
     };
 
-    var throwParseError = function (msg) {
+    var throwParseError = function (msg, overrideIndex) {
       var ret = new html_scanner.ParseError;
       ret.message = msg || "bad formatting in HTML template";
       ret.file = source_name;
-      ret.line = contents.substring(0, index).split('\n').length;
+      var theIndex = (typeof overrideIndex === 'number' ? overrideIndex : index);
+      ret.line = contents.substring(0, theIndex).split('\n').length;
       throw ret;
     };
 
@@ -96,12 +97,14 @@ html_scanner = {
         throwParseError("unclosed <"+tagName+">");
       var tagContents = rest.slice(0, end.index);
       var contentsStartIndex = index;
-      advance(end.index + end[0].length);
 
       // act on the tag
       html_scanner._handleTag(results, tagName, tagAttribs, tagContents,
                               throwParseError, contentsStartIndex,
                               tagStartIndex);
+
+      // advance afterwards, so that line numbers in errors are correct
+      advance(end.index + end[0].length);
     }
 
     return results;
@@ -143,29 +146,38 @@ html_scanner = {
 
     // <body> or <template>
 
-    if (tag === "template") {
-      var name = attribs.name;
-      if (! name)
-        throwParseError("Template has no 'name' attribute");
+    try {
+      if (tag === "template") {
+        var name = attribs.name;
+        if (! name)
+          throwParseError("Template has no 'name' attribute");
 
-      var renderFuncCode = Spacebars.compile(
-        contents, {
-          isTemplate: true,
-          sourceName: 'Template "' + name + '"'
-        });
+        var renderFuncCode = Spacebars.compile(
+          contents, {
+            isTemplate: true,
+            sourceName: 'Template "' + name + '"'
+          });
 
-      results.js += "\nTemplate.__define__(" + JSON.stringify(name) +
-        ", " + renderFuncCode + ");\n";
-    } else {
-      // <body>
-      if (hasAttribs)
-        throwParseError("Attributes on <body> not supported");
+        results.js += "\nTemplate.__define__(" + JSON.stringify(name) +
+          ", " + renderFuncCode + ");\n";
+      } else {
+        // <body>
+        if (hasAttribs)
+          throwParseError("Attributes on <body> not supported");
 
-      var renderFuncCode = Spacebars.compile(
-        contents, { sourceName: "<body>" });
+        var renderFuncCode = Spacebars.compile(
+          contents, { sourceName: "<body>" });
 
-      // We may be one of many `<body>` tags.
-      results.js += "\nUI.body.contentParts.push(UI.Component.extend({render: " + renderFuncCode + "}));\nMeteor.startup(function () { if (! UI.body.INSTANTIATED) { UI.body.INSTANTIATED = true; UI.materialize(UI.body, document.body); } });\n";
+        // We may be one of many `<body>` tags.
+        results.js += "\nUI.body.contentParts.push(UI.Component.extend({render: " + renderFuncCode + "}));\nMeteor.startup(function () { if (! UI.body.INSTANTIATED) { UI.body.INSTANTIATED = true; UI.materialize(UI.body, document.body); } });\n";
+      }
+    } catch (e) {
+      if (e.scanner) {
+        // The error came from Spacebars
+        throwParseError(e.message, contentsStartIndex + e.offset);
+      } else {
+        throw e;
+      }
     }
   }
 };
