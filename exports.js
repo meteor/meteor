2926e08acc05c5919ce8eb52d60bbe21bdf2410f
David Greenspan <dgreenspan@alum.mit.edu>
2014-01-06 13:48:31 -0800

    Route Spacebars errors properly
    Now Spacebars syntax errors (as well as html_scanner errors) go through proper error reporting channels rather than throwing exceptions.  For simple cases, we are back to showing nice errors like Handlebars did on devel, with context.
    
    Next:  Comb the spacebars compiler for errors that are thrown or lack good line numbers.  Go through our own list of bad error messages.

--- exports.js
+++ exports.js
@@ -5,6 +5,9 @@ HTML.codePointToString = codePointToString;
 
 HTML.TEMPLATE_TAG_POSITION = TEMPLATE_TAG_POSITION,
 
+// Could move Scanner into its own package if use it for other stuff.
+HTML.Scanner = Scanner;
+
 HTML._$ = {
   // stuff exposed for testing
   Scanner: Scanner,
