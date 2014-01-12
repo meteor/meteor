3265839bab1ace3f12daa58555ae7a63ed2549af
Zoltan Olah <zol@percolatestudio.com>
2014-01-11 19:46:54 -0800

    Add mrt comment to migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -6,6 +6,9 @@
 # Ex2: ./migrate.sh 2 - Migrates up or down to the specified version.
 # Ex3: ./migrate.sh latest --settings ../settings.json - Migrates up to the 
 #      latest version and passes extra arguments to meteor.
+#
+# Note, calls meteor. If you're using mrt and require a specific version of 
+# meteor, you change line 22 of this script to 'mrt'.
 
 if test -z "$1"
 then
