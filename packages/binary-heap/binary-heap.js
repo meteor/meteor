7f2c6bb09b75e6e943fc5bfabd523766627529c2
Slava Kim <slava@meteor.com>
2014-02-17 20:47:41 -0800

    The implementation of Heap

--- packages/binary-heap/binary-heap.js
+++ packages/binary-heap/binary-heap.js
@@ -1,40 +1,180 @@
+// Constructor of Heap
+// - comparator - Function - given two items returns a number
+// - initData - Array - the initial data in a format:
+//      Object:
+//        - id - String - unique id of the item
+//        - value - Any - the data value
+//    the contents of initData is retained
 BinaryHeap = function (comparator, initData) {
   if (! _.isFunction(comparator))
     throw new Error('Passed comparator is invalid, should be a comparison function');
   var self = this;
   self._comparator = comparator;
-  self._map = {};
+  self._heapIdx = {};
   self._heap = [];
 
   if (_.isArray(initData))
     self._initFromData(initData);
 };
 
-var idStringify, idParse;
+var idStringify;
 if (Package.minimongo) {
   idStringify = Package.minimongo.LocalCollection._idStringify;
-  idParse = Package.minimongo.LocalCollection._idParse;
 } else {
   // XXX: These can't deal with special strings like '__proto__'
   // XXX: or '{ looksLike: "object" }' or numbers.
   idStringify = function (id) { return JSON.stringify(id); };
-  idParse = function (id) { return JSON.parse(id); }
 }
 
 _.extend(BinaryHeap.prototype, {
-  _initFromData: function (data) {},
+  _initFromData: function (data) {
+    var self = this;
 
-  get: function (id) {},
-  set: function (id, value) {},
-  remove: function (id) {},
-  has: function (id) {},
-  empty: function (id) {},
-  clear: function () {},
-  forEach: function (iterator) {},
-  size: function () {},
-  setDefault: function () {},
-  clone: function () {},
+    self._heap = _.clone(data);
 
-  maxElementId: function () {}
+    _.each(data, function (o, i) {
+      self._heapIdx[idStringify(o.id)] = i;
+    });
+
+    for (var i = parentIdx(data.length); i >= 0; i--)
+      self._heapify(i);
+  },
+
+  _heapify: function (idx) {
+    var self = this;
+
+    while (leftChildIdx(idx) < self.size()) {
+      var left = leftChildIdx(idx);
+      var right = rightChildIdx(idx);
+      var largest = idx;
+
+      if (left < self.size() &&
+          self._comparator(self._get(left), self._get(largest)) > 0) {
+        largest = left;
+      }
+      if (right < self.size() &&
+          self._comparator(self._get(right), self._get(largest)) > 0) {
+        largest = right;
+      }
+
+      if (largest === idx)
+        break;
+
+      self._swap(largest, idx);
+      idx = largest;
+    }
+  },
+
+  _upHeap: function (idx) {
+    var self = this;
+    var value = self._get(idx);
+    var ancestor = idx;
+
+    while (ancestor > 0) {
+      var parent = parentIdx(ancestor);
+      if (self._comparator(self._get(parent), value) < 0)
+        ancestor = parent;
+      else
+        break;
+    }
+
+    if (ancestor !== idx)
+      self._swap(ancestor, idx);
+  },
+
+  // Internal: gets raw data object placed on idxth place in heap
+  _get: function (idx) {
+    var self = this;
+    return self._heap[idx].value;
+  },
+
+  _swap: function (idxA, idxB) {
+    var self = this;
+    var A = self._heap[idxA];
+    var B = self._heap[idxB];
+
+    self._heapIdx[idStringify(A.id)] = idxB;
+    self._heapIdx[idStringify(B.id)] = idxA;
+
+    self._heap[idxA] = B;
+    self._heap[idxB] = A;
+  },
+
+  get: function (id) {
+    var self = this;
+    return self._get(self._heapIdx[idStringify(id)]);
+  },
+  set: function (id, value) {
+    var self = this;
+
+    if (self.has(id)) {
+      if (self.get(id) === value)
+        return;
+      else
+        self.remove(id);
+    }
+
+    self._heapIdx[idStringify(id)] = self._heap.length;
+    self._heap.push({ id: id, value: value });
+    self._upHeap(self._heap.length - 1);
+  },
+  remove: function (id) {
+    var self = this;
+    var strId = idStringify(id);
+
+    if (! self.has(id)) {
+      var last = self._heap.length - 1;
+      var idx = self._heapIdx[strId];
+      self._swap(idx, last);
+      self._heap.pop();
+      self._heapify(idx);
+      delete self._heapIdx[strId];
+    }
+  },
+  has: function (id) {
+    var self = this;
+    return self._heapIdx[idStringify(id)] !== undefined;
+  },
+  empty: function (id) {
+    var self = this;
+    return !self.size();
+  },
+  clear: function () {
+    var self = this;
+    self._heap = [];
+    self._heapIdx = {};
+  },
+  forEach: function (iterator) {
+    var self = this;
+    _.each(self._heap, function (obj) {
+      return iterator(obj.value, obj.id);
+    });
+  },
+  size: function () {
+    var self = this;
+    return self._heap.length;
+  },
+  setDefault: function (id, def) {
+    var self = this;
+    if (self.has(id))
+      return self.get(id);
+    self.set(id, def);
+    return def;
+  },
+  clone: function () {
+    var self = this;
+    var clone = new BinaryHeap(self._comparator);
+    clone._heap = EJSON.clone(self._heap);
+    clone._heapIdx = EJSON.clone(self._heapIdx);
+  },
+
+  maxElementId: function () {
+    var self = this;
+    return self.size() ? self._heap[0].id : null;
+  }
 });
+
+function leftChildIdx (i) { return i * 2 + 1; }
+function rightChildIdx (i) { return i * i + 2; }
+function parentIdx (i) { return (i - 1) >> 1; }
 
