e5b2e4498c8b98b550a9c0ce42d86ead02398b68
Justin SB <justin@fathomdb.com>
2014-03-12 12:40:39 -0400

    Clean up and document repeatable_random.js

--- packages/livedata/repeatable_random.js
+++ packages/livedata/repeatable_random.js
@@ -1,3 +1,18 @@
+// RepeatableRandom allows for generation of pseudo-random values, from a seed.
+//
+// We use this for consistent 'random' numbers across the client and server.  Here,
+// we want to generate probably-unique IDs on the client, and we ideally want the server
+// to generate the same IDs when it executes the method.
+//
+// For generated values to be the same, we must seed ourselves the same way,
+// and we must keep track of the current state of our pseudo-random generators.
+// We call this state the scope. By default, we use the current DDP method invocation as our scope.
+// DDP now allows the client to specify a randomSeed.  If a randomSeed is provided it will be used
+// to seed our random sequences.  In this way, client and server method calls will generate the same values.
+//
+// We expose multiple streams, each keyed by a string; each stream is independent and seeded differently
+// (but predictably).  By using multiple streams, we support reordering of requests,
+// as long as they occur on different streams.
 RepeatableRandom = function (options) {
   var self = this;
 
@@ -9,21 +24,15 @@ RepeatableRandom = function (options) {
   this._sequences = {};
 };
 
-// We don't expect to use this global scope object in production, only in the tests.
-// Nonetheless, it provides a place of last resort where we can store global state
-// TODO: Should we assert that we are testing, or somehow provide this only for the tests?
-//  (But then the tests wouldn't be testing the same thing...)
-var globalRepeatableRandomScope = {};
-
+// Returns the random stream with the specified key.
+// This first tries to use the DDP method invocation as the scope;
+// if we're not in a method invocation, then we can use fallbackScope instead.
+// Otherwise we generate an ephemeral  scope, which will be random but not repeatable.
 Meteor.repeatableRandom = function (key, fallbackScope) {
-  var scope = DDP._CurrentInvocation.get() || fallbackScope; // || globalRepeatableRandomScope;
-//  if (scope == globalRepeatableRandomScope) {
-//    // TODO: Find a better solution here
-//    Meteor._debug("Using global repeatable random scope");
-//  }
+  var scope = DDP._CurrentInvocation.get() || fallbackScope;
   if (!scope) {
     // We aren't in a method invocation, there was no scope passed in, so
-    //  we aren't actually repeatable
+    // the sequence won't actually be repeatable.
     Meteor._debug("Requested repeatable random, but no scope available");
     var seeds = [Random.hexString(20), key];
     return Random.create.apply(null, seeds);
@@ -38,6 +47,9 @@ Meteor.repeatableRandom = function (key, fallbackScope) {
 };
 
 _.extend(RepeatableRandom.prototype, {
+  // Get a random sequence with the specified key, creating it if does not exist.
+  // New sequences are seeded with the seed concatenated with the key.
+  // By passing a seed into Random.create, we use the Alea generator.
   _sequence: function (key) {
     var self = this;
     
