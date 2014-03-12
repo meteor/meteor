7408a66e32bbcb61e2e1c99667857185dc8af8e5
David Gonzalez <gonzalez.dalex@gmail.com>
2014-03-12 11:17:26 -0700

    Updated Stylus to use v0.42.3

--- packages/stylus/package.js
+++ packages/stylus/package.js
@@ -8,7 +8,7 @@ Package._transitional_registerBuildPlugin({
   sources: [
     'plugin/compile-stylus.js'
   ],
-  npmDependencies: { stylus: "0.42.2", nib: "1.0.2" }
+  npmDependencies: { stylus: "0.42.3", nib: "1.0.2" }
 });
 
 Package.on_test(function (api) {
