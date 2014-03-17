meteor/meteor

commit d049bf75063e952e02b7bd719f575072147071e8

Use faye-websocket for server-to-server DDP
This matches what the SockJS server uses; now we only need to understand
and fix bugs in the implementation of one websocket npm module.

Some notes:

  - I actually trust that it's possible to close a connection before it
    successfully connects, which allows me to simplify the code a
    lot (since there shouldn't be multiple connections active per
    ClientStream). I put in some assertions to make sure this is the
    case, though.  (Note that this module also has a simpler model,
    where there's a single object representing the client connection,
    not a "client" object that spawns "connections".)

  - We now print connect errors as well as post-connect errors.  (This
    required adding a flag to keep tests quiet since it makes an
    expected-to-fail-to-connect connection.)  We need a better approach
    to stream error handling, though.

  - We used to have a test to make sure that a certain not-user-visible
    callback is called within a Fiber; structuring the code such that
    this test is still possible would lead to the code being less
    consistent and harder to read, so I dropped the test.

  - Fix a few bugs where we weren't using Meteor.setTimeout.

packages/livedata/livedata_server.js
@@ -1160,7 +1160,7 @@ _.extend(Server.prototype, {
       // drop all future data coming over this connection on the
       // floor. We don't want to confuse things.
       socket.removeAllListeners('data');
-      setTimeout(function () {
+      Meteor.setTimeout(function () {
         socket.send(stringifyDDP({msg: 'failed', version: version}));
         socket.close();
       }, timeout);

packages/livedata/livedata_connection.js
@@ -49,7 +49,10 @@ var Connection = function (url, options) {
     self._stream = new LivedataTest.ClientStream(url, {
       retry: options.retry,
       headers: options.headers,
-      _sockjsOptions: options._sockjsOptions
+      _sockjsOptions: options._sockjsOptions,
+      // To keep some tests quiet (because we don't have a real API for handling
+      // client-stream-level errors).
+      _dontPrintErrors: options._dontPrintErrors
     });
   }
 

packages/livedata/livedata_tests.js
@@ -683,9 +683,10 @@ if (Meteor.isServer) {
   testAsyncMulti("livedata - connect fails to unknown place", [
     function (test, expect) {
       var self = this;
-      self.conn = DDP.connect("example.com");
+      self.conn = DDP.connect("example.com", {_dontPrintErrors: true});
       Meteor.setTimeout(expect(function () {
         test.isFalse(self.conn.status().connected, "Not connected");
+        self.conn.close();
       }), 500);
     }
   ]);

packages/livedata/package.js
@@ -3,7 +3,11 @@ Package.describe({
   internal: true
 });
 
-Npm.depends({sockjs: "0.3.8", websocket: "1.0.8"});
+// We use 'faye-websocket' for connections in server-to-server DDP, mostly
+// because it's the same library used as a server in sockjs, and it's easiest to
+// deal with a single websocket implementation.  (Plus, its maintainer is easy
+// to work with on pull requests.)
+Npm.depends({sockjs: "0.3.8", "faye-websocket": "0.7.2"});
 
 Package.on_use(function (api) {
   api.use(['check', 'random', 'ejson', 'json', 'underscore', 'deps',

packages/livedata/stream_client_nodejs.js
@@ -11,43 +11,16 @@
 // ping frames or with DDP-level messages.)
 LivedataTest.ClientStream = function (endpoint, options) {
   var self = this;
+  options = options || {};
+
   self.options = _.extend({
     retry: true
   }, options);
 
-  // WebSocket-Node https://github.com/Worlize/WebSocket-Node
-  // Chosen because it can run without native components. It has a
-  // somewhat idiosyncratic API. We may want to use 'ws' instead in the
-  // future.
-  //
-  // Since server-to-server DDP is still an experimental feature, we only
-  // require the module if we actually create a server-to-server
-  // connection. This is a minor efficiency improvement, but moreover: while
-  // 'websocket' doesn't require native components, it tries to use some
-  // optional native components and prints a warning if it can't load
-  // them. Since native components in packages don't work when transferred to
-  // other architectures yet, this means that require('websocket') prints a
-  // spammy log message when deployed to another architecture. Delaying the
-  // require means you only get the log message if you're actually using the
-  // feature.
-  self.client = new (Npm.require('websocket').client)();
+  self.client = null;  // created in _launchConnection
   self.endpoint = endpoint;
-  self.currentConnection = null;
 
-  options = options || {};
-  self.headers = options.headers || {};
-
-  self.client.on('connect', Meteor.bindEnvironment(
-    function (connection) {
-      return self._onConnect(connection);
-    },
-    "stream connect callback"
-  ));
-
-  self.client.on('connectFailed', function (error) {
-    // XXX: Make this do something better than make the tests hang if it does not work.
-    return self._lostConnection();
-  });
+  self.headers = self.options.headers || {};
 
   self._initCommon();
 
@@ -63,7 +36,7 @@ _.extend(LivedataTest.ClientStream.prototype, {
   send: function (data) {
     var self = this;
     if (self.currentStatus.connected) {
-      self.currentConnection.send(data);
+      self.client.send(data);
     }
   },
 
@@ -73,80 +46,37 @@ _.extend(LivedataTest.ClientStream.prototype, {
     self.endpoint = url;
   },
 
-  _onConnect: function (connection) {
+  _onConnect: function (client) {
     var self = this;
+
+    if (client !== self.client) {
+      // This connection is not from the last call to _launchConnection.
+      // But _launchConnection calls _cleanup which closes previous connections.
+      // It's our belief that this stifles future 'open' events, but maybe
+      // we are wrong?
+      throw new Error("Got open from inactive client");
+    }
 
     if (self._forcedToDisconnect) {
       // We were asked to disconnect between trying to open the connection and
       // actually opening it. Let's just pretend this never happened.
-      connection.close();
+      self.client.close();
+      self.client = null;
       return;
     }
 
     if (self.currentStatus.connected) {
-      // We already have a connection. It must have been the case that
-      // we started two parallel connection attempts (because we
-      // wanted to 'reconnect now' on a hanging connection and we had
-      // no way to cancel the connection attempt.) Just ignore/close
-      // the latecomer.
-      connection.close();
-      return;
+      // We already have a connection. It must have been the case that we
+      // started two parallel connection attempts (because we wanted to
+      // 'reconnect now' on a hanging connection and we had no way to cancel the
+      // connection attempt.) But this shouldn't happen (similarly to the client
+      // !== self.client check above).
+      throw new Error("Two parallel connections?");
     }
 
-    if (self.connectionTimer) {
-      clearTimeout(self.connectionTimer);
-      self.connectionTimer = null;
-    }
-
-    var onError = Meteor.bindEnvironment(
-      function (_this, error) {
-        if (self.currentConnection !== _this)
-          return;
-
-        Meteor._debug("stream error", error.toString(),
-                      (new Date()).toDateString());
-        self._lostConnection();
-      },
-      "stream error callback"
-    );
-
-    connection.on('error', function (error) {
-      // We have to pass in `this` explicitly because bindEnvironment
-      // doesn't propagate it for us.
-      onError(this, error);
-    });
-
-    var onClose = Meteor.bindEnvironment(
-      function (_this) {
-        if (self.options._testOnClose)
-          self.options._testOnClose();
-
-        if (self.currentConnection !== _this)
-          return;
-
-        self._lostConnection();
-      },
-      "stream close callback"
-    );
-
-    connection.on('close', function () {
-      // We have to pass in `this` explicitly because bindEnvironment
-      // doesn't propagate it for us.
-      onClose(this);
-    });
-
-    connection.on('message', function (message) {
-      if (self.currentConnection !== this)
-        return; // old connection still emitting messages
-
-      if (message.type === "utf8") // ignore binary frames
-        _.each(self.eventCallbacks.message, function (callback) {
-          callback(message.utf8Data);
-        });
-    });
+    self._clearConnectionTimer();
 
     // update status
-    self.currentConnection = connection;
     self.currentStatus.status = "connected";
     self.currentStatus.connected = true;
     self.currentStatus.retryCount = 0;
@@ -161,10 +91,10 @@ _.extend(LivedataTest.ClientStream.prototype, {
     var self = this;
 
     self._clearConnectionTimer();
-    if (self.currentConnection) {
-      var conn = self.currentConnection;
-      self.currentConnection = null;
-      conn.close();
+    if (self.client) {
+      var client = self.client;
+      self.client = null;
+      client.close();
     }
   },
 
@@ -181,25 +111,63 @@ _.extend(LivedataTest.ClientStream.prototype, {
     var self = this;
     self._cleanup(); // cleanup the old socket, if there was one.
 
-    // launch a connect attempt. we have no way to track it. we either
-    // get an _onConnect event, or we don't.
+    // Since server-to-server DDP is still an experimental feature, we only
+    // require the module if we actually create a server-to-server
+    // connection.
+    var FayeWebSocket = Npm.require('faye-websocket');
 
-    // XXX: set up a timeout on this.
+    // We would like to specify 'ddp' as the subprotocol here. The npm module we
+    // used to use as a client would fail the handshake if we ask for a
+    // subprotocol and the server doesn't send one back (and sockjs doesn't).
+    // Faye doesn't have that behavior; it's unclear from reading RFC 6455 if
+    // Faye is erroneous or not.  So for now, we don't specify protocols.
+    var client = self.client = new FayeWebSocket.Client(
+      toWebsocketUrl(self.endpoint),
+      [/*no subprotocols*/],
+      {headers: self.headers}
+    );
 
-    // we would like to specify 'ddp' as the protocol here, but
-    // unfortunately WebSocket-Node fails the handshake if we ask for
-    // a protocol and the server doesn't send one back (and sockjs
-    // doesn't). also, related: I guess we have to accept that
-    // 'stream' is ddp-specific
-    self.client.connect(toWebsocketUrl(self.endpoint),
-                        undefined, // protocols
-                        undefined, // origin
-                        self.headers);
-
-    if (self.connectionTimer)
-      clearTimeout(self.connectionTimer);
-    self.connectionTimer = setTimeout(
+    self._clearConnectionTimer();
+    self.connectionTimer = Meteor.setTimeout(
       _.bind(self._lostConnection, self),
       self.CONNECT_TIMEOUT);
+
+    self.client.on('open', Meteor.bindEnvironment(function () {
+      return self._onConnect(client);
+    }, "stream connect callback"));
+
+    var clientOnIfCurrent = function (event, description, f) {
+      self.client.on(event, Meteor.bindEnvironment(function () {
+        // Ignore events from any connection we've already cleaned up.
+        if (client !== self.client)
+          return;
+        f.apply(this, arguments);
+      }, description));
+    };
+
+    clientOnIfCurrent('error', 'stream error callback', function (error) {
+      if (!self.options._dontPrintErrors)
+        Meteor._debug("stream error", error.message);
+
+      // XXX: Make this do something better than make the tests hang if it does
+      // not work.
+      self._lostConnection();
+    });
+
+
+    clientOnIfCurrent('close', 'stream close callback', function () {
+      self._lostConnection();
+    });
+
+
+    clientOnIfCurrent('message', 'stream message callback', function (message) {
+      // Ignore binary frames, where message.data is a Buffer
+      if (typeof message.data !== "string")
+        return;
+
+      _.each(self.eventCallbacks.message, function (callback) {
+        callback(message.data);
+      });
+    });
   }
 });

packages/livedata/stream_client_tests.js
@@ -1,17 +1,24 @@
 var Fiber = Npm.require('fibers');
 
-Tinytest.addAsync("stream client - callbacks run in a fiber", function (test, onComplete) {
-  stream = new LivedataTest.ClientStream(
-    Meteor.absoluteUrl(),
-    {
-      _testOnClose: function () {
-        test.isTrue(Fiber.current);
-        onComplete();
-      }
-    }
-  );
-  stream.on('reset', function () {
-    test.isTrue(Fiber.current);
-    stream.disconnect();
-  });
-});
+testAsyncMulti("stream client - callbacks run in a fiber", [
+  function (test, expect) {
+    var stream = new LivedataTest.ClientStream(Meteor.absoluteUrl());
+
+    var messageFired = false;
+    var resetFired = false;
+
+    stream.on('message', expect(function () {
+      test.isTrue(Fiber.current);
+      if (resetFired)
+        stream.disconnect();
+      messageFired = true;
+    }));
+
+    stream.on('reset', expect(function () {
+      test.isTrue(Fiber.current);
+      if (messageFired)
+        stream.disconnect();
+      resetFired = true;
+    }));
+  }
+]);

packages/retry/retry.js
@@ -57,7 +57,7 @@ _.extend(Retry.prototype, {
     var timeout = self._timeout(count);
     if (self.retryTimer)
       clearTimeout(self.retryTimer);
-    self.retryTimer = setTimeout(fn, timeout);
+    self.retryTimer = Meteor.setTimeout(fn, timeout);
     return timeout;
   }
 

commit 0a2b213682ccf3f2641316e44088ec4a99062698

Ensure that hook.each is called from a Fiber
That gives us enough guarantees that we can simplify the error-handling
code.

We also ensure that the "new connection" handler in livedata_server runs
in a Fiber, which fixes a bug introduced in d049bf7506 where connections
from pre-pre1 clients would crash (due to Meteor.setTimeout which only
works in a Fiber).

packages/callback-hook/hook.js
@@ -57,7 +57,10 @@ _.extend(Hook.prototype, {
     callback = Meteor.bindEnvironment(
       callback,
       self.exceptionHandler || function (exception) {
-        self.exception = exception;
+        // Note: this relies on the undocumented fact that if bindEnvironment's
+        // onException throws, and you are invoking the callback either in the
+        // browser or from within a Fiber in Node, the exception is propagated.
+        throw exception;
       }
     );
 
@@ -83,26 +86,18 @@ _.extend(Hook.prototype, {
   //
   each: function (iterator) {
     var self = this;
+
+    // Invoking bindEnvironment'd callbacks outside of a Fiber in Node doesn't
+    // run them to completion (and exceptions thrown from onException are not
+    // propagated), so we need to be in a Fiber.
+    Meteor._nodeCodeMustBeInFiber();
+
     var ids = _.keys(self.callbacks);
     for (var i = 0;  i < ids.length;  ++i) {
       var id = ids[i];
       // check to see if the callback was removed during iteration
       if (_.has(self.callbacks, id)) {
         var callback = self.callbacks[id];
-
-        if (! self.exceptionHandler) {
-          var originalCallback = callback;
-          callback = function (/*arguments*/) {
-            self.exception = null;
-            var ret = originalCallback.apply(null, arguments);
-            if (self.exception) {
-              var exception = self.exception;
-              self.exception = null;
-              throw exception;
-            }
-            return ret;
-          };
-        }
 
         if (! iterator(callback))
           break;

packages/livedata/livedata_server.js
@@ -1099,7 +1099,9 @@ Server = function () {
             sendError("Already connected", msg);
             return;
           }
-          self._handleConnect(socket, msg);
+          Fiber(function () {
+            self._handleConnect(socket, msg);
+          }).run();
           return;
         }
 

packages/meteor/dynamics_browser.js
@@ -46,10 +46,16 @@ Meteor.bindEnvironment = function (func, onException, _this) {
       currentValues = boundValues;
       var ret = func.apply(_this, _.toArray(arguments));
     } catch (e) {
+      // note: callback-hook currently relies on the fact that if onException
+      // throws in the browser, the wrapped call throws.
       onException(e);
     } finally {
       currentValues = savedValues;
     }
     return ret;
   };
+};
+
+Meteor._nodeCodeMustBeInFiber = function () {
+  // no-op on browser
 };

packages/meteor/dynamics_nodejs.js
@@ -4,9 +4,13 @@ var Fiber = Npm.require('fibers');
 
 var nextSlot = 0;
 
-var noFiberMessage = "Meteor code must always run within a Fiber. " +
-                     "Try wrapping callbacks that you pass to non-Meteor " +
-                     "libraries with Meteor.bindEnvironment.";
+Meteor._nodeCodeMustBeInFiber = function () {
+  if (!Fiber.current) {
+    throw new Error("Meteor code must always run within a Fiber. " +
+                    "Try wrapping callbacks that you pass to non-Meteor " +
+                    "libraries with Meteor.bindEnvironment.");
+  }
+};
 
 Meteor.EnvironmentVariable = function () {
   this.slot = nextSlot++;
@@ -14,16 +18,14 @@ Meteor.EnvironmentVariable = function () {
 
 _.extend(Meteor.EnvironmentVariable.prototype, {
   get: function () {
-    if (!Fiber.current)
-      throw new Error(noFiberMessage);
+    Meteor._nodeCodeMustBeInFiber();
 
     return Fiber.current._meteor_dynamics &&
       Fiber.current._meteor_dynamics[this.slot];
   },
 
   withValue: function (value, func) {
-    if (!Fiber.current)
-      throw new Error(noFiberMessage);
+    Meteor._nodeCodeMustBeInFiber();
 
     if (!Fiber.current._meteor_dynamics)
       Fiber.current._meteor_dynamics = [];
@@ -61,8 +63,7 @@ _.extend(Meteor.EnvironmentVariable.prototype, {
 // callback, and when an exception is raised a debug message will be
 // printed with the description.
 Meteor.bindEnvironment = function (func, onException, _this) {
-  if (!Fiber.current)
-    throw new Error(noFiberMessage);
+  Meteor._nodeCodeMustBeInFiber();
 
   var boundValues = _.clone(Fiber.current._meteor_dynamics || []);
 
@@ -87,6 +88,9 @@ Meteor.bindEnvironment = function (func, onException, _this) {
         Fiber.current._meteor_dynamics = _.clone(boundValues);
         var ret = func.apply(_this, args);
       } catch (e) {
+        // note: callback-hook currently relies on the fact that if onException
+        // throws and you were originally calling the wrapped callback from
+        // within a Fiber, the wrapped call throws.
         onException(e);
       } finally {
         Fiber.current._meteor_dynamics = savedValues;