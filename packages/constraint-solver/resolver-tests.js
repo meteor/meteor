8fcc9e41b93b2c5cff9fe26faa821f960988ce9c
Slava Kim <slava@meteor.com>
2014-03-18 17:06:39 -0700

    A test for locked version cost function

--- packages/constraint-solver/resolver-tests.js
+++ packages/constraint-solver/resolver-tests.js
@@ -38,7 +38,7 @@ Tinytest.add("constraint solver - resolver, get exact deps", function (test) {
   test.equal(resolver.resolve(["A"]), [A100, B100, C100, D100, E100, F100]);
 });
 
-Tinytest.add("constraint solver - resolver, cost function", function (test) {
+Tinytest.add("constraint solver - resolver, cost function - pick latest", function (test) {
   var resolver = new ConstraintSolver.Resolver();
   var A100 = new ConstraintSolver.UnitVersion("A", "1.0.0", "1.0.0");
   var A110 = new ConstraintSolver.UnitVersion("A", "1.1.0", "1.0.0");
@@ -72,6 +72,44 @@ Tinytest.add("constraint solver - resolver, cost function", function (test) {
   });
 
   test.equal(AnBSolution, [A100, B100, C120]);
+});
+
+Tinytest.add("constraint solver - resolver, cost function - avoid upgrades", function (test) {
+  var resolver = new ConstraintSolver.Resolver();
+  var A100 = new ConstraintSolver.UnitVersion("A", "1.0.0", "1.0.0");
+  var A110 = new ConstraintSolver.UnitVersion("A", "1.1.0", "1.0.0");
+  var B100 = new ConstraintSolver.UnitVersion("B", "1.0.0", "1.0.0");
+  var B110 = new ConstraintSolver.UnitVersion("B", "1.1.0", "1.0.0");
+  var C100 = new ConstraintSolver.UnitVersion("C", "1.0.0", "1.0.0");
+
+  resolver.addUnitVersion(A100);
+  resolver.addUnitVersion(A110);
+  resolver.addUnitVersion(B100);
+  resolver.addUnitVersion(B110);
+  resolver.addUnitVersion(C100);
+
+  A100.addDependency("B");
+  A100.addConstraint(resolver.getConstraint("B", "1.1.0"));
+  A110.addDependency("C");
+  A110.addConstraint(resolver.getConstraint("C", "1.0.0"));
+
+  // We had one dependency on B and the previous run of resolver told us to us
+  // B@1.0.0. Now we are adding the package A in a conservative manner. The
+  // constraint solver should keep B from upgrading by picking a newer version
+  // of A that uses C.
+  var lockedVersions = [B100];
+  var solution = resolver.resolve(["A", "B"], [], [], {
+    costFunction: function (choices) {
+      return _.reduce(choices, function (sum, uv) {
+        var lockedVersion = _.find(lockedVersions, function (luv) { return luv.name === uv.name; });
+        if (! lockedVersion || lockedVersion === uv)
+          return sum;
+        return sum + 100;
+      }, 0);
+    }
+  });
+
+  test.equal(solution, [A110, B100, C100]);
 });
 
 function semver2number (semverStr) {
