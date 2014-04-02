cedad2f2ceee493f21deaf40f0f3323c98696a84
Justin SB <justin@fathomdb.com>
2014-04-02 12:59:10 -0400

    Revert minimongo generating ids using randomStream (we don't need it)

--- packages/minimongo/minimongo.js
+++ packages/minimongo/minimongo.js
@@ -508,8 +508,8 @@ LocalCollection.prototype.insert = function (doc, callback) {
   if (!_.has(doc, '_id')) {
     // if you really want to use ObjectIDs, set this global.
     // Meteor.Collection specifies its own ids and does not use this code.
-    doc._id = LocalCollection._useOID ? new LocalCollection._ObjectID(DDP.RandomStreams.makeMongoOid(self.name))
-                                      : DDP.RandomStreams.makeCollectionId(self.name);
+    doc._id = LocalCollection._useOID ? new LocalCollection._ObjectID()
+                                      : Random.id();
   }
   var id = doc._id;
 
