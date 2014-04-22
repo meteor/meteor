9d88e2b5c23344f1ea24cb3dde352d42540798df
David Greenspan <dgreenspan@alum.mit.edu>
2014-04-22 13:47:57 -0700

    Use refactored html.js in "old" ui-package Blaze
    Some failing tests, which are hopefully mostly just tests of the modified code that need to be ported.
    
    The main work was replacing the use of pseudo-attributes $special and $dynamic with the new HTML.Attrs(...) / array representation.  Some functions were also rewritten to use Visitors (`toJS`, old `toHTML`/`toText`/`evaluateAttributes`, `replaceSpecials` in the compiler, but not `optimize` yet or `materialize`).
    
    Create the "blaze-tools" package to hold toJS and other functions that are useful for template parsers/compilers that need to read or generate JS.

--- html.js
+++ html.js
@@ -1,4 +1,9 @@
 
+HTML = {};
+
+var IDENTITY = function (x) { return x; };
+var SLICE = Array.prototype.slice;
+
 // Tag instances are `instanceof HTML.Tag`.
 //
 // Tag objects should be considered immutable.
@@ -8,6 +13,7 @@ HTML.Tag = function () {};
 HTML.Tag.prototype.tagName = ''; // this will be set per Tag subclass
 HTML.Tag.prototype.attrs = null;
 HTML.Tag.prototype.children = Object.freeze ? Object.freeze([]) : [];
+HTML.Tag.prototype.htmljsType = HTML.Tag.htmljsType = ['Tag'];
 
 // Given "p", create and assign `HTML.P` if it doesn't already exist.
 // Then return it.  `tagName` must have proper case (usually all lowercase).
@@ -39,18 +45,28 @@ var makeTagConstructor = function (tagName) {
 
     var i = 0;
     var attrs = arguments.length && arguments[0];
-    if (attrs && (typeof attrs === 'object') &&
-        (attrs.constructor === Object)) {
-      instance.attrs = attrs;
-      i++;
+    if (attrs && (typeof attrs === 'object')) {
+      if (attrs.constructor === Object) {
+        instance.attrs = attrs;
+        i++;
+      } else if (attrs instanceof HTML.Attrs) {
+        var array = attrs.value;
+        if (array.length === 1) {
+          instance.attrs = array[0];
+        } else if (array.length > 1) {
+          instance.attrs = array;
+        }
+        i++;
+      }
     }
+
 
     // If no children, don't create an array at all, use the prototype's
     // (frozen, empty) array.  This way we don't create an empty array
     // every time someone creates a tag without `new` and this constructor
     // calls itself with no arguments (above).
     if (i < arguments.length)
-      instance.children = Array.prototype.slice.call(arguments, i);
+      instance.children = SLICE.call(arguments, i);
 
     return instance;
   };
@@ -73,6 +89,7 @@ var CharRef = HTML.CharRef = function (attrs) {
   this.html = attrs.html;
   this.str = attrs.str;
 };
+CharRef.prototype.htmljsType = CharRef.htmljsType = ['CharRef'];
 
 var Comment = HTML.Comment = function (value) {
   if (! (this instanceof Comment))
@@ -86,9 +103,97 @@ var Comment = HTML.Comment = function (value) {
   // Kill illegal hyphens in comment value (no way to escape them in HTML)
   this.sanitizedValue = value.replace(/^-|--+|-$/g, '');
 };
+Comment.prototype.htmljsType = Comment.htmljsType = ['Comment'];
 
+var Raw = HTML.Raw = function (value) {
+  if (! (this instanceof Raw))
+    // called without `new`
+    return new Raw(value);
 
-//---------- KNOWN ELEMENTS
+  if (typeof value !== 'string')
+    throw new Error('HTML.Raw must be constructed with a string');
+
+  this.value = value;
+};
+Raw.prototype.htmljsType = Raw.htmljsType = ['Raw'];
+
+// Not an HTMLjs node, but a wrapper to pass multiple attrs dictionaries
+// to a tag (for the purpose of implementing dynamic attributes).
+var Attrs = HTML.Attrs = function (/*attrs dictionaries*/) {
+  // Work with or without `new`.  If not called with `new`,
+  // perform instantiation by recursively calling this constructor.
+  // We can't pass varargs, so pass no args.
+  var instance = (this instanceof Attrs) ? this : new Attrs;
+
+  instance.value = SLICE.call(arguments);
+
+  return instance;
+};
+
+HTML.isArray = function (x) {
+  return (x instanceof Array);
+};
+
+HTML.isNully = function (node) {
+  if (node == null)
+    // null or undefined
+    return true;
+
+  if (HTML.isArray(node)) {
+    // is it an empty array or an array of all nully items?
+    for (var i = 0; i < node.length; i++)
+      if (! HTML.isNully(node[i]))
+        return false;
+    return true;
+  }
+
+  return false;
+};
+
+// The HTML spec and the DOM API (in particular `setAttribute`) have different
+// definitions of what characters are legal in an attribute.  The HTML
+// parser is extremely permissive (allowing, for example, `<a %=%>`), while
+// `setAttribute` seems to use something like the XML grammar for names (and
+// throws an error if a name is invalid, making that attribute unsettable).
+// If we knew exactly what grammar browsers used for `setAttribute`, we could
+// include various Unicode ranges in what's legal.  For now, allow ASCII chars
+// that are known to be valid XML, valid HTML, and settable via `setAttribute`:
+//
+// * Starts with `:`, `_`, `A-Z` or `a-z`
+// * Consists of any of those plus `-`, `.`, and `0-9`.
+//
+// See <http://www.w3.org/TR/REC-xml/#NT-Name> and
+// <http://dev.w3.org/html5/markup/syntax.html#syntax-attributes>.
+HTML.isValidAttributeName = function (name) {
+  return /^[:_A-Za-z][:_A-Za-z0-9.\-]*/.test(name);
+};
+
+// If `attrs` is an array of attributes dictionaries, combines them
+// into one.  Removes attributes that are "nully."
+HTML.flattenAttributes = function (attrs) {
+  if (! attrs)
+    return attrs;
+
+  var isArray = HTML.isArray(attrs);
+  if (attrs.length === 0)
+    return null;
+
+  var result = {};
+  for (var i = 0, N = (isArray ? attrs.length : 1); i < N; i++) {
+    var oneAttrs = (isArray ? attrs[i] : attrs);
+    for (var name in oneAttrs) {
+      if (! HTML.isValidAttributeName(name))
+        throw new Error("Illegal HTML attribute name: " + name);
+      var value = oneAttrs[name];
+      if (! HTML.isNully(value))
+        result[name] = value;
+    }
+  }
+
+  return result;
+};
+
+////////////////////////////// KNOWN ELEMENTS
 
 // These lists of known elements are public.  You can use them, for example, to
 // write a helper that determines the proper case for an SVG element name.
@@ -152,139 +257,329 @@ HTML.getSymbolName = function (tagName) {
 for (var i = 0; i < HTML.knownElementNames.length; i++)
   HTML.ensureTag(HTML.knownElementNames[i]);
 
-////////////////////////////////////////////////////////////////////////////////
 
-callReactiveFunction = function (func) {
-  var result;
-  var cc = Deps.currentComputation;
-  var h = Deps.autorun(function (c) {
-    result = func();
-  });
-  h.onInvalidate(function () {
-    if (cc)
-      cc.invalidate();
-  });
-  if (Deps.active) {
-    Deps.onInvalidate(function () {
-      h.stop();
-      func.stop && func.stop();
-    });
-  } else {
-    h.stop();
-    func.stop && func.stop();
-  }
-  return result;
-};
+////////////////////////////// VISITORS
 
-stopWithLater = function (instance) {
-  if (instance.materialized && instance.materialized.isWith) {
-    if (Deps.active) {
-      instance.materialized();
-    } else {
-      if (instance.data) // `UI.With`
-        instance.data.stop();
-      else if (instance.v) // `Spacebars.With`
-        instance.v.stop();
-    }
-  }
-};
-
-// Call all functions and instantiate all components, when fine-grained
-// reactivity is not needed (for example, in attributes).
-HTML.evaluate = function (node, parentComponent) {
-  if (node == null) {
-    return node;
-  } else if (typeof node === 'function') {
-    return HTML.evaluate(callReactiveFunction(node), parentComponent);
-  } else if (node instanceof Array) {
-    var result = [];
-    for (var i = 0; i < node.length; i++)
-      result.push(HTML.evaluate(node[i], parentComponent));
-    return result;
-  } else if (typeof node.instantiate === 'function') {
-    // component
-    var instance = node.instantiate(parentComponent || null);
-    var content = instance.render('STATIC');
-    stopWithLater(instance);
-    return HTML.evaluate(content, instance);
-  }  else if (node instanceof HTML.Tag) {
-    var newChildren = [];
-    for (var i = 0; i < node.children.length; i++)
-      newChildren.push(HTML.evaluate(node.children[i], parentComponent));
-    var newTag = HTML.getTag(node.tagName).apply(null, newChildren);
-    newTag.attrs = {};
-    for (var k in node.attrs)
-      newTag.attrs[k] = HTML.evaluate(node.attrs[k], parentComponent);
-    return newTag;
-  } else {
-    return node;
-  }
-};
-
-var extendAttrs = function (tgt, src, parentComponent) {
+// _assign is like _.extend or the upcoming Object.assign.
+// Copy src's own, enumerable properties onto tgt and return
+// tgt.
+var _hasOwnProperty = Object.prototype.hasOwnProperty;
+var _assign = function (tgt, src) {
   for (var k in src) {
-    if (k === '$dynamic')
-      continue;
-    if (! HTML.isValidAttributeName(k))
-      throw new Error("Illegal HTML attribute name: " + k);
-    var value = HTML.evaluate(src[k], parentComponent);
-    if (! HTML.isNully(value))
-      tgt[k] = value;
+    if (_hasOwnProperty.call(src, k))
+      tgt[k] = src[k];
   }
+  return tgt;
 };
 
-// Process the `attrs.$dynamic` directive, if present, returning the final
-// attributes dictionary.  The value of `attrs.$dynamic` must be an array
-// of attributes dictionaries or functions returning attribute dictionaries.
-// These attributes are used to extend `attrs` as long as they are non-nully.
-// All attributes are "evaluated," calling functions and instantiating
-// components.
-HTML.evaluateAttributes = function (attrs, parentComponent) {
-  if (! attrs)
-    return attrs;
 
-  var result = {};
-  extendAttrs(result, attrs, parentComponent);
+HTML.Visitor = function () {};
 
-  if ('$dynamic' in attrs) {
-    if (! (attrs.$dynamic instanceof Array))
-      throw new Error("$dynamic must be an array");
-    // iterate over attrs.$dynamic, calling each element if it
-    // is a function and then using it to extend `result`.
-    var dynamics = attrs.$dynamic;
-    for (var i = 0; i < dynamics.length; i++) {
-      var moreAttrs = dynamics[i];
-      if (typeof moreAttrs === 'function')
-        moreAttrs = moreAttrs();
-      extendAttrs(result, moreAttrs, parentComponent);
+HTML.Visitor.extend = function (options) {
+  var curType = this;
+  var subType = function HTMLVisitorSubtype(/*arguments*/) {
+    HTML.Visitor.apply(this, arguments);
+  };
+  subType.prototype = new curType;
+  subType.extend = curType.extend;
+  if (options)
+    _assign(subType.prototype, options);
+  return subType;
+};
+
+_assign(HTML.Visitor.prototype, {
+  visit: function (content/*, ...*/) {
+    if (content == null)
+      // null or undefined.
+      return this.visitNull.apply(this, arguments);
+
+    if (typeof content === 'object') {
+      if (content.htmljsType) {
+        switch (content.htmljsType) {
+        case HTML.Tag.htmljsType:
+          return this.visitTag.apply(this, arguments);
+        case CharRef.htmljsType:
+          return this.visitCharRef.apply(this, arguments);
+        case Comment.htmljsType:
+          return this.visitComment.apply(this, arguments);
+        case Raw.htmljsType:
+          return this.visitRaw.apply(this, arguments);
+        default:
+          throw new Error("Unknown htmljs type: " + content.htmljsType);
+        }
+      }
+
+      if (HTML.isArray(content))
+        return this.visitArray.apply(this, arguments);
+
+      return this.visitObject.apply(this, arguments);
+
+    } else if ((typeof content === 'string') ||
+               (typeof content === 'boolean') ||
+               (typeof content === 'number')) {
+      return this.visitPrimitive.apply(this, arguments);
+
+    } else if (typeof content === 'function') {
+      return this.visitFunction.apply(this, arguments);
     }
+
+    throw new Error("Unexpected object in htmljs: " + content);
+
+  },
+  visitNull: function (nullOrUndefined/*, ...*/) {},
+  visitPrimitive: function (stringBooleanOrNumber/*, ...*/) {},
+  visitArray: function (array/*, ...*/) {},
+  visitComment: function (comment/*, ...*/) {},
+  visitCharRef: function (charRef/*, ...*/) {},
+  visitRaw: function (raw/*, ...*/) {},
+  visitTag: function (tag/*, ...*/) {},
+  visitObject: function (obj/*, ...*/) {
+    throw new Error("Unexpected object in htmljs: " + obj);
+  },
+  visitFunction: function (obj/*, ...*/) {
+    throw new Error("Unexpected function in htmljs: " + obj);
   }
+});
 
-  return result;
+HTML.TransformingVisitor = HTML.Visitor.extend({
+  visitNull: IDENTITY,
+  visitPrimitive: IDENTITY,
+  visitArray: function (array/*, ...*/) {
+    var argsCopy = SLICE.call(arguments);
+    var result = array;
+    for (var i = 0; i < array.length; i++) {
+      var oldItem = array[i];
+      argsCopy[0] = oldItem;
+      var newItem = this.visit.apply(this, argsCopy);
+      if (newItem !== oldItem) {
+        // copy `array` on write
+        if (result === array)
+          result = array.slice();
+        result[i] = newItem;
+      }
+    }
+    return result;
+  },
+  visitComment: IDENTITY,
+  visitCharRef: IDENTITY,
+  visitRaw: IDENTITY,
+  visitObject: IDENTITY,
+  visitTag: function (tag/*, ...*/) {
+    var oldChildren = tag.children;
+    var argsCopy = SLICE.call(arguments);
+    argsCopy[0] = oldChildren;
+    var newChildren = this.visit.apply(this, argsCopy);
+
+    var oldAttrs = tag.attrs;
+    argsCopy[0] = oldAttrs;
+    var newAttrs = this.visitAttributes.apply(this, argsCopy);
+
+    if (newAttrs === oldAttrs && newChildren === oldChildren)
+      return tag;
+
+    var newTag = HTML.getTag(tag.tagName).apply(null, newChildren);
+    newTag.attrs = newAttrs;
+    return newTag;
+  },
+  visitAttributes: function (attrs/*, ...*/) {
+    if (HTML.isArray(attrs)) {
+      var argsCopy = SLICE.call(arguments);
+      var result = attrs;
+      for (var i = 0; i < attrs.length; i++) {
+        var oldItem = attrs[i];
+        argsCopy[0] = oldItem;
+        var newItem = this.visitAttributes.apply(this, argsCopy);
+        if (newItem !== oldItem) {
+          // copy on write
+          if (result === attrs)
+            result = attrs.slice();
+          result[i] = newItem;
+        }
+      }
+      return result;
+    }
+
+    var oldAttrs = attrs;
+    var newAttrs = oldAttrs;
+    if (oldAttrs) {
+      var attrArgs = [null, null];
+      attrArgs.push.apply(attrArgs, arguments);
+      for (var k in oldAttrs) {
+        var oldValue = oldAttrs[k];
+        attrArgs[0] = k;
+        attrArgs[1] = oldValue;
+        var newValue = this.visitAttribute.apply(this, attrArgs);
+        if (newValue !== oldValue) {
+          // copy on write
+          if (newAttrs === oldAttrs)
+            newAttrs = _assign({}, oldAttrs);
+          newAttrs[k] = newValue;
+        }
+      }
+    }
+
+    return newAttrs;
+  },
+  visitAttribute: function (name, value, tag/*, ...*/) {
+    var args = SLICE.call(arguments, 2);
+    args[0] = value;
+    return this.visit.apply(this, args);
+  }
+});
+
+////////////////////////////// TOHTML
+
+// Escaping modes for outputting text when generating HTML.
+HTML.TEXTMODE = {
+  STRING: 1,
+  RCDATA: 2,
+  ATTRIBUTE: 3
 };
 
-HTML.Tag.prototype.evaluateAttributes = function (parentComponent) {
-  return HTML.evaluateAttributes(this.attrs, parentComponent);
+HTML.ToTextVisitor = HTML.Visitor.extend({
+  visitNull: function (nullOrUndefined) {
+    return '';
+  },
+  visitPrimitive: function (stringBooleanOrNumber) {
+    var str = String(stringBooleanOrNumber);
+    if (this.textMode === HTML.TEXTMODE.RCDATA) {
+      return str.replace(/&/g, '&amp;').replace(/</g, '&lt;');
+    } else if (this.textMode === HTML.TEXTMODE.ATTRIBUTE) {
+      // escape `&` and `"` this time, not `&` and `<`
+      return str.replace(/&/g, '&amp;').replace(/"/g, '&quot;');
+    } else {
+      return str;
+    }
+  },
+  visitArray: function (array) {
+    var parts = [];
+    for (var i = 0; i < array.length; i++)
+      parts.push(this.visit(array[i]));
+    return parts.join('');
+  },
+  visitComment: function (comment) {
+    throw new Error("Can't have a comment here");
+  },
+  visitCharRef: function (charRef) {
+    if (this.textMode === HTML.TEXTMODE.RCDATA ||
+        this.textMode === HTML.TEXTMODE.ATTRIBUTE) {
+      return charRef.html;
+    } else {
+      return charRef.str;
+    }
+  },
+  visitRaw: function (raw) {
+    return raw.value;
+  },
+  visitTag: function (tag) {
+    // Really we should just disallow Tags here.  However, at the
+    // moment it's useful to stringify any HTML we find.  In
+    // particular, when you include a template within `{{#markdown}}`,
+    // we render the template as text, and since there's currently
+    // no way to make the template be *parsed* as text (e.g. `<template
+    // type="text">`), we hackishly support HTML tags in markdown
+    // in templates by parsing them and stringifying them.
+    return this.visit(this.toHTML(tag));
+  },
+  visitObject: function (x) {
+    throw new Error("Unexpected object in htmljs in toText: " + x);
+  },
+  toHTML: function (node) {
+    return HTML.toHTML(node);
+  }
+});
+
+HTML.toText = function (content, textMode) {
+  if (! textMode)
+    throw new Error("textMode required for HTML.toText");
+  if (! (textMode === HTML.TEXTMODE.STRING ||
+         textMode === HTML.TEXTMODE.RCDATA ||
+         textMode === HTML.TEXTMODE.ATTRIBUTE))
+    throw new Error("Unknown textMode: " + textMode);
+
+  var visitor = new HTML.ToTextVisitor;
+  visitor.textMode = textMode;
+
+  return visitor.visit(content);
 };
 
-HTML.Raw = function (value) {
-  if (! (this instanceof HTML.Raw))
-    // called without `new`
-    return new HTML.Raw(value);
+HTML.ToHTMLVisitor = HTML.Visitor.extend({
+  visitNull: function (nullOrUndefined) {
+    return '';
+  },
+  visitPrimitive: function (stringBooleanOrNumber) {
+    var str = String(stringBooleanOrNumber);
+    return str.replace(/&/g, '&amp;').replace(/</g, '&lt;');
+  },
+  visitArray: function (array) {
+    var parts = [];
+    for (var i = 0; i < array.length; i++)
+      parts.push(this.visit(array[i]));
+    return parts.join('');
+  },
+  visitComment: function (comment) {
+    return '<!--' + comment.sanitizedValue + '-->';
+  },
+  visitCharRef: function (charRef) {
+    return charRef.html;
+  },
+  visitRaw: function (raw) {
+    return raw.value;
+  },
+  visitTag: function (tag) {
+    var attrStrs = [];
 
-  if (typeof value !== 'string')
-    throw new Error('HTML.Raw must be constructed with a string');
+    var attrs = tag.attrs;
+    if (attrs) {
+      for (var k in attrs) {
+        var v = this.toText(attrs[k], HTML.TEXTMODE.ATTRIBUTE);
+        attrStrs.push(' ' + k + '="' + v + '"');
+      }
+    }
 
-  this.value = value;
-};
+    var tagName = tag.tagName;
+    var startTag = '<' + tagName + attrStrs.join('') + '>';
 
-HTML.EmitCode = function (value) {
-  if (! (this instanceof HTML.EmitCode))
-    // called without `new`
-    return new HTML.EmitCode(value);
+    var children = tag.children;
+    var childStrs = [];
+    var content;
+    if (tagName === 'textarea') {
 
-  if (typeof value !== 'string')
-    throw new Error('HTML.EmitCode must be constructed with a string');
+      for (var i = 0; i < children.length; i++)
+        childStrs.push(this.toText(children[i], HTML.TEXTMODE.RCDATA));
 
-  this.value = value;
+      content = childStrs.join('');
+      if (content.slice(0, 1) === '\n')
+        // TEXTAREA will absorb a newline, so if we see one, add
+        // another one.
+        content = '\n' + content;
+
+    } else {
+      for (var i = 0; i < children.length; i++)
+        childStrs.push(this.visit(children[i]));
+
+      content = childStrs.join('');
+    }
+
+    var result = startTag + content;
+
+    if (children.length || ! HTML.isVoidElement(tagName)) {
+      // "Void" elements like BR are the only ones that don't get a close
+      // tag in HTML5.  They shouldn't have contents, either, so we could
+      // throw an error upon seeing contents here.
+      result += '</' + tagName + '>';
+    }
+
+    return result;
+  },
+  visitObject: function (x) {
+    throw new Error("Unexpected object in htmljs in toHTML: " + x);
+  },
+  toText: function (node, textMode) {
+    return HTML.toText(node, textMode);
+  }
+});
+
+HTML.toHTML = function (content) {
+  return (new HTML.ToHTMLVisitor).visit(content);
 };
