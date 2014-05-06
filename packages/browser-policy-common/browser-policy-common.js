a8673d01cd395d516539f91c2a8112bde89c8fd6
Emily Stark <emily@meteor.com>
2014-05-05 22:22:45 -0700

    Set X-Content-Type-Options in browser-policy-content

--- packages/browser-policy-common/browser-policy-common.js
+++ packages/browser-policy-common/browser-policy-common.js
@@ -19,9 +19,29 @@ WebApp.connectHandlers.use(function (req, res, next) {
         BrowserPolicy.framing._constructXFrameOptions();
   var csp = BrowserPolicy.content &&
         BrowserPolicy.content._constructCsp();
-  if (xFrameOptions)
+  if (xFrameOptions) {
     res.setHeader("X-Frame-Options", xFrameOptions);
-  if (csp)
+  }
+  if (csp) {
     res.setHeader("Content-Security-Policy", csp);
+  }
+  next();
+});
+
+// We use `rawConnectHandlers` to set X-Content-Type-Options on all
+// requests, including static files.
+// XXX We should probably use `rawConnectHandlers` for X-Frame-Options
+// and Content-Security-Policy too, but let's make sure that doesn't
+// break anything first (e.g. the OAuth popup flow won't work well with
+// a CSP that disallows inline scripts).
+WebApp.rawConnectHandlers.use(function (req, res, next) {
+  if (BrowserPolicy._runningTest())
+    return next();
+
+  var contentTypeOptions = BrowserPolicy.content &&
+        BrowserPolicy.content._xContentTypeOptions();
+  if (contentTypeOptions) {
+    res.setHeader("X-Content-Type-Options", contentTypeOptions);
+  }
   next();
 });
--- packages/browser-policy/browser-policy-test.js
+++ packages/browser-policy/browser-policy-test.js
@@ -151,3 +151,9 @@ Tinytest.add("browser-policy - x-frame-options", function (test) {
     BrowserPolicy.framing.restrictToOrigin("bar.com");
   });
 });
+
+Tinytest.add("browser-policy - X-Content-Type-Options", function (test) {
+  test.equal(BrowserPolicy.content._xContentTypeOptions(), "nosniff");
+  BrowserPolicy.content.allowContentTypeSniffing();
+  test.equal(BrowserPolicy.content._xContentTypeOptions(), undefined);
+});
