0d767e746f70c1fd290305b703b1c4960d5cdc26
Avital Oliver <avital@thewe.net>
2014-05-08 16:38:35 -0700

    WIP: The beginning of a (failing) end-to-end test for stats reporting

---
+++ tools/tests/report-stats.js
@@ -0,0 +1,28 @@
+var selftest = require('../selftest.js');
+var Sandbox = selftest.Sandbox;
+
+selftest.define("report-stats", function () {
+  var s = new Sandbox;
+
+  run = s.run("create", "foo");
+  run.expectExit(0);
+  s.cd("foo");
+
+  // verify that identifier file exists for new apps
+  var identifier = s.read(".meteor/identifier");
+  selftest.expectEqual(!! identifier, true);
+  selftest.expectEqual(identifier.length > 0, true);
+
+  // verify that identifier file when running 'meteor bundle' on old
+  // apps
+  s.unlink(".meteor/identifier");
+  s.run("bundle", "foo.tar.gz");
+  identifier = s.read(".meteor/identifier");
+  selftest.expectEqual(!! identifier, true);
+  selftest.expectEqual(identifier.length > 0, true);
+
+  // TODO:
+  // - test that we actually send stats
+  // - opt out
+  // - test both the logged in state and the logged out state
+});
