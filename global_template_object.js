b6e952a266f933feb14f326364d653673970dbf9
David Greenspan <dgreenspan@alummit.edu>
2014-05-13 18:02:24 -0700

    Basic template inclusions work
    Events are not attached.
    
    contentBlock doesn’t work.

--- global_template_object.js
+++ global_template_object.js
@@ -2,12 +2,16 @@
 // to this object.
 Template = {};
 
-Template.__define__ = function (templateName, renderFunc) {
+Template.__define__ = function (templateName, templateFunc) {
   if (Template.hasOwnProperty(templateName))
     throw new Error("There are multiple templates named '" + templateName + "'. Each template needs a unique name.");
 
-  var templateClass = UI.Component2.extend({
-    render: renderFunc
+  var templateClass = UI.TemplateComponent.extend({
+    templateName: templateName,
+    constructor: function TemplateComponent() {
+      UI.TemplateComponent.apply(this, arguments);
+    },
+    renderTemplate: templateFunc
   });
 
   Template[templateName] = templateClass.prototype;
