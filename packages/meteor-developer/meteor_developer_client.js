f7b6aa47a6d23c39cb1698305aaeca487c8adc0c
Tom Coleman <tom@thesnail.org>
2014-05-28 13:23:04 +1000

    Added a `userEmail` option to MD accounts oauth.

--- packages/meteor-developer/meteor_developer_client.js
+++ packages/meteor-developer/meteor_developer_client.js
@@ -4,7 +4,13 @@ MeteorDeveloperAccounts = {};
 // @param credentialRequestCompleteCallback {Function} Callback function to call on
 //   completion. Takes one argument, credentialToken on success, or Error on
 //   error.
-var requestCredential = function (credentialRequestCompleteCallback) {
+var requestCredential = function (options, credentialRequestCompleteCallback) {
+  // support a callback without options
+  if (! credentialRequestCompleteCallback && typeof options === "function") {
+    credentialRequestCompleteCallback = options;
+    options = null;
+  }
+
   var config = ServiceConfiguration.configurations.findOne({
     service: 'meteor-developer'
   });
@@ -20,8 +26,12 @@ var requestCredential = function (credentialRequestCompleteCallback) {
         METEOR_DEVELOPER_URL + "/oauth2/authorize?" +
         "state=" + credentialToken +
         "&response_type=code&" +
-        "client_id=" + config.clientId +
-        "&redirect_uri=" + Meteor.absoluteUrl("_oauth/meteor-developer?close");
+        "client_id=" + config.clientId;
+
+  if (options && options.userEmail)
+    loginUrl += '&user_email=' + encodeURIComponent(options.userEmail);
+
+  loginUrl += "&redirect_uri=" + Meteor.absoluteUrl("_oauth/meteor-developer?close");
 
   OAuth.showPopup(
     loginUrl,
