5716db5049c84381a7a5e08831bb16e2bf2d464d
Andrew Mao <mizzao@gmail.com>
2014-06-02 18:09:34 -0400

    Use a try/catch block to check for localStorage - fixes #1291, fixes #1688.
    Accessing `window.localStorage` can immediately throw an exception, which needs to be caught.
    For more information, see http://www.w3.org/TR/webstorage/#dom-localstorage.

--- packages/localstorage/localstorage.js
+++ packages/localstorage/localstorage.js
@@ -1,33 +1,38 @@
 // Meteor._localStorage is not an ideal name, but we can change it later.
 
-if (window.localStorage) {
-  // Let's test to make sure that localStorage actually works. For example, in
-  // Safari with private browsing on, window.localStorage exists but actually
-  // trying to use it throws.
+// Accessing window.localStorage can immediately throw an error in IE (#1291).
+// For resiliency against this, we encase the below in a try/catch.
+try { 
+  if (window.localStorage) {
+    // Let's test to make sure that localStorage actually works. For example, in
+    // Safari with private browsing on, window.localStorage exists but actually
+    // trying to use it throws.
 
-  var key = '_localstorage_test_' + Random.id();
-  var retrieved;
-  try {
-    window.localStorage.setItem(key, key);
-    retrieved = window.localStorage.getItem(key);
-    window.localStorage.removeItem(key);
-  } catch (e) {
-    // ... ignore
-  }
-  if (key === retrieved) {
-    Meteor._localStorage = {
-      getItem: function (key) {
-        return window.localStorage.getItem(key);
-      },
-      setItem: function (key, value) {
-        window.localStorage.setItem(key, value);
-      },
-      removeItem: function (key) {
-        window.localStorage.removeItem(key);
-      }
-    };
+    var key = '_localstorage_test_' + Random.id();
+    var retrieved;
+    try {
+      window.localStorage.setItem(key, key);
+      retrieved = window.localStorage.getItem(key);
+      window.localStorage.removeItem(key);
+    } catch (e) {
+      // ... ignore
+    }
+    if (key === retrieved) {
+      Meteor._localStorage = {
+	getItem: function (key) {
+	  return window.localStorage.getItem(key);
+	},
+	setItem: function (key, value) {
+	  window.localStorage.setItem(key, value);
+	},
+	removeItem: function (key) {
+	  window.localStorage.removeItem(key);
+	}
+      };
+    }
   }
 }
+catch (e) {}
 
 if (!Meteor._localStorage) {
   Meteor._debug(
