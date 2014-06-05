6394261d51e61b85299b83fdf8d74b069619edc7
Slava Kim <slava@meteor.com>
2014-06-04 18:57:15 -0700

    Tests for CSS url rewrites fixed to the new implementation
    i.e. rewrite only style sheets taken from /packages/ folder

--- packages/minifiers/urlrewriting-tests.js
+++ packages/minifiers/urlrewriting-tests.js
@@ -4,8 +4,7 @@ Tinytest.add("minifiers - url rewriting when merging", function (test) {
     return "body { color: green; background: top center url(" + backgroundPath + ") black, bottom center url(" + backgroundPath + "); }"
   };
 
-  var filename = 'dir/subdir/style.css';
-  var parseOptions = { source: filename, position: true };
+  var parseOptions = { source: null, position: true };
 
   var t = function(relativeUrl, absoluteUrl, desc) {
     var ast1 = CssTools.parseCss(stylesheet(relativeUrl), parseOptions);
@@ -15,17 +14,30 @@ Tinytest.add("minifiers - url rewriting when merging", function (test) {
     test.equal(CssTools.stringifyCss(ast1), CssTools.stringifyCss(ast2), desc);
   };
 
-  t('../image.png', 'dir/image.png', 'parent directory');
-  t('./../image.png', 'dir/image.png', 'parent directory');
-  t('../subdir2/image.png', 'dir/subdir2/image.png', 'cousin directory');
+  parseOptions.source = 'packages/nameOfPackage/style.css';
+  t('../image.png', 'packages/image.png', 'parent directory');
+  t('./../image.png', 'packages/image.png', 'parent directory');
+  t('../nameOfPackage2/image.png', 'packages/nameOfPackage2/image.png', 'cousin directory');
   t('../../image.png', 'image.png', 'grand parent directory');
-  t('./image.png', 'dir/subdir/image.png', 'current directory');
-  t('./child/image.png', 'dir/subdir/child/image.png', 'child directory');
-  t('child/image.png', 'dir/subdir/child/image.png', 'child directory');
+  t('./image.png', 'packages/nameOfPackage/image.png', 'current directory');
+  t('./child/image.png', 'packages/nameOfPackage/child/image.png', 'child directory');
+  t('child/image.png', 'packages/nameOfPackage/child/image.png', 'child directory');
   t('/image.png', '/image.png', 'absolute url');
   t('"/image.png"', '"/image.png"', 'double quoted url');
   t("'/image.png'", "'/image.png'", 'single quoted url');
-  t('"./../image.png"', '"dir/image.png"', 'quoted parent directory');
+  t('"./../image.png"', '"packages/image.png"', 'quoted parent directory');
+  t('http://i.imgur.com/fBcdJIh.gif', 'http://i.imgur.com/fBcdJIh.gif', 'complete URL');
+  t('"http://i.imgur.com/fBcdJIh.gif"', '"http://i.imgur.com/fBcdJIh.gif"', 'complete quoted URL');
+  t('data:image/png;base64,iVBORw0K=', 'data:image/png;base64,iVBORw0K=', 'data URI');
+  t('http://', 'http://', 'malformed URL');
+
+  parseOptions.source = 'application/client/dir/other-style.css';
+  t('./image.png', '/image.png', 'base path is root');
+  t('./child/image.png', '/child/image.png', 'child directory from root');
+  t('child/image.png', '/child/image.png', 'child directory from root');
+  t('/image.png', '/image.png', 'absolute url');
+  t('"/image.png"', '"/image.png"', 'double quoted url');
+  t("'/image.png'", "'/image.png'", 'single quoted url');
   t('http://i.imgur.com/fBcdJIh.gif', 'http://i.imgur.com/fBcdJIh.gif', 'complete URL');
   t('"http://i.imgur.com/fBcdJIh.gif"', '"http://i.imgur.com/fBcdJIh.gif"', 'complete quoted URL');
   t('data:image/png;base64,iVBORw0K=', 'data:image/png;base64,iVBORw0K=', 'data URI');
