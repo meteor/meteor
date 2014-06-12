83132a921f659a9abccea0e853437c5f5932a70d
Matthew Arbesfeld <arbesfeld@gmail.com>
2014-06-11 17:25:31 -0700

    Remove unecessary import from less_tests

--- packages/less/package.js
+++ packages/less/package.js
@@ -14,6 +14,6 @@ Package._transitional_registerBuildPlugin({
 Package.on_test(function (api) {
   api.use(['test-helpers', 'tinytest', 'less', 'templating']);
   api.add_files(['less_tests.less', 'less_tests.js', 'less_tests.html',
-                 'less_tests.import.less', 'less_tests_empty.less'],
+                 'less_tests_empty.less'],
                 'client');
 });
