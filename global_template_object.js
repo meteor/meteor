c3b7707ed80dd6d034c6a31f2bddad106c0a9207
David Greenspan <dgreenspan@alum.mit.edu>
2014-06-26 18:23:31 -0700

    Implement lookup, more builtins
    Move lookup to templates, not views!

--- global_template_object.js
+++ global_template_object.js
@@ -11,9 +11,56 @@ Template.__define__ = function (templateName, templateFunc) {
     __render: templateFunc,
     __makeView: function (contentFunc, elseFunc) {
       var view = Blaze.View('Template.' + templateName, tmpl.__render);
-      view.templateContentFunc = contentFunc;
-      view.templateElseFunc = elseFunc;
+      view.template = tmpl;
+      view.templateContentBlock = { __makeView: function () {
+        return Blaze.View('(contentBlock)', contentFunc);
+      } };
+      view.templateElseBlock = { __makeView: function () {
+        return Blaze.View('(elseBlock)', elseFunc);
+      } };
       return view;
+    },
+    // Implements {{foo}} where `name` is "foo"
+    // and `component` is the component the tag is found in
+    // (the lexical "self," on which to look for methods).
+    // If a function is found, it is bound to the object it
+    // was found on.  Returns a function,
+    // non-function value, or null.
+    __lookup: function (name, options) {
+      var template = this;
+      var lookupTemplate = options && options.template;
+
+      if (/^\./.test(name)) {
+        // starts with a dot. must be a series of dots which maps to an
+        // ancestor of the appropriate height.
+        if (!/^(\.)+$/.test(name))
+          throw new Error("id starting with dot must be a series of dots");
+
+        var theWith = Blaze.getCurrentView('with');
+        for (var i = 1; (i < name.length) && theWith; i++)
+          theWith = Blaze.getParentView(theWith, 'with');
+
+        return (theWith ? theWith.dataVar.get() : null);
+
+      } else if (name in template) {
+        return Blaze._bindToCurrentDataIfIsFunction(template[name]);
+      } else if (lookupTemplate && _.has(Template, name)) {
+        return Template[name];
+      } else if (UI._globalHelpers[name]) {
+        return Blaze._bindToCurrentDataIfIsFunction(UI._globalHelpers[name]);
+      } else {
+        var data = Blaze.getCurrentData();
+        if (data)
+          return Blaze._bindIfIsFunction(data[name], data);
+      }
+      return null;
+    },
+    __lookupTemplate: function (name) {
+      var result = this.__lookup(name, {template:true});
+
+      if (! result)
+        throw new Error("No such template: " + name);
+      return result;
     }
   };
 
@@ -23,7 +70,11 @@ Template.__define__ = function (templateName, templateFunc) {
 // Define a template `Template.__body__` that renders its
 // `__contentParts`.
 Template.__define__('__body__', function () {
-  return Template.__body__.__contentParts;
+  var parts = Template.__body__.__contentParts;
+  // enable lookup by setting `view.template`
+  for (var i = 0; i < parts.length; i++)
+    parts[i].template = Template.__body__;
+  return parts;
 });
 Template.__body__.__contentParts = []; // array of Blaze.Views
 
@@ -37,6 +88,8 @@ var instantiateBody = function () {
   if (Template.__body__.__isInstantiated)
     return;
   Template.__body__.__isInstantiated = true;
-  Blaze.materializeView(Template.__body__.__makeView()).attach(document.body);
+  var view = Template.__body__.__makeView();
+  Template.__body__.__view = view;
+  Blaze.materializeView(view).attach(document.body);
 };
 Template.__body__.__instantiate = instantiateBody;
