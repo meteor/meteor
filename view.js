4a6b56539923c7853a20f594dab69fdf3efa907d
David Greenspan <dgreenspan@alum.mit.edu>
2014-06-30 16:37:13 -0700

    Blaze.isTemplate, Blaze.runTemplate

--- view.js
+++ view.js
@@ -178,6 +178,8 @@ Blaze._expandView = function (view, parentView) {
 Blaze.HTMLJSExpander = HTML.TransformingVisitor.extend();
 Blaze.HTMLJSExpander.def({
   visitObject: function (x) {
+    if (Blaze.isTemplate(x))
+      x = Blaze.runTemplate(x);
     if (x instanceof Blaze.View)
       return Blaze._expandView(x, this.parentView);
 
@@ -271,8 +273,31 @@ Blaze.withCurrentView = function (view, func) {
   }
 };
 
-Blaze.render = function (contentFunc) {
-  return Blaze.materializeView(Blaze.View('render', contentFunc));
+Blaze.isTemplate = function (t) {
+  return t && (typeof t.__makeView === 'function');
+};
+
+Blaze.runTemplate = function (t/*, args*/) {
+  if (! Blaze.isTemplate(t))
+    throw new Error("Not a template: " + t);
+  var restArgs = Array.prototype.slice.call(arguments, 1);
+  return t.__makeView.apply(t, restArgs);
+};
+
+Blaze.render = function (content, parentView) {
+  parentView = parentView || Blaze.currentView;
+
+  var view;
+  if (typeof content === 'function') {
+    view = Blaze.View('render', content);
+  } else if (Blaze.isTemplate(content)) {
+    view = Blaze.runTemplate(content);
+  } else {
+    if (! (content instanceof Blaze.View))
+      throw new Error("Expected a function, template, or View in Blaze.render");
+    view = content;
+  }
+  return Blaze.materializeView(view, parentView);
 };
 
 Blaze.toHTML = function (htmljs, parentView) {
