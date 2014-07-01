2d361b219f1f5f65b825dc575e43f3dab7013e10
Avital Oliver <avital@thewe.net>
2014-06-30 19:35:07 -0700

    also mv global_template_object.js templating.js in package.js

--- packages/templating/package.js
+++ packages/templating/package.js
@@ -22,7 +22,7 @@ Package.on_use(function (api) {
   // XXX would like to do the following only when the first html file
   // is encountered
 
-  api.add_files('global_template_object.js', 'client');
+  api.add_files('templating.js', 'client');
   api.export('Template', 'client');
 
   // html_scanner.js emits client code that calls Meteor.startup and
