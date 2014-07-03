34d30f52ce137ca018fe02ca3cac2d6d0a0f064e
David Greenspan <dgreenspan@alum.mit.edu>
2014-07-02 20:44:14 -0700

    Catch exceptions thrown from template helpers

--- packages/blaze/exceptions.js
+++ packages/blaze/exceptions.js
@@ -1,11 +1,6 @@
-// XXX This is dead code but we should probably still do something like this.
-// Change "Meteor UI" to "Blaze".  Actually, "Exception in Blaze" is cryptic
-// and misleading; better to make it clear the fault is in user code, as in
-// "Exception in 'created' callback" etc.
-
 var debugFunc;
 
-// Meteor UI calls into user code in many places, and it's nice to catch exceptions
+// We call into user code in many places, and it's nice to catch exceptions
 // propagated from user code immediately so that the whole system doesn't just
 // break.  Catching exceptions is easy; reporting them is hard.  This helper
 // reports exceptions.
@@ -22,7 +17,7 @@ var debugFunc;
 //
 // An optional second argument overrides the default message.
 
-reportUIException = function (e, msg) {
+Blaze.reportException = function (e, msg) {
   if (! debugFunc)
     // adapted from Deps
     debugFunc = function () {
@@ -34,5 +29,18 @@ reportUIException = function (e, msg) {
   // In Chrome, `e.stack` is a multiline string that starts with the message
   // and contains a stack trace.  Furthermore, `console.log` makes it clickable.
   // `console.log` supplies the space between the two arguments.
-  debugFunc()(msg || 'Exception in Meteor UI:', e.stack || e.message);
+  debugFunc()(msg || 'Exception caught in template:', e.stack || e.message);
+};
+
+Blaze.wrapCatchingExceptions = function (f, where) {
+  if (typeof f !== 'function')
+    return f;
+
+  return function () {
+    try {
+      return f.apply(this, arguments);
+    } catch (e) {
+      Blaze.reportException(e, 'Exception in ' + where + ':');
+    }
+  };
 };
