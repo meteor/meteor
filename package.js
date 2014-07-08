69db2527d04bb6e56c662e1693f6ad8a479fad9a
ekatek <ekate@meteor.com>
2014-07-07 20:50:50 -0700

    ripping out internal, name and test from package.js files

--- package.js
+++ package.js
@@ -1,9 +1,6 @@
 Package.describe({
-  name: "templating",
-  test: "templating-test",
   summary: "Allows templates to be defined in .html files",
-  version: '1.0.0',
-  internal: true
+  version: '1.0.0'
 });
 
 // Today, this package is closely intertwined with Handlebars, meaning
