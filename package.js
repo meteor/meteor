f6750a92d32954b5199c93b7f8513565810abfc5
ekatek <ekate@meteor.com>
2014-07-07 20:50:50 -0700

    ripping out internal, name and test from package.js files

--- package.js
+++ package.js
@@ -1,10 +1,6 @@
-
 Package.describe({
-  name: "html-tools",
-  test: "html-tools-test",
   summary: "Standards-compliant HTML tools",
-  version: '1.0.0',
-  internal: true
+  version: '1.0.0'
 });
 
 Package.on_use(function (api) {
