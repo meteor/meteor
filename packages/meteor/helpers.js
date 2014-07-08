e92164a97648979347ddc91e9c57967ee7589c77
Chris Mather <mather.chris@gmail.com>
2014-07-07 19:49:53 -0700

    Fix Meteor._inherits to copy Parent static properties
    Problem:
    Static properties of the parent function ("class") were not copied to the child
    function as expected. This is because the _.each underscore helper does not
    propertly iterate over function properties.
    
    Solution:
    Don't use _.each to copy parent properties to the child function. Instead,
    iterate directly and copy hasOwn properties to the child function.
    
    An alternative solution is to make the _.each helper work properly with
    functions in the underscore package.

--- packages/meteor/helpers.js
+++ packages/meteor/helpers.js
@@ -119,10 +119,13 @@ _.extend(Meteor, {
   //   _.extend(ClassB.prototype, { ... })
   // Inspired by CoffeeScript's `extend` and Google Closure's `goog.inherits`.
   _inherits: function (Child, Parent) {
-    // copy static fields
-    _.each(Parent, function (prop, field) {
-      Child[field] = prop;
-    });
+    // copy Parent static properties
+    for (var key in Parent) {
+      // make sure we only copy hasOwnProperty properties vs. prototype
+      // properties
+      if (_.has(Parent, key))
+        Child[key] = Parent[key];
+    }
 
     // a middle member of prototype chain: takes the prototype from the Parent
     var Middle = function () {
