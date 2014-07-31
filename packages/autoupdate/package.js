c5acfdf8c12b9c791bd4fb004e12df4b438a6f32
David Glasser <glasser@meteor.com>
2014-07-30 19:24:47 -0700

    add missing underscore dep

--- packages/autoupdate/package.js
+++ packages/autoupdate/package.js
@@ -6,7 +6,7 @@ Package.describe({
 Package.on_use(function (api) {
   api.use('webapp', 'server');
   api.use(['deps', 'retry'], 'client');
-  api.use(['livedata', 'mongo-livedata'], ['client', 'server']);
+  api.use(['livedata', 'mongo-livedata', 'underscore'], ['client', 'server']);
   api.use('deps', 'client');
   api.use('reload', 'client', {weak: true});
 
