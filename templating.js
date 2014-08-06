71baf16f26c3f1d9a568d1f4b2cee1ac829a5657
David Greenspan <dgreenspan@alum.mit.edu>
2014-08-06 16:05:27 -0700

    Rename Template.__create__, __define__, and others
    * `Template.__create__` is now `new Template`
    * `Template = Blaze.Template`
    * `Template` is a constructor function
    * `Template.foo instanceof Template` (but keep `Blaze.isTemplate`)
    * No more `Blaze.runTemplate` (use `template.constructView`)
    * `constructView` instead of `__makeView`
    * template objects have `__kind` and `__render`
    * No `__viewName`, `__templateName`
    * `Template.__body__` is now `Template._body_` (and renamed methods)
    * No `Template.__lookup__`, instead `(name in Template) && (Template[name] instance of Template)`
    * `Template.__define__` replaced by `Template.__assign(name, template)`

--- templating.js
+++ templating.js
@@ -1,61 +1,18 @@
-// Create an empty template object. Packages and apps add templates on
-// to this object.
-Template = {};
 
-// `Template` is not a function so this is not a real function prototype,
-// but it is used as the prototype of all `Template.foo` objects.
-// Naming a template "prototype" will cause an error.
-Template.prototype = (function () {
-  // IE 8 exposes function names in the enclosing scope, so
-  // use this IIFE to catch it.
-  return (function Template() {}).prototype;
-})();
 
-Template.prototype.helpers = function (dict) {
-  for (var k in dict)
-    this[k] = dict[k];
-};
+// Packages and apps add templates on to this object.
+Template = Blaze.Template;
 
-Template.__updateTemplateInstance = function (view) {
-  // Populate `view.templateInstance.{firstNode,lastNode,data}`
-  // on demand.
-  var tmpl = view._templateInstance;
-  if (! tmpl) {
-    tmpl = view._templateInstance = {
-      $: function (selector) {
-        if (! view.domrange)
-          throw new Error("Can't use $ on component with no DOM");
-        return view.domrange.$(selector);
-      },
-      findAll: function (selector) {
-        return Array.prototype.slice.call(this.$(selector));
-      },
-      find: function (selector) {
-        var result = this.$(selector);
-        return result[0] || null;
-      },
-      data: null,
-      firstNode: null,
-      lastNode: null,
-      autorun: function (f) {
-        return view.autorun(f);
-      },
-      __view__: view
-    };
+// Like `Template[name] = template` but also checks for duplicates
+// and illegal template names that won't work.
+Template.__assign = function (name, template) {
+  if (name in Template) {
+    if (Template[name] instanceof Template)
+      throw new Error("There are multiple templates named '" + name + "'. Each template needs a unique name.");
+    throw new Error("This template name is reserved: " + name);
   }
 
-  tmpl.data = Blaze.getViewData(view);
-
-  if (view.domrange && !view.isDestroyed) {
-    tmpl.firstNode = view.domrange.firstNode();
-    tmpl.lastNode = view.domrange.lastNode();
-  } else {
-    // on 'created' or 'destroyed' callbacks we don't have a DomRange
-    tmpl.firstNode = null;
-    tmpl.lastNode = null;
-  }
-
-  return tmpl;
+  Template[name] = template;
 };
 
 UI._templateInstance = function () {
@@ -63,159 +20,44 @@ UI._templateInstance = function () {
   if (! templateView)
     throw new Error("No current template");
 
-  return Template.__updateTemplateInstance(templateView);
+  return Template.updateTemplateInstance(templateView);
 };
 
-Template.prototype.events = function (eventMap) {
-  var template = this;
-  template.__eventMaps = (template.__eventMaps || []);
-  var eventMap2 = {};
-  for (var k in eventMap) {
-    eventMap2[k] = (function (k, v) {
-      return function (event/*, ...*/) {
-        var view = this; // passed by EventAugmenter
-        var data = Blaze.getElementData(event.currentTarget);
-        if (data == null)
-          data = {};
-        var args = Array.prototype.slice.call(arguments);
-        var tmplInstance = Template.__updateTemplateInstance(view);
-        args.splice(1, 0, tmplInstance);
-        return v.apply(data, args);
-      };
-    })(k, eventMap[k]);
-  }
-
-  template.__eventMaps.push(eventMap2);
-};
-
-Template.prototype.__makeView = function (contentFunc, elseFunc) {
-  var template = this;
-  var view = Blaze.View(this.__viewName, this.__render);
-  view.template = template;
-
-  view.templateContentBlock = (
-    contentFunc ? Template.__create__('(contentBlock)', contentFunc) : null);
-  view.templateElseBlock = (
-    elseFunc ? Template.__create__('(elseBlock)', elseFunc) : null);
-
-  if (template.__eventMaps ||
-      typeof template.events === 'object') {
-    view.onMaterialized(function () {
-      if (! template.__eventMaps &&
-          typeof template.events === "object") {
-        // Provide limited back-compat support for `.events = {...}`
-        // syntax.  Pass `template.events` to the original `.events(...)`
-        // function.  This code must run only once per template, in
-        // order to not bind the handlers more than once, which is
-        // ensured by the fact that we only do this when `__eventMaps`
-        // is falsy, and we cause it to be set now.
-        Template.prototype.events.call(template, template.events);
-      }
-
-      _.each(template.__eventMaps, function (m) {
-        Blaze._addEventMap(view, m, view);
-      });
-    });
-  }
-
-  if (template.__initView)
-    template.__initView(view);
-
-  if (template.created) {
-    view.onCreated(function () {
-      var inst = Template.__updateTemplateInstance(view);
-      template.created.call(inst);
-    });
-  }
-
-  if (template.rendered) {
-    view.onRendered(function () {
-      var inst = Template.__updateTemplateInstance(view);
-      template.rendered.call(inst);
-    });
-  }
-
-  if (template.destroyed) {
-    view.onDestroyed(function () {
-      var inst = Template.__updateTemplateInstance(view);
-      template.destroyed.call(inst);
-    });
-  }
-
-  return view;
-};
-
-var _hasOwnProperty = Object.prototype.hasOwnProperty;
-
-Template.__lookup__ = function (templateName) {
-  if (! _hasOwnProperty.call(Template, templateName))
-    return null;
-  var tmpl = Template[templateName];
-  if (Template.__isTemplate__(tmpl))
-    return tmpl;
-  return null;
-};
-
-Template.__create__ = function (viewName, templateFunc, initView) {
-  var tmpl = new Template.prototype.constructor;
-  tmpl.__viewName = viewName;
-  tmpl.__render = templateFunc;
-  if (initView)
-    tmpl.__initView = initView;
-
-  return tmpl;
-};
-
-Template.__define__ = function (templateName, templateFunc) {
-  if (_hasOwnProperty.call(Template, templateName)) {
-    if (Template[templateName].__makeView)
-      throw new Error("There are multiple templates named '" + templateName + "'. Each template needs a unique name.");
-    throw new Error("This template name is reserved: " + templateName);
-  }
-
-  var tmpl = Template.__create__('Template.' + templateName, templateFunc);
-  tmpl.__templateName = templateName;
-
-  Template[templateName] = tmpl;
-  return tmpl;
-};
-
-Template.__isTemplate__ = function (x) {
-  return x && x.__makeView;
-};
-
-// Define a template `Template.__body__` that renders its
-// `__contentParts`.
-Template.__define__('__body__', function () {
-  var parts = Template.__body__.__contentParts;
+// Define a template `Template._body_` that renders its
+// `contentViews`.  `<body>` tags (of which there may be
+// multiple) will have their contents added to it.
+Template.__assign('_body_', new Template('_body_', function () {
+  var parts = Template._body_.contentViews;
   // enable lookup by setting `view.template`
   for (var i = 0; i < parts.length; i++)
-    parts[i].template = Template.__body__;
+    parts[i].template = Template._body_;
   return parts;
-});
-Template.__body__.__contentParts = []; // array of Blaze.Views
+}));
+Template._body_.contentViews = []; // array of Blaze.Views
+Template._body_.view = null;
 
-// Define `Template.__body__.__instantiate()` as a function that
-// renders `Template.__body__` into `document.body`, at most once
-// (calling it a second time does nothing).  This function does
-// not use `this`, so you can safely call:
-// `Meteor.startup(Template.__body__.__instantiate)`.
-Template.__body__.__isInstantiated = false;
-var instantiateBody = function () {
-  if (Template.__body__.__isInstantiated)
-    return;
-  Template.__body__.__isInstantiated = true;
-  var range = Blaze.render(Template.__body__);
-  Template.__body__.__view = range.view;
-  range.attach(document.body);
+Template._body_.addContent = function (renderFunc) {
+  var kind = 'body_content_' + Template._body_.contentViews.length;
+
+  Template._body_.contentViews.push(Blaze.View(kind, renderFunc));
 };
-Template.__body__.__instantiate = instantiateBody;
 
+// This function does not use `this` and so it may be called
+// as `Meteor.startup(Template._body_.renderIntoDocument)`.
+Template._body_.renderToDocument = function () {
+  // Only do it once.
+  if (Template._body_.view)
+    return;
+
+  var range = UI.render(Template._body_);
+  Template._body_.view = range.view;
+  UI.insert(range, document.body);
+};
 
 // Renders a template (eg `Template.foo`), returning a DOMRange. The
 // range will keep updating reactively.
 UI.render = function (tmpl) {
-  if (! Template.__isTemplate__(tmpl))
+  if (! (tmpl instanceof Template))
     throw new Error("Template required here");
 
   return Blaze.render(tmpl);
@@ -223,7 +65,7 @@ UI.render = function (tmpl) {
 
 // Same as `UI.render` with a data context passed in.
 UI.renderWithData = function (tmpl, data) {
-  if (! Template.__isTemplate__(tmpl))
+  if (! (tmpl instanceof Template))
     throw new Error("Template required here");
   if (typeof data === 'function')
     throw new Error("Data argument can't be a function"); // XXX or can it?
@@ -261,4 +103,4 @@ UI.remove = function (range) {
   range.destroy();
 };
 
-UI.body = Template.__body__;
+UI.body = Template._body_;
