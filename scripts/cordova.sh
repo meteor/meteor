53f6e0890f7a8b41d75dce0b430cbc1e3ee88eac
Sashko Stubailo <sashko@mit.edu>
2014-08-12 15:23:27 -0700

    Made node point to local node

--- scripts/cordova.sh
+++ scripts/cordova.sh
@@ -13,10 +13,17 @@ cd "$ORIG_DIR"
 ANDROID_BUNDLE="$SCRIPT_DIR/android_bundle"
 DEV_BUNDLE="$SCRIPT_DIR/dev_bundle"
 
-export PATH=${ANDROID_BUNDLE}/android-sdk/tools:${ANDROID_BUNDLE}/android-sdk/platform-tools:${PATH};
+# add android stuff
+export PATH=${ANDROID_BUNDLE}/android-sdk/tools:${ANDROID_BUNDLE}/android-sdk/platform-tools:${PATH}
 
+# add ant
 export ANT_HOME=${ANDROID_BUNDLE}/apache-ant-1.9.4
 export PATH=${ANT_HOME}/bin:${PATH}
+
+# add node
+export PATH=${DEV_BUNDLE}/bin/node:${PATH}
+
+export NODE_PATH="${DEV_BUNDLE}/lib/node_modules"
 
 exec ${DEV_BUNDLE}/lib/node_modules/cordova/bin/cordova "$@"
 
