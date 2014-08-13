565468e04252e4a8caaf82454bb139d3704c742a
Sashko Stubailo <sashko@mit.edu>
2014-08-12 17:31:37 -0700

    The bundle doesn't work on linux if made on mac

--- scripts/generate-android-bundle.sh
+++ scripts/generate-android-bundle.sh
@@ -9,75 +9,73 @@ BUNDLE_VERSION="0.1"
 cd "`dirname "$0"`"/..
 CHECKOUT_DIR=`pwd`
 
-for UNAME in Linux Darwin; do
-    echo $UNAME
+UNAME = uname
 
-    DIR=`mktemp -d -t generate-android-bundle-XXXXXXXX`
-    trap 'rm -rf "$DIR" >/dev/null 2>&1' 0
+DIR=`mktemp -d -t generate-android-bundle-XXXXXXXX`
+trap 'rm -rf "$DIR" >/dev/null 2>&1' 0
 
-    echo BUILDING IN "$DIR"
+echo BUILDING IN "$DIR"
 
-    cd "$DIR"
-    chmod 755 .
-    umask 022
+cd "$DIR"
+chmod 755 .
+umask 022
 
-    # Download Android SDK
-    if [ "$UNAME" == "Linux" ]; then
-        # not guaranteed to have java yikes
-        # let's just see if they have it and prompt to install?
+# Download Android SDK
+if [ "$UNAME" == "Linux" ]; then
+    # not guaranteed to have java yikes
+    # let's just see if they have it and prompt to install?
 
-        curl -O http://dl.google.com/android/android-sdk_r23.0.2-linux.tgz
-        tar xzf android-sdk_r23.0.2-linux.tgz > /dev/null
-        rm android-sdk_r23.0.2-linux.tgz
+    curl -O http://dl.google.com/android/android-sdk_r23.0.2-linux.tgz
+    tar xzf android-sdk_r23.0.2-linux.tgz > /dev/null
+    rm android-sdk_r23.0.2-linux.tgz
 
-        mv android-sdk-linux android-sdk
+    mv android-sdk-linux android-sdk
 
-        curl -O s3.amazonaws.com/android-bundle/jre-7u67-linux-i586.gz
-        tar zxvf jre-7u67-linux-i586.gz > /dev/null
-        rm jre-7u67-linux-i586.gz
+    curl -O s3.amazonaws.com/android-bundle/jre-7u67-linux-i586.gz
+    tar zxvf jre-7u67-linux-i586.gz > /dev/null
+    rm jre-7u67-linux-i586.gz
 
-        mv jre1.7.0_67 jre
+    mv jre1.7.0_67 jre
 
-    else
-        curl -O http://dl.google.com/android/android-sdk_r23.0.2-macosx.zip
-        unzip android-sdk_r23.0.2-macosx.zip > /dev/null
-        rm android-sdk_r23.0.2-macosx.zip
+else
+    curl -O http://dl.google.com/android/android-sdk_r23.0.2-macosx.zip
+    unzip android-sdk_r23.0.2-macosx.zip > /dev/null
+    rm android-sdk_r23.0.2-macosx.zip
 
-        mv android-sdk-macosx android-sdk
-    fi
+    mv android-sdk-macosx android-sdk
+fi
 
-    {
-        curl -O http://www.motorlogy.com/apache//ant/binaries/apache-ant-1.9.4-bin.tar.gz
-        tar xzf apache-ant-1.9.4-bin.tar.gz
-        rm apache-ant-1.9.4-bin.tar.gz
+{
+    curl -O http://www.motorlogy.com/apache//ant/binaries/apache-ant-1.9.4-bin.tar.gz
+    tar xzf apache-ant-1.9.4-bin.tar.gz
+    rm apache-ant-1.9.4-bin.tar.gz
 
-        # the below asks for confirmation... echo y seems to work lol
-    
-        # platform tools
-        echo y | android-sdk/tools/android update sdk -t platform-tools -u
+    # the below asks for confirmation... echo y seems to work lol
 
-        # the platform that cordova likes
-        echo y | android-sdk/tools/android update sdk -t android-19 -u
+    # platform tools
+    echo y | android-sdk/tools/android update sdk -t platform-tools -u
 
-        # system image for android 19
-        echo y | android-sdk/tools/android update sdk -t sys-img-armeabi-v7a-android-19 --all -u
+    # the platform that cordova likes
+    echo y | android-sdk/tools/android update sdk -t android-19 -u
 
-        # build tools
-        echo y | android-sdk/tools/android update sdk -t "build-tools-20.0.0" -u
+    # system image for android 19
+    echo y | android-sdk/tools/android update sdk -t sys-img-armeabi-v7a-android-19 --all -u
 
-        # intel HAXM
-        # echo y | android-sdk/tools/android update sdk -t "extra-intel-Hardware_Accelerated_Execution_Manager" -u
-        # android-sdk/tools/android create avd -t 1 -n test
-    } &> /dev/null
+    # build tools
+    echo y | android-sdk/tools/android update sdk -t "build-tools-20.0.0" -u
 
-    echo BUNDLING
+    # intel HAXM
+    # echo y | android-sdk/tools/android update sdk -t "extra-intel-Hardware_Accelerated_Execution_Manager" -u
+    # android-sdk/tools/android create avd -t 1 -n test
+} &> /dev/null
 
-    cd "$DIR"
-    echo "${BUNDLE_VERSION}" > .bundle_version.txt
+echo BUNDLING
 
-    echo "going to save in: ${CHECKOUT_DIR}/android_bundle_${UNAME}_${BUNDLE_VERSION}.tar.gz"
+cd "$DIR"
+echo "${BUNDLE_VERSION}" > .bundle_version.txt
 
-    tar czf "${CHECKOUT_DIR}/android_bundle_${UNAME}_${BUNDLE_VERSION}.tar.gz" . &> /dev/null
+echo "going to save in: ${CHECKOUT_DIR}/android_bundle_${UNAME}_${BUNDLE_VERSION}.tar.gz"
 
-    echo DONE
-done
+tar czf "${CHECKOUT_DIR}/android_bundle_${UNAME}_${BUNDLE_VERSION}.tar.gz" . &> /dev/null
+
+echo DONE
