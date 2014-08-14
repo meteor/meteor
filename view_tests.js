b35f30bd8f110688c37687c3c26110aeabdad04d
David Greenspan <dgreenspan@alum.mit.edu>
2014-08-14 14:17:43 -0700

    Fix DOMRange onAttached; view.{first,last}Node()
    DOMRange#setMembers was causing DOMRange#onAttached callbacks to get called, masking a bug in onViewReady where DOMRange#attached was misspelled DOMRange#isAttached.

--- view_tests.js
+++ view_tests.js
@@ -32,7 +32,11 @@ if (Meteor.isClient) {
     test.isFalse(v.isAttached);
     test.equal(buf, 'c0r1');
     test.equal(canonicalizeHtml(div.innerHTML), "");
+    test.throws(function () { v.firstNode(); }, /View must be attached/);
+    test.throws(function () { v.lastNode(); }, /View must be attached/);
     Blaze.insert(v, div);
+    test.equal(typeof (v.firstNode().nodeType), "number");
+    test.equal(typeof (v.lastNode().nodeType), "number");
     test.isTrue(v.isRendered);
     test.isTrue(v.isAttached);
     test.equal(buf, 'c0r1');
@@ -48,6 +52,11 @@ if (Meteor.isClient) {
     Blaze.remove(v);
     test.equal(buf, 'c0r1y1r2y2d2');
     test.equal(canonicalizeHtml(div.innerHTML), "");
+
+    buf = "";
+    R.set("baz");
+    Deps.flush();
+    test.equal(buf, "");
   });
 
 }
