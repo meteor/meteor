b167e174ecc25782002960e6d6b03c9773683b25
Sashko Stubailo <sashko@mit.edu>
2014-08-20 20:09:06 -0700

    Add reload on resume package

---
+++ packages/reload-on-resume/package.js
@@ -0,0 +1,9 @@
+Package.describe({
+  summary: "On Cordova, only allow the app to reload when the app is resumed.",
+  version: '1.0.0'
+});
+
+Package.on_use(function (api) {
+  api.use(['reload', 'deps'], 'client.cordova');
+  api.add_files("reload-on-resume.js", 'client.cordova');
+});
