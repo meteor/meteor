meteor/meteor

commit 0760ffbc369c5dbaa1424546d6d71cc270c3c66e

Put additional 'no-upgrade' constraints only on root deps.
And depending on the `breaking` option, set different type of constraints.

packages/constraint-solver/constraint-solver.js
@@ -127,8 +127,18 @@ ConstraintSolver.PackagesResolver.prototype.resolve =
 
   var dc = self._splitDepsToConstraints(dependencies, constraints);
 
+  // Never allow to downgrade a version of a direct dependency in regards to the
+  // previous solution.
+  // Depending on whether the option `breaking` is set or not, allow only
+  // compatible upgrades or any upgrades.
   _.each(options.previousSolution, function (uv) {
-    dc.constraints.push(new ConstraintSolver.Constraint(uv.name, ">=" + uv.version));
+    // if not a root dependency, there is no 'no-upgrade' constraint
+    if (! _.contains(dependencies, uv.name))
+      return;
+
+    var constrType = options.breaking ? ">=" : "";
+    dc.constraints.push(
+      new ConstraintSolver.Constraint(uv.name, constrType + uv.version));
   });
 
   options.rootDependencies = dc.dependencies;

commit 55d1901729219367a643ccc89a6e9635da880fe1

Remove dormant "no-downgrade constraint" code
Also removes 'update --minor' and 'add --force'.

In the constraint solver, previousSolution (ie, .meteor/versions) was
intended to be used for three purposes:

  (a) The *heuristic* used in the state graph walk to decide what state
      to look at next:
       - VERY STRONGLY prefers not to downgrade *root* dependencies or
         change them to incompatible newer versions
       - Kinda Strongly prefers not to upgrade root dependencies far
       - mildy prefers not to change transitive dependencies (in any
         direction)

  (b) Actual *constraints* are added to the set of constraints we are
      trying to solve, which state that *root* dependencies are not
      to be downgraded.  Additionally, in most invocation contexts,
      constraints are added that say that *root* dependencies are
      not to be upgraded to incompatible versions (1.3.0 -> 2.0.0).
      The only contexts that lack the secondary constraints are
      part of 'update --minor', and 'add --force'.

  (c) A more recent change (past few weeks): When running the constraint
      solver, we actually run the core constraint solving algorithm
      twice. The first time, we add *equality* constraints for all
      of the previous versions.  If that succeeds, great!  We're not
      changing any versions (we may add packages that we didn't have
      before or drop packages that are no longer needed, though).

      Otherwise, we run the constraint solver again without the equality
      constraints.

However, due to a bug introduced in May in 0760ffbc369, (b) actually
*never happened*.  The commit was the one that intended to make the
(b) constraints only be for root dependencies. But the line:

     if (! _.contains(dependencies, uv.name))

is comparing 'uv.name' (something like 'foo#web.browser') to
'dependencies' (a list of things like 'foo'). So in fact, it thinks that
no dependencies are root and doesn't apply the constraints.

packages/constraint-solver/constraint-solver.js
@@ -144,7 +144,6 @@ ConstraintSolver.PackagesResolver.prototype._loadPackageInfo = function (
 //  - packageName - string name
 //  - version - string constraint (ex.: "1.2.3", ">=2.3.4", "=3.3.3")
 // options:
-//  - breaking - set this flag to true if breaking upgrades are allowed
 //  - upgrade - list of dependencies for which upgrade is prioritized higher
 //  than keeping the old version
 //  - previousSolution - mapping from package name to a version that was used in
@@ -156,7 +155,6 @@ ConstraintSolver.PackagesResolver.prototype.resolve = function (
   // clone because we mutate options
   options = _.extend({
     _testing: false,
-    breaking: false,
     upgrade: []
   }, options || {});
 
@@ -169,7 +167,6 @@ ConstraintSolver.PackagesResolver.prototype.resolve = function (
 
   check(options, {
     _testing: Match.Optional(Boolean),
-    breaking: Match.Optional(Boolean),
     upgrade: [String],
     previousSolution: Match.Optional(Object)
   });
@@ -234,20 +231,6 @@ ConstraintSolver.PackagesResolver.prototype.resolve = function (
   if (!res) {
     // Either we didn't have a previous solution, or it doesn't work. Try again
     // without locking in the previous solution as strict equality.
-    //
-    // However, we still don't want you to downgrade a version of a direct
-    // dependency in regards to the previous solution.
-    // Depending on whether the option `breaking` is set or not, allow only
-    // compatible upgrades or any upgrades.
-    _.each(options.previousSolution, function (uv) {
-      // if not a root dependency, there is no 'no-upgrade' constraint
-      if (! _.contains(dependencies, uv.name))
-        return;
-
-      var constrType = options.breaking ? ">=" : "";
-      dc.constraints.push(
-        self.resolver.getConstraint(uv.name, constrType + uv.version));
-    });
 
     res = self.resolver.resolve(
       dc.dependencies, dc.constraints, resolverOptions);

tools/commands-packages.js
@@ -1522,7 +1522,6 @@ main.registerCommand({
     var messages = buildmessage.capture(function () {
       newVersions = catalog.complete.resolveConstraints(allPackages, {
         previousSolution: versions,
-        breaking: !options.minor,
         upgrade: upgradePackages
       }, {
         ignoreProjectDeps: true
@@ -1572,11 +1571,7 @@ main.registerCommand({
   name: 'add',
   minArgs: 1,
   maxArgs: Infinity,
-  requiresApp: true,
-  options: {
-    // XXX come up with a better option name, like --allow-downgrades
-    force: { type: Boolean, required: false }
-  }
+  requiresApp: true
 }, function (options) {
   // For every package name specified, add it to our list of package
   // constraints. Don't run the constraint solver until you have added all of
@@ -1700,13 +1695,9 @@ main.registerCommand({
     versions = project.getVersions();
 
     // Call the constraint solver.
-    var resolverOpts =  {
-      previousSolution: versions,
-      breaking: !!options.force
-    };
     newVersions = catalog.complete.resolveConstraints(
       allPackages,
-      resolverOpts,
+      { previousSolution: versions },
       { ignoreProjectDeps: true });
     if ( ! newVersions) {
       // XXX: Better error handling.