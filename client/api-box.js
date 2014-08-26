525768061ec66178340251113a389fe6618b28f8
Sashko Stubailo <sashko@mit.edu>
2014-08-25 17:24:31 -0700

    Add some basic JSDoc stuff to generate a js file for docs

---
+++ client/api-box.js
@@ -0,0 +1,40 @@
+Template.autoApiBox.helpers({
+  apiData: function () {
+    var longname = this;
+    var root = DocsData;
+
+    _.each(longname.split("."), function (pathSegment) {
+      root = root[pathSegment];
+    });
+
+    return root;
+  },
+  typeNames: function (nameList) {
+    // change names if necessary
+    nameList = _.map(nameList, function (name) {
+      if (name === "function") {
+        return "Function";
+      }
+
+      return name;
+    });
+
+    return nameList.join(" | ");
+  },
+  paramsSentence: function () {
+    var params = this.params;
+
+    var paramNames = _.map(params, function (param) {
+      if (param.optional) {
+        return "[" + param.name + "]";
+      }
+
+      return param.name;
+    });
+
+    return paramNames.join(", ");
+  },
+  link: function () {
+    return this.longname.replace(".", "_").toLowerCase();
+  }
+});
