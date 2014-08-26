2058caaa31ae49c546c3f3ddbc67406cb6900dee
David Greenspan <dgreenspan@alum.mit.edu>
2014-08-26 15:53:05 -0700

    All-but-remove the `ui` package
    Also improve info strings on blaze and ui
    
    Not in this commit:
    * Change all uses of `ui` package to use `blaze`
    * Change all references to UI symbol to Blaze
    * Change docs accordingly

--- package.js
+++ package.js
@@ -26,10 +26,10 @@ Package.on_use(function (api) {
   api.export('Template', 'client');
 
   // html_scanner.js emits client code that calls Meteor.startup and
-  // UI, so anybody using templating (eg apps) need to implicitly use
+  // Blaze, so anybody using templating (eg apps) need to implicitly use
   // 'meteor' and 'ui'.
-  api.use('ui');
-  api.imply(['meteor', 'ui'], 'client');
+  api.use('blaze');
+  api.imply(['meteor', 'blaze'], 'client');
 });
 
 Package.on_test(function (api) {
