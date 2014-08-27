7358986f131648ee61b1f4e9bf93aa2267454294
David Glasser <glasser@meteor.com>
2014-08-26 20:43:22 -0700

    Revert "Fix infinite reload loop running tests"
    This reverts commit 35d816a36347e9f4d21d0c57a0f894cb848203f0.

--- packages/autoupdate/autoupdate_client.js
+++ packages/autoupdate/autoupdate_client.js
@@ -130,7 +130,7 @@ Autoupdate._retrySubscription = function () {
               attachStylesheetLink(newLink);
             });
           }
-          else if (doc._id === 'version' && doc.version !== autoupdateVersion && handle) {
+          else if (doc._id === 'version' && doc.version !== autoupdateVersion) {
             handle && handle.stop();
             Package.reload.Reload._reload();
           }
