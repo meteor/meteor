c76f506496c94760d705a5b9eeb11b2af9d663de
Justin SB <justin@fathomdb.com>
2014-08-26 20:24:37 -0700

    Spiderable now uses a flag to determine when the initial JS has loaded
    Other approaches seemed to be heuristics that broke down in edge cases:
    a timeout wasn't guaranteed if your JS was slow; page.load in PhantomJS
    wasn't firing reliably because of long polling.

--- packages/spiderable/phantom_script.js
+++ packages/spiderable/phantom_script.js
@@ -1,26 +1,40 @@
 // 'url' is assigned to in a statement before this.
 var page = require('webpage').create();
+
+var isReady = function () {
+  return page.evaluate(function () {
+    if (typeof Meteor === 'undefined'
+        || typeof(Meteor.status) === 'undefined'
+        || !Meteor.status().connected) {
+      return false;
+    }
+    if (typeof Package === 'undefined'
+        || typeof(Package.spiderable) === 'undefined'
+        || typeof(Package.spiderable.Spiderable) === 'undefined'
+        || !Package.spiderable.Spiderable._initialSubscriptionsStarted) {
+      return false;
+    }
+    Deps.flush();
+    return DDP._allSubscriptionsReady();
+  });
+};
+
+var dumpPageContent = function () {
+  var out = page.content;
+  out = out.replace(/<script[^>]+>(.|\n|\r)*?<\/script\s*>/ig, '');
+  out = out.replace('<meta name="fragment" content="!">', '');
+  console.log(out);
+};
+
 page.open(url, function(status) {
   if (status === 'fail')
     phantom.exit();
-  setInterval(function() {
-    var ready = page.evaluate(function () {
-      if (typeof Meteor !== 'undefined'
-          && typeof(Meteor.status) !== 'undefined'
-          && Meteor.status().connected) {
-        Deps.flush();
-        return DDP._allSubscriptionsReady();
-      }
-      return false;
-    });
-    if (ready) {
-      var out = page.content;
-      out = out.replace(/<script[^>]+>(.|\n|\r)*?<\/script\s*>/ig, '');
-      out = out.replace('<meta name="fragment" content="!">', '');
-      console.log(out);
-      phantom.exit();
-    }
-  }, 100);
 });
 
+setInterval(function() {
+  if (isReady()) {
+    dumpPageContent();
+    phantom.exit();
+  }
+}, 100);
 
