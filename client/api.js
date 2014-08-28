e19a5fcce0f946ba439db709bbfd6790abc1eee9
David Greenspan <dgreenspan@alum.mit.edu>
2014-08-28 02:36:00 -0700

    More docs fixes from MDG feedback

--- client/api.js
+++ client/api.js
@@ -2059,7 +2059,7 @@ Template.api.ui_templateinstance = {
   id: "ui_templateinstance",
   name: "UI.templateInstance()",
   locus: "Client",
-  descr: ["The template instance corresponding to the current template helper, event handler, callback, or autorun.  If there isn't one, `null`."]
+  descr: ["The [template instance](#template_inst) corresponding to the current template helper, event handler, callback, or autorun.  If there isn't one, `null`."]
 };
 
 Template.api.ui_parentdata = {
