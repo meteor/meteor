ce70133efc6766b705c757688c0a73b2f4115013
Slava Kim <slava@meteor.com>
2014-09-05 17:55:37 -0700

    Bump package versions

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Allows templates to be defined in .html files",
-  version: '1.0.6-rc0'
+  version: '1.0.6-rc1'
 });
 
 // Today, this package is closely intertwined with Handlebars, meaning
