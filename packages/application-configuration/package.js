782962e555f9be8b9ad99c0a586375c639261cad
Avital Oliver <avital@thewe.net>
2014-09-06 17:22:27 -0700

    Crutch for Galaxy apps
    Once 04cc467582c6929097245ac58b117cb0c3739935 is released,
    we can get rid of this.

---
+++ packages/application-configuration/package.js
@@ -0,0 +1,11 @@
+Package.describe({
+  summary: "Interaction with the configuration sources for your apps",
+  version: '1.0.1'
+});
+
+Package.on_use(function (api) {
+  api.use(['logging', 'underscore', 'ddp', 'ejson', 'follower-livedata']);
+  api.use('mongo', {unordered: true});
+  api.add_files(['config.js'], 'server');
+  api.export('AppConfig', 'server');
+});
