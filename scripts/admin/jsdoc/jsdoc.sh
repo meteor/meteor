c69447b03ce5e30b21ebb763bbdc7f3d340551b7
Sashko Stubailo <sashko@mit.edu>
2014-09-10 09:54:26 -0700

    Add comment explaining change

--- scripts/admin/jsdoc/jsdoc.sh
+++ scripts/admin/jsdoc/jsdoc.sh
@@ -9,6 +9,9 @@ INFINITY=10000
 
 cd $TOPDIR
 
+# Call git grep to find all js files with the appropriate comment tags,
+# and only then pass it to JSDoc which will parse the JS files.
+# This is a whole lot faster than calling JSDoc recursively.
 git grep -al "@summary" | xargs -L ${INFINITY} -t \
     ${TOPDIR}/dev_bundle/bin/node \
     ${TOPDIR}/dev_bundle/lib/node_modules/.bin/jsdoc \
