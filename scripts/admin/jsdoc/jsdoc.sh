0c3e0e786a820e9e6b6f5b6a2d1583c5235a4f1d
Sashko Stubailo <sashko@mit.edu>
2014-09-10 09:46:30 -0700

    Make JSDoc like 100 times faster by searching files with grep first
    Conflicts:
    	scripts/admin/jsdoc/jsdoc.sh

--- scripts/admin/jsdoc/jsdoc.sh
+++ scripts/admin/jsdoc/jsdoc.sh
@@ -5,10 +5,12 @@ cd $(dirname $0)
 cd ../../..
 TOPDIR=$(pwd)
 
+INFINITY=10000
+
 cd $TOPDIR
 
-${TOPDIR}/dev_bundle/bin/node \
+git grep -al "@summary" | xargs -L ${INFINITY} -t \
+    ${TOPDIR}/dev_bundle/bin/node \
     ${TOPDIR}/dev_bundle/lib/node_modules/.bin/jsdoc \
     -t "${TOPDIR}/scripts/admin/jsdoc/docdata-jsdoc-template" \
-    -c "${TOPDIR}/scripts/admin/jsdoc/jsdoc-conf.json" \
-    -r "${TOPDIR}/packages/"
+    -c "${TOPDIR}/scripts/admin/jsdoc/jsdoc-conf.json"
