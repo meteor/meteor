3379a1353316440c04c09ff2763b8fcb10c0faf3
David Glasser <glasser@meteor.com>
2014-09-10 17:38:31 -0700

    Use ChildProcess kill method to reload CSS
    Using the method instead of process.kill means that we don't send the
    signal if we already know the process is dead and that we don't throw on
    ESRCH (ie, the kernel telling us the process is gone).  (If the process
    has already died we will soon get around to doing a different kind of
    restart.)
    
    Fixes #2550.

--- tools/run-app.js
+++ tools/run-app.js
@@ -627,7 +627,7 @@ _.extend(AppRunner.prototype, {
         var oldFuture = self.runFuture = new Future;
 
         // Notify the server that new client assets have been added to the build.
-        process.kill(appProcess.proc.pid, 'SIGUSR2');
+        appProcess.proc.kill('SIGUSR2');
 
         // Establish a watcher on the new files.
         setupClientWatcher();
