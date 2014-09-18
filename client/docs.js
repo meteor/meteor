9dc0e36b91ee0bc9248f137ac17bd99c6e3a0bc6
Emily Stark <emily@meteor.com>
2014-09-17 23:00:23 -0400

    update docs to 0.9.2.2

--- client/docs.js
+++ client/docs.js
@@ -1,5 +1,5 @@
 Template.headline.release = function () {
-  return Meteor.release ? "0.9.2.1" : "(checkout)";
+  return Meteor.release ? "0.9.2.2" : "(checkout)";
 };
 
 Meteor.startup(function () {
