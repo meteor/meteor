cbe4d6dd6db1d8f764761e0dbb7097cdfef9e941
ekatek <ekate@meteor.com>
2014-09-22 22:07:33 -0700

    increment package versions

--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "The Meteor command-line tool",
-  version: '1.0.31-rc5'
+  version: '1.0.31-rc6'
 });
 
 Package.includeTool();
--- packages/package-version-parser/package.js
+++ packages/package-version-parser/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Parses Meteor Smart Package version string",
-  version: "2.0.0-rc0"
+  version: "2.0.0-rc1"
 });
 
 Npm.depends({
