1de88191c9933420fda1718a8e237d02192f8bfe
Sashko Stubailo <sashko@mit.edu>
2014-09-23 10:26:24 -0700

    Fix typo

--- tools/package-source.js
+++ tools/package-source.js
@@ -591,7 +591,8 @@ _.extend(PackageSource.prototype, {
       /**
        * @summary Define a build plugin. A build plugin extends the build
        * process for apps and packages that use this package. For example,
-       * the a package could compile CoffeeScript files into JavaScript.
+       * the `coffeescript` package could compile CoffeeScript source files
+       * into JavaScript.
        * @param  {Object} [options]
        * @param {String} options.name A cosmetic name, must be unique in the
        * package.
