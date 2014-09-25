92f75b19cb35a294b8b460f77377b3f8ec91aebf
Sashko Stubailo <sashko@mit.edu>
2014-09-25 10:14:23 -0700

    Add additional test assertion

--- packages/accounts-base/accounts_url_tests.js
+++ packages/accounts-base/accounts_url_tests.js
@@ -1,7 +1,11 @@
 Tinytest.add("accounts - parse urls for accounts-password",
   function (test) {
-    _.each(["reset-password", "verify-email", "enroll-account"],
-        function (hashPart) {
+    var actions = ["reset-password", "verify-email", "enroll-account"];
+
+    // make sure the callback was called the right number of times
+    var actionsParsed = [];
+
+    _.each(actions, function (hashPart) {
       var fakeToken = "asdf";
       
       // test backcompat with URLs that only contain the token and no email
@@ -11,6 +15,8 @@ Tinytest.add("accounts - parse urls for accounts-password",
         test.equal(token, fakeToken);
         test.equal(email, null);
         test.equal(action, hashPart);
+
+        actionsParsed.push(action);
       });
 
       // test new URLs with token and email
@@ -21,6 +27,11 @@ Tinytest.add("accounts - parse urls for accounts-password",
         test.equal(token, fakeToken);
         test.equal(email, fakeEmail);
         test.equal(action, hashPart);
+
+        actionsParsed.push(action);
       });
     });
+
+    // make sure each action is called twice, in order
+    test.equal(actionsParsed, _.flatten(_.zip(actions, actions)));
   });
