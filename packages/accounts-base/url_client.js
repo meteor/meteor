b1f7132454391baf2788581b23a56e2133e45dff
Sashko Stubailo <sashko@mit.edu>
2014-09-24 21:39:22 -0700

    Document accounts callbacks

--- packages/accounts-base/url_client.js
+++ packages/accounts-base/url_client.js
@@ -47,6 +47,19 @@ _.each(accountsPaths, function (urlPart) {
 // XXX these should be moved to accounts-password eventually. Right now
 // this is prevented by the need to set autoLoginEnabled=false, but in
 // some bright future we won't need to do that anymore.
+
+/**
+ * @summary Register a function to call when a reset password link is clicked
+ * in an email sent by
+ * [`Accounts.sendResetPasswordEmail`](#accounts_sendresetpasswordemail).
+ * @param  {Function} callback The function to call. It is given two arguments:
+ *
+ * 1. A password reset token that can be passed to
+ * [`Accounts.resetPassword`](#accounts_resetpassword)
+ * 2. A function to call when the password reset UI flow is complete. The normal
+ * login process is suspended until this function is called, so that the
+ * password for user A can be reset even if user B was logged in.
+ */
 Accounts.onResetPasswordLink = function (callback) {
   if (accountsCallbacks["reset-password"]) {
     Meteor._debug("Accounts.onResetPasswordLink was called more than once." +
@@ -56,6 +69,19 @@ Accounts.onResetPasswordLink = function (callback) {
   accountsCallbacks["reset-password"] = callback;
 };
 
+/**
+ * @summary Register a function to call when an email verification link is
+ * clicked in an email sent by
+ * [`Accounts.sendVerificationEmail`](#accounts_sendverificationemail).
+ * @param  {Function} callback The function to call. It is given two arguments:
+ *
+ * 1. An email verification token that can be passed to
+ * [`Accounts.verifyEmail`](#accounts_verifyemail)
+ * 2. A function to call when the email verification UI flow is complete.
+ * The normal login process is suspended until this function is called, so
+ * that the user can be notified that they are verifying their email before
+ * being logged in.
+ */
 Accounts.onVerifyEmailLink = function (callback) {
   if (accountsCallbacks["verify-email"]) {
     Meteor._debug("Accounts.onVerifyEmailLink was called more than once." +
@@ -65,6 +91,19 @@ Accounts.onVerifyEmailLink = function (callback) {
   accountsCallbacks["verify-email"] = callback;
 };
 
+/**
+ * @summary Register a function to call when an account enrollment link is
+ * clicked in an email sent by
+ * [`Accounts.sendEnrollmentEmail`](#accounts_sendenrollmentemail).
+ * @param  {Function} callback The function to call. It is given two arguments:
+ *
+ * 1. A password reset token that can be passed to
+ * [`Accounts.resetPassword`](#accounts_resetpassword) to give the newly
+ * enrolled account a password.
+ * 2. A function to call when the enrollment UI flow is complete.
+ * The normal login process is suspended until this function is called, so that
+ * user A can be enrolled even if user B was logged in.
+ */
 Accounts.onEnrollAccountLink = function (callback) {
   if (accountsCallbacks["enroll-account"]) {
     Meteor._debug("Accounts.onEnrollAccountLink was called more than once." +
