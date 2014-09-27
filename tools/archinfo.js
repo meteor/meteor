86bf309b18684c569275510b5217206d79b41d12
Tom Freudenberg <tom.freudenberg@4commerce.de>
2014-09-27 19:42:55 +0200

    add: support for ARMv6l and ARMv7l architecture like RaPi and UDOO and others

--- tools/archinfo.js
+++ tools/archinfo.js
@@ -150,6 +150,10 @@ var host = function () {
         _host = "os.linux.x86_32";
       else if (_.contains(["x86_64", "amd64", "ia64"], machine))
         _host = "os.linux.x86_64";
+      else if (_.contains(["armv6l"], machine))
+        _host = "os.linux.armv6l";
+      else if (_.contains(["armv7l"], machine))
+        _host = "os.linux.armv7l";
       else
         throw new Error("Unsupported architecture: " + machine);
     }
--- tools/bundler.js
+++ tools/bundler.js
@@ -1458,6 +1458,8 @@ _.extend(ServerTarget.prototype, {
     var archToPlatform = {
       'os.linux.x86_32': 'Linux_i686',
       'os.linux.x86_64': 'Linux_x86_64',
+      'os.linux.armv6l': 'Linux_armv6l',
+      'os.linux.armv7l': 'Linux_armv7l',
       'os.osx.x86_64': 'Darwin_x86_64'
     };
     var platform = archToPlatform[self.arch];
