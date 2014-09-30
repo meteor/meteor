1ac45850de3e5e379d9e0211fc1ac8a09420a76f
Sashko Stubailo <sashko@mit.edu>
2014-09-29 21:09:25 -0700

    Replace todos with new example

---
+++ examples/todos/lib/collections.js
@@ -0,0 +1,15 @@
+Lists = new Meteor.Collection('lists');
+
+// Calculate a default name for a list in the form of 'List A'
+Lists.defaultName = function() {
+  var nextLetter = 'A', nextName = 'List ' + nextLetter;
+  while (Lists.findOne({name: nextName})) {
+    // not going to be too smart here, can go past Z
+    nextLetter = String.fromCharCode(nextLetter.charCodeAt(0) + 1);
+    nextName = 'List ' + nextLetter;
+  }
+
+  return nextName;
+};
+
+Todos = new Meteor.Collection('todos');
