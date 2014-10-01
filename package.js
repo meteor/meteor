aab60730af789a38ddf215a0c865ab108c6ddc77
Sashko Stubailo <sashko@mit.edu>
2014-10-01 15:43:05 -0700

    Bump all of the version numbers again, to republish with the unipackage.json fix

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Allows templates to be defined in .html files",
-  version: '1.0.8-pre.0'
+  version: '1.0.8-pre.1'
 });
 
 // Today, this package is closely intertwined with Handlebars, meaning
