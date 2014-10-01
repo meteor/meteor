ce15336d3ea90c54ca06fb4c9899c9f992898771
Maxime Quandalle <maxime.quandalle@gmail.com>
2014-10-02 00:08:46 +0200

    Add tests for the appcache package
    Currently tests only verify the manifest presence and the header
    validity. It should also verify the other sections validity
    
    cf. #2716

---
+++ packages/appcache/appcache_tests.js
@@ -0,0 +1,34 @@
+var manifestURL = '/app.manifest';
+
+var appcacheTest = function(cb) {
+  return function(test, next) {
+    HTTP.get(manifestURL, function (err, res) {
+      cb(test, res);
+      next();
+    });
+  };
+};
+
+Tinytest.addAsync('appcache - presence', appcacheTest(function(test, manifest) {
+  test.equal(manifest.statusCode, 200, 'manifest not served');
+}));
+
+Tinytest.addAsync('appcache - validity', appcacheTest(function(test, manifest) {
+  var lines = manifest.content.split("\n");
+  var i = 0;
+
+  var nextLinesMatch = function(expectedList) {
+    _.each(expectedList, function(expected) {
+      var testFunc = _.isRegExp(expected) ? "matches" : "equal";
+      test[testFunc](lines[i++], expected);
+    });
+  };
+
+  // Verify header validity
+  nextLinesMatch([
+    "CACHE MANIFEST",
+    "",
+    /^#\s[0-9a-f]+$/,
+    /^#\s[0-9a-f]+$/,
+  ]);
+}));
