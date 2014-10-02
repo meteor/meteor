976b1fcda1f5f4a5ea544d3635177dd07b81f205
Justin SB <justin@fathomdb.com>
2014-10-02 15:21:11 -0700

    Bump version of com.meteor.cordova-update for Android fix

--- packages/meteor-platform/package.js
+++ packages/meteor-platform/package.js
@@ -68,5 +68,5 @@ Package.on_use(function(api) {
 
 Cordova.depends({
   'org.apache.cordova.device': '0.2.11',
-  'com.meteor.cordova-update': 'https://github.com/meteor/com.meteor.cordova-update/tarball/0e10582dd4998bfc5f39455591ce450f9ec2c2cd'
+  'com.meteor.cordova-update': 'https://github.com/meteor/com.meteor.cordova-update/tarball/1f3213de1852542171ee3abb07399434058cd74b'
 });
