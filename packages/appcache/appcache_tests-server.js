709c379f890ff38531a9d61efc3c280c877061b1
Nick Martin <nim@meteor.com>
2014-10-06 23:08:05 -0700

    Run appcache size check after user code has had a chance to run.
    This way the size check takes calls to Meteor.AppCache.config()
    into account, so users can remove files from the cache and get
    rid of the warning.
    
    Also, add an internal option to totally disable the warning and
    use it in tests.

--- packages/appcache/appcache_tests-server.js
+++ packages/appcache/appcache_tests-server.js
@@ -1,10 +1,11 @@
-// For our testing purpose, we don't want the cache manifest to be active, all
-// we want is the cache manifest to be manually request-able so we can read it
-// and verify its validity from the client.
-// This is because caching the test files would make tests non deterministic
-// depending on the state of the browser cache.
-// Do do that we disable the "manifest" attribute of the <html> tag.
-
+// For our testing purpose, we don't want the cache manifest to be
+// active, all we want is the cache manifest to be manually request-able
+// so we can read it and verify its validity from the client.  This is
+// because caching the test files would make tests non deterministic
+// depending on the state of the browser cache.  To do that we disable
+// the "manifest" attribute of the <html> tag. This runs after appcache
+// registers its hook, so this hook overrides the return value of the
+// real hook.
 WebApp.addHtmlAttributeHook(function (request) {
   return { manifest: "" };
 });
@@ -16,5 +17,6 @@ Meteor.AppCache.config({
     '/online/',
     '/bigimage.jpg',
     '/largedata.json'
-  ]
+  ],
+  _disableSizeCheck: true // don't print warnings
 });
