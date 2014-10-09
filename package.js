a72ccbec450d85a8f7a7a25fecc23308db90786a
Nick Martin <nim@meteor.com>
2014-10-09 15:36:52 -0700

    Bump versions for 0.9.4-rc.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Allows templates to be defined in .html files",
-  version: '1.0.8-rc.0'
+  version: '1.0.8-rc.2'
 });
 
 // Today, this package is closely intertwined with Handlebars, meaning
