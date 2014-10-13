cb97ed238bb8c14a13edc6057f78f4dc8bbbe064
Avital Oliver <avital@thewe.net>
2014-10-13 09:55:16 -0700

    Add clarifying comment to failing pre-release versions self-test

--- tools/tests/publish.js
+++ tools/tests/publish.js
@@ -270,10 +270,19 @@ selftest.define("list-with-a-new-version",
 });
 
 
+// Test that we only try to upgrade to pre-release versions of
+// packages (eg 0.0.1-rc, 0.0.2-pre, ...) if there is at least one
+// package already on a pre-release verison. That is -- adding a
+// single pre-release version of a package is opting into "try to find
+// use pre-release versions of any package if necessary"
 selftest.define("do-not-update-to-rcs",
     ["slow", "net", "test-package-server", "checkout"],
     function () {
 
+  // This test needs to run with a stub warehouse, since otherwise we
+  // might find outselves running while a Meteor release is being
+  // prepared, in which case we already have some packages in
+  // pre-release version.
   var s = new Sandbox({warehouse: {
     "v1": {recommended: true}
   }});
