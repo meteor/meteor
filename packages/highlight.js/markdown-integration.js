5c570d16ab965599a20d2dec60c76767a40dfea4
Sashko Stubailo <sashko@mit.edu>
2014-10-15 23:07:24 -0700

    Add totally new highlight.js package

---
+++ packages/highlight.js/markdown-integration.js
@@ -0,0 +1,36 @@
+if (Package.markdown) {
+  var decodeEntitiesAndHighlight;
+
+  if (Meteor.isClient) {
+    decodeEntitiesAndHighlight = function (codeWithEntities) {
+      var decoded = $('<div/>').html(codeWithEntities).text();
+      return hljs.highlightAuto(decoded);
+    };
+  } else {
+    var entities = new Npm.require("html-entities").XmlEntities();
+    decodeEntitiesAndHighlight = function (codeWithEntities) {
+      var decoded = entities.decode(codeWithEntities);
+      return hljs.highlightAuto(decoded);
+    };
+  }
+
+  var oldConstructor = Package.markdown.Showdown.converter;
+
+  Package.markdown.Showdown.converter = function (options) {
+    var converter = new oldConstructor(options);
+    var oldMakeHtml = converter.makeHtml;
+
+    converter.makeHtml = function (text) {
+      text = oldMakeHtml(text);
+
+      text = text.replace(/<pre><code>([\s\S]*?)<\/code><\/pre>/g, function (fullBlock, codeOnly) {
+        var result = decodeEntitiesAndHighlight(codeOnly);
+        return "<pre><code class='hljs " + result.language + "'>" + result.value + "</code></pre>";
+      });
+
+      return text;
+    };
+
+    return converter;
+  };
+}
