cafd46617659e1d5b650f57c8871beab5148ed0d
Slava Kim <slava@meteor.com>
2014-10-17 16:06:26 -0700

    Edits to todo's mobile config

--- examples/todos/mobile-config.js
+++ examples/todos/mobile-config.js
@@ -29,10 +29,19 @@ App.launchScreens({
   'ipad_portrait': 'resources/splash/Default-Portrait~ipad.png',
   'ipad_portrait_2x': 'resources/splash/Default-Portrait@2x~ipad.png',
   'ipad_landscape': 'resources/splash/Default-Landscape~ipad.png',
-  'ipad_landscape_2x': 'resources/splash/Default-Landscape@2x~ipad.png'
+  'ipad_landscape_2x': 'resources/splash/Default-Landscape@2x~ipad.png',
+
+  // Android
+  'android_ldpi_portrait': 'resources/splash/splash.9.png',
+  'android_ldpi_landscape': 'resources/splash/splash.9.png',
+  'android_mdpi_portrait': 'resources/splash/splash.9.png',
+  'android_mdpi_landscape': 'resources/splash/splash.9.png',
+  'android_hdpi_portrait': 'resources/splash/splash.9.png',
+  'android_hdpi_landscape': 'resources/splash/splash.9.png',
+  'android_xhdpi_portrait': 'resources/splash/splash.9.png',
+  'android_xhdpi_landscape': 'resources/splash/splash.9.png'
 });
 
 App.setPreference('StatusBarOverlaysWebView', 'false');
 App.setPreference('StatusBarBackgroundColor', '#000000');
-App.setPreference('SplashScreen', 'screen');
 
