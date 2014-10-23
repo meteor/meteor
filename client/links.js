21e0904f17707030a672666f3d6db48db2cec953
Sashko Stubailo <sashko@mit.edu>
2014-10-23 11:00:01 -0700

    Links are doing pretty well!

--- client/links.js
+++ client/links.js
@@ -2,26 +2,46 @@ Tracker.autorun(function () {
   // returns a "location" like object with all of the url parts
   var current = Iron.Location.get();
 
-  console.log(current);
+  // redirect routes with no trailing slash
   if (current.path === "/basic") {
-    Session.set("fullApi", false);
+    Iron.Location.go("/basic/");
+    return;
   } else if (current.path === "/full") {
+    Iron.Location.go("/full/");
+    return;
+  }
+
+  if (current.path === "/basic/") {
+    Session.set("fullApi", false);
+  } else if (current.path === "/full/") {
     Session.set("fullApi", true);
   } else {
     if (current.hash) {
       // XXX COMPAT WITH old docs
-      Iron.Location.go("/full");
+      Iron.Location.go("/full/");
     } else {
-      Iron.Location.go("/basic");
+      Iron.Location.go("/basic/");
     }
   }
 
-  Tracker.afterFlush(function () {
-    setTimeout(function () {
-      console.log($(".main-content").offset(), $(current.hash).offset());
-      $(".main-content").animate({
-          scrollTop: $(".main-content").scrollTop() + $(current.hash).offset().top
-      }, 1000);
-    }, 0);
-  });
+  if (current.hash) {
+    Tracker.afterFlush(function () {
+      setTimeout(function () {
+        var targetLocation;
+
+        if (current.hash === "#top") {
+          targetLocation = 0;
+        } else {
+          var foundElement = $(current.hash);
+          if (foundElement.get(0)) {
+            targetLocation = $(".main-content").scrollTop() + foundElement.offset().top;
+          }
+        }
+
+        $(".main-content").animate({
+            scrollTop: targetLocation
+        }, 1000);
+      }, 0);
+    });
+  }
 });
