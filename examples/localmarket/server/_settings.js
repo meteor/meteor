3a50054e202c313b0b7e657a84c54a9d634d17e6
Nick Martin <nim@meteor.com>
2014-10-27 21:57:32 -0700

    Point to a wiki page detailing how to configure twitter keys.
    Wiki page doesn't have content yet.

--- examples/localmarket/server/_settings.js
+++ examples/localmarket/server/_settings.js
@@ -1,4 +1,7 @@
 // Provide defaults for Meteor.settings
+//
+// To configure your own Twitter keys, see:
+//   https://github.com/meteor/meteor/wiki/Configuring-Twitter-in-Local-Market
 if (typeof Meteor.settings === 'undefined')
   Meteor.settings = {};
 
