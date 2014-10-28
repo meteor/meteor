842bfac8df78d7a3ea0b9caec5ce76497a48afb8
Emily Stark <emily@meteor.com>
2014-10-28 01:03:08 -0700

    bump package versions for rc.11

--- packages/accounts-ui/package.js
+++ packages/accounts-ui/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Simple templates to add login widgets to an app",
-  version: "1.1.3-rc.0"
+  version: "1.1.3-rc.1"
 });
 
 Package.on_use(function (api) {
--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "The Meteor command-line tool",
-  version: '1.0.35-rc.12'
+  version: '1.0.35-rc.13'
 });
 
 Package.includeTool();
