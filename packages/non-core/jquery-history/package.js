69c92eb24a741054330496d415c0511ba8601c1a
Sashko Stubailo <sashko@mit.edu>
2014-10-28 15:49:41 -0700

    Add deprecation message and bump version for jquery-history

--- packages/non-core/jquery-history/package.js
+++ packages/non-core/jquery-history/package.js
@@ -1,9 +1,11 @@
 Package.describe({
-  summary: "pushState module from the jQuery project",
-  version: "1.0.1"
+  name: "jquery-history",
+  summary: "Deprecated package for HTML5 pushState",
+  version: "1.0.2"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom('1.0');
   api.use('json', 'client');
   api.use('jquery', 'client');
   api.add_files(['history.adapter.jquery.js',
