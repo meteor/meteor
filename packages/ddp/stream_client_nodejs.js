3a61be67ab5a10e2ed519159ea67585f7798d881
David Glasser <glasser@meteor.com>
2014-10-28 18:44:37 -0700

    Use Faye's proxy support.

--- packages/ddp/stream_client_nodejs.js
+++ packages/ddp/stream_client_nodejs.js
@@ -111,6 +111,17 @@ _.extend(LivedataTest.ClientStream.prototype, {
     }
   },
 
+  _getProxyUrl: function (targetUrl) {
+    var self = this;
+    // Similar to code in tools/http-helpers.js.
+    var proxy = process.env.HTTP_PROXY || process.env.http_proxy || null;
+    // if we're going to a secure url, try the https_proxy env variable first.
+    if (targetUrl.match(/^wss:/)) {
+      proxy = process.env.HTTPS_PROXY || process.env.https_proxy || proxy;
+    }
+    return proxy;
+  },
+
   _launchConnection: function () {
     var self = this;
     self._cleanup(); // cleanup the old socket, if there was one.
@@ -120,16 +131,22 @@ _.extend(LivedataTest.ClientStream.prototype, {
     // connection.
     var FayeWebSocket = Npm.require('faye-websocket');
 
+    var targetUrl = toWebsocketUrl(self.endpoint);
+    var fayeOptions = { headers: self.headers };
+    var proxyUrl = self._getProxyUrl(targetUrl);
+    if (proxyUrl) {
+      fayeOptions.proxy = { origin: proxyUrl };
+    };
+
     // We would like to specify 'ddp' as the subprotocol here. The npm module we
     // used to use as a client would fail the handshake if we ask for a
     // subprotocol and the server doesn't send one back (and sockjs doesn't).
     // Faye doesn't have that behavior; it's unclear from reading RFC 6455 if
     // Faye is erroneous or not.  So for now, we don't specify protocols.
+    var subprotocols = [];
+
     var client = self.client = new FayeWebSocket.Client(
-      toWebsocketUrl(self.endpoint),
-      [/*no subprotocols*/],
-      {headers: self.headers}
-    );
+      targetUrl, subprotocols, fayeOptions);
 
     self._clearConnectionTimer();
     self.connectionTimer = Meteor.setTimeout(
