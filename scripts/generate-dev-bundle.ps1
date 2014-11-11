2a8e2c0dcc86caf89e2964315fade2500a5044f7
Sashko Stubailo <s.stubailo@gmail.com>
2014-11-10 16:40:30 -0800

    PS1 script downloads the prebuilt binaries.
    Some npm modules are disabled:
    
    - netroute
    - kexec
    - cordova
    
    also there is some copy-paste from dev-bundle-package.json that we need to
    fix.

--- scripts/generate-dev-bundle.ps1
+++ scripts/generate-dev-bundle.ps1
@@ -1,39 +1,84 @@
-$DIR = "generate-dev-bundle-XXXXXXXX"
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$DIR = $scriptPath + "\generate-dev-bundle-XXXXXXXX"
+echo $DIR
 
-New-Item -Type Directory -Name $DIR
+cmd /C "rmdir /S /Q ${DIR}"
+
+New-Item -Type Directory -Name generate-dev-bundle-XXXXXXXX
 Set-Location $DIR
 
-New-Item -Type Directory -Name "build"
-Set-Location "build"
+# install dev-bundle-package.json
+mkdir build
+cd build
+mkdir npm-install
+cd npm-install
 
-### Compile Node
-# powershell mistakenly thinks that git throws an error, suppress it
-Invoke-Expression "git clone https://github.com/meteor/node.git" 2> out-null
-Set-Location "node"
-Invoke-Expression "git checkout v0.10.33-with-npm-5821"
-cmd.exe /c '.\vcbuild.bat nosign'
+npm config set loglevel error
+cp "${DIR}/../dev-bundle-package.json" package.json
+npm install
+npm shrinkwrap
+
+rm -Recurse -Force "${DIR}\build\npm-install\node_modules\.bin"
+cp -R node_modules "${DIR}\lib\node_modules"
+
+# XXX we need to copy the package.json etc
+# XXX delete extra fibers code
+
+cd ../..
+
+cd lib
+
+# commented out ones don't work on Windows, we don't plan to support them in the first release
+echo "{}" | Out-File package.json -Encoding ascii # otherwise it doesn't install in local dir
+npm install request@2.47.0
+npm install fstream@1.0.2
+npm install tar@1.0.2
+# npm install kexec@0.2.0
+npm install source-map@0.1.40
+npm install browserstack-webdriver@2.41.1
+npm install node-inspector@0.7.4
+npm install chalk@0.5.1
+npm install sqlite3@3.0.2
+# npm install netroute@0.2.5
+npm install phantomjs@1.9.12
+npm install http-proxy@1.6.0
+npm install esprima@1.2.2
+npm install https://github.com/meteor/node-eachline/tarball/ff89722ff94e6b6a08652bf5f44c8fffea8a21da
+# npm install "https://github.com/meteor/cordova-cli/tarball/0c9b3362c33502ef8f6dba514b87279b9e440543"
+
 Set-Location ..
-### Done compiling Node
 
-### Compile Mongo
-Invoke-Expression "git clone git://github.com/meteor/mongo.git" 2> out-null
-Set-Location "mongo"
-Invoke-Expression "git checkout ssl-r2.4.8"
-C:\Python27\Scripts\scons mongo.exe mongod.exe --ssl --extrapath=C:\OpenSSL-Win32 -j 2
+mkdir bin
+Set-Location bin
+
+$webclient = New-Object System.Net.WebClient
+
+# download Mongo
+$mongo_name = "mongodb-win32-i386-2.6.5"
+$mongo_link = "https://fastdl.mongodb.org/win32/${mongo_name}.zip"
+$mongo_zip = "$DIR\bin\mongo.zip"
+$webclient.DownloadFile($mongo_link, $mongo_zip)
+
+$shell = new-object -com shell.application
+$zip = $shell.NameSpace($mongo_zip)
+foreach($item in $zip.items()) {
+  $shell.Namespace("$DIR\bin").copyhere($item, 0x14) # 0x10 - overwrite, 0x4 - no dialog
+}
+cp "$DIR\bin\$mongo_name\bin\mongod.exe" .
+cp "$DIR\bin\$mongo_name\bin\mongo.exe" .
+
+rm -Recurse -Force $mongo_zip
+rm -Recurse -Force "$DIR\bin\$mongo_name"
+
+# download node
+$node_link = "http://nodejs.org/dist/v0.10.33/node.exe"
+$webclient.DownloadFile($node_link, "$DIR\bin\node.exe")
+# install npm
+echo "{}" | Out-File package.json -Encoding ascii # otherwise it doesn't install in local dir
+npm install npm
+cp node_modules\npm\bin\npm.cmd .
+
+# mark the version
+echo "0.3.62" | Out-File .bundle_version.txt -Encoding ascii
+
 Set-Location ..
-### done compiling Mongo
-
-### Download browserstack local
-$browserstack = "browserstack";
-New-Item -Type Directory -Name "browserstack"
-$url = "https://www.browserstack.com/browserstack-local/BrowserStackLocal-win32.zip"
-$file = "$pwd/$browserstack/BrowserStackLocal-win32.zip"
-(New-Object System.Net.WebClient).DownloadFile($url,$file)
-### Done downloading browserstack local
-
-Set-Location ..
-Set-Location ..
-
-# do it twice because once doesn't work lol
-# Remove-Item -Force -Recurse $DIR 2> out-null
-# Remove-Item -Force -Recurse $DIR
