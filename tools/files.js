c7b8439eff0e3140185a2365640085687fceec31
Sashko Stubailo <s.stubailo@gmail.com>
2014-11-11 19:56:00 -0800

    Write the correct EOL for .gitignore

--- tools/files.js
+++ tools/files.js
@@ -105,18 +105,18 @@ files.addToGitignore = function (dirPath, entry) {
   var filepath = path.join(dirPath, ".gitignore");
   if (fs.existsSync(filepath)) {
     var data = fs.readFileSync(filepath, 'utf8');
-    var lines = data.split(/\n/);
+    var lines = data.split(os.EOL);
     if (_.any(lines, function (x) { return x === entry; })) {
       // already there do nothing
     } else {
       // rewrite file w/ new entry.
-      if (data.substr(-1) !== "\n") data = data + "\n";
-      data = data + entry + "\n";
+      if (data.substr(-1) !== os.EOL) data = data + os.EOL;
+      data = data + entry + os.EOL;
       fs.writeFileSync(filepath, data, 'utf8');
     }
   } else {
     // doesn't exist, just write it.
-    fs.writeFileSync(filepath, entry + "\n", 'utf8');
+    fs.writeFileSync(filepath, entry + os.EOL, 'utf8');
   }
 };
 
