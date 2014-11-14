b3b66dd89b155b160c0ec92dcb1d07d55ab0cb92
Sashko Stubailo <sashko@mit.edu>
2014-11-14 10:49:37 -0800

    Revert "Change all uses of word elide to omit"
    This reverts commit 442274f266ebb689f1182f3817fcf25341799823.

--- tools/tropohouse.js
+++ tools/tropohouse.js
@@ -289,7 +289,7 @@ _.extend(exports.Tropohouse.prototype, {
       var combinedDirectory = self.packagePath(packageName, newPackageLinkTarget);
       isopack.saveToPath(combinedDirectory, {
         // We got this from the server, so we can't rebuild it.
-        omitBuildInfo: true
+        elideBuildInfo: true
       });
 
       if (process.platform === "win32") {
