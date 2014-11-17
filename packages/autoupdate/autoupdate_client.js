3372e660d0bebd0d94ad9bc55bc87212277c1913
Daniel Dornhardt <daniel@dornhardt.com>
2014-11-17 15:11:16 +0100

    Fix Css autoupdate for pages with ROOT_URL_PATH_PREFIX set
    For pages using a ROOT_URL="" setting with a path component (eg.
    "myproject.com/beta"), the CSS autoupdate would break the page, because
    it would set the autoupdate CSS files' URL to /<longidstring>.css, while it
    should have been /beta/<longidstring>.css. Added the required
    ROOT_URL_PATH_PREFIX.

--- packages/autoupdate/autoupdate_client.js
+++ packages/autoupdate/autoupdate_client.js
@@ -127,7 +127,7 @@ Autoupdate._retrySubscription = function () {
                 newLink.setAttribute("rel", "stylesheet");
                 newLink.setAttribute("type", "text/css");
                 newLink.setAttribute("class", "__meteor-css__");
-                newLink.setAttribute("href", css.url);
+                newLink.setAttribute("href", __meteor_runtime_config__.ROOT_URL_PATH_PREFIX + css.url);
                 attachStylesheetLink(newLink);
               });
             } else {
