cb7a18f89a4a60b5433592887098e4793c9433af
David Glasser <glasser@meteor.com>
2014-11-24 12:18:52 -0800

    fix selftest-test

--- tools/tests/selftest-test.js
+++ tools/tests/selftest-test.js
@@ -5,21 +5,12 @@ var files = require('../files.js');
 selftest.define("selftest-from-warehouse", ['checkout'], function () {
   var s = new Sandbox({
     warehouse: {
-      v1: { recommended: true},
-      v2: { recommended: true }
+      v1: { recommended: true }
     }
   });
-  var run;
-
-  var toolsPackage;
-  selftest.doOrThrow(function() {
-    toolsPackage = selftest.getToolsPackage();
-  });
-  var toolsVersion = toolsPackage.name + '@' +
-        toolsPackage.version;
 
   // Create an app with the latest release.
-  run = s.run("self-test", "do-nothing");
+  var run = s.run("self-test", "do-nothing");
   run.waitSecs(15);
   run.expectExit(0);
 });
