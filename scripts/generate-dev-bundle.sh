fff79abea8654756b6be7ac3efbce4eda3e0fef6
Ben Newman <ben@meteor.com>
2014-11-25 18:28:57 -0500

    Source build-dev-bundle-common.sh in generate-dev-bundle.sh.

--- scripts/generate-dev-bundle.sh
+++ scripts/generate-dev-bundle.sh
@@ -3,73 +3,16 @@
 set -e
 set -u
 
-UNAME=$(uname)
-ARCH=$(uname -m)
-
-if [ "$UNAME" == "Linux" ] ; then
-    if [ "$ARCH" != "i686" -a "$ARCH" != "x86_64" ] ; then
-        echo "Unsupported architecture: $ARCH"
-        echo "Meteor only supports i686 and x86_64 for now."
-        exit 1
-    fi
-
-    OS="linux"
-
-    stripBinary() {
-        strip --remove-section=.comment --remove-section=.note $1
-    }
-elif [ "$UNAME" == "Darwin" ] ; then
-    SYSCTL_64BIT=$(sysctl -n hw.cpu64bit_capable 2>/dev/null || echo 0)
-    if [ "$ARCH" == "i386" -a "1" != "$SYSCTL_64BIT" ] ; then
-        # some older macos returns i386 but can run 64 bit binaries.
-        # Probably should distribute binaries built on these machines,
-        # but it should be OK for users to run.
-        ARCH="x86_64"
-    fi
-
-    if [ "$ARCH" != "x86_64" ] ; then
-        echo "Unsupported architecture: $ARCH"
-        echo "Meteor only supports x86_64 for now."
-        exit 1
-    fi
-
-    OS="osx"
-
-    # We don't strip on Mac because we don't know a safe command. (Can't strip
-    # too much because we do need node to be able to load objects like
-    # fibers.node.)
-    stripBinary() {
-        true
-    }
-else
-    echo "This OS not yet supported"
-    exit 1
-fi
-
-PLATFORM="${UNAME}_${ARCH}"
-
-# save off meteor checkout dir as final target
-cd "`dirname "$0"`"/..
-CHECKOUT_DIR=`pwd`
-
 # Read the bundle version from the meteor shell script.
 BUNDLE_VERSION=$(perl -ne 'print $1 if /BUNDLE_VERSION=(\S+)/' meteor)
 if [ -z "$BUNDLE_VERSION" ]; then
     echo "BUNDLE_VERSION not found"
     exit 1
 fi
-echo "Building dev bundle $BUNDLE_VERSION"
 
-DIR=`mktemp -d -t generate-dev-bundle-XXXXXXXX`
-trap 'rm -rf "$DIR" >/dev/null 2>&1' 0
-
-echo BUILDING IN "$DIR"
-
-cd "$DIR"
-chmod 755 .
-umask 022
-mkdir build
-cd build
+source "$(dirname $0)/build-dev-bundle-common.sh"
+echo CHECKOUT DIR IS "$CHECKOUT_DIR"
+echo BUILDING DEV BUNDLE "$BUNDLE_VERSION" IN "$DIR"
 
 # ios-sim is used to run iPhone simulator from the command-line. Doesn't make
 # sense to build it for linux.
