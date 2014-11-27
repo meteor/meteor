9f9058b16a52f0231922483c4bb1dc5946571d36
Zoltan Olah <zol@percolatestudio.com>
2014-11-27 12:08:56 -0800

    Bumps version to 1.1.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Allows you to define and run scheduled jobs across multiple servers.",
-  version: "1.0.0",
+  version: "1.1.0",
   name: "percolatestudio:synced-cron",
   git: "https://github.com/percolatestudio/meteor-synced-cron.git"
 });
@@ -9,7 +9,7 @@ Npm.depends({later: "1.1.6"});
 
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@0.9.1.1');
-  api.use(['underscore', 'check', 'mongo'], 'server');
+  api.use(['underscore', 'check', 'mongo', 'logging'], 'server');
   api.add_files(['synced-cron-server.js'], "server");
   api.export('SyncedCron', 'server');
 });
