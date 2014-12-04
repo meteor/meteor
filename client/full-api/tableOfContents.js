a2e661759ef7b6383cb1bf1cdba49f018b05b39a
Sashko Stubailo <sashko@mit.edu>
2014-12-04 12:16:49 -0800

    Add ability to define custom link for ToC, and add link for spiderable

--- client/full-api/tableOfContents.js
+++ client/full-api/tableOfContents.js
@@ -317,6 +317,7 @@ var toc = [
     "random",
     "stylus",
     "showdown",
+    {name: "spiderable", link: "https://atmospherejs.com/meteor/spiderable"},
     "stylus",
     "underscore",
     "webapp"
@@ -364,9 +365,12 @@ Template.nav.helpers({
         else {
           if (typeof(item) === "string")
             item = {name: item};
+
+          var id = item.name && name_to_id(item.name) || undefined;
+
           ret.push(_.extend({
             type: "section",
-            id: item.name && name_to_id(item.name) || undefined,
+            link: "#/full/" + id,
             depth: depth,
             style: ''
           }, item));
