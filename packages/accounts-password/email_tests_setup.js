3d924adec82ec0ba7b0d93f5ce79a24d7287e753
Netanel Gilad <netanelgilad@gmail.com>
2014-12-05 15:41:14 +0200

    cosmetic comments.

--- packages/accounts-password/email_tests_setup.js
+++ packages/accounts-password/email_tests_setup.js
@@ -12,13 +12,14 @@ Accounts.emailTemplates.resetPassword.html =
     return url;
   };
 
-// override the from address and return the users email
+// override the from address
 Accounts.emailTemplates.resetPassword.from =
   Accounts.emailTemplates.enrollAccount.from =
     Accounts.emailTemplates.verifyEmail.from = function (user) {
       return 'test@meteor.com';
     };
 
+// add a custom header to check against
 Accounts.emailTemplates.headers = {
   'My-Custom-Header' : 'Cool'
 };
