5273546c1a4743f1af7a7077dea7aeeee8995230
Nick Martin <nim@meteor.com>
2014-12-09 11:36:59 -0800

    Bump version numbers

--- packages/less/package.js
+++ packages/less/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "The dynamic stylesheet language",
-  version: "1.0.10_1"
+  version: "1.0.10_2"
 });
 
 Package._transitional_registerBuildPlugin({
--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "The Meteor command-line tool",
-  version: '1.0.34_1'
+  version: '1.0.34_2'
 });
 
 Package.includeTool();
