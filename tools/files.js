20dcc8e099c62e77fa3a0e42d33446106ea3c1a1
Slava Kim <slava@meteor.com>
2014-12-11 15:02:58 -0800

    Put a forgotten return statement

--- tools/files.js
+++ tools/files.js
@@ -1146,6 +1146,7 @@ if (process.platform === "win32") {
     if (_.isString(fileData)) {
       return fileData.replace(new RegExp(os.EOL, "g"), "\n");
     }
+    return fileData;
   };
 
   files.rename = function (from, to) {
