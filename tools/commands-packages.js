517bd5ec5121cf5580a21568b2ff638f34111b83
Sashko Stubailo <s.stubailo@gmail.com>
2014-12-18 16:10:03 -0800

    Convert paths and add --unpacked option to tarball

--- tools/commands-packages.js
+++ tools/commands-packages.js
@@ -2007,13 +2007,21 @@ main.registerCommand({
   maxArgs: 2,
   hidden: true,
 
+  options: {
+    // Copy the tarball contents to the output directory instead of making a
+    // tarball (useful for testing the release process)
+    "unpacked": { type: Boolean, required: false }
+  },
+
   // In this function, we want to use the official catalog everywhere, because
   // we assume that all packages have been published (along with the release
   // obviously) and we want to be sure to only bundle the published versions.
   catalogRefresh: new catalog.Refresh.OnceAtStart({ ignoreErrors: false })
 }, function (options) {
   var releaseNameAndVersion = options.args[0];
-  var outputDirectory = options.args[1];
+
+  // We get this as an argument, so it is an OS path. Make it a standard path.
+  var outputDirectory = files.convertToStandardPath(options.args[1]);
 
   var trackAndVersion = utils.splitReleaseName(releaseNameAndVersion);
   var releaseTrack = trackAndVersion[0];
@@ -2149,9 +2157,14 @@ main.registerCommand({
         toolRecord.path,
         'meteor'));
 
-    files.createTarball(
-      tmpTropo.root,
-      files.pathJoin(outputDirectory, 'meteor-bootstrap-' + osArch + '.tar.gz'));
+    if (options.unpacked) {
+      files.cp_r(tmpTropo.root, outputDirectory);
+    } else {
+      files.createTarball(
+        tmpTropo.root,
+        files.pathJoin(outputDirectory,
+          'meteor-bootstrap-' + osArch + '.tar.gz'));
+    }
   });
 
   return 0;
