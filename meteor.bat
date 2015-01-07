6acb377191ad120a620155e22ee60ab94c3c0552
Slava Kim <slava@meteor.com>
2015-01-07 12:31:35 -0800

    Automatically download the dev bundle if such is not present on Windows

--- meteor.bat
+++ meteor.bat
@@ -1,4 +1,9 @@
 @echo off
+
+IF NOT EXIST "%~dp0\dev_bundle" (
+  PowerShell.exe -version 2.0 -file "%~dp0\scripts\windows\download-dev-bundle.ps1"
+)
+
 SET NODE_PATH=%~dp0\dev_bundle\lib\node_modules
 
 "%~dp0\dev_bundle\bin\node.exe" "%~dp0\tools\main.js" %*
