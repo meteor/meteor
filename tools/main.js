cf1fc9287220b563cc14ad874804e11ca273819b
Sashko Stubailo <s.stubailo@gmail.com>
2015-01-08 14:20:57 -0800

    Revert "Add hack to stop downloading newest release"
    This reverts commit a92038fd4ef874e21c77187ea3722f08c64aacc9.

--- tools/main.js
+++ tools/main.js
@@ -785,15 +785,6 @@ Fiber(function () {
     }
     releaseName = null;
   } else {
-    // XXX Hack for Windows because we don't want to do springboarding in this
-    // version
-    if (releaseOverride) {
-      Console.error("Sorry, this Windows preview doesn't support --release.");
-    }
-
-    releaseOverride = release.name;
-    // XXX end hack for Windows springboarding
-    
     // Running from an install
     if (releaseOverride) {
       // Use the release explicitly specified on the command line.
