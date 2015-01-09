4b24ba39df785fb958f71ed0ddc4f2a1e3a8f957
Slava Kim <slava@meteor.com>
2015-01-09 12:09:33 -0800

    Check if current dev_bundle matches the dev_bundle required on Windows

--- meteor.bat
+++ meteor.bat
@@ -1,7 +1,18 @@
 @echo off
 
-IF NOT EXIST "%~dp0\dev_bundle" (
-  PowerShell.exe -version 2.0 -file "%~dp0\scripts\windows\download-dev-bundle.ps1"
+rem only if we are running from a checkout
+IF EXIST "%~dp0\.git" (
+  rem if dev_bundle is not present, get it
+  IF NOT EXIST "%~dp0\dev_bundle" (
+    PowerShell.exe -version 2.0 -file "%~dp0\scripts\windows\download-dev-bundle.ps1"
+  )
+
+  rem if dev_bundle is the wrong version, remove it and get a new one
+  PowerShell.exe -version 2.0 -file "%~dp0\scripts\windows\check-dev-bundle.ps1"
+  IF errorlevel 1 (
+    rmdir /s /q "%~dp0\dev_bundle"
+    PowerShell.exe -version 2.0 -file "%~dp0\scripts\windows\download-dev-bundle.ps1"
+  )
 )
 
 SET NODE_PATH=%~dp0\dev_bundle\lib\node_modules
