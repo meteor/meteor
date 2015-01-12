d96c6ab077d00ca702ae9749e8a63bfd4b17ce43
ekatek <ekate@meteor.com>
2015-01-12 14:11:49 -0800

    cosmetic changes to package skeleton
    Some cosmetic changes to the package skeleton:
    - Move version to the top
    - Move git up and write a comment explaining it. Now the
      comment for documentation no longer dominates the stanza!
    
    These are arranges from most to least optional (in some sense).
    The documentation is considered the most optional because removing that
    line doesn't remove the documentation: the line is mostly there for
    an override. I think this makes sense.

--- tools/skel-pack/package.js
+++ tools/skel-pack/package.js
@@ -1,12 +1,13 @@
 Package.describe({
   name: '~name~',
+  version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: '',
-  version: '0.0.1',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md',
-  git: ''
+  documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
