68b684901c0a6907f87aca531c6d21002a946017
Sashko Stubailo <sashko@mit.edu>
2015-01-14 16:04:04 -0800

    Re-add deps on npm-bcrypt and npm-node-aes-gcm

--- packages/accounts-password/package.js
+++ packages/accounts-password/package.js
@@ -4,7 +4,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.use('npm-bcrypt@=0.7.7', 'server');
+  api.use('npm-bcrypt@0.7.8-winr.3');
 
   api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
