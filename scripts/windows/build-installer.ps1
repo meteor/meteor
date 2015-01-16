815ea173ea01bc7ab7a3e0f24302d11fad7488f0
Slava Kim <slava@meteor.com>
2015-01-16 14:14:41 -0800

    Construct installer url based on the meteor release

--- scripts/windows/build-installer.ps1
+++ scripts/windows/build-installer.ps1
@@ -3,8 +3,8 @@ $script_path = (split-path -parent $MyInvocation.MyCommand.Definition) + "\"
 
 If ($Args.Count -ne 1) {
   echo "Usage:"
-  echo "build-installer.ps1 <BOOTSTRAP TARBALL VERSION>"
-  echo "Bootstrap tarball version will be compiled into Installer."
+  echo "build-installer.ps1 <METEOR RELEASE>"
+  echo "The bootstrap tarball url will be compiled into the installer binary based on the Meteor release string."
   echo ""
   exit 1
 }
@@ -14,7 +14,7 @@ echo ("Bootstrap tarball version " + $Args[0])
 
 # Set the version
 $version = $Args[0].replace("`n","").replace("`r","")
-(Get-Content ($script_path + "InstallMeteor.cs")) | Foreach-Object {$_ -replace '__BOOTSTRAP_VERSION__',$version} | Out-File ($script_path + "InstallMeteor_.cs")
+(Get-Content ($script_path + "InstallMeteor.cs")) | Foreach-Object {$_ -replace '__METEOR_RELEASE__',$version} | Out-File ($script_path + "InstallMeteor_.cs")
 
 Invoke-Expression ($env:WINDIR + "\Microsoft.NET\Framework\v3.5\csc.exe /out:" + $script_path + "InstallMeteor.exe " + $script_path + "InstallMeteor_.cs /debug /nologo")
 echo "Done"
