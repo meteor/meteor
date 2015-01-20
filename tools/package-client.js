80e373adf329f2bb4c388d2974f89ac1274e5f4c
Sashko Stubailo <sashko@mit.edu>
2015-01-20 12:28:39 -0800

    Revert "REVERT THIS COMMIT"
    This reverts commit 113d5fe9b017cafc6df0d4c5c71a52489a8c3cf2.

--- tools/package-client.js
+++ tools/package-client.js
@@ -509,6 +509,13 @@ exports.updatePackageMetadata = function (options) {
     buildmessage.error("Summary must be under 100 chars.");
     return;
   }
+  if (dataToUpdate["longDescription"].length > 1500) {
+    buildmessage.error(
+      "Longform package description is too long. Meteor uses the section of",
+      "the Markdown documentation file between the first and second",
+      "headings. That section must be less than 1500 characters long.");
+    return;
+  }
 
   // Update the general metadata.
   var versionIdentifier = { packageName: name, version: version };
@@ -620,6 +627,13 @@ exports.publishPackage = function (options) {
       "Your documentation file is blank, so users may have trouble figuring " +
       "out how to use your package. Please fill it out, or " +
       "set 'documentation: null' in your Package.describe");
+    return;
+  }
+  if (readmeInfo && readmeInfo.excerpt.length > 1500) {
+    buildmessage.error(
+      "Longform package description is too long. Meteor uses the section of",
+      "the Markdown documentation file between the first and second",
+      "headings. That section must be less than 1500 characters long.");
     return;
   }
 
