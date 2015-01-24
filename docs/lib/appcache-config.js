02f799e041501a34982ad77e34ac0c3eae6a24a8
David Glasser <glasser@meteor.com>
2015-01-23 17:06:44 -0800

    docs: disable appcache on Firefox
    Fixes #3248.

--- docs/lib/appcache-config.js
+++ docs/lib/appcache-config.js
@@ -1,11 +1,12 @@
-// Turn off appcache on Safari. Apparently Safari 7's AppCache is
-// totally busted. In particular, this fact combined with our
-// "RELOAD_SAFETYBELT" strategy causes infinite reloads in Safari at
-// times.
-//
-// See http://stackoverflow.com/questions/22888945/safari-7-application-cache-does-not-work
 if (Meteor.isServer) {
   Meteor.AppCache.config({
-    safari: false
+    // Turn off appcache on Safari. Apparently Safari 7's AppCache is totally
+    // busted. In particular, this fact combined with our "RELOAD_SAFETYBELT"
+    // strategy causes infinite reloads in Safari at times.
+    //
+    // See http://stackoverflow.com/questions/22888945/safari-7-application-cache-does-not-work
+    safari: false,
+    // ... and Firefox too.  See eg https://github.com/meteor/meteor/issues/3248
+    firefox: false
   });
 }
