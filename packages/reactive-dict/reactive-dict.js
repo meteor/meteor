9afa16cfffd3541e6f944733507f250c2e443762
Abigail Watson <awatson1978@gmail.com>
2015-01-29 13:35:39 -0500

    styleguide updates, _.each(), circuit-breaker in set(), missing comma, and all the tests work now

--- packages/reactive-dict/reactive-dict.js
+++ packages/reactive-dict/reactive-dict.js
@@ -1,18 +1,18 @@
 // XXX come up with a serialization method which canonicalizes object key
 // order, which would allow us to use objects as values for equals.
-var stringify = function (value) {
+var stringify = function(value) {
   if (value === undefined)
     return 'undefined';
   return EJSON.stringify(value);
 };
-var parse = function (serialized) {
+var parse = function(serialized) {
   if (serialized === undefined || serialized === 'undefined')
     return undefined;
   return EJSON.parse(serialized);
 };
 
 // XXX COMPAT WITH 0.9.1 : accept migrationData instead of dictName
-ReactiveDict = function (dictName) {
+ReactiveDict = function(dictName) {
   // this.keys: key -> value
   if (dictName) {
     if (typeof dictName === 'string') {
@@ -40,40 +40,34 @@ _.extend(ReactiveDict.prototype, {
   // to take an object of key/value pairs, similar to backbone
   // http://backbonejs.org/#Model-set
 
-  set: function (key_or_object, value) {
+  set: function(key_or_object, value) {
     var self = this;
 
-    // if the first argument is a string, use the original key/value method
-    if (typeof key_or_object === 'string') {
-
-      value = stringify(value);
-
-      var oldSerializedValue = 'undefined';
-      if (_.has(self.keys, key_or_object)) oldSerializedValue = self.keys[key_or_object];
-      if (value === oldSerializedValue)
-        return;
-      self.keys[key_or_object] = value;
-
-      var changed = function (v) {
-        v && v.changed();
-      };
-
-      changed(self.keyDeps[key_or_object]);
-      if (self.keyValueDeps[key_or_object]) {
-        changed(self.keyValueDeps[key_or_object][oldSerializedValue]);
-        changed(self.keyValueDeps[key_or_object][value]);
-      }
-
-    // if the first argument is an object and the second is undefined
-    // we treat it as an object of key/value pairs, and iterate through it
-    } else if ((typeof key_or_object === 'object') && (value === undefined)) {
+    if ((typeof key_or_object === 'object') && (value === undefined)) {
       self._setObject(key_or_object);
-    } else {
-      throw new Error("Key must be a string or object of key/value pairs: " + key);
+      return;
+    }
+
+    value = stringify(value);
+
+    var oldSerializedValue = 'undefined';
+    if (_.has(self.keys, key_or_object)) oldSerializedValue = self.keys[key_or_object];
+    if (value === oldSerializedValue)
+      return;
+    self.keys[key_or_object] = value;
+
+    var changed = function(v) {
+      v && v.changed();
+    };
+
+    changed(self.keyDeps[key_or_object]);
+    if (self.keyValueDeps[key_or_object]) {
+      changed(self.keyValueDeps[key_or_object][oldSerializedValue]);
+      changed(self.keyValueDeps[key_or_object][value]);
     }
   },
 
-  setDefault: function (key, value) {
+  setDefault: function(key, value) {
     var self = this;
     // for now, explicitly check for undefined, since there is no
     // ReactiveDict.clear().  Later we might have a ReactiveDict.clear(), in which case
@@ -83,14 +77,14 @@ _.extend(ReactiveDict.prototype, {
     }
   },
 
-  get: function (key) {
+  get: function(key) {
     var self = this;
     self._ensureKey(key);
     self.keyDeps[key].depend();
     return parse(self.keys[key]);
   },
 
-  equals: function (key, value) {
+  equals: function(key, value) {
     var self = this;
 
     // Mongo.ObjectID is in the 'mongo' package
@@ -109,27 +103,27 @@ _.extend(ReactiveDict.prototype, {
     // XXX we could allow arrays as long as we recursively check that there
     // are no objects
     if (typeof value !== 'string' &&
-        typeof value !== 'number' &&
-        typeof value !== 'boolean' &&
-        typeof value !== 'undefined' &&
-        !(value instanceof Date) &&
-        !(ObjectID && value instanceof ObjectID) &&
-        value !== null)
+      typeof value !== 'number' &&
+      typeof value !== 'boolean' &&
+      typeof value !== 'undefined' &&
+      !(value instanceof Date) &&
+      !(ObjectID && value instanceof ObjectID) &&
+      value !== null)
       throw new Error("ReactiveDict.equals: value must be scalar");
     var serializedValue = stringify(value);
 
     if (Tracker.active) {
       self._ensureKey(key);
 
-      if (! _.has(self.keyValueDeps[key], serializedValue))
+      if (!_.has(self.keyValueDeps[key], serializedValue))
         self.keyValueDeps[key][serializedValue] = new Tracker.Dependency;
 
       var isNew = self.keyValueDeps[key][serializedValue].depend();
       if (isNew) {
-        Tracker.onInvalidate(function () {
+        Tracker.onInvalidate(function() {
           // clean up [key][serializedValue] if it's now empty, so we don't
           // use O(n) memory for n = values seen ever
-          if (! self.keyValueDeps[key][serializedValue].hasDependents())
+          if (!self.keyValueDeps[key][serializedValue].hasDependents())
             delete self.keyValueDeps[key][serializedValue];
         });
       }
@@ -139,27 +133,27 @@ _.extend(ReactiveDict.prototype, {
     if (_.has(self.keys, key)) oldValue = parse(self.keys[key]);
     return EJSON.equals(oldValue, value);
   },
-  clear: function(key){
+  clear: function(key) {
     var self = this;
     self.set(key, null);
     return true;
   },
-  toggle: function(key){
+  toggle: function(key) {
     var self = this;
 
     // toggle currently only works on boolean values
     // an elseif is used without a terminating else so that we can
     // explicitely handle the true and false cases
     // while leaving null and undefined cases alone
-    if(self.get(key) === true){
+    if (self.get(key) === true) {
       self.set(key, false);
-    }else if(self.get(key) === false){
+    } else if (self.get(key) === false) {
       self.set(key, true);
     }
     return true;
   },
 
-  _remove: function(key){
+  _remove: function(key) {
     var self = this;
 
     // making a distinction between null and undefined here
@@ -172,16 +166,17 @@ _.extend(ReactiveDict.prototype, {
 
     self.set(key, undefined);
   },
-  _setObject: function(object){
+  _setObject: function(object) {
     var self = this;
-    for (var key in object) {
-      if(object.hasOwnProperty(key)){
-        self.set(key, object[key]);
-      }
-    }
-  }
 
-  _ensureKey: function (key) {
+    _.each(object, function(value, key){
+      if (value) {
+        self.set(key, value);
+      }
+    });
+  },
+
+  _ensureKey: function(key) {
     var self = this;
     if (!(key in self.keyDeps)) {
       self.keyDeps[key] = new Tracker.Dependency;
@@ -191,7 +186,7 @@ _.extend(ReactiveDict.prototype, {
 
   // Get a JSON value that can be passed to the constructor to
   // create a new ReactiveDict with the same contents as this one
-  _getMigrationData: function () {
+  _getMigrationData: function() {
     // XXX sanitize and make sure it's JSONible?
     return this.keys;
   }
