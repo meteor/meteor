c8cd470444d93f7336f27bc6d17c2b70e7afcf36
David Glasser <glasser@meteor.com>
2015-02-03 18:39:22 -0800

    Update other places that mention the Node version

--- tools/bundler.js
+++ tools/bundler.js
@@ -1825,7 +1825,7 @@ var writeSiteArchive = function (targets, outputPath, options) {
 
       builder.write('README', { data: new Buffer(
 "This is a Meteor application bundle. It has only one external dependency:\n" +
-"Node.js 0.10.33 or newer. To run the application:\n" +
+"Node.js 0.10.36 or newer. To run the application:\n" +
 "\n" +
 "  $ (cd programs/server && npm install)\n" +
 "  $ export MONGO_URL='mongodb://user:password@host:port/databasename'\n" +
