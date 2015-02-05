f4da8d49276c6952d1aa2608e66c666803a871b6
Slava Kim <slava@meteor.com>
2015-02-05 15:09:11 -0800

    More Cordova on Windows self-tests

--- tools/tests/cordova-windows-errors.js
+++ tools/tests/cordova-windows-errors.js
@@ -5,14 +5,23 @@ var Sandbox = selftest.Sandbox;
 
 selftest.define("windows prints correct message when it can't do mobile things", ["windows"], function () {
   var s = new Sandbox();
-  
+
   var run;
 
   s.createApp("myapp", "standard-app");
-  s.cd("myapp");
+  s.cd("myapp", function () {
+    _.each(['run', 'add-platform'], function (command) {
+      _.each(['ios', 'android', 'ios-device', 'android-device'], function (platform) {
+        run = s.run(command, platform);
+        // We print some warning that involves saying you're on Windows
+        run.matchErr("Windows");
+      });
+    });
+  });
 
-  run = s.run("run", "android");
-
-  // We print some warning that involves saying you're on Windows
-  run.matchErr("Windows");
+  s.run("create", "--example", "todos");
+  s.cd("todos", function () {
+    run = s.run("build");
+    run.matchErr("Windows");
+  });
 });
