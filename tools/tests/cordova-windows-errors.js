85dcd56628f64af99a43aa8ad78f78d96c155cd7
Sashko Stubailo <s.stubailo@gmail.com>
2015-02-05 10:56:25 -0800

    Add a self test for windows cordova message

---
+++ tools/tests/cordova-windows-errors.js
@@ -0,0 +1,18 @@
+var files = require('../files.js');
+var selftest = require('../selftest.js');
+var Sandbox = selftest.Sandbox;
+
+
+selftest.define("windows prints correct message when it can't do mobile things", function () {
+  var s = new Sandbox();
+  
+  var run;
+
+  s.createApp("myapp", "standard-app");
+  s.cd("myapp");
+
+  run = s.run("run", "android");
+
+  // We print some warning that involves saying you're on Windows
+  run.matchErr("Windows");
+});
