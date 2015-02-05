2c1e27d9731145ce0b65b293d6ee0251bbd1055e
Slava Kim <slava@meteor.com>
2015-02-05 14:04:59 -0800

    Add a self-test for meteor script parsing

---
+++ tools/tests/meteor-script-link-tests.js
@@ -0,0 +1,11 @@
+var selftest = require('../selftest.js');
+var files = require('../files.js');
+
+selftest.define("linking to meteor script works correctly on Windows", function () {
+  var location =
+    '/C/some obscure/location/on disk/with some unicode/Вот тебе и юникод/';
+  var script = files._generateScriptLinkToMeteorScript(location);
+  var parsedLocation = files._getLocationFromScriptLinkToMeteorScript(script);
+  selftest.expectEqual(parsedLocation, location);
+});
+
