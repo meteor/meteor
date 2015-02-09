b7926e688a7ff2f62c233153b69d4135f1ccbe7e
Sashko Stubailo <sashko@mit.edu>
2015-02-09 13:23:24 -0800

    Bump a bunch of version numbers for new preview

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Allows templates to be defined in .html files",
-  version: '1.0.11-winr.5'
+  version: '1.0.11-winr.6'
 });
 
 // Today, this package is closely intertwined with Handlebars, meaning
