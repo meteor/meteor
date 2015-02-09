466559a8c41ff8b623f56d1a04037a0c5ad485a7
Sashko Stubailo <sashko@mit.edu>
2015-02-09 11:58:39 -0800

    Switch to using a fork of fibers on Windows

--- scripts/dev-bundle-server-package.js
+++ scripts/dev-bundle-server-package.js
@@ -4,12 +4,23 @@
 // We put this in a JS file so that it can contain comments. It is processed
 // into a package.json file by generate-dev-bundle.sh.
 
+var fibersVersion;
+if (process.platform === "win32") {
+  // We have a fork of fibers off of version 1.0.5 that searches farther for
+  // the isolate thread. This problem is a result of antivirus programs messing
+  // with the thread counts on Windows.
+  // Duplicated in dev-bundle-tool-package.js
+  fibersVersion = "https://github.com/meteor/node-fibers/tarball/d519f0c5971c33d99c902dad346b817e84bab001";
+} else {
+  fibersVersion = "1.0.5";
+}
+
 var packageJson = {
   name: "meteor-dev-bundle",
   // Version is not important but is needed to prevent warnings.
   version: "0.0.0",
   dependencies: {
-    fibers: "1.0.5",
+    fibers: fibersVersion,
     // Not yet upgrading Underscore from 1.5.2 to 1.7.0 (which should be done
     // in the package too) because we should consider using lodash instead
     // (and there are backwards-incompatible changes either way).
