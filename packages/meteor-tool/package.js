410ddfff9ae050866ebbe22a8968282307afa0f7
David Glasser <glasser@meteor.com>
2015-02-10 12:18:43 -0800

    OPLOG-BACKLOG@3

--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "The Meteor command-line tool",
-  version: '1.0.41-oplog-backlog.2'
+  version: '1.0.41-oplog-backlog.3'
 });
 
 Package.includeTool();
--- packages/mongo/package.js
+++ packages/mongo/package.js
@@ -9,7 +9,7 @@
 
 Package.describe({
   summary: "Adaptor for using MongoDB and Minimongo over DDP",
-  version: '1.0.12-oplog-backlog.1'
+  version: '1.0.12-oplog-backlog.2'
 });
 
 Npm.depends({
