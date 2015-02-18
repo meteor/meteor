e12b60a956b949c72a6664f945399531aa685723
alanning <alanning@gmail.com>
2015-02-18 15:17:01 -0500

    Add example of using roles with accounts-ui

---
+++ examples/rolesWithAccountsUI/rolesWithAccountsUI.js
@@ -0,0 +1,23 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
---
+++ examples/rolesWithAccountsUI/server/startup.js
@@ -0,0 +1,19 @@
+Meteor.startup(function () {
+
+  console.log('Running server startup code...');
+
+  Accounts.onCreateUser(function (options, user) {
+    Roles.addRolesToUserObj(user, ['admin','view-secrets']);
+
+    if (options.profile) {
+      // include the user profile
+      user.profile = options.profile
+    }
+
+    // other user object changes...
+    // ...
+    
+    return user;
+  });
+
+});
