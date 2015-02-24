c8c84f5a9c6ebf7a50de6d9b43c3d5ccce55cf49
Slava Kim <slava@meteor.com>
2015-02-24 13:06:13 -0800

    Adapt the jenkins build script for the new installer.

--- scripts/windows/build-installer.ps1
+++ scripts/windows/build-installer.ps1
@@ -1,5 +1,6 @@
 $ErrorActionPreference = "Stop"
 $script_path = (split-path -parent $MyInvocation.MyCommand.Definition) + "\"
+$conf_path = $script_path + "wix-installer\WiXInstaller\Configuration.wxi"
 
 If ($Args.Count -ne 1) {
   echo "Usage:"
@@ -14,8 +15,17 @@ echo ("Bootstrap tarball version " + $Args[0])
 
 # Set the version
 $version = $Args[0].replace("`n","").replace("`r","")
-(Get-Content ($script_path + "InstallMeteor.cs")) | Foreach-Object {$_ -replace '__METEOR_RELEASE__',$version} | Out-File ($script_path + "InstallMeteor_.cs")
+(Get-Content ($conf_path + "_")) | Foreach-Object {$_ -replace '__METEOR_RELEASE__',$version} | Out-File ($conf_path)
 
-Invoke-Expression ($env:WINDIR + "\Microsoft.NET\Framework\v3.5\csc.exe /out:" + $script_path + "InstallMeteor.exe " + $script_path + "InstallMeteor_.cs /debug /nologo")
+# download 7za.exe, build dependency that we don't want to build from scratch
+$7za_url = "https://s3.amazonaws.com/meteor-windows/build-deps/7za.exe"
+$client = new-object System.Net.WebClient
+$client.DownloadFile($7za_url, $script_path + "wix-installer\WiXInstaller\Resources\7za.exe")
+
+Push-Location wix-installer
+Invoke-Expression ("cmd /c build.bat")
+Pop-Location
+
+#rm $conf_path
 echo "Done"
 
--- scripts/windows/wix-installer/WiXBalExtension/inc/wixver.h
+++ scripts/windows/wix-installer/WiXBalExtension/inc/wixver.h
@@ -3,11 +3,11 @@
 #define _VERSION_FILE_H_
 
 #define szVerMajorMinor "3.7"
-#define szVerMajorMinorBuildRev "3.7.5533.1995"
+#define szVerMajorMinorBuildRev "3.7.5534.7090"
 #define rmj	3
 #define rmm	7
-#define rbd	5533
-#define rev	1995
+#define rbd	5534
+#define rev	7090
 #define szVerName	"BalExtensionExt Release"
 
 #endif
