132102e9a5ecdc0115efb5dc19ff832045ac90e5
David Glasser <glasser@meteor.com>
2015-02-25 17:38:51 -0800

    Make observeChanges throw on bad query
    Fixes #2534.
    
    In the past, a bad query would be retried forever, causing the
    observeChanges to hang.
    
    (In the common case of a DDP subscription, this would also cause the
    current DDP connection to completely stop reading messages, and would
    make methods that do writes to the same collection (on ANY DDP
    connection) never get their 'updated' messages.  See
    https://github.com/meteor/meteor/issues/2534#issuecomment-73355050
    These two secondary problems may still need to be fixed but at least
    the root cause should be addressed.)
    
    This only fixes PollingObserveDriver, not OplogObserveDriver, but this
    issue typically only affects PollingObserveDriver because we don't
    choose to use OplogObserveDriver when minimongo can't understand a
    query.

--- packages/mongo/observe_changes_tests.js
+++ packages/mongo/observe_changes_tests.js
@@ -332,12 +332,40 @@ if (Meteor.isServer) {
 testAsyncMulti("observeChanges - bad query", [
   function (test, expect) {
     var c = makeCollection();
-    test.throws(function () {
-      c.find({__id: {$in: null}}).observeChanges({
-        added: function () {
-          test.fail("added shouldn't be called");
-        }
-      });
-    });
+    var observeThrows = function () {
+      test.throws(function () {
+        c.find({__id: {$in: null}}).observeChanges({
+          added: function () {
+            test.fail("added shouldn't be called");
+          }
+        });
+      }, '$in needs an array');
+    };
+
+    if (Meteor.isClient) {
+      observeThrows();
+      return;
+    }
+
+    // Test that if two copies of the same bad observeChanges run in parallel
+    // and are de-duped, both observeChanges calls will throw.
+    var Fiber = Npm.require('fibers');
+    var Future = Npm.require('fibers/future');
+    var f1 = new Future;
+    var f2 = new Future;
+    Fiber(function () {
+      // The observeChanges call in here will yield when we talk to mongod,
+      // which will allow the second Fiber to start and observe a duplicate
+      // query.
+      observeThrows();
+      f1['return']();
+    }).run();
+    Fiber(function () {
+      test.isFalse(f1.isResolved());  // first observe hasn't thrown yet
+      observeThrows();
+      f2['return']();
+    }).run();
+    f1.wait();
+    f2.wait();
   }
 ]);
