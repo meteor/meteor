c8ebdd2473581713bd1dfa84b55938281bd8888e
hwillson <hugh@octonary.com>
2015-02-26 14:17:01 -0500

    Reverting back to using secure = true with Meteor.absoluteUrl() and added the force-ssl package to make sure docs are always served via SSL.

--- docs/client/lib/navigate.js
+++ docs/client/lib/navigate.js
@@ -1,7 +1,3 @@
 navigate = function (hash) {
-  var secure = false;
-  if (/^https:/.test(window.location.href)) {
-    secure = true;
-  }
-  window.location.replace(Meteor.absoluteUrl(null, { secure: secure }) + hash);
+  window.location.replace(Meteor.absoluteUrl(null, { secure: true }) + hash);
 };
