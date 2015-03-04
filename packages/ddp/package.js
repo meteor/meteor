eec489215e72591500b1b1a090a21134293fbca7
Sashko Stubailo <sashko@stubailo.com>
2015-03-04 13:51:46 -0800

    Bump a bunch of version numbers

--- packages/ddp/package.js
+++ packages/ddp/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Meteor's latency-compensated distributed data framework",
-  version: '1.0.15-winr.5'
+  version: '1.0.15-winr.6'
 });
 
 // We use 'faye-websocket' for connections in server-to-server DDP, mostly
