395f65859b19f8cbefa8b763c93a3b4eca384287
Sashko Stubailo <sashko@stubailo.com>
2015-03-09 13:40:26 -0700

    Re-add helpful changes from f2840103ca23dedd458524ad4e01609069ce28cb

--- packages/spacebars-tests/template_tests.js
+++ packages/spacebars-tests/template_tests.js
@@ -10,7 +10,19 @@ var nodesToArray = function (array) {
   return _.map(array, _.identity);
 };
 
+var inDocument = function (elem) {
+  while ((elem = elem.parentNode)) {
+    if (elem == document) {
+      return true;
+    }
+  }
+  return false;
+};
+
 var clickIt = function (elem) {
+  if (!inDocument(elem))
+    throw new Error("Can't click on elements without first adding them to the document");
+
   // jQuery's bubbling change event polyfill for IE 8 seems
   // to require that the element in question have focus when
   // it receives a simulated click.
