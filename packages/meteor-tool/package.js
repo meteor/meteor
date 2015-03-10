891a9c38939ecc62c3b616068aad66d525aa5ae7
David Glasser <glasser@meteor.com>
2015-03-10 11:49:25 -0700

    1.0.4-rc.2

--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "The Meteor command-line tool",
-  version: '1.0.42-rc.2'
+  version: '1.0.42-rc.3'
 });
 
 Package.includeTool();
--- packages/webapp/package.js
+++ packages/webapp/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Serves a Meteor app over HTTP",
-  version: '1.2.0-rc.0'
+  version: '1.2.0-rc.1'
 });
 
 Npm.depends({connect: "2.9.0",
