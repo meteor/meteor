f14464b59add4469cbb569be14f39f003df0e164
David Glasser <glasser@davidglasser.net>
2015-03-27 16:22:44 -0700

    findMongoPids: use pgrep where available
    ps corrupts some non-ASCII characters on OS X.  pgrep doesn't, but isn't
    available everywhere.
    
    Fixes #3999.

--- tools/tests/mongo.js
+++ tools/tests/mongo.js
@@ -19,3 +19,38 @@ selftest.define("mongo failover", ["slow"], function () {
   run.expectEnd();
   run.expectExit(0);
 });
+
+var testMeteorMongo = function (appDir) {
+  var s = new Sandbox();
+  s.createApp(appDir, 'standard-app');
+  s.cd(appDir);
+
+  var run = s.run();
+  run.waitSecs(15);
+  run.match(appDir);
+  run.match('proxy');
+  run.match('Started MongoDB');
+
+  var mongoRun = s.run('mongo');
+  mongoRun.match('MongoDB shell');
+  mongoRun.match('connecting to: 127.0.0.1');
+  // Note: when mongo shell's input is not a tty, there is no prompt.
+  mongoRun.write('db.version()\n');
+  mongoRun.match(/2\.6\.\d+/);
+  mongoRun.stop();
+};
+
+selftest.define("meteor mongo", function () {
+  testMeteorMongo('asdfzasdf');
+});
+
+// Regression test for #3999.  Note the Cyrillic character in the pathname.
+//
+// XXX This test fails on Windows for two different reasons:
+// - With the Unicode directory name, `meteor run` fails to start mongod
+// - If you change appDir to not have the Unicode character, the reads
+//   from the mongo shell process seem to be randomly corrupted
+// https://github.com/meteor/windows-preview/issues/145
+selftest.define("meteor mongo in unicode dir", function () {
+  testMeteorMongo('asdf\u0442asdf');
+});
