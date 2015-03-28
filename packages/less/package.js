d2427c9b8e0c6481b3277fe56d7f47a360ce44d8
Avital Oliver <avital@thewe.net>
2015-03-28 12:17:28 -0700

    1.1-rc.3

--- packages/less/package.js
+++ packages/less/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "The dynamic stylesheet language",
-  version: "1.0.14-rc.1"
+  version: "1.0.14-rc.2"
 });
 
 Package.registerBuildPlugin({
--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "The Meteor command-line tool",
-  version: '1.1.1-win.9'
+  version: '1.1.1-win.11'
 });
 
 Package.includeTool();
