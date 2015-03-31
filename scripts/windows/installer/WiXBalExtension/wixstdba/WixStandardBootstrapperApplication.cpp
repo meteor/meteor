d978bc7c82d82f80e4fbc04715e2341f57c9ea75
Slava Kim <slava@meteor.com>
2015-03-31 12:35:16 -0700

    Don't broadcast a dead message that blocks the process from exiting

--- scripts/windows/installer/WiXBalExtension/wixstdba/WixStandardBootstrapperApplication.cpp
+++ scripts/windows/installer/WiXBalExtension/wixstdba/WixStandardBootstrapperApplication.cpp
@@ -2183,12 +2183,6 @@ LExit:
 		LOC_STRING* pLocString = NULL;
 		m_state = state;
 
-		// If we just finished the installation, we need to broadcast an
-		// WM_SETTINGCHANGE message so that CMD picks up the new path
-		if (WIXSTDBA_STATE_APPLIED == m_state) {
-			SendMessageTimeout(HWND_BROADCAST, WM_SETTINGCHANGE, 0, (LPARAM)L"Environment", SMTO_ABORTIFHUNG, 5000, NULL);
-		}
-
 		// If our install is at the end (success or failure) and we're not showing full UI or
 		// we successfully installed the prerequisite then exit (prompt for restart if required).
 		if ((WIXSTDBA_STATE_APPLIED <= m_state && BOOTSTRAPPER_DISPLAY_FULL > m_command.display) ||
