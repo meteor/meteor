274b77984734c440bab930af43584a7e75ae5896
Avital Oliver <avital@thewe.net>
2015-04-21 11:14:00 -0700

    Windows Installer: New API for downloading tarball
    This API actually lets us download the bootstrap tarball from cloudfront
    on Windows Server, /and/ correctly reads the proxy settings from Internet
    Explorer.

--- scripts/windows/build-installer.ps1
+++ scripts/windows/build-installer.ps1
@@ -21,11 +21,16 @@ $semverVersion = $version.Split("@")[-1].split("-")[0]
   $_ -replace '__METEOR_RELEASE__',$version `
      -replace '__METEOR_RELEASE_SEMVER__',$semverVersion} | Out-File -Encoding ascii ($conf_path)
 
-# download 7za.exe, build dependency that we don't want to build from scratch
-echo "Downloading binary dependencies: 7za"
-$7za_url = "https://s3.amazonaws.com/meteor-windows/build-deps/7za.exe"
-$client = new-object System.Net.WebClient
-$client.DownloadFile($7za_url, $script_path + "installer\WiXInstaller\Resources\7za.exe")
+
+$web_client = new-object System.Net.WebClient
+
+# download 7za.exe, a build dependency that we don't want to compile each time
+$7za_path = $script_path + "installer\WiXInstaller\Resources\7za.exe"
+if (!(Test-Path $7za_path)) {
+	echo "Downloading binary dependencies: 7za"
+	$7za_url = "https://s3.amazonaws.com/meteor-windows/build-deps/7za.exe"
+	$web_client.DownloadFile($7za_url, $7za_path)
+}
 
 Push-Location installer
 Invoke-Expression ("cmd /c build.bat")
