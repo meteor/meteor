179b2b9b177d79475029fa14d793572699ee00ac
Kelly <copleykj@gmail.com>
2015-05-14 23:59:36 -0400

    new package for easy crosswalk integration

---
+++ packages/crosswalk/package.js
@@ -0,0 +1,9 @@
+Package.describe({
+    name: 'crosswalk',
+    summary: 'Crosswalk integration for Cordova Android builds',
+    version: '1.2.0'
+});
+
+Cordova.depends({
+    'cordova-plugin-crosswalk-webview': 'https://github.com/crosswalk-project/cordova-plugin-crosswalk-webview/tarball/ef4d7244f107c86b85a75edd5492f8e2f63b1bd7'
+});
