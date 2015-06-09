d7be19b35669d260c40d931773ef4d50f765d13e
David Glasser <glasser@davidglasser.net>
2015-06-09 00:04:08 -0700

    comments about other registerSourceHandler calls

--- packages/stylus/plugin/compile-stylus.js
+++ packages/stylus/plugin/compile-stylus.js
@@ -4,6 +4,9 @@ var nib = Npm.require('nib');
 var path = Npm.require('path');
 var Future = Npm.require('fibers/future');
 
+// XXX BBP probably rewrite to registerCompiler even if we can't implement
+// good @imports because stylus doesn't let you override @import processing.
+// (maybe deprecate this package?  put out a call for PRs?)
 Plugin.registerSourceHandler("styl", {archMatching: 'web'}, function (compileStep) {
   var f = new Future;
   stylus(compileStep.read().toString('utf8'))
