be42d7295f72373d8d1f8550561107551a326788
David Glasser <glasser@davidglasser.net>
2015-06-08 21:02:55 -0700

    self-test: show pattern for match-timeout

--- tools/selftest.js
+++ tools/selftest.js
@@ -222,10 +222,12 @@ _.extend(Matcher.prototype, {
     var timer = null;
     if (timeout) {
       timer = setTimeout(function () {
+        var pattern = self.matchPattern;
         self.matchPattern = null;
         self.matchStrict = null;
         self.matchFuture = null;
-        f['throw'](new TestFailure('match-timeout', { run: self.run }));
+        f['throw'](new TestFailure('match-timeout', { run: self.run,
+                                                      pattern: pattern }));
       }, timeout * 1000);
     }
 
@@ -1794,7 +1796,8 @@ var runTests = function (options) {
                                          frames[0].file);
         Console.rawError("  => " + failure.reason + " at " +
                          relpath + ":" + frames[0].line + "\n");
-        if (failure.reason === 'no-match' || failure.reason === 'junk-before') {
+        if (failure.reason === 'no-match' || failure.reason === 'junk-before' ||
+            failure.reason === 'match-timeout') {
           Console.arrowError("Pattern: " + failure.details.pattern, 2);
         }
         if (failure.reason === "wrong-exit-code") {
