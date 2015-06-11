a71aa46d72511a0cffaea595e7df0109737f095c
Sashko Stubailo <sashko@stubailo.com>
2015-06-10 20:46:06 -0700

    Fix one file

--- tools/auth-client.js
+++ tools/auth-client.js
@@ -1,42 +1,44 @@
-var auth = require('./auth.js');
-var Console = require('./console.js').Console;
-var ServiceConnection = require('./service-connection.js');
-var httpHelpers = require('./http-helpers.js');
+import auth from './auth';
+import { Console } from './console.js';
+import ServiceConnection from './service-connection';
+import httpHelpers from './http-helpers';
 
-exports.AlreadyPrintedMessageError = function () {};
+export function AlreadyPrintedMessageError() {}
 
 // Opens a DDP connection to a package server. Loads the packages needed for a
 // DDP connection, then calls DDP connect to the package server URL in config,
 // using a current user-agent header composed by http-helpers.js.
-exports.openServiceConnection = function (serverUrl) {
+export function openServiceConnection(serverUrl) {
   return new ServiceConnection(
     serverUrl,
-    {headers: {"User-Agent": httpHelpers.getUserAgent()},
-     _dontPrintErrors: true});
-};
+    { headers: { 'User-Agent': httpHelpers.getUserAgent() },
+      _dontPrintErrors: true });
+}
 
 
 // Handle an error thrown on attempting to connect. Print a message if it is a
 // known error type, else throw the error.
 //
 // err: error
-// label: name of the service that we are trying to use (ex: "package server")
-exports.handleConnectionError = function (error, label) {
-  if (error instanceof exports.AlreadyPrintedMessageError) {
-    // do nothing
-  } else if (error.errorType === 'Meteor.Error') {
-    var errorMsg = "Error from " + label;
+// label: name of the service that we are trying to use (ex: 'package server')
+export function handleConnectionError(error, label) {
+  if (error instanceof AlreadyPrintedMessageError) {
+    return;
+  }
+
+  if (error.errorType === 'Meteor.Error') {
+    var errorMsg = 'Error from ' + label;
     if (error.message) {
-      errorMsg += ": " + error.message;
+      errorMsg += ': ' + error.message;
     }
     Console.warn(errorMsg);
-  } else if (error.errorType === "DDP.ConnectionError") {
-    Console.warn("Error connecting to " + label + ": "
+  } else if (error.errorType === 'DDP.ConnectionError') {
+    Console.warn('Error connecting to ' + label + ': '
                  + error.message);
   } else {
     throw error;
   }
-};
+}
 
 // Returns a logged-in DDP connection to the given URL, or null if
 // we cannot log in. If an error unrelated to login occurs
@@ -45,27 +47,27 @@ exports.handleConnectionError = function (error, label) {
 //
 //  url: the url of the connection (ex: config.getPackageServerUrl)
 //  domain: the domain (ex: packages.meteor.com)
-//  sessionType: the name of the connection (ex: "package-server")
+//  sessionType: the name of the connection (ex: 'package-server')
 //
-exports.loggedInConnection = function (url, domain, sessionType) {
+export function loggedInConnection(url, domain, sessionType) {
   // Make sure that we are logged in with Meteor Accounts so that we can
   // do an OAuth flow.
-  if (auth.maybePrintRegistrationLink({onlyAllowIfRegistered: true})) {
+  if (auth.maybePrintRegistrationLink({ onlyAllowIfRegistered: true })) {
     // Oops, we're logged in but with a deferred-registration account.
     // Message has already been printed.
-    throw new exports.AlreadyPrintedMessageError;
+    throw new AlreadyPrintedMessageError();
   }
 
-  if (! auth.isLoggedIn()) {
+  if (!auth.isLoggedIn()) {
     // XXX we should have a better account signup page.
     Console.error(
-      "Please log in with your Meteor developer account.",
-      "If you don't have one,",
-      "you can quickly create one at www.meteor.com.");
+      'Please log in with your Meteor developer account.',
+      'If you don\'t have one,',
+      'you can quickly create one at www.meteor.com.');
     auth.doUsernamePasswordLogin({ retry: true });
   }
 
-  var conn = exports.openServiceConnection(url);
+  var conn = openServiceConnection(url);
   var accountsConfiguration = auth.getAccountsConfiguration(conn);
   try {
     auth.loginWithTokenOrOAuth(
@@ -76,13 +78,13 @@ exports.loggedInConnection = function (url, domain, sessionType) {
       sessionType
     );
   } catch (err) {
-    if (err.message === "access-denied") {
+    if (err.message === 'access-denied') {
       // Maybe we thought we were logged in, but our token had been
       // revoked.
       Console.error(
-        "It looks like you have been logged out!",
-        "Please log in with your Meteor developer account. If you don't have",
-        "one, you can quickly create one at www.meteor.com.");
+        'It looks like you have been logged out!',
+        'Please log in with your Meteor developer account. If you don\'t have',
+        'one, you can quickly create one at www.meteor.com.');
       auth.doUsernamePasswordLogin({ retry: true });
       auth.loginWithTokenOrOAuth(
         conn,
@@ -96,4 +98,4 @@ exports.loggedInConnection = function (url, domain, sessionType) {
     }
   }
   return conn;
-};
+}
