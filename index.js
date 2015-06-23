bdc8d05d9d47c6774c3af73394ec97f3487a71bd
Ben Newman <ben@meteor.com>
2015-06-23 14:24:56 -0400

    Expose the Babel parse function.

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var getDefaultOptions = require("./options").getDefaults;
 // first, modify the result, and then pass those options to compile.
 exports.getDefaultOptions = getDefaultOptions;
 
+exports.parse = function parse(source, options) {
+  return require("babel-core").parse(source, options);
+};
+
 exports.compile = function compile(source, options) {
   options = options || getDefaultOptions();
   return require("babel-core").transform(source, options);
