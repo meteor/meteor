38ce0d1f3c6c067d6947771019e8ef1bee4317cf
Ben Newman <ben@meteor.com>
2015-06-23 16:36:22 -0400

    Bump meteor-babel version to 0.1.12.

--- scripts/dev-bundle-tool-package.js
+++ scripts/dev-bundle-tool-package.js
@@ -21,7 +21,7 @@ var packageJson = {
   version: "0.0.0",
   dependencies: {
     fibers: fibersVersion,
-    "meteor-babel": "0.1.11",
+    "meteor-babel": "0.1.12",
     // Not yet upgrading Underscore from 1.5.2 to 1.7.0 (which should be done
     // in the package too) because we should consider using lodash instead
     // (and there are backwards-incompatible changes either way).
