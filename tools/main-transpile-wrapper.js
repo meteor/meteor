9b5ffcabde3adfbd45395f930045b0779cf3ddff
Ben Newman <ben@meteor.com>
2015-06-24 16:35:04 -0400

    Upgrade meteor-babel and install meteor-promise to polyfill Promise.

--- tools/main-transpile-wrapper.js
+++ tools/main-transpile-wrapper.js
@@ -3,9 +3,13 @@
 
 require('meteor-babel/register'); // #RemoveInProd this line is removed in isopack.js
 
+// Install a global ES6-compliant Promise constructor that knows how to
+// run all its callbacks in Fibers.
+global.Promise = require("meteor-promise");
+
 // Include helpers from NPM so that the compiler doesn't need to add boilerplate
 // at the top of every file
-require("meteor-babel").runtime();
+require("meteor-babel").installRuntime();
 
 // Installs source map support with a hook to add functions to look for source
 // maps in custom places
