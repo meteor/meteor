69fc61df2606a849f59ee762815aea8c7b835e65
Martijn Walraven <martijn@martijnwalraven.com>
2015-07-09 09:10:53 -0700

    Update to Cordova 5.1.1

--- scripts/dev-bundle-tool-package.js
+++ scripts/dev-bundle-tool-package.js
@@ -57,7 +57,7 @@ var packageJson = {
     // 2.4.0 (more or less, the package.json change isn't committed) plus our PR
     // https://github.com/williamwicks/node-eachline/pull/4
     eachline: "https://github.com/meteor/node-eachline/tarball/ff89722ff94e6b6a08652bf5f44c8fffea8a21da",
-    cordova: "5.0.0",
+    cordova: "5.1.1",
     "ios-sim": "4.1.1",
     pathwatcher: "4.1.0",
     'lru-cache': '2.6.4'
