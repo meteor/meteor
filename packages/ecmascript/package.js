72e4a2523e98799fb8bf4c227c2a00cd3fde1902
Ben Newman <ben@meteor.com>
2015-07-14 23:54:10 -0400

    Polyfill Promise constructor for ecmascript-dependent apps/packages.
    I'm going to hold off on adding polyfills for Map and Set until we've had
    a chance to try them out in tool code.

--- packages/ecmascript/package.js
+++ packages/ecmascript/package.js
@@ -5,6 +5,10 @@ Package.describe({
   documentation: 'README.md'
 });
 
+Npm.depends({
+  "meteor-promise": "0.2.4"
+});
+
 Package.registerBuildPlugin({
   name: 'compile-ecmascript',
   use: ['babel-compiler@5.6.17'],
@@ -14,4 +18,14 @@ Package.registerBuildPlugin({
 Package.onUse(function (api) {
   api.use('isobuild:compiler-plugin@1.0.0');
   api.imply('babel-runtime@0.1.0');
+
+  api.addFiles("promise_server.js", "server");
+  api.addFiles(
+    // This may not be the most robust way of referring to an NPM asset,
+    // but at least api.addFiles will fail if the file does not exist.
+    '.npm/package/node_modules/meteor-promise/promise_client.js',
+    'client',
+    { bare: true }
+  );
+  api.export("Promise");
 });
