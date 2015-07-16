c1e7f76d388a48c390650bfcad54d24a73f02680
Slava Kim <slava@meteor.com>
2015-07-16 11:12:24 -0700

    Fix the CSS splitting test

--- tools/tests/standard-minification.js
+++ tools/tests/standard-minification.js
@@ -10,28 +10,29 @@ selftest.define('standard-minifiers - CSS splitting', function (options) {
 
   s.testWithAllClients(function (run) {
     run.waitSecs(5);
-    run.match("myapp");
-    run.match("proxy");
-    run.match("MongoDB");
-    run.match("your app");
-    run.match("running at");
-    run.match("localhost");
+    run.match('myapp');
+    run.match('proxy');
+    run.match('MongoDB');
+    run.match('your app');
+    run.match('running at');
+    run.match('localhost');
 
     run.connectClient();
     run.waitSecs(20);
+    run.match('client connected');
 
     run.match('the number of stylesheets: <2>');
     run.match('the color of the tested 4097th property: <rgb(0, 128, 0)>');
 
     s.append('client/lots-of-styles.main.styl', `
-  .class-4097
-    color: blue
-  `);
+.class-4097
+  color: blue
+`);
 
-    run.waitSecs(40);
-    run.match("Client modified -- refreshing");
+    run.waitSecs(60);
+    run.match('Client modified -- refreshing');
     run.match('the number of stylesheets: <2>');
-    run.match('the color of the tested 4097th property: <rgb(0, 0, 128)>');
+    run.match('the color of the tested 4097th property: <rgb(0, 0, 255)>');
 
     run.stop();
   }, '--production');
