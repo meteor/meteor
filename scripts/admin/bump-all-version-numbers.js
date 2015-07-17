ed43873fb0d9ddfca09530888cb621e2c714ae27
ekatek <ekate@meteor.com>
2015-07-17 11:57:44 -0700

    prepare to make a release

--- scripts/admin/bump-all-version-numbers.js
+++ scripts/admin/bump-all-version-numbers.js
@@ -6,19 +6,23 @@ var _ = require("../../packages/underscore/underscore.js")._;
 var packageNames = _.rest(process.argv, 2);
 
 _.each(packageNames, function (name) {
-  // name = "packages/" + name + "/package.js";
+  try {
+    name = "packages/" + name + "/package.js";
 
   var content = fs.readFileSync(name, {encoding: "utf-8"});
 
-  match = content.match(/\d+\.\d+\.\d+-winr.\d+/);
+  match = content.match(/\d+\.\d+\.\d+/);
   if (match) {
     var versionNumber = match[0];
     var s = versionNumber.split(".");
     s[3] = (parseInt(s[3], 10) + 1);
-    var incremented = s.join(".");
+    var incremented = s.join(".") + "-galaxy.0";
 
     content = content.replace(versionNumber, incremented);
-    console.log(match[0], match[1], incremented);
+    console.log(name, match[0], match[1], incremented);
     fs.writeFileSync(name, content);
-  }
+   }
+   }catch(e) {
+     console.log(e);
+   }
 });
