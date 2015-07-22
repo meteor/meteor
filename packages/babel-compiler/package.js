937defa9bcb80e2b9b54927d066d3fe9ed00205e
Ben Newman <ben@meteor.com>
2015-07-21 21:50:12 -0400

    Upgrade meteor-babel to 0.4.6 and thus babel-core to 5.8.3.

--- packages/babel-compiler/package.js
+++ packages/babel-compiler/package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: "babel-compiler",
-  summary: "Parser/transpiler for ECMAScript 6+ syntax",
+  summary: "Parser/transpiler for ECMAScript 2015+ syntax",
   // Tracks the npm version below.  Use wrap numbers to increment
   // without incrementing the npm version.
-  version: '5.7.3'
+  version: '5.8.3'
 });
 
 Npm.depends({
-  'meteor-babel': '0.4.5'
+  'meteor-babel': '0.4.6'
 });
 
 Package.onUse(function (api) {
