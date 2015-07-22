155d3aef1a2d1e31804dc22f13478fb2e665ff1a
Ben Newman <ben@meteor.com>
2015-07-21 22:04:34 -0400

    Make ecmascript depend on latest babel-compiler and promise packages.

--- packages/ecmascript/package.js
+++ packages/ecmascript/package.js
@@ -1,20 +1,20 @@
 Package.describe({
   name: 'ecmascript',
-  version: '0.1.1',
+  version: '0.1.2',
   summary: 'Compiler plugin that supports ES2015+ in all .js files',
   documentation: 'README.md'
 });
 
 Package.registerBuildPlugin({
   name: 'compile-ecmascript',
-  use: ['babel-compiler@5.7.3'],
+  use: ['babel-compiler@5.8.3'],
   sources: ['plugin.js']
 });
 
 Package.onUse(function (api) {
   api.use('isobuild:compiler-plugin@1.0.0');
   api.imply('babel-runtime@0.1.2');
-  api.imply('promise@0.4.0');
+  api.imply('promise@0.4.1');
 });
 
 Package.onTest(function (api) {
