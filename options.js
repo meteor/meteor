ecd684a3fe5045a663367e0795c9aeed9c578cf4
Ben Newman <ben@meteor.com>
2015-07-23 13:12:23 -0400

    Don't use the jscript plugin by default.
    It's only necessary in older web browsers (e.g. IE6-8), and it has the
    effect of wrapping every class method in an IIFE, which is probably not
    desirable.

--- options.js
+++ options.js
@@ -25,7 +25,6 @@ exports.getDefaults = function getDefaults(features) {
       "es7.objectRestSpread",
       "es6.destructuring",
       "es7.trailingFunctionCommas",
-      "jscript",
       "flow"
     ]
   };
