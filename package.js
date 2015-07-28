7cf111bc7c2e328ce1c4cbfba29d8160a3f73729
Sashko Stubailo <sashko@stubailo.com>
2015-07-28 10:33:40 -0700

    Static HTML package and templating refactor
    1. Make a package called `static-html` that just compiles `<head>` and `<body>` tags inside `.html` files. Much like templating but without the templates.
    2. Refactor `templating` to avoid duplicating code.
    
    Split out code that would be shared between `templating` and `static-html` into a new `templating-tools` package. These tools could also be used to simplify implementation of other build plugins, like `simple:markdown-templating`.
    
    This also has the added benefit of moving as much code as humanly possible out of the `templating` package, so that it can be reused in other packages.
    
    1. `templating-tools` package and its README
    2. `static-html` package and its README
    3. `caching-html-compiler` is not new code; it is just code factored out of the batch plugin version of `templating`, but the README and some comments are new.
    
    1. `tools/tests/static-html.js` tests static html and error handling
    2. `templating-tools/html-scanner-tests.js` tests `scanHtmlForTags` and `compileTagsWithSpacebars` together.
    
    All unit tests pass on this branch.

---
+++ package.js
@@ -0,0 +1,28 @@
+Package.describe({
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Define static page content in .html files',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.registerBuildPlugin({
+  name: "compileStaticHtmlBatch",
+  use: [
+    'caching-html-compiler',
+    'ecmascript',
+    'templating-tools',
+    'underscore'
+  ],
+  sources: [
+    'static-html.js'
+  ]
+});
+
+Package.onUse(function(api) {
+  api.use('isobuild:compiler-plugin@1.0.0');
+
+  // Body attributes are compiled to code that uses Meteor.startup
+  api.imply('meteor', 'client');
+});
