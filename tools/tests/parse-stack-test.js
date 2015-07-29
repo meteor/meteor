617c22a79a555c38659c6debd75535129245a82d
Sashko Stubailo <sashko@stubailo.com>
2015-07-29 11:50:02 -0700

    Add simple test for parse-stack

---
+++ tools/tests/parse-stack-test.js
@@ -0,0 +1,24 @@
+import selftest from '../selftest';
+import { parse, markBottom } from '../parse-stack';
+import _ from 'underscore';
+import Fiber from 'fibers';
+import Future from 'fibers/future';
+
+selftest.define("parse-stack - parse stack traces without fibers", () => {
+  const err = new Error();
+  const parsedStack = parse(err);
+
+  selftest.expectEqual(_.last(parsedStack[0].file.split("/")),
+    "parse-stack-test.js");
+
+  markBottom(() => {
+    const markedErr = new Error();
+    const parsedStack = parse(markedErr);
+
+    // The stack trace should only contain this one function since we marked the
+    // bottom
+    selftest.expectEqual(parsedStack.length, 1);
+    selftest.expectEqual(_.last(parsedStack[0].file.split("/")),
+      "parse-stack-test.js");
+  })();
+});
