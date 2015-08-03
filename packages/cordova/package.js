b618de8cd1bc3f2b79bcd2bf413fd660a3812969
Martijn Walraven <martijn@martijnwalraven.com>
2015-08-03 14:11:34 +0200

    Only depend on ios-sim on OS X

--- packages/cordova/package.js
+++ packages/cordova/package.js
@@ -13,9 +13,15 @@ Package.onUse(function (api) {
   api.export("Cordova", "server");
 });
 
-Npm.depends({
+var npmDependencies = {
   // We use our own branch because cordova-lib does not respect the silent option
   // https://github.com/meteor/cordova-lib/tree/respect-silent/cordova-lib
   // Can't download the tarball from GitHub because the package.json is in a subdirectory
-  "cordova-lib": "https://s3.amazonaws.com/android-bundle/cordova-lib-0000000000000000000000000000000000000000.tar.gz",
-  "ios-sim": "4.1.1"});
+  "cordova-lib": "https://s3.amazonaws.com/android-bundle/cordova-lib-0000000000000000000000000000000000000000.tar.gz"
+};
+
+if (process.platform === 'darwin') {
+  npmDependencies["ios-sim"] = "4.1.1";
+}
+
+Npm.depends(npmDependencies);
