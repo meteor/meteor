c16422dc377109750d2b86a3022f02cbe5bac8f9
Sashko Stubailo <sashko@stubailo.com>
2015-08-06 13:28:13 -0700

    Add metadata to templating-tools

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'templating-tools',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Tools to scan HTML and compile tags when building a templating package',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/meteor/meteor',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
