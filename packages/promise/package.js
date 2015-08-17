3504319d1b4f6d789c35fccae31b2502d0ab5994
Ben Newman <ben@meteor.com>
2015-08-17 12:30:52 -0400

    Upgrade meteor-{babel,promise} dependencies in tool and package code.

--- packages/promise/package.js
+++ packages/promise/package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: "promise",
-  version: "0.4.3-plugins.0",
+  version: "0.4.6-plugins.0",
   summary: "ECMAScript 2015 Promise polyfill with Fiber support",
   git: "https://github.com/meteor/promise",
   documentation: "README.md"
 });
 
 Npm.depends({
-  "meteor-promise": "0.4.3"
+  "meteor-promise": "0.4.6"
 });
 
 Package.onUse(function(api) {
