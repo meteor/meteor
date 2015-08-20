db0d6ca41df4a92d9e9e793b6c27426e7c8301da
Martijn Walraven <martijn@martijnwalraven.com>
2015-08-20 07:10:54 +0200

    Fix tests for parseServerOptionsForRunCommand that stopped working due to recent changes

--- tools/tests/cordova-run.js
+++ tools/tests/cordova-run.js
@@ -1,6 +1,6 @@
 import selftest from '../tool-testing/selftest.js';
 import utils from '../utils/utils.js';
-import { parseServerOptionsForRunCommand } from '../cli/commands-cordova.js';
+import { parseServerOptionsForRunCommand } from '../cli/commands.js';
 
 selftest.define('get mobile server argument for meteor run', ['cordova'], function () {
   // on emulator
@@ -9,13 +9,13 @@ selftest.define('get mobile server argument for meteor run', ['cordova'], functi
   // => mobile server should be localhost:3000
   selftest.expectEqual(parseServerOptionsForRunCommand({
     port: "3000"
-  }).mobileServerUrl, { host: "localhost", port: "3000", protocol: "http://" });
+  }).parsedMobileServerUrl, { host: "localhost", port: "3000", protocol: "http://" });
 
   // meteor run -p example.com:3000
   // => mobile server should be localhost:3000
   selftest.expectEqual(parseServerOptionsForRunCommand({
     port: "example.com:3000"
-  }).mobileServerUrl, { host: "localhost", port: "3000", protocol: "http://" });
+  }).parsedMobileServerUrl, { host: "localhost", port: "3000", protocol: "http://" });
 
   // on device
 
@@ -24,14 +24,14 @@ selftest.define('get mobile server argument for meteor run', ['cordova'], functi
   selftest.expectEqual(parseServerOptionsForRunCommand({
     port: "3000",
     args: ["ios-device"]
-  }).mobileServerUrl, { host: utils.ipAddress(), port: "3000", protocol: "http://" });
+  }).parsedMobileServerUrl, { host: utils.ipAddress(), port: "3000", protocol: "http://" });
 
   // meteor run -p example.com:3000 on device
   // => mobile server should be <detected ip>:3000
   selftest.expectEqual(parseServerOptionsForRunCommand({
     port: "example.com:3000",
     args: ["android-device"]
-  }).mobileServerUrl, { host: utils.ipAddress(), port: "3000", protocol: "http://" });
+  }).parsedMobileServerUrl, { host: utils.ipAddress(), port: "3000", protocol: "http://" });
 
   // meteor run -p example.com:3000 --mobile-server 4000 => error, mobile
   // server must include a hostname
@@ -47,19 +47,19 @@ selftest.define('get mobile server argument for meteor run', ['cordova'], functi
   selftest.expectEqual(parseServerOptionsForRunCommand({
     port: "example.com:3000",
     "mobile-server": "example.com"
-  }).mobileServerUrl, { protocol: "http://", host: "example.com", port: undefined });
+  }).parsedMobileServerUrl, { protocol: "http://", host: "example.com", port: undefined });
 
   // meteor run -p example.com:3000 --mobile-server https://example.com =>
   // mobile server should be https://example.com
   selftest.expectEqual(parseServerOptionsForRunCommand({
     port: "example.com:3000",
     "mobile-server": "https://example.com"
-  }).mobileServerUrl, { host: "example.com", protocol: "https://", port: undefined });
+  }).parsedMobileServerUrl, { host: "example.com", protocol: "https://", port: undefined });
 
   // meteor run -p example.com:3000 --mobile-server http://example.com:4000 =>
   // mobile server should be http://example.com:4000
   selftest.expectEqual(parseServerOptionsForRunCommand({
     port: "example.com:3000",
     "mobile-server": "http://example.com:4000"
-  }).mobileServerUrl, { host: "example.com", port: "4000", protocol: "http://" });
+  }).parsedMobileServerUrl, { host: "example.com", port: "4000", protocol: "http://" });
 });
