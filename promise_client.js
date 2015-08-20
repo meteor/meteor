2f71aa03411d073a4aa389a2ffcc8916256719c0
Ben Newman <ben@meteor.com>
2015-08-20 13:05:13 -0400

    Wrap Promise callbacks with Meteor.bindEnvironment on the client.
    The server-side implementation already invokes Promise callbacks in a way
    that preserves the dynamic environment of the .then call site, but the
    client-side implementation was neglecting to do the same.

--- promise_client.js
+++ promise_client.js
@@ -1,3 +1,18 @@
 // See https://github.com/then/promise#usage for an explanation of why we
 // require promise/domains here.
-Promise = require("promise/domains");
+var MeteorPromise = require("promise/domains");
+
+var es6PromiseThen = MeteorPromise.prototype.then;
+MeteorPromise.prototype.then = function (onResolved, onRejected) {
+  if (typeof Meteor === "object" &&
+      typeof Meteor.bindEnvironment === "function") {
+    return es6PromiseThen.call(
+      this,
+      onResolved && Meteor.bindEnvironment(onResolved),
+      onRejected && Meteor.bindEnvironment(onRejected)
+    );
+  }
+  return es6PromiseThen.call(this, onResolved, onRejected);
+};
+
+Promise = MeteorPromise;
