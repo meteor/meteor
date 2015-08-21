6f188e923b5b8f2cb12ac4197b3502c37a74e617
Ben Newman <ben@meteor.com>
2015-08-21 18:22:08 -0400

    Upgrade ecmascript-collections NPM package to 0.1.5.

--- packages/ecmascript-collections/package.js
+++ packages/ecmascript-collections/package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: "ecmascript-collections",
-  version: "0.1.4",
+  version: "0.1.5",
   summary: "Polyfills for ECMAScript 2015 Map and Set",
   git: "https://github.com/meteor/ecmascript-collections",
   documentation: "README.md"
 });
 
 Npm.depends({
-  "ecmascript-collections": "0.1.4"
+  "ecmascript-collections": "0.1.5"
 });
 
 Package.onUse(function(api) {
