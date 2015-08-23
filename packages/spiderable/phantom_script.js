66cc75ead72fe68130ebd292d6094d8c4bcce12e
Tom Kelsey <tomwasd@users.noreply.github.com>
2015-08-23 16:31:16 +0100

    Don't change the DOM until everything has loaded

--- packages/spiderable/phantom_script.js
+++ packages/spiderable/phantom_script.js
@@ -14,17 +14,21 @@ var isReady = function () {
       return false;
     }
     
-    // Remove all script tags except JSON-LD
-    var scriptTags = document.querySelectorAll('script:not([type="application/ld+json"])');
-    for (var i = 0; i < scriptTags.length; i++) {
-      scriptTags[i].parentNode.removeChild(scriptTags[i]);
-    }
-
-    // Remove meta fragment content
-    var fragment = document.querySelector('meta[name="fragment"][content="!"]');
-    fragment.parentNode.removeChild(fragment);
+    var spiderableIsReady = Package.spiderable.Spiderable.isReady();
     
-    return Package.spiderable.Spiderable.isReady();
+    if (spiderableIsReady) {
+      // Remove all script tags except JSON-LD
+      var scriptTags = document.querySelectorAll('script:not([type="application/ld+json"])');
+      for (var i = 0; i < scriptTags.length; i++) {
+        scriptTags[i].parentNode.removeChild(scriptTags[i]);
+      }
+
+      // Remove meta fragment content
+      var fragment = document.querySelector('meta[name="fragment"][content="!"]');
+      fragment.parentNode.removeChild(fragment);
+    }
+    
+    return spiderableIsReady;
   });
 };
 
