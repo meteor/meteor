d9fe23bc60234dbe72909c203ee10f75641e5820
Avital Oliver <avital@thewe.net>
2015-08-26 12:38:12 -0700

    Let `Meteor.call` within `observe` call server methods.
    Since `observe` callbacks fire synchronously, before this change,
    if you run a method on the client that leads to an observe
    callback firing, and in that observe callback you call `Meteor.call`,
    it would act the same as when you call `Meteor.call` within a method
    stub -- that is, it would call the method stub but not send the
    method invocation to the server.

---
+++ packages/minimongo/call_in_observe_test.js
@@ -0,0 +1,34 @@
+// This file is a regression test for https://github.com/meteor/meteor/issues/907
+
+Meteor.methods({
+  isRunningOnServer: function () {
+    return Meteor.isServer;
+  },
+  insertIntoLocalCollection: function () {
+    if (Meteor.isClient) {
+      LocalCollection.insert({});
+    }
+  }
+});
+
+if (Meteor.isClient) {
+  var LocalCollection = new Meteor.Collection(null);
+
+  testAsyncMulti("Meteor.call inside observe sends method to server", [
+    function (test, expect) {
+      var done = expect();
+
+      LocalCollection.find().observe({
+        added: function () {
+          Meteor.call("isRunningOnServer", function (err, res) {
+            test.isFalse(err);
+            test.equal(res, true);
+            done();
+          });
+        }
+      });
+
+      Meteor.call("insertIntoLocalCollection");
+    }
+  ]);
+}
