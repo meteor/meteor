8356b73847d530cd504c54111a3fcd9a66eeb4af
Dan Dascalescu <ddascalescu+github@gmail.com>
2015-08-27 08:16:51 -0700

    Add name and git to package.js

--- packages/sha/package.js
+++ packages/sha/package.js
@@ -1,9 +1,12 @@
 Package.describe({
-  summary: "SHA256 implementation",
-  version: "1.0.4-plugins.0"
+  name: 'sha256',
+  version: '1.0.4-plugins.0'
+  summary: 'SHA256 implementation',
+  git: 'https://github.com/meteor/meteor/tree/devel/packages/sha',
+  documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
   api.export('SHA256');
-  api.addFiles(['sha256.js'], ['client', 'server']);
+  api.addFiles('sha256.js', ['client', 'server']);
 });
