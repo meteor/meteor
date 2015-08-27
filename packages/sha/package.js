d54df8321bf1a8c6d13083a9bcf82c9be1131fb3
Dan Dascalescu <ddascalescu+github@gmail.com>
2015-08-27 12:22:27 -0700

    Trim Package.describe for core packages

--- packages/sha/package.js
+++ packages/sha/package.js
@@ -1,12 +1,10 @@
 Package.describe({
-  name: 'sha256',
   version: '1.0.4-plugins.0'
   summary: 'SHA256 implementation',
-  git: 'https://github.com/meteor/meteor/tree/devel/packages/sha',
-  documentation: 'README.md'
+  git: 'https://github.com/meteor/meteor',
 });
 
 Package.onUse(function (api) {
   api.export('SHA256');
-  api.addFiles('sha256.js', ['client', 'server']);
+  api.addFiles('sha256.js');
 });
