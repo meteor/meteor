f219f7a7b108117ab47a1fc6cd6b1a916193aca9
David Glasser <glasser@meteor.com>
2015-08-31 15:15:55 -0700

    Release METEOR@1.1.0.3-cdnfix.2

--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "The Meteor command-line tool",
-  version: '1.1.5-cdnfix.0'
+  version: '1.1.5-cdnfix.1'
 });
 
 Package.includeTool();
--- packages/webapp/package.js
+++ packages/webapp/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Serves a Meteor app over HTTP",
-  version: '1.2.1-cdnfix.0'
+  version: '1.2.1-cdnfix.1'
 });
 
 Npm.depends({connect: "2.9.0",
