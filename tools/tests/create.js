b381a675bf9d6d4616849476cdee10c87c0e841f
Sashko Stubailo <sashko@stubailo.com>
2015-08-31 19:48:31 -0700

    Fix create test

--- tools/tests/create.js
+++ tools/tests/create.js
@@ -7,7 +7,7 @@ selftest.define("create", function () {
   // Can we create an app? Yes!
   var run = s.run("create", "foobar");
   run.waitSecs(15);
-  run.match("foobar: created");
+  run.match("Created a new Meteor app in foobar.");
   run.match("To run your new app");
   run.expectExit(0);
 
