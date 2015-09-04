201100d244882df90ccbb379a3fd95b214563c94
Zoltan Olah <zol@percolatestudio.com>
2015-09-04 14:52:03 -0700

    Bumps version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Allows you to define and run db migrations.",
-  version: "0.7.6",
+  version: "0.8.6",
   name: "percolate:migrations",
   git: "https://github.com/percolatestudio/meteor-migrations.git"
 });
