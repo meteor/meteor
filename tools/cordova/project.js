98886f98ef71994fe43d23794270af8c24c3d006
David Greenspan <dgreenspan@alum.mit.edu>
2015-09-09 15:51:32 -0700

    Protect String.prototype from shelljs (#5107)
    cordova-lib uses shelljs which modifies String.prototype in the tool
    (and build plugin) environment.  This code prevents the pollution.

--- tools/cordova/project.js
+++ tools/cordova/project.js
@@ -5,7 +5,7 @@ import assert from 'assert';
 import chalk from 'chalk';
 import semver from 'semver';
 
-import isopackets from '../tool-env/isopackets.js'
+import isopackets from '../tool-env/isopackets.js';
 import files from '../fs/files.js';
 import utils from '../utils/utils.js';
 import { Console } from '../console/console.js';
@@ -14,6 +14,7 @@ import main from '../cli/main.js';
 import httpHelpers from '../utils/http-helpers.js';
 import { execFileSync, execFileAsync } from '../utils/processes.js';
 
+import './protect-string-proto.js'; // must always come before 'cordova-lib'
 import { cordova as cordova_lib, events as cordova_events, CordovaError }
   from 'cordova-lib';
 import cordova_util from 'cordova-lib/src/cordova/util.js';
