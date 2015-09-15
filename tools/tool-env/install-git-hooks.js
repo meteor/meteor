d4c23e226c1ef833ab662816f8d95ba12cfc33c0
Mitar <mitar.git@tnode.com>
2015-09-14 18:51:05 -0700

    Do not fail if meteor tool is not run from a git clone.

--- tools/tool-env/install-git-hooks.js
+++ tools/tool-env/install-git-hooks.js
@@ -33,6 +33,12 @@ const METEOR_HOOK_LINE_REGEX = /.*meteor-hook.*/;
 
 export default function installGitHooks() {
   allPossibleHooks.map((hookName) => {
+    if (!files.exists(hookDestination)) {
+      // Don't do anything if the hook destination does not exist, eg.,
+      // we are not running from a git clone.
+      return;
+    }
+
     const hookFile = files.pathJoin(hookDestination, hookName);
     const sourceFile = files.pathJoin(hookSource, hookName);
     const meteorHookFile = hookFile + METEOR_HOOK_SUFFIX;
