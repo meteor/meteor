578d5ade094a1c0e356f99797e76b652c828d7f3
Ben Newman <ben@meteor.com>
2015-09-16 15:56:40 -0400

    Include es5-sham.js with the es5-shim package.
    This ensures adequate implemenations of APIs like Object.create are
    available in IE8, so that packages like ecmascript-collections can depend
    on those APIs without completely breaking the simplest of apps in IE8.

--- packages/es5-shim/package.js
+++ packages/es5-shim/package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: "es5-shim",
-  version: "0.1.0-rc.0",
+  version: "4.1.13-rc.0",
   summary: "Shims and polyfills to improve ECMAScript 5 support",
   documentation: "README.md"
 });
 
 Npm.depends({
-  "es5-shim": "4.1.7"
+  "es5-shim": "4.1.13"
 });
 
 Package.onUse(function(api) {
@@ -18,8 +18,13 @@ Package.onUse(function(api) {
   api.addFiles("import_globals.js");
 
   var es5ShimPath = ".npm/package/node_modules/es5-shim/es5-shim.js";
+  var es5ShamPath = ".npm/package/node_modules/es5-shim/es5-sham.js";
 
-  api.addFiles(es5ShimPath, "client", {
+  api.addFiles([
+    es5ShimPath,
+    // Limited but necessary polyfills for APIs like Object.create.
+    es5ShamPath
+  ], "client", {
     // Files in the es5-shim package are already wrapped in closures.
     bare: true
   });
