3469b78793f9698327cc06cf75d19ad9532eee94
Sashko Stubailo <sashko@stubailo.com>
2015-09-17 11:25:24 -0700

    Bump package versions for RC 16

--- packages/blaze/package.js
+++ packages/blaze/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Meteor Reactive Templating library",
-  version: '2.1.3-rc.0'
+  version: '2.1.3-rc.1'
 });
 
 Package.onUse(function (api) {
--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "The Meteor command-line tool",
-  version: '1.1.7-rc.14'
+  version: '1.1.7-rc.15'
 });
 
 Package.includeTool();
