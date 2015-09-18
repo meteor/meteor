f3a8328bf1528a1c2201048a888db9fcc98faea0
Ben Newman <ben@meteor.com>
2015-09-18 13:06:13 -0400

    Upgrade ecmascript-collections to support ES2015 Array methods.
    Closes #5199.

--- packages/ecmascript-collections/collections-tests.js
+++ packages/ecmascript-collections/collections-tests.js
@@ -34,6 +34,22 @@ Tinytest.add("ecmascript-containers - Set", function (test) {
   });
 });
 
+Tinytest.add("core-js - Array", function (test) {
+  test.equal(Array.from("123", Number), [1, 2, 3]);
+  test.equal(Array.of(1, 3, 5), [1, 3, 5]);
+  test.equal(
+    Array(5).fill("oyez"),
+    ["oyez", "oyez", "oyez", "oyez", "oyez"]
+  );
+
+  function isOdd(n) {
+    return n % 2;
+  }
+
+  test.equal([2, 3, 4].find(isOdd), 3);
+  test.equal([2, 3, 4].findIndex(isOdd), 1);
+});
+
 Tinytest.add("core-js - Number", function () {
   // Make sure https://github.com/grigio/meteor-babel/issues/5 is not a
   // problem for us.
--- packages/ecmascript-collections/package.js
+++ packages/ecmascript-collections/package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: "ecmascript-collections",
-  version: "0.1.5-rc.1",
+  version: "0.1.6-rc.1",
   summary: "Polyfills for ECMAScript 2015 Map and Set",
   git: "https://github.com/meteor/ecmascript-collections",
   documentation: "README.md"
 });
 
 Npm.depends({
-  "ecmascript-collections": "0.1.5"
+  "ecmascript-collections": "0.1.6"
 });
 
 Package.onUse(function(api) {
