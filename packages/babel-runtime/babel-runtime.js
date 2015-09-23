7fcf770825184fe32f1daa87cee64e6e57e74f0c
Paul Bastowski <pbastowski@gmail.com>
2015-09-24 01:12:41 +0200

    Fixed bad createDecoratedFunction definition.

--- packages/babel-runtime/babel-runtime.js
+++ packages/babel-runtime/babel-runtime.js
@@ -26,11 +26,37 @@ function canDefineNonEnumerableProperties() {
 // something capitalized and more descriptive, like `BabelRuntime`.
 babelHelpers = {
   // Provides support for es7.decorators
-  createDecoratedClass: function (obj) {
-    return obj && obj.__esModule ? obj : {
-      "default": obj
-    };
-  },
+  createDecoratedClass: (function () {
+        function defineProperties(target, descriptors, initializers) {
+            for (var i = 0; i < descriptors.length; i++) {
+                var descriptor = descriptors[i];
+                var decorators = descriptor.decorators;
+                var key = descriptor.key;
+                delete descriptor.key;
+                delete descriptor.decorators;
+                descriptor.enumerable = descriptor.enumerable || false;
+                descriptor.configurable = true;
+                if ('value' in descriptor || descriptor.initializer) descriptor.writable = true;
+                if (decorators) {
+                    for (var f = 0; f < decorators.length; f++) {
+                        var decorator = decorators[f];
+                        if (typeof decorator === 'function') { descriptor = decorator(target, key, descriptor) || descriptor; } else { throw new TypeError('The decorator for method ' + descriptor.key + ' is of the invalid type ' + typeof decorator); }
+                    }
+                    if (descriptor.initializer !== undefined) {
+                        initializers[key] = descriptor;
+                        continue;
+                    }
+                }
+                Object.defineProperty(target, key, descriptor);
+            }
+        }
+
+        return function (Constructor, protoProps, staticProps, protoInitializers, staticInitializers) {
+            if (protoProps) defineProperties(Constructor.prototype, protoProps, protoInitializers);
+            if (staticProps) defineProperties(Constructor, staticProps, staticInitializers);
+            return Constructor;
+        };
+    })(),
 
   // Provides support for es6.modules and "import ... from" keywords
   interopRequireDefault: function (obj) {
