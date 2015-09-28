dfd967e1e2ea27dc20b34e2bd17f38de70ebed72
Connor Hallett <connor.hallett@gmail.com>
2015-09-27 20:45:47 -0400

    Update package.js
    Add dependency for check package

--- roles/package.js
+++ roles/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Authorization package for Meteor",
-  version: "1.2.12",
+  version: "1.2.13",
   git: "https://github.com/alanning/meteor-roles.git",
   name: "alanning:roles"
 });
@@ -8,7 +8,7 @@ Package.describe({
 Package.on_use(function (api) {
   api.versionsFrom && api.versionsFrom("METEOR@0.9.0");
   var both = ['client', 'server'];
-  api.use(['underscore', 'accounts-base'], both);
+  api.use(['underscore', 'accounts-base', 'check'], both);
   api.use(['handlebars'], 'client', {weak: true});
 
   // This is needed due to Meteor Issue #1358
