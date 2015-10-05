dabe0b85212749a6fc5dedd42f87bceeca209afa
alanning <alanning@gmail.com>
2015-10-05 02:47:27 -0400

    document Roles.subscription property

--- roles/client/subscriptions.js
+++ roles/client/subscriptions.js
@@ -1,10 +1,21 @@
 "use strict"
 
 
-// Subscribe to get the currently logged in user's permissions.
-//
-// Publish function depends on `this.userId` so it automatically
-// re-runs when logged-in user changes.
+/**
+ * Subscription handle for the currently logged in user's permissions.
+ *
+ * NOTE: The corresponding publish function, `_roles`, depends on
+ * `this.userId` so it will automatically re-run when the currently
+ * logged-in user changes.
+ *
+ * @example
+ *
+ *     `Roles.subscription.ready()` // => `true` if user roles have been loaded
+ *
+ * @property subscription
+ * @type Object
+ * @for Roles
+ */
 
 Tracker.autorun(function () {
   Roles.subscription = Meteor.subscribe("_roles")
