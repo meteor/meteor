93498f368bbf55afaed19792a9dfb48492c1d830
Dominik Ferber <dominik.ferber@gmail.com>
2015-10-12 19:30:37 +0200

    fix(internal): Add test for getRelativePath
    #31

--- tests/lib/util/internal/getRelativePath.js
+++ tests/lib/util/internal/getRelativePath.js
@@ -8,11 +8,17 @@ getRelativePath.__set__('getRootPath', function (filename) {
   if (filename === '/Users/anon/git/meteor-project/client/file.js') {
     return '/Users/anon/git/meteor-project'
   }
+  return false
 })
 
 describe('getRelativePath', function () {
   it('gets the correct relative path', function () {
     const result = getRelativePath('/Users/anon/git/meteor-project/client/file.js')
     assert.equal(result, 'client/file.js')
+  })
+
+  it('returns false if no root path is found', function () {
+    const result = getRelativePath('/Users/anon/git/file.js')
+    assert.equal(result, false)
   })
 })
