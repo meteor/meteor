b01fd758bb4d35a06dc6f337d763cdb53bdb6376
JonTheNiceGuy Spriggs <jon@sprig.gs>
2015-10-15 08:49:11 +0000

    Google's OAuth credentials page does not permit trailing slashes. This patch resolves this.

--- packages/google/google_configure.js
+++ packages/google/google_configure.js
@@ -1,7 +1,14 @@
 Template.configureLoginServiceDialogForGoogle.helpers({
   siteUrl: function () {
     return Meteor.absoluteUrl();
-  }
+  },
+  siteUrlNoTrail: function () {
+    if (Meteor.absoluteUrl().slice(-1) === '/') {
+      return Meteor.absoluteUrl().slice(0, 1);
+    } else {
+      return Meteor.absoluteUrl();
+    }
+}
 });
 
 Template.configureLoginServiceDialogForGoogle.fields = function () {
