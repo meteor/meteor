e50d7d68aa83fc7c7ba42c78550ca10d2ca78112
Sashko Stubailo <sashko@stubailo.com>
2015-10-22 11:41:20 -0700

    Bump all version numbers

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Allows templates to be defined in .html files",
-  version: '1.1.4'
+  version: '1.1.5-rc.0'
 });
 
 // Today, this package is closely intertwined with Handlebars, meaning
