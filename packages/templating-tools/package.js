d039e17f14e6f3dc035842706ff1e7b1f58a0feb
sebakerckhof <seba.kerckhof@gmail.com>
2015-11-02 21:46:28 +0100

    Split standard-minifiers in 2 separate packages

--- packages/templating-tools/package.js
+++ packages/templating-tools/package.js
@@ -16,13 +16,13 @@ Package.onUse(function(api) {
     'ecmascript',
     'spacebars-compiler',
 
-    // minifiers is a weak dependency of spacebars-compiler; adding it here
+    // minifiers-js is a weak dependency of spacebars-compiler; adding it here
     // ensures that the output is minified.  (Having it as a weak dependency means
     // that we don't ship uglify etc with built apps just because
     // boilerplate-generator uses spacebars-compiler.)
     // XXX maybe uglify should be applied by this plugin instead of via magic
     // weak dependency.
-    'minifiers'
+    'minifiers-js'
   ]);
 
   api.addFiles([
