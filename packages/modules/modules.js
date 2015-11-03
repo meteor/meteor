6be64fdc7e40df0ca952e8609c18204f32c4179f
Ben Newman <ben@meteor.com>
2015-11-03 09:43:11 -0500

    New package implementing the runtime API for a CommonJS module system.
    When used, the 'modules' package provides a CommonJS require/exports
    module system via the exported meteorInstall function. Once that
    underlying system is available, ECMAScript 2015 import/export syntax can
    be easily compiled into require/exports.

---
+++ packages/modules/modules.js
@@ -0,0 +1,1 @@
+meteorInstall = makeInstaller();
---
+++ packages/modules/package.js
@@ -0,0 +1,33 @@
+Package.describe({
+  name: "modules",
+  version: "0.3.0",
+  summary: "CommonJS module system",
+  git: "https://github.com/benjamn/install",
+  documentation: "README.md"
+});
+
+Npm.depends({
+  install: "0.3.0"
+});
+
+Package.onUse(function(api) {
+  api.use("meteor", {
+    unordered: true
+  });
+
+  api.addFiles(".npm/package/node_modules/install/install.js", [
+    "client",
+    "server"
+  ], {
+    bare: true
+  });
+
+  api.addFiles("modules.js");
+  api.export("meteorInstall");
+});
+
+Package.onTest(function(api) {
+  api.use("tinytest");
+  api.use("modules");
+  api.addFiles("modules-tests.js");
+});
