42bdbeb91bad88cbe43c9b3ee78612cdf95dba20
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-11-23 21:58:01 +0100

    Release of velocity:meteor-tool@1.1.10_2
    And velocity:METEOR@1.2.1_2
    
    Improves mirror caching for app testing.

--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'velocity:meteor-tool',
   summary: 'The Meteor command-line tool with extensions for Velocity',
-  version: '1.1.10_1'
+  version: '1.1.10_2'
 });
 
 Package.includeTool();
