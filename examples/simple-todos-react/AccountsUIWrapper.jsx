4fdf3139e459d07d42fb262c4cd702d3ebf075a3
okland <omri.klinger@gmail.com>
2015-11-26 09:00:26 +0200

    Update AccountsUIWrapper.jsx
    Fix warning of using deprecated React.findDOMNode

--- examples/simple-todos-react/AccountsUIWrapper.jsx
+++ examples/simple-todos-react/AccountsUIWrapper.jsx
@@ -2,7 +2,7 @@ AccountsUIWrapper = React.createClass({
   componentDidMount() {
     // Use Meteor Blaze to render login buttons
     this.view = Blaze.render(Template.loginButtons,
-      React.findDOMNode(this.refs.container));
+      ReactDOM.findDOMNode(this.refs.container));
   },
   componentWillUnmount() {
     // Clean up Blaze view
