af0364835be34118393e23d3cd98169fe4efa825
Evan You <yyx990803@gmail.com>
2015-12-02 15:32:09 -0500

    add search integration

--- site/themes/meteor/source/script/main.js
+++ site/themes/meteor/source/script/main.js
@@ -126,7 +126,11 @@
   })
 
   // search box
-  // TODO
+  (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
+  (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
+  e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
+  })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
+  _st('install','h1KW95Mz3fNyf4cnqVtr','2.0.0');
   
   // mobile
   document.querySelector('#mobile-header .menu-button')
