9e81bbf8fa2a9457b6a8338eaa23e9f3d47ae21a
Evan You <yyx990803@gmail.com>
2015-12-03 14:26:48 -0500

    fix search box script

--- site/themes/meteor/source/script/main.js
+++ site/themes/meteor/source/script/main.js
@@ -126,7 +126,7 @@
   })
 
   // search box
-  (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
+  ;(function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
   (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
   e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
   })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
