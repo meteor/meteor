4365e25cb9f077c3f369440817413ec790dfe21e
Sashko Stubailo <sashko@stubailo.com>
2015-12-04 15:52:45 -0800

    Add Hubspot tracking

--- site/themes/meteor/source/script/main.js
+++ site/themes/meteor/source/script/main.js
@@ -149,4 +149,12 @@
 ​
   ga('create', 'UA-30093278-6', 'auto');
   ga('send', 'pageview');
+
+  // Hubspot
+  ;(function(d,s,i,r) {
+    if (d.getElementById(i)){return;}
+    var n=d.createElement(s),e=d.getElementsByTagName(s)[0];
+    n.id=i;n.src='//js.hs-analytics.net/analytics/'+(Math.ceil(new Date()/r)*r)+'/520701.js';
+    e.parentNode.insertBefore(n, e);
+  })(document,"script","hs-analytics",300000);
 })()
