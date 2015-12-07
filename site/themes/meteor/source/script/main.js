c464d79df882d24fb4363f3615c7e6293be0b9fe
Sashko Stubailo <sashko@stubailo.com>
2015-12-07 09:52:28 -0800

    Remove lame unicode whitespace

--- site/themes/meteor/source/script/main.js
+++ site/themes/meteor/source/script/main.js
@@ -146,7 +146,7 @@
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-​
+
   ga('create', 'UA-30093278-6', 'auto');
   ga('send', 'pageview');
 
