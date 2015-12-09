77b628d72356fc0f610788f9b88a68b07bcfd47f
Ben Newman <ben@meteor.com>
2015-12-09 12:03:22 -0500

    Upgrade es5-shim to fix #5759.

--- packages/es5-shim/package.js
+++ packages/es5-shim/package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: "es5-shim",
-  version: "4.1.14",
+  version: "4.3.1",
   summary: "Shims and polyfills to improve ECMAScript 5 support",
   documentation: "README.md"
 });
 
 Npm.depends({
-  "es5-shim": "4.1.13"
+  "es5-shim": "4.3.1"
 });
 
 Package.onUse(function(api) {
