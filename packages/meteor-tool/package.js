4b808c2a3e3870807187ba8f5d999457413c890f
Chris Watts <devgrok@users.noreply.github.com>
2015-12-11 18:31:00 +1100

    increment and mark -dev

--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,6 +1,8 @@
 Package.describe({
   summary: "The Meteor command-line tool",
-  version: '1.1.11-dev'
+  version: '1.1.11-dev',
+  name: 'devgrok:meteor-tool',
+  git: "https://github.com/devgrok/meteor.git",
 });
 
 Package.includeTool();
