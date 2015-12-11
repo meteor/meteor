15516c6b2232880bbca1c6e607cdf3ca18cc6339
Avital Oliver <avital@thewe.net>
2015-12-11 14:19:23 -0800

    Bump a package version number

--- packages/babel-compiler/package.js
+++ packages/babel-compiler/package.js
@@ -6,7 +6,7 @@ Package.describe({
   // isn't possible because you can't publish a non-recommended
   // release with package versions that don't have a pre-release
   // identifier at the end (eg, -dev)
-  version: '5.8.25-modules.0'
+  version: '5.8.25-modules.1'
 });
 
 Npm.depends({
