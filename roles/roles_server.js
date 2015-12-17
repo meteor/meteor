c61f668f54e7b272127bd0090d2e25c2fd93187f
Mitar <mitar.git@tnode.com>
2015-12-17 15:16:50 +0100

    Remove unnecessary index.

--- roles/roles_server.js
+++ roles/roles_server.js
@@ -4,9 +4,9 @@
 
 Meteor.roles._ensureIndex({name: 1}, {unique: 1});
 
-Meteor.users._ensureIndex({'roles.role': 1});
-Meteor.users._ensureIndex({'roles.partition': 1});
+// Index only on "roles.role" is not needed because the combined index works for it as well.
 Meteor.users._ensureIndex({'roles.role': 1, 'roles.partition': 1});
+Meteor.users._ensureIndex({'roles.partition': 1});
 
 /**
  * Publish logged-in user's roles so client-side checks can work.
