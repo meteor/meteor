f241cbe44fb604396d39ef7839c940e727412569
Ben Newman <ben@meteor.com>
2015-12-18 13:53:22 -0500

    Define $ and jQuery globally, as they used to be.
    Fixes #5819.

--- packages/jquery/main.js
+++ packages/jquery/main.js
@@ -1,1 +1,10 @@
-exports.$ = exports.jQuery = require("./jquery.js");
+var jQuery = require("./jquery.js");
+var global = this;
+
+// Provide values for the exported variables of the jquery package.
+exports.$ = exports.jQuery = jQuery;
+
+// There's no stopping legacy code from referring to window.$ or
+// window.jQuery, so we have to keep defining those properties globally,
+// but at least the exports of this package will be reliable.
+global.$ = global.jQuery = jQuery;
