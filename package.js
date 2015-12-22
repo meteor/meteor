d93e38a10e1e22e9e30431854becc261c0c9889e
Ben Newman <ben@meteor.com>
2015-12-22 12:56:42 -0500

    Bump 1.3 beta release numbers.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Additional tests for Spacebars",
-  version: '1.0.5-modules.1'
+  version: '1.0.5-modules.2'
 });
 
 // These tests are in a separate package to avoid a circular dependency
