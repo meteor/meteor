023bb088f3ad748ed4efff95c45b661b8e5ba3d2
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-12-26 20:03:05 +0100

    Release of velocity:meteor-tool@1.1.10_3
    And velocity:METEOR@1.2.1_3

--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'velocity:meteor-tool',
   summary: 'The Meteor command-line tool with extensions for Velocity',
-  version: '1.1.10_2'
+  version: '1.1.10_3'
 });
 
 Package.includeTool();
