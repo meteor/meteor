fb89e46c8cd09310f5aff7665e3d1d3697e93b34
Avital Oliver <avital@thewe.net>
2016-01-08 10:52:25 -0800

    Small code review changes

--- packages/meteor-env-dev/env.js
+++ packages/meteor-env-dev/env.js
@@ -1,15 +1,16 @@
 if (typeof global === "object" && global.process) {
+  // We're on the server
   process = global.process;
-}
+} else {
+  if (typeof process !== "object") {
+    process = {};
+  }
 
-if (typeof process !== "object") {
-  process = {};
-}
+  if (typeof process.env !== "object") {
+    process.env = {};
+  }
 
-if (typeof process.env !== "object") {
-  process.env = {};
-}
-
-if (typeof process.env.NODE_ENV !== "string") {
-  process.env.NODE_ENV = "development";
+  if (typeof process.env.NODE_ENV !== "string") {
+    process.env.NODE_ENV = "development";
+  }
 }
--- packages/meteor-env-prod/env.js
+++ packages/meteor-env-prod/env.js
@@ -1,15 +1,16 @@
 if (typeof global === "object" && global.process) {
+  // We're on the server, just use the real `process` object.
   process = global.process;
-}
+} else {
+  if (typeof process !== "object") {
+    process = {};
+  }
 
-if (typeof process !== "object") {
-  process = {};
-}
+  if (typeof process.env !== "object") {
+    process.env = {};
+  }
 
-if (typeof process.env !== "object") {
-  process.env = {};
-}
-
-if (typeof process.env.NODE_ENV !== "string") {
-  process.env.NODE_ENV = "production";
+  if (typeof process.env.NODE_ENV !== "string") {
+    process.env.NODE_ENV = "production";
+  }
 }
