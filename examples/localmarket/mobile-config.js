831b8511e9bf54cabebe7b1cefe58dc007f8065c
Martijn Walraven <martijn@martijnwalraven.com>
2016-01-13 12:56:55 +0100

    Remove unnecessary app icon and launch image sizes from Local Market example

--- examples/localmarket/mobile-config.js
+++ examples/localmarket/mobile-config.js
@@ -9,13 +9,11 @@ App.info({
 
 App.icons({
   // iOS
-  'iphone': 'resources/icons/icon-60x60.png',
   'iphone_2x': 'resources/icons/icon-60x60@2x.png',
   'ipad': 'resources/icons/icon-76x76.png',
   'ipad_2x': 'resources/icons/icon-76x76@2x.png',
 
   // Android
-  'android_ldpi': 'resources/icons/icon-36x36.png',
   'android_mdpi': 'resources/icons/icon-48x48.png',
   'android_hdpi': 'resources/icons/icon-72x72.png',
   'android_xhdpi': 'resources/icons/icon-96x96.png'
@@ -23,7 +21,6 @@ App.icons({
 
 App.launchScreens({
   // iOS
-  'iphone': 'resources/splash/splash-320x480.png',
   'iphone_2x': 'resources/splash/splash-320x480@2x.png',
   'iphone5': 'resources/splash/splash-320x568@2x.png',
   'ipad_portrait': 'resources/splash/splash-768x1024.png',
@@ -32,8 +29,6 @@ App.launchScreens({
   'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
 
   // Android
-  'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
-  'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
   'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
   'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
   'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
@@ -44,4 +39,3 @@ App.launchScreens({
 
 App.setPreference('StatusBarOverlaysWebView', 'false');
 App.setPreference('StatusBarBackgroundColor', '#000000');
-
