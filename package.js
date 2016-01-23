cb60f687381fca5e1936b6c2918680f3a016b5b8
Martijn Walraven <martijn@martijnwalraven.com>
2016-01-22 21:02:21 -0800

    Bump package versions for 1.3-cordova-beta.2 release

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Allows templates to be defined in .html files",
-  version: '1.1.6-cordova.1'
+  version: '1.1.6-cordova.2'
 });
 
 // Today, this package is closely intertwined with Handlebars, meaning
