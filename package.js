85e0afa193bfe1bd985ac9d9d1800aa069f0338d
Ben Newman <ben@meteor.com>
2016-02-02 11:08:42 -0500

    Bump 1.3 beta release numbers.
    The beta released with this commit (modules-beta.6) included everything on
    the release-1.3 branch up to bdf64da3391b85217048706c7fc752a18624715e
    ("Avoid circular package.json resolution chains.").
    
    Unfortunately, additional commits were pushed to the branch before this
    commit was pushed, so the release tag release/METEOR@1.3-modules-beta.6
    does not correspond to any commit in the branch history.
    
    Rather than attempting to rewrite the branch history, I decided to amend
    this commit with this explanation before pushing it.
    
    To see exactly what was released with the sixth beta:
    
      git fetch --tags
      git log release/METEOR@1.3-modules-beta.6

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Additional tests for Spacebars",
-  version: '1.0.5-modules.5'
+  version: '1.0.5-modules.6'
 });
 
 // These tests are in a separate package to avoid a circular dependency
