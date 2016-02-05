a2616efdb63841231817a5ac85e058e8fd29fa83
Ben Newman <ben@meteor.com>
2016-02-04 23:32:26 -0500

    Bump 1.3 beta release numbers.

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  version: '1.0.4-modules.6',
+  version: '1.0.4-modules.7',
   // Brief, one-line summary of the package.
   summary: 'Define static page content in .html files',
   git: 'https://github.com/meteor/meteor',
