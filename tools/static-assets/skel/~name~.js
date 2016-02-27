bce22a3038255ba5b3e04ba084b93539e2500d1e
Wexpo Lyu <laosb@lns.unihubs.com>
2016-02-27 20:25:45 +0800

    Use ReactiveVar instead of Sessions.

--- tools/static-assets/skel/~name~.js
+++ tools/static-assets/skel/~name~.js
@@ -1,17 +1,17 @@
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
+  let Counter = new ReactiveVar(0);
 
   Template.hello.helpers({
     counter: function () {
-      return Session.get('counter');
+      return Counter.get();
     }
   });
 
   Template.hello.events({
     'click button': function () {
       // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+      Counter.set(Counter.get() + 1);
     }
   });
 }
@@ -19,5 +19,6 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    // remember, in a both-side file, these code will still be sent to client.
   });
 }
