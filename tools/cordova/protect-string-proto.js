757660a85d9eedb68bfe4a6b42ad63f4ba3d2070
Ben Newman <ben@meteor.com>
2016-02-29 17:28:40 -0500

    Prevent erroneous calls to String.prototype.{to,toEnd}.
    Fixes #5107.

--- tools/cordova/protect-string-proto.js
+++ tools/cordova/protect-string-proto.js
@@ -7,12 +7,42 @@
 // One JS library in particular, String.js (before v3.3.1), is
 // sensitive to String prototype pollution.
 //
-// Fortunately, `cordova-lib` does not seem to rely on the presence of
-// `String#to` or `String#toEnd` (or this code would break it).
-//
 // This code can be removed when `shelljs` cleans up its act and
 // `cordova-lib` uses a new version, or when `cordova-lib` moves away
 // from `shelljs`.
 
-Object.defineProperty(String.prototype, 'to', { set: function () {} });
-Object.defineProperty(String.prototype, 'toEnd', { set: function () {} });
+function makeDescriptor() {
+  let value;
+
+  // This descriptor allows the property to remain non-enumerable while
+  // still permitting controlled modifications of its value.
+  return {
+    enumerable: false,
+
+    get() {
+      return value;
+    },
+
+    set(newValue) {
+      if (typeof newValue === "function") {
+        value = function () {
+          // Ignore calls that likely originate from
+          // https://github.com/jprichardson/string.js/blob/adc2e9d1b8/lib/string.js#L726
+          if (arguments.length > 0 ||
+              this != "teststring") {
+            return newValue.apply(this, arguments);
+          }
+        };
+      } else {
+        value = newValue;
+      }
+
+      return newValue;
+    }
+  };
+}
+
+Object.defineProperties(String.prototype, {
+  to: makeDescriptor(),
+  toEnd: makeDescriptor()
+});
