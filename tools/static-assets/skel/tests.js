4476895022526515877dcc3c363ba22e511b823a
Tom Coleman <tom@thesnail.org>
2016-03-01 15:39:06 +1100

    Refactored skeleton:
     - Split files into the correct place, given the guide
     - Import html file
     - Removed test [it's not a demo]
     - Use ReactiveVar rather than Session
     - Added an info section to guide people further

--- tools/static-assets/skel/tests.js
+++
@@ -1,8 +0,0 @@
-import assert from "assert";
-import {name as appName} from "./~name~.js";
-
-describe("~name~", () => {
-  it("should export its name", () => {
-    assert.strictEqual(appName, "~name~");
-  });
-});
