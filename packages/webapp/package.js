36e8706c0486134083f25dcbb3af44da4eafd5a0
Martijn Walraven <martijn@martijnwalraven.com>
2016-03-02 14:30:48 +0100

    Update cordova-plugin-meteor-webapp dependency to 1.1.1

--- packages/webapp/package.js
+++ packages/webapp/package.js
@@ -16,7 +16,7 @@ Npm.strip({
 Cordova.depends({
   'cordova-plugin-whitelist': '1.2.1',
   'cordova-plugin-wkwebview-engine': '1.0.2',
-  'cordova-plugin-meteor-webapp': '0.10.0'
+  'cordova-plugin-meteor-webapp': '1.1.1'
 });
 
 Package.onUse(function (api) {
