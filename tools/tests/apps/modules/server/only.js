a92aaee133a5eb4608975b169a8e2d88e4892a74
Ben Newman <ben@meteor.com>
2016-03-07 15:44:15 -0500

    Test that various packages can now be imported using pure Node.
    Closes #6165.
    Closes #6173.
    Closes #6285.
    Closes #6313.
    Closes #6373.

--- tools/tests/apps/modules/server/only.js
+++ tools/tests/apps/modules/server/only.js
@@ -1,4 +1,12 @@
 import assert from "assert";
+
+import "github";
+import "aws-sdk";
+import "stripe";
+import "winston";
+import "mssql";
+
 assert.strictEqual(Meteor.isServer, true);
 assert.strictEqual(Meteor.isClient, false);
+
 export default module.id;
