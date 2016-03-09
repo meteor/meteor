c03055fefa9ece2aeed5dfc54ed288612b8d8ed3
Dominik Ferber <dominik.ferber@gmail.com>
2016-03-09 19:09:51 +0100

    test: fix completeness tests

--- tests/index.js
+++ tests/index.js
@@ -1,24 +1,24 @@
-import plugin from '../lib/index'
+import { rules, configs } from '../lib/index'
 import assert from 'assert'
 import fs from 'fs'
 import path from 'path'
 
-const rules = fs.readdirSync(path.resolve(__dirname, '../lib/rules/'))
+const ruleNames = fs.readdirSync(path.resolve(__dirname, '../lib/rules/'))
   .filter(f => path.extname(f) === '.js')
   .map(f => path.basename(f, '.js'))
 
 describe('all rule files should be exported by the plugin', () => {
-  rules.forEach(ruleName => {
+  ruleNames.forEach(ruleName => {
     it(`should export ${ruleName}`, () => {
-      assert(plugin.rules.hasOwnProperty(ruleName))
+      assert(rules.hasOwnProperty(ruleName))
     })
   })
 })
 
 describe('configurations', () => {
-  rules.forEach(ruleName => {
+  ruleNames.forEach(ruleName => {
     it(`should have a recommended configuration for ${ruleName}`, () => {
-      assert(plugin.configs.recommended.rules.hasOwnProperty(`meteor/${ruleName}`))
+      assert(configs.recommended.rules.hasOwnProperty(`meteor/${ruleName}`))
     })
   })
 })
