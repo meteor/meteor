1d4e17246aae11ef794a1fe6aee88a5a9946878b
Tom Coleman <tom@thesnail.org>
2016-03-10 15:46:18 +1100

    For #6436 -- allow test drivers to `.start()` on the server

--- packages/meteor/test_environment.js
+++ packages/meteor/test_environment.js
@@ -10,8 +10,7 @@ var TEST_METADATA = JSON.parse(TEST_METADATA_STR || "{}");
 Meteor.isTest = !!TEST_METADATA.isTest;
 Meteor.isAppTest = !!TEST_METADATA.isAppTest;
 
-
-if (Meteor.isClient && (Meteor.isTest || Meteor.isAppTest)) {
+if (Meteor.isTest || Meteor.isAppTest) {
   Meteor.startup(function() {
     var testDriverPackageName = TEST_METADATA.driverPackage;
     if (typeof testDriverPackageName !== "string") {
@@ -23,11 +22,18 @@ if (Meteor.isClient && (Meteor.isTest || Meteor.isAppTest)) {
       throw new Error("Can't find test driver package: " + testDriverPackageName);
     }
 
-    if (typeof testDriverPackage.runTests !== "function") {
-      throw new Error("Test driver package " + testDriverPackageName
-        + " missing `runTests` export");
+    // On the client, the test driver *must* define `runTests`
+    if (Meteor.isClient) {
+      if (typeof testDriverPackage.runTests !== "function") {
+        throw new Error("Test driver package " + testDriverPackageName
+          + " missing `runTests` export");
+      }
+      testDriverPackage.runTests();  
+    } else {
+      // The server can optionally define `start`
+      if (typeof testDriverPackage.start === "function") {
+        testDriverPackage.start();
+      }
     }
-
-    testDriverPackage.runTests();
   });
 }
