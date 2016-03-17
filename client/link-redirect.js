f896ef881c608a027158cd9f1ec92820f203f192
Tom Coleman <tom@thesnail.org>
2016-03-17 15:59:36 +1100

    Removed simple docs + concepts and tried to link to appropriate places

--- client/link-redirect.js
+++ client/link-redirect.js
@@ -1,5 +1,80 @@
 // make links backwards compatible - for example, #deps -> #tracker
 
+// Links from the old basic API into the closest full-api section
+var BASIC_TO_FULL_LINKS = {
+  'learning-resources': 'guide',
+  'command-line': 'commandline',
+  'templates': 'templates_api',
+  'defining-templates': 'templates_api',
+  'Template-helpers': 'template_helpers',
+  'Template-events': 'template_events',
+  'Template-onRendered': 'template_onRendered',
+  'Blaze-TemplateInstance-findAll': 'template_findAll',
+  'Blaze-TemplateInstance-find': 'template_find',
+  'session': 'session',
+  'Session-set': 'session_set',
+  'Session-get': 'session_get',
+  'tracker': 'tracker',
+  'Tracker-autorun': 'tracker_autorun',
+  'collections': 'collections',
+  'Mongo-Collection': 'mongo_collection',
+  'Mongo-Collection-findOne': 'findone',
+  'Mongo-Collection-find': 'find',
+  'Mongo-Collection-insert': 'insert',
+  'Mongo-Collection-update': 'update',
+  'Mongo-Collection-remove': 'remove',
+  'Mongo-Collection-allow': 'allow',
+  'Mongo-Collection-deny': 'deny',
+  'accounts': 'accounts_api',
+  'loginButtons': 'accountsui',
+  'Meteor-user': 'meteor_user',
+  'Meteor-userId': 'meteor_userid',
+  'Meteor-users': 'meteor_users',
+  'currentUser': 'template_currentuser',
+  'methods': 'methods_header',
+  'Meteor-methods': 'meteor_methods',
+  'Meteor-call': 'meteor_call',
+  'Meteor-Error': 'meteor_error',
+  'pubsub': 'publishandsubscribe',
+  'Meteor-publish': 'meteor_publish',
+  'Meteor-subscribe': 'meteor_subscribe',
+  'environment': 'core',
+  'Meteor-isClient': 'meteor_isclient',
+  'Meteor-isServer': 'meteor_isserver',
+  'Meteor-startup': 'meteor_startup',
+  'packages': 'packages',
+  'searchingforpackages': 'packages',
+  'accountsui': 'accountsui',
+  'coffeescript': 'coffeescript',
+  'email': 'email',
+  'jquery': 'jquery',
+  'http': 'http',
+  'less': 'less',
+  'markdown': 'markdown',
+  'underscore': 'underscore',
+  'spiderable': 'spiderable',
+};
+
+var BASIC_TO_GUIDE_LINKS = {
+  filestructure: 'structure.html',
+  buildingmobileapps: 'mobile.html',
+  quickstart: '#quickstart',
+};
+
+var FULL_TO_GUIDE_LINKS = {
+  whatismeteor: '#what-is-meteor',
+  sevenprinciples: '#what-is-meteor',
+  quickstart: '#quickstart',
+  structuringyourapp: 'structure.html',
+  dataandsecurity: 'security.html',
+  livehtmltemplates: '#what-is-meteor',
+  usingpackages: 'user-packages.html',
+  namespacing: 'structure.html',
+  deploying: 'deployment.html',
+  writingpackages: 'writing-packages.html',
+};
+
+
 Meteor.startup(function () {
   var getRedirect = function (hash) {
     if (hash.indexOf("deps") !== -1) {
@@ -34,8 +109,27 @@ Meteor.startup(function () {
       return "meteorbuild";
     }
 
-    if (hash === "whatismeteor" || hash === "sevenprinciples") {
-      window.location.replace("http://guide.meteor.com/#what-is-meteor");
+    if (hash.indexOf("reactivity") !== -1) {
+      return "/full/tracker";
+    }
+
+    var parts = hash.split('/');
+    if (parts[1] === 'basic') {
+      var fullLink = BASIC_TO_FULL_LINKS[parts[2]];
+      if (fullLink) {
+        return 'full/' + fullLink;
+      }
+
+      var guideLink = BASIC_TO_GUIDE_LINKS[parts[2]];
+      if (guideLink) {
+        window.location.replace('http://guide.meteor.com/' + guideLink);
+      } 
+    }
+    if (parts[1] === 'full') {
+      var guideLink = FULL_TO_GUIDE_LINKS[parts[2]];
+      if (guideLink) {
+        window.location.replace('http://guide.meteor.com/' + guideLink);
+      } 
     }
 
     // don't redirect
@@ -44,6 +138,7 @@ Meteor.startup(function () {
 
   var curLink = window.location.hash.slice(1);
   var redirect = getRedirect(curLink);
+  console.log(redirect)
 
   if (redirect) {
     window.location = "#" + redirect;
