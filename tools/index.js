8248d86fb2aeb27aac30d0ca0682e0ab8008320c
Ben Newman <ben@meteor.com>
2016-04-01 12:04:38 -0400

    Consolidate exit code logic for `meteor npm` and `meteor node`.
    Follow-up to #6675.

--- tools/index.js
+++ tools/index.js
@@ -1,18 +1,4 @@
-var spawnBinProcess = require('./cli/dev-bundle-bin-commands.js').process
-if (spawnBinProcess) {
-  // On Node 0.10 on Windows, stdout and stderr don't get flushed when calling
-  // `process.exit`. We use a workaround for now, but this should be fixed on
-  // Node 0.12, so when we upgrade let's remember to remove this clause, and the
-  // file it requires. See https://github.com/joyent/node/issues/3584
-  // This same comment and require is in ./cli/main.js
-  if (process.platform === 'win32') {
-    require('./tool-env/flush-buffers-on-exit-in-windows.js');
-  }
-
-  spawnBinProcess.on('exit', function (exitCode) {
-    process.exit(exitCode);
-  });
-} else {
+if (! require('./cli/dev-bundle-bin-commands.js').process) {
   // Set up the Babel transpiler
   require('./tool-env/install-babel.js');
 
