aaba293f4578a155d9ad644d6c22c04fb3d83ef6
Ben Newman <ben@meteor.com>
2016-04-06 09:26:25 -0400

    Load jquery as a peer dependency if installed by app.
    Fixes #6626.

--- packages/jquery/main.js
+++ packages/jquery/main.js
@@ -1,5 +1,10 @@
-var jQuery = require("./jquery.js");
 var global = this;
+
+try {
+  var jQuery = require("jquery");
+} catch (e) {
+  jQuery = require("./jquery.js");
+}
 
 // Provide values for the exported variables of the jquery package.
 exports.$ = exports.jQuery = jQuery;
@@ -7,4 +12,5 @@ exports.$ = exports.jQuery = jQuery;
 // There's no stopping legacy code from referring to window.$ or
 // window.jQuery, so we have to keep defining those properties globally,
 // but at least the exports of this package will be reliable.
-global.$ = global.jQuery = jQuery;
+global.$ = global.$ || jQuery;
+global.jQuery = global.jQuery || jQuery;
