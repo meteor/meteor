37659529ca58c00c7c7216694009847ffc09d095
Ben Newman <ben@meteor.com>
2016-04-06 10:05:45 -0400

    Eliminate check package dependency from babel-compiler.

--- packages/babel-compiler/package.js
+++ packages/babel-compiler/package.js
@@ -6,7 +6,7 @@ Package.describe({
   // isn't possible because you can't publish a non-recommended
   // release with package versions that don't have a pre-release
   // identifier at the end (eg, -dev)
-  version: '6.6.1'
+  version: '6.6.2'
 });
 
 Npm.depends({
@@ -18,8 +18,6 @@ Package.onUse(function (api) {
     'babel.js',
     'babel-compiler.js'
   ], 'server');
-
-  api.use('check@1.1.0');
 
   api.export('Babel', 'server');
   api.export('BabelCompiler', 'server');
