3be69066590e621a2ba885853f38f2ecfcbcfb02
Zoltan Olah <zol@percolatestudio.com>
2016-04-08 09:39:51 -0700

    Release version 1.3.2 with dependency on Meteor 1.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Allows you to define and run scheduled jobs across multiple servers.",
-  version: "1.3.1",
+  version: "1.3.2",
   name: "percolate:synced-cron",
   git: "https://github.com/percolatestudio/meteor-synced-cron.git"
 });
@@ -8,13 +8,13 @@ Package.describe({
 Npm.depends({later: "1.1.6"});
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@0.9.1.1');
+  api.versionsFrom('METEOR@1.3');
   api.use(['underscore', 'check', 'mongo', 'logging'], 'server');
   api.add_files(['synced-cron-server.js'], "server");
   api.export('SyncedCron', 'server');
 });
 
 Package.onTest(function (api) {
-  api.use(['percolate:synced-cron', 'tinytest']);
+  api.use(['percolate:synced-cron', 'tinytest', 'underscore', 'logging']);
   api.add_files('synced-cron-tests.js', ['server']);
 });
