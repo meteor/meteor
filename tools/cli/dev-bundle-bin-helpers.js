7c994feb778fdbd7caa7f5d64796f8f3b758b97e
Ben Newman <ben@meteor.com>
2016-04-25 10:10:24 -0400

    Make sure env.Path === env.PATH on Windows.

--- tools/cli/dev-bundle-bin-helpers.js
+++ tools/cli/dev-bundle-bin-helpers.js
@@ -8,15 +8,20 @@ exports.getCommandPath = function (command) {
 
 exports.getEnv = function () {
   var env = Object.create(process.env);
-
-  env.PATH = [
+  var paths = [
     // When npm looks for node, it must find dev_bundle/bin/node.
     binDir,
     // Also make available any scripts installed by packages in
     // dev_bundle/lib/node_modules, such as node-gyp.
     path.join(devBundleDir, "lib", "node_modules", ".bin"),
-    env.PATH
-  ].join(path.delimiter);
+  ];
+
+  var PATH = env.PATH || env.Path;
+  if (PATH) {
+    paths.push(PATH);
+  }
+
+  env.PATH = paths.join(path.delimiter);
 
   if (process.platform === "win32") {
     // On Windows we provide a reliable version of python.exe for use by
@@ -27,6 +32,11 @@ exports.getEnv = function () {
     // We don't try to install a compiler toolchain on the developer's
     // behalf, but setting GYP_MSVS_VERSION helps select the right one.
     env.GYP_MSVS_VERSION = env.GYP_MSVS_VERSION || "2015";
+
+    // While the original process.env object allows for case insensitive
+    // access on Windows, Object.create interferes with that behavior, so
+    // here we ensure env.PATH === env.Path on Windows.
+    env.Path = env.PATH;
   }
 
   return env;
