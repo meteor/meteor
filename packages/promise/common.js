12ba78979df35dcae153524a82915b984e4d6b3b
Ben Newman <ben@meteor.com>
2016-04-25 15:17:34 -0400

    Upgrade meteor-promise to 0.7.1.
    Note that this may be a breaking change for server-side code that uses
    Promise.denodeify, Promise.nodeify, Function.prototype.async, or
    Function.prototype.asyncApply, since those APIs have been removed for the
    sake of simplicity.

---
+++ packages/promise/common.js
@@ -0,0 +1,21 @@
+var global = this;
+
+if (typeof global.Promise === "function") {
+  exports.Promise = global.Promise;
+} else {
+  exports.Promise = require("promise/lib/es6-extensions");
+}
+
+exports.Promise.prototype.done = function (onFulfilled, onRejected) {
+  var self = this;
+
+  if (arguments.length > 0) {
+    self = this.then.apply(this, arguments);
+  }
+
+  self.then(null, function (err) {
+    Meteor._setImmediate(function () {
+      throw err;
+    });
+  });
+};
