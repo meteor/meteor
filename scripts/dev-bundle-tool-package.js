d82b175c727049588fb2112c2f46090b33ff8b58
Ben Newman <ben@meteor.com>
2016-04-27 19:38:49 -0400

    Use fibers@1.0.10 now that our Windows fork has been merged.

--- scripts/dev-bundle-tool-package.js
+++ scripts/dev-bundle-tool-package.js
@@ -4,17 +4,6 @@
 // We put this in a JS file so that it can contain comments. It is processed
 // into a package.json file by generate-dev-bundle.sh.
 
-var fibersVersion;
-if (process.platform === "win32") {
-  // We have a fork of fibers off of version 1.0.5 that searches farther for
-  // the isolate thread. This problem is a result of antivirus programs messing
-  // with the thread counts on Windows.
-  // Duplicated in dev-bundle-server-package.js
-  fibersVersion = "https://github.com/meteor/node-fibers/tarball/d519f0c5971c33d99c902dad346b817e84bab001";
-} else {
-  fibersVersion = "1.0.8";
-}
-
 var packageJson = {
   name: "meteor-dev-bundle-tool",
   // Version is not important but is needed to prevent warnings.
@@ -23,7 +12,7 @@ var packageJson = {
     // Explicit dependency because we are replacing it with a bundled version
     // and we want to make sure there are no dependencies on a higher version
     npm: "2.15.1",
-    fibers: fibersVersion,
+    fibers: "1.0.10",
     "meteor-babel": "0.9.2",
     "meteor-promise": "0.5.1",
     // So that Babel 6 can emit require("babel-runtime/helpers/...") calls.
