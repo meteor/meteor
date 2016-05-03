249e4498b739366bc41410ab187502fb91d77839
Tom Coleman <tom@thesnail.org>
2016-05-03 16:33:18 -0700

    Increase timeout on test self-test (#6964)

--- tools/tests/run.js
+++ tools/tests/run.js
@@ -430,12 +430,12 @@ selftest.define("'meteor test --port' accepts/rejects proper values", function (
   runAddPackage.expectExit(0);
 
   run = s.run("test", "--port", "3700", "--driver-package", "practicalmeteor:mocha");
-  run.waitSecs(30);
+  run.waitSecs(60);
   run.match('App running at: http://localhost:3700/');
   run.stop();
 
   run = s.run("test", "--port", "127.0.0.1:3700", "--driver-package", "practicalmeteor:mocha");
-  run.waitSecs(30);
+  run.waitSecs(60);
   run.match('App running at: http://127.0.0.1:3700/');
   run.stop();
 });
