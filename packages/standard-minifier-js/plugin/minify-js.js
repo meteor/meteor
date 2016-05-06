29bdb8300d061a206f65808d7a9fa6dc28dcd6f8
Wexpo Lyu <laosb@lns.unihubs.com>
2016-05-06 15:23:54 +0800

    Don't reminify *.min.js

--- packages/standard-minifier-js/plugin/minify-js.js
+++ packages/standard-minifier-js/plugin/minify-js.js
@@ -33,7 +33,12 @@ UglifyJSMinifier.prototype.processFilesForBundle = function (files, options) {
 
   var allJs = '';
   files.forEach(function (file) {
-    allJs += UglifyJSMinify(file.getContentsAsString(), minifyOptions).code;
+    // Don't reminify *.min.js.
+    if (/\.min\.js$/.test(file.getPathInBundle())) {
+      allJs += file.getContentsAsString();
+    } else {
+      allJs += UglifyJSMinify(file.getContentsAsString(), minifyOptions).code;
+    }
     allJs += '\n\n';
 
     Plugin.nudge();
