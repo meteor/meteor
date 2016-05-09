b3bcdbafda77322c5994516dd917955ac7f18706
Ben Newman <ben@meteor.com>
2016-05-09 17:31:29 -0400

    Disable progress spinner when running headless CI tests.

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -10,21 +10,39 @@ case $CIRCLE_NODE_INDEX in
 0)
   echo "Running test-packages"
   echo "Running self-test (1): A-Com"
-  ./packages/test-in-console/run.sh && ./meteor self-test --file "^[a-b]|^c[a-n]|^co[a-l]|^compiler-plugins" --exclude "$SELF_TEST_EXCLUDE"
+  ./packages/test-in-console/run.sh
+  ./meteor self-test --headless \
+      --file "^[a-b]|^c[a-n]|^co[a-l]|^compiler-plugins" \
+      --exclude "$SELF_TEST_EXCLUDE"
   ;;
 1)
   echo "Running self-test (2): Con-K"
   echo "Running self-test (3): L-O"
-  ./meteor self-test --file "^co[n-z]|^c[p-z]|^[d-k]" --exclude "$SELF_TEST_EXCLUDE" && ./meteor self-test --file "^[l-o]" --exclude "$SELF_TEST_EXCLUDE"
+  ./meteor self-test --headless \
+      --file "^co[n-z]|^c[p-z]|^[d-k]" \
+      --exclude "$SELF_TEST_EXCLUDE"
+  ./meteor self-test --headless \
+      --file "^[l-o]" \
+      --exclude "$SELF_TEST_EXCLUDE"
   ;;
 2)
   echo "Running self-test (4): P"
   echo "Running self-test (5): Run"
-  ./meteor self-test --file "^p" --exclude "$SELF_TEST_EXCLUDE"  && ./meteor self-test --file "^run" --exclude "$SELF_TEST_EXCLUDE"
+  ./meteor self-test --headless \
+      --file "^p" \
+      --exclude "$SELF_TEST_EXCLUDE"
+  ./meteor self-test --headless \
+      --file "^run" \
+      --exclude "$SELF_TEST_EXCLUDE"
   ;;
 3)
   echo "Running self-test (6): R-So"
   echo "Running self-test (7): Sp-Z"
-  ./meteor self-test --file "^r(?!un)|^s[a-o]" --exclude "$SELF_TEST_EXCLUDE" && ./meteor self-test --file "^s[p-z]|^[t-z]|^command-line" --exclude "$SELF_TEST_EXCLUDE"
+  ./meteor self-test --headless \
+      --file "^r(?!un)|^s[a-o]" \
+      --exclude "$SELF_TEST_EXCLUDE"
+  ./meteor self-test --headless \
+      --file "^s[p-z]|^[t-z]|^command-line" \
+      --exclude "$SELF_TEST_EXCLUDE"
   ;;
 esac
