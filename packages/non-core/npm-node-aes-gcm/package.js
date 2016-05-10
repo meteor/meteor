971206dfe61f41812899ae8f66208494b961fb5f
Ben Newman <ben@meteor.com>
2016-05-10 13:16:40 -0400

    Use a wrapper package for node-aes-gcm.
    This package specifies a "main" module in its package.json that has a
    .node file extension, which is currently not something Meteor supports.
    This needs to be fixed on Meteor's side, but in the meantime I wanted this
    package to continue working with Meteor 1.3.

--- packages/non-core/npm-node-aes-gcm/package.js
+++ packages/non-core/npm-node-aes-gcm/package.js
@@ -1,14 +1,15 @@
 Package.describe({
   summary: "Wrapper around the node-aes-gcm npm package",
-  version: '0.1.3_6',
+  version: '0.1.7_2',
   documentation: null
 });
 
 Npm.depends({
-  'node-aes-gcm': '0.1.3'
+  'meteor-node-aes-gcm': '0.1.7'
 });
 
 Package.onUse(function (api) {
+  api.use("modules@0.6.1");
   api.export('NpmModuleNodeAesGcm', 'server');
   api.addFiles('wrapper.js', 'server');
 });
