6aec3603c872f93b88532d7d571e0426d8461339
Ben Newman <ben@meteor.com>
2016-05-19 09:23:20 -0400

    Separate install-runtime.js from install-babel.js.

--- tools/tool-env/install-babel.js
+++ tools/tool-env/install-babel.js
@@ -1,9 +1,8 @@
 // This file exists because it is the file in the tool that is not automatically
 // transpiled by Babel
 
-var meteorBabel = require("meteor-babel");
-
 function babelRegister() {
+  var meteorBabel = require("meteor-babel");
   var path = require("path");
   var toolsPath = path.dirname(__dirname);
   var meteorPath = path.dirname(toolsPath);
@@ -18,31 +17,4 @@ function babelRegister() {
 
 babelRegister(); // #RemoveInProd this line is removed in isopack.js
 
-// Install ES2015-complaint polyfills for Object, Array, String, Function,
-// Symbol, Map, and Set, patching the native implementations if available.
-require("meteor-ecmascript-runtime");
-
-// Install a global ES2015-compliant Promise constructor that knows how to
-// run all its callbacks in Fibers.
-var Promise = global.Promise = global.Promise ||
-  require("promise/lib/es6-extensions");
-require("meteor-promise").makeCompatible(Promise, require("fibers"));
-
-// Verify that the babel-runtime package is available to be required.
-var regeneratorRuntime = require("babel-runtime/regenerator").default;
-
-// Use Promise.asyncApply to wrap calls to runtime.async so that the
-// entire async function will run in its own Fiber, not just the code that
-// comes after the first await.
-var realAsync = regeneratorRuntime.async;
-regeneratorRuntime.async = function () {
-  return Promise.asyncApply(realAsync, regeneratorRuntime, arguments);
-};
-
-// Install global.meteorBabelHelpers so that the compiler doesn't need to
-// add boilerplate at the top of every file
-meteorBabel.defineHelpers();
-
-// Installs source map support with a hook to add functions to look for source
-// maps in custom places
-require('./source-map-retriever-stack.js');
+require("./install-runtime.js");
