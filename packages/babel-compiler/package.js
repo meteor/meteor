9fa732d588253ab8be0fd1e4ab56c39353a44c14
Ben Newman <ben@meteor.com>
2016-05-19 09:52:15 -0400

    Upgrade meteor-babel to v0.10.2.
    Among other general improvements, the installed node_modules directory has
    been reduced in size from 500MB+ to 30MB.

--- packages/babel-compiler/package.js
+++ packages/babel-compiler/package.js
@@ -6,11 +6,11 @@ Package.describe({
   // isn't possible because you can't publish a non-recommended
   // release with package versions that don't have a pre-release
   // identifier at the end (eg, -dev)
-  version: '6.7.0'
+  version: '6.8.0'
 });
 
 Npm.depends({
-  'meteor-babel': '0.9.3'
+  'meteor-babel': '0.10.2'
 });
 
 Package.onUse(function (api) {
