add36ae322ca60b05b98f316a47d0cc8efce4b2d
Tom Coleman <tom@thesnail.org>
2016-05-19 11:27:11 -0700

    Added test to check *updating* constraints

--- tools/tests/update-tests.js
+++ tools/tests/update-tests.js
@@ -7,7 +7,7 @@ const SIMPLE_WAREHOUSE = {
   v2: { recommended: true }
 };
 
-selftest.define("'meteor update' alters `.meteor/packages`", () => {
+selftest.define("'meteor update' adds constraints to `.meteor/packages`", () => {
   const s = new Sandbox({
     warehouse: SIMPLE_WAREHOUSE,
     fakeMongo: true
@@ -25,5 +25,29 @@ selftest.define("'meteor update' alters `.meteor/packages`", () => {
   const packages = s.read(".meteor/packages");
   if (!packages.match('meteor-base@')) {
     selftest.fail("Failed to add a version specifier to `meteor-base` package");
+  }
+});
+
+selftest.define("'meteor update' alters constraints in `.meteor/packages`", () => {
+  const s = new Sandbox({
+    warehouse: SIMPLE_WAREHOUSE,
+    fakeMongo: true
+  });
+
+  s.createApp("myapp", "very-simple-app-with-no-package-constraints", {
+    release: DEFAULT_RELEASE_TRACK + '@v1'
+  });
+  s.cd("myapp");
+
+  // change the package file to be an old version
+  s.write('.meteor/packages', 'meteor-base@0.0.1');
+
+  run = s.run("update");
+  run.match("updated to Meteor v2");
+  run.expectExit(0);
+
+  const packages = s.read(".meteor/packages");
+  if (packages.match('meteor-base@0.0.1')) {
+    selftest.fail("Failed to update the version specifier for the `meteor-base` package");
   }
 });
