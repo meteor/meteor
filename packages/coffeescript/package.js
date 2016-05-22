4f176b349072d1593597b53b79fc6efcf1f3b536
Geoffrey Booth <webmaster@geoffreybooth.com>
2016-05-22 15:05:58 -0700

    Move all the CoffeeScript testing-related files into `packages/coffeescript/tests` so as not to confuse the impressionable `ecmascript` package

--- packages/coffeescript/package.js
+++ packages/coffeescript/package.js
@@ -21,15 +21,15 @@ Package.onUse(function (api) {
 Package.onTest(function (api) {
   api.use(['coffeescript', 'tinytest']);
   api.use(['coffeescript-test-helper', 'ecmascript'], ['client', 'server']);
-  api.addFiles('bare_test_setup.coffee', ['client'], {bare: true});
-  api.addFiles('bare_tests.js', ['client']);
+  api.addFiles('tests/bare_test_setup.coffee', ['client'], {bare: true});
+  api.addFiles('tests/bare_tests.js', ['client']);
   api.addFiles([
-    'coffeescript_test_setup.js',
+    'tests/coffeescript_test_setup.js',
     'tests/coffeescript_tests.coffee',
     'tests/coffeescript_strict_tests.coffee',
     'tests/es2015_module.js',
     'tests/litcoffeescript_tests.litcoffee',
     'tests/litcoffeescript_tests.coffee.md',
-    'coffeescript_tests.js'
+    'tests/coffeescript_tests.js'
   ], ['client', 'server']);
 });
