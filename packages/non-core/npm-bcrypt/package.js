edbe9fb50ba8e001ce95f215b265204a1bc1c849
Ben Newman <ben@meteor.com>
2016-06-09 10:47:50 -0400

    Upgrade bcrypt to 0.8.6.

--- packages/non-core/npm-bcrypt/package.js
+++ packages/non-core/npm-bcrypt/package.js
@@ -1,11 +1,11 @@
 Package.describe({
   summary: "Wrapper around the bcrypt npm package",
-  version: '0.8.5',
+  version: '0.8.6',
   documentation: null
 });
 
 Npm.depends({
-  bcrypt: '0.8.5'
+  bcrypt: '0.8.6'
 });
 
 Package.onUse(function (api) {
