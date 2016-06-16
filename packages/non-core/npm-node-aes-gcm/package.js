225d2d7b657f7450cbf7d4da4f967e459a2af0ad
Ben Newman <ben@meteor.com>
2016-06-16 19:09:59 -0400

    Roll node-aes-gcm back to a version compatible with Node 0.10.

--- packages/non-core/npm-node-aes-gcm/package.js
+++ packages/non-core/npm-node-aes-gcm/package.js
@@ -1,11 +1,11 @@
 Package.describe({
   summary: "Wrapper around the node-aes-gcm npm package",
-  version: '0.1.7_6',
+  version: '0.1.5',
   documentation: null
 });
 
 Npm.depends({
-  'node-aes-gcm': '0.1.7'
+  'node-aes-gcm': '0.1.5'
 });
 
 Package.onUse(function (api) {
