a0a1985b2931c9dc0aefa38cc172d9bd6117bc53
Ben Newman <ben@meteor.com>
2016-06-16 15:17:05 -0400

    Upgrade npm dependency of npm-node-aes-gcm package to v0.1.7.

--- packages/non-core/npm-node-aes-gcm/package.js
+++ packages/non-core/npm-node-aes-gcm/package.js
@@ -1,11 +1,11 @@
 Package.describe({
   summary: "Wrapper around the node-aes-gcm npm package",
-  version: '0.1.3_6',
+  version: '0.1.7_4',
   documentation: null
 });
 
 Npm.depends({
-  'node-aes-gcm': '0.1.3'
+  'node-aes-gcm': '0.1.7'
 });
 
 Package.onUse(function (api) {
