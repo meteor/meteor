e025d3054a930fd4340873c9ed73eac61e8270c7
Ben Newman <ben@meteor.com>
2016-06-20 16:26:12 -0400

    Upgrade meteor-babel and reify to latest versions.
    The bulk of this diff is due to flattening npm-shrinkwrap.json files
    because we're now using npm@3 instead of npm@2.

--- packages/modules/package.js
+++ packages/modules/package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: "modules",
-  version: "0.6.3",
+  version: "0.6.4",
   summary: "CommonJS module system",
   documentation: "README.md"
 });
 
 Npm.depends({
-  reify: "0.3.3"
+  reify: "0.3.4"
 });
 
 Package.onUse(function(api) {
