887cf9670b14f0205be14ffd575c5151da731bcb
Ben Newman <ben@meteor.com>
2016-06-22 16:56:05 -0400

    Bump package versions for the official 1.3.4 release.

--- packages/http/package.js
+++ packages/http/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Make HTTP calls to remote servers",
-  version: '1.1.7-rc.2'
+  version: '1.1.7'
 });
 
 Npm.depends({
--- packages/stylus/package.js
+++ packages/stylus/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Expressive, dynamic, robust CSS',
-  version: "2.512.3-rc.2"
+  version: "2.512.3"
 });
 
 Package.registerBuildPlugin({
