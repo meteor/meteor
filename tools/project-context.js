2e73838926a6df656681ac4ec522de089671d8a8
Wexpo Lyu <laosb@lns.unihubs.com>
2016-07-03 12:41:18 +0800

    Relative path resolves to project directory.
    Fixing #7292

--- tools/project-context.js
+++ tools/project-context.js
@@ -1,5 +1,6 @@
 var assert = require("assert");
 var _ = require('underscore');
+var path = require('path');
 
 var archinfo = require('./utils/archinfo.js');
 var buildmessage = require('./utils/buildmessage.js');
@@ -89,9 +90,11 @@ _.extend(ProjectContext.prototype, {
     // writes to; used by `meteor test` so that you can test your
     // app in parallel to writing it, with an isolated database.
     // You can override the default .meteor/local by specifying
-    // METEOR_LOCAL_DIR.
+    // METEOR_LOCAL_DIR. You can use relative path if you want it
+    // relative to your project directory.
     self.projectLocalDir = process.env.METEOR_LOCAL_DIR ?
-      process.env.METEOR_LOCAL_DIR : (options.projectLocalDir ||
+      path.resolve(options.projectLocalDir, process.env.METEOR_LOCAL_DIR)
+      : (options.projectLocalDir ||
       files.pathJoin(self.projectDir, '.meteor', 'local'));
 
     // Used by 'meteor rebuild'; true to rebuild all packages, or a list of
