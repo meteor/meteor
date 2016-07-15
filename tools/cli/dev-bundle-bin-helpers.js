578d40a75368fa90c9468b37895acef4369a0fb1
Ben Newman <ben@meteor.com>
2016-07-15 15:50:58 -0400

    Use node-gyp to infer GYP_MSVS_VERSION when possible.
    As a refinement of my previous commit, we will continue setting the
    $GYP_MSVS_VERSION variable to "2015" except in cases where the Python
    library behind node-gyp succeeds in inferring the correct version.
    
    Note that the Python library will never infer "2015" by itself, even
    though that's the version recommended by the node-gyp setup instructions:
    
      https://github.com/nodejs/node-gyp#installation.
    
    Note also that the Windows machines used by `meteor admin get-machine` and
    build.meteor.com have version "2010e", which can be inferred.
    
    In other words, this commit allows the following groups of developers to
    avoid setting $GYP_MSVS_VERSION manually:
    
      1. Developers who followed the node-gyp setup instructions.
      2. Package authors who run `meteor publish-for-arch` on build machines.

--- tools/cli/dev-bundle-bin-helpers.js
+++ tools/cli/dev-bundle-bin-helpers.js
@@ -34,17 +34,67 @@ exports.getEnv = function() {
     env.PATH = paths.join(path.delimiter);
 
     if (process.platform === "win32") {
-      // On Windows we provide a reliable version of python.exe for use by
-      // node-gyp (the tool that rebuilds binary node modules). #WinPy
-      env.PYTHON = env.PYTHON || path.join(
-        devBundleDir, "python", "python.exe");
-
-      // While the original process.env object allows for case insensitive
-      // access on Windows, Object.create interferes with that behavior,
-      // so here we ensure env.PATH === env.Path on Windows.
-      env.Path = env.PATH;
+      return addWindowsVariables(devBundleDir, env);
     }
 
     return env;
   });
 };
+
+// Caching env.GYP_MSVS_VERSION allows us to avoid invoking Python every
+// time Meteor runs an npm command. TODO Store this on disk?
+var cachedMSVSVersion;
+
+function addWindowsVariables(devBundleDir, env) {
+  // On Windows we provide a reliable version of python.exe for use by
+  // node-gyp (the tool that rebuilds binary node modules). #WinPy
+  env.PYTHON = env.PYTHON || path.join(
+    devBundleDir, "python", "python.exe");
+
+  // While the original process.env object allows for case insensitive
+  // access on Windows, Object.create interferes with that behavior,
+  // so here we ensure env.PATH === env.Path on Windows.
+  env.Path = env.PATH;
+
+  if (cachedMSVSVersion) {
+    env.GYP_MSVS_VERSION = cachedMSVSVersion;
+  }
+
+  if (env.GYP_MSVS_VERSION) {
+    return Promise.resolve(env);
+  }
+
+  // If $GYP_MSVS_VERSION was not provided, use the gyp Python library to
+  // infer it, or default to 2015 if that doesn't work.
+  return new Promise(function (resolve) {
+    var nodeGypPylibDir = path.join(
+      devBundleDir, "lib", "node_modules", "node-gyp", "gyp", "pylib"
+    );
+
+    var child = require("child_process").spawn(env.PYTHON, ["-c", [
+      "from gyp.MSVSVersion import SelectVisualStudioVersion",
+      "try:",
+      "  print SelectVisualStudioVersion(allow_fallback=False).short_name",
+      "except:",
+      "  print 2015"
+    ].join("\n")], {
+      env: env,
+      cwd: nodeGypPylibDir,
+      stdio: "pipe"
+    });
+
+    var chunks = [];
+    child.stdout.on("data", function (chunk) {
+      chunks.push(chunk.toString("utf8"));
+    });
+
+    function finish(codeOrError) {
+      env.GYP_MSVS_VERSION = cachedMSVSVersion =
+        codeOrError ? "2015" : chunks.join("").replace(/^\s+|\s+$/g, "");
+      resolve(env);
+    }
+
+    child.on("error", finish);
+    child.on("exit", finish);
+  });
+}
