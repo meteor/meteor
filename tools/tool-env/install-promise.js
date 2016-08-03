36afd7f5c3d3ccb36f1002095d1eca8112f69215
Ben Newman <ben@meteor.com>
2016-08-03 15:18:34 -0400

    Defend global.Promise against misbehaving polyfills.

--- tools/tool-env/install-promise.js
+++ tools/tool-env/install-promise.js
@@ -1,10 +1,24 @@
 // Install a global ES2015-compliant Promise constructor that knows how to
 // run all its callbacks in Fibers.
 
-var Promise = global.Promise = global.Promise ||
+var Promise = global.Promise ||
   require("promise/lib/es6-extensions");
 
-require("meteor-promise").makeCompatible(
-  Promise,
-  require("fibers")
-);
+function makeCompatible(newPromise) {
+  require("meteor-promise").makeCompatible(
+    newPromise,
+    require("fibers")
+  );
+}
+
+makeCompatible(Promise);
+
+Object.defineProperty(global, "Promise", {
+  get: function () {
+    return Promise;
+  },
+
+  // Make the new Promise compatible with Fibers, but do not allow further
+  // modifications to global.Promise, e.g. by misbehaving polyfills.
+  set: makeCompatible
+});
