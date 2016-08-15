9478f35fa4116dc3ef35ef7fcd4ddc19ca97e1ee
Ben Newman <ben@meteor.com>
2016-08-15 14:52:02 -0400

    Bump package versions for 1.4.1-rc.2 release.

--- packages/standard-minifier-css/package.js
+++ packages/standard-minifier-css/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'standard-minifier-css',
-  version: '1.2.0-rc.1',
+  version: '1.2.0-rc.2',
   summary: 'Standard css minifier used with Meteor apps by default.',
   documentation: 'README.md'
 });
