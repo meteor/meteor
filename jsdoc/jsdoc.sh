e088fc141d21cfe5623081a1f286c30a78c5fce0
Tom Coleman <tom@thesnail.org>
2016-08-29 12:28:45 +1000

    Update to work with blaze-less code

--- jsdoc/jsdoc.sh
+++ jsdoc/jsdoc.sh
@@ -9,7 +9,12 @@ cd "$METEOR_DIR"
 # Call git grep to find all js files with the appropriate comment tags,
 # and only then pass it to JSDoc which will parse the JS files.
 # This is a whole lot faster than calling JSDoc recursively.
-git grep -al "@summary" | xargs -L ${INFINITY} -t \
+(git grep -al "@summary" \
+ # also run the git grep inside submodules. Possibly these docs should live
+ # elsewhere, but this works for now
+ && cd packages-for-isopackets/blaze \
+ && git grep -al "@summary" | sed -e 's/^/packages-for-isopackets\/blaze\//') \
+  | xargs -L ${INFINITY} -t \
     "$TOPDIR/node_modules/.bin/jsdoc" \
     -t "$TOPDIR/jsdoc/docdata-jsdoc-template" \
     -c "$TOPDIR/jsdoc/jsdoc-conf.json" \
