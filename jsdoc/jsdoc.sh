067299ea57f6b1cbef7a32504697bc138f0b6626
Tom Coleman <tom@thesnail.org>
2016-08-29 12:53:35 +1000

    Can't put a comment there

--- jsdoc/jsdoc.sh
+++ jsdoc/jsdoc.sh
@@ -9,9 +9,10 @@ cd "$METEOR_DIR"
 # Call git grep to find all js files with the appropriate comment tags,
 # and only then pass it to JSDoc which will parse the JS files.
 # This is a whole lot faster than calling JSDoc recursively.
+#
+# also run the git grep inside submodules. Possibly these docs should live
+# elsewhere, but this works for now
 (git grep -al "@summary" \
- # also run the git grep inside submodules. Possibly these docs should live
- # elsewhere, but this works for now
  && cd packages-for-isopackets/blaze \
  && git grep -al "@summary" | sed -e 's/^/packages-for-isopackets\/blaze\//') \
   | xargs -L ${INFINITY} -t \
