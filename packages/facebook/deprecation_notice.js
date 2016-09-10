6aba0d8797611efef4e464ecc61a2df8751d7c5f
laosb <laosb@lasteam.net>
2016-09-10 14:36:06 +0800

    Updated facebook.

--- packages/facebook/deprecation_notice.js
+++ packages/facebook/deprecation_notice.js
@@ -2,8 +2,8 @@ console.warn();
 console.warn('This package (facebook) has been deprecated.');
 console.warn("You should not use this anymore.");
 console.warn(
-    "If you need the export Facebook, you can now simply use accounts-facebook" +
+    "If you need the export Facebook, you can now simply use facebook-oauth" +
     "and import from it. If you need the Blaze OAuth configuration UI, add the " +
-    "blaze-accounts-config-ui-facebook."
+    "facebook-config-ui along with accounts-ui."
 );
 console.warn();
