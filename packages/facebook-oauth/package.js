730228da0aecb21d872a40695ffb166a49e83fb8
laosb <laosb@lasteam.net>
2016-09-10 14:33:29 +0800

    Created facebook-oauth.

---
+++ packages/facebook-oauth/package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  summary: "Facebook OAuth flow",
+  version: "1.3.0"
+});
+
+Package.onUse(function(api) {
+  api.use('oauth2', ['client', 'server']);
+  api.use('oauth', ['client', 'server']);
+  api.use('http', ['server']);
+  api.use('underscore', 'server');
+  api.use('random', 'client');
+  api.use('service-configuration', ['client', 'server']);
+
+  api.addFiles('facebook_client.js', 'client');
+  api.addFiles('facebook_server.js', 'server');
+});
