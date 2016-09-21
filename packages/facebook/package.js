c761e98ab1f8ebdbea28c8af79e1a68eb24b3fad
Wexpo Lyu <laosb@lns.unihubs.com>
2016-09-22 06:23:10 +0800

    Update package.js

--- packages/facebook/package.js
+++ packages/facebook/package.js
@@ -5,14 +5,6 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.use('oauth2', ['client', 'server']);
-  api.use('oauth', ['client', 'server']);
-  api.use('http', ['server']);
-  api.use('templating@1.2.13', 'client');
-  api.use('underscore', 'server');
-  api.use('random', 'client');
-  api.use('service-configuration', ['client', 'server']);
-  api.use('accounts-facebook@1.1.0');
   api.use('facebook-oauth');
   api.use('facebook-config-ui', 'client');
 
