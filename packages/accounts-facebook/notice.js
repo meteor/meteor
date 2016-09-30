30f612fa908f1aff9023dfe1e81c9dda4001d898
laosb <laosb@lasteam.net>
2016-09-30 16:57:01 +0800

    Updated the notice.

--- packages/accounts-facebook/notice.js
+++ packages/accounts-facebook/notice.js
@@ -1,5 +1,5 @@
 if (Package['accounts-ui'] && (Package['facebook-config-ui'] === undefined)) {
     console.info("You're using accounts-ui and accounts-facebook, but didn't",
-        "install the configuration UI for Facebook OAuth. You can do it by",
-        "adding facebook-config-ui.");
+        "install the configuration UI for Facebook OAuth.");
+    console.info("You can install it with `meteor add facebook-config-ui`.");
 }
