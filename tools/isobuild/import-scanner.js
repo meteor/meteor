meteor/meteor

commit 97ca5f1836646670629fde92fa993e021a89c5f2

Don't pass watchSet when instantiating Resolver objects.
We used the watchSet only when reading package.json files, and I think we
can add package.json files to the watchSet in the ImportScanner instead.

tools/isobuild/import-scanner.js
@@ -125,7 +125,6 @@ export default class ImportScanner {
       targetArch: bundleArch,
       extensions,
       nodeModulesPaths,
-      watchSet,
 
       onPackageJson(path, pkg) {
         return scanner._addPkgJsonToOutput(path, pkg);

tools/isobuild/compiler-plugin.js
@@ -878,7 +878,6 @@ export class PackageSourceBatch {
       targetArch: this.processor.arch,
       extensions: this.importExtensions,
       nodeModulesPaths,
-      watchSet: this.unibuild.watchSet,
       onMissing(id) {
         const error = new Error("Cannot find module '" + id + "'");
         error.code = "MODULE_NOT_FOUND";

tools/isobuild/resolver.js
@@ -48,7 +48,6 @@ export default class Resolver {
     targetArch,
     extensions = [".js", ".json"],
     nodeModulesPaths = [],
-    watchSet = null,
     onPackageJson,
     onMissing,
     statOrNull = optimisticStatOrNull,
@@ -57,7 +56,6 @@ export default class Resolver {
     this.extensions = extensions;
     this.targetArch = targetArch;
     this.nodeModulesPaths = nodeModulesPaths;
-    this.watchSet = watchSet;
     this.onPackageJson = onPackageJson;
     this.onMissing = onMissing;
     this.statOrNull = statOrNull;

commit 5c9d72450d8a819dfce1ac8a3862431916ae4a61

Watch package.json files reported from Resolver#resolve.
This change should mitigate any regressions from my earlier commit
97ca5f1836646670629fde92fa993e021a89c5f2.

tools/isobuild/import-scanner.js
@@ -785,6 +785,11 @@ export default class ImportScanner {
       };
 
       this._addFile(pkgJsonPath, pkgFile);
+
+      this.watchSet.addFile(
+        pkgJsonPath,
+        optimisticHashOrNull(pkgJsonPath),
+      );
     }
   }
 }