af7403e045cef4a8a29a1e88fb048a2c26f39fe2
Tom Coleman <tom@thesnail.org>
2016-10-05 14:26:27 +1100

    We need to export the Facebook symbol

--- packages/facebook-oauth/package.js
+++ packages/facebook-oauth/package.js
@@ -9,7 +9,10 @@ Package.onUse(function(api) {
   api.use('http', ['server']);
   api.use('underscore', 'server');
   api.use('random', 'client');
+  api.use('service-configuration', 'client');
 
   api.addFiles('facebook_client.js', 'client');
   api.addFiles('facebook_server.js', 'server');
+
+  api.export('Facebook');
 });
