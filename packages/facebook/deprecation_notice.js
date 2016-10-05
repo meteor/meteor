fe136fe8e02c1a50f359573ff0ec7cc9e869ec0d
Tom Coleman <tom@thesnail.org>
2016-10-05 14:26:33 +1100

    Change the wording a little

--- packages/facebook/deprecation_notice.js
+++ packages/facebook/deprecation_notice.js
@@ -1,9 +1,11 @@
 console.warn();
 console.warn('This package (facebook) has been deprecated.');
-console.warn("You should not use this anymore.");
 console.warn(
-    "If you need the export Facebook, you can now simply use facebook-oauth" +
-    "and import from it. If you need the Blaze OAuth configuration UI, add the " +
-    "facebook-config-ui along with accounts-ui."
+  "To use the `Facebook` symbol, use the `facebook-oauth` package " +
+  "and import from it."
+);
+console.warn(
+    "If you need the Blaze OAuth configuration UI, add " +
+    "`facebook-config-ui` alongside `accounts-ui`."
 );
 console.warn();
