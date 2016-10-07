58a6d743c86673688c08121a27cf43c67cec968d
Jesse Rosenberger <github@jro.cc>
2016-10-07 13:14:30 +0300

    `Console.success` should provide an indicator when pretty is disabled
    `Console.success` is currently called in only one place but it relies on the fact that `pretty` console mode is enabled for it to make any sense.  This adds the ability to pass an "ugly" (less pretty?) message to `Console.success` (default: "success") and changes the Cordova dependency checker to use "installed" when showing a met-dependency.
    
    Fixes meteor/meteor#7883

--- tools/console/console.js
+++ tools/console/console.js
@@ -920,12 +920,12 @@ _.extend(Console.prototype, {
 
   // A wrapper around Console.info. Prints the message out in green (if pretty),
   // with the CHECKMARK as the bullet point in front of it.
-  success: function (message) {
+  success: function (message, uglySuccessKeyword = "success") {
     var self = this;
     var checkmark;
 
     if (! self._pretty) {
-      return self.info(message);
+      return self.info(`${message}: ${uglySuccessKeyword}`);
     }
 
     if (process.platform === "win32") {
