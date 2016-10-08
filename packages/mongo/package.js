cc5bb8661108c9e0640a917363e8234bc071f351
Ben Newman <ben@meteor.com>
2016-10-08 13:57:58 -0400

    Use npm-mongo@2.2.10_1 in mongo@1.1.12_4.

--- packages/mongo/package.js
+++ packages/mongo/package.js
@@ -9,7 +9,7 @@
 
 Package.describe({
   summary: "Adaptor for using MongoDB and Minimongo over DDP",
-  version: '1.1.12_3'
+  version: '1.1.12_4'
 });
 
 Npm.depends({
@@ -21,7 +21,7 @@ Npm.strip({
 });
 
 Package.onUse(function (api) {
-  api.use('npm-mongo@1.5.48_1', 'server');
+  api.use('npm-mongo@2.2.10_1', 'server');
   api.use('allow-deny');
 
   api.use([
