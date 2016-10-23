bdee913b1b4d9589f9026fd450deb8a43e9af11f
Ben Newman <ben@meteor.com>
2016-10-23 17:30:10 -0400

    Support npm packages with native .node "main" modules.
    Note that the value of the "main" property must include the ".node"
    extension explicitly, because I have yet to encounter an npm package with
    a native "main" module that omits the ".node" extension, and I am loathe
    to start calling files.stat(id + ".node") uselessly for every single
    missing module.
    
    Fixes #7947.

--- tools/tests/apps/modules/server/only.js
+++ tools/tests/apps/modules/server/only.js
@@ -6,6 +6,11 @@ import "stripe";
 import "winston";
 import "mssql";
 
+// This package has a native .node module as the "main" property of its
+// package.json file. (#7947)
+import { start } from "idle-gc";
+assert.strictEqual(typeof start, "function");
+
 assert.strictEqual(Meteor.isServer, true);
 assert.strictEqual(Meteor.isClient, false);
 
