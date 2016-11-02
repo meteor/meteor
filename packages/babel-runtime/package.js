11c7b2aee2920708a4458996b1a96c2fbddcc96e
Ben Newman <ben@meteor.com>
2016-11-02 17:55:38 -0400

    Bump package versions for 1.4.2.1-beta.1 release.

--- packages/babel-runtime/package.js
+++ packages/babel-runtime/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "babel-runtime",
   summary: "Runtime support for output of Babel transpiler",
-  version: '1.0.0-beta.0',
+  version: '1.0.0-beta.1',
   documentation: 'README.md'
 });
 
