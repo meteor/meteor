51bb575e0055205a4cd3ffd51afe2b4c558a7565
Ben Newman <ben@meteor.com>
2016-11-08 17:38:18 -0500

    Bump ecmascript version to 0.6.0 to republish.
    Temporarily adding version constraints so that we can publish
    independently from the Meteor release.

--- packages/ecmascript/package.js
+++ packages/ecmascript/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ecmascript',
-  version: '0.5.9',
+  version: '0.6.0',
   summary: 'Compiler plugin that supports ES2015+ in all .js files',
   documentation: 'README.md'
 });
@@ -13,14 +13,14 @@ Package.registerBuildPlugin({
 
 Package.onUse(function (api) {
   api.use('isobuild:compiler-plugin@1.0.0');
-  api.use('babel-compiler');
+  api.use('babel-compiler@6.13.0');
 
   // The following api.imply calls should match those in
   // ../coffeescript/package.js.
-  api.imply('modules');
-  api.imply('ecmascript-runtime');
-  api.imply('babel-runtime');
-  api.imply('promise');
+  api.imply('modules@0.7.7');
+  api.imply('ecmascript-runtime@0.3.15');
+  api.imply('babel-runtime@1.0.0');
+  api.imply('promise@0.8.8');
 
   api.addFiles("ecmascript.js", "server");
   api.export("ECMAScript", "server");
