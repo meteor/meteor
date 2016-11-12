meteor/meteor

commit c5809a4a1c90a4878a6883639e2d1b6da0698aca

Allow files.cp_r to copy non-directories.

tools/fs/files.js
@@ -434,9 +434,7 @@ files.mkdir_p = function (dir, mode) {
   return pathIsDirectory(p);
 };
 
-// Roughly like cp -R. 'from' should be a directory. 'to' can either
-// be a directory, or it can not exist (in which case it will be
-// created with mkdir_p).
+// Roughly like cp -R.
 //
 // The output files will be readable and writable by everyone that the umask
 // allows, and executable by everyone (modulo umask) if the original file was
@@ -450,49 +448,55 @@ files.mkdir_p = function (dir, mode) {
 // If options.ignore is present, it should be a list of regexps. Any
 // file whose basename matches one of the regexps, before
 // transformation, will be skipped.
-files.cp_r = function (from, to, options) {
-  options = options || {};
+files.cp_r = function(from, to, options = {}) {
+  from = files.pathResolve(from);
 
-  var absFrom = files.pathResolve(from);
-  files.mkdir_p(to, 0o755);
+  const stat = options.preserveSymlinks
+    ? files.lstat(from)
+    : files.stat(from);
 
-  _.each(files.readdir(from), function (f) {
-    if (_.any(options.ignore || [], function (pattern) {
-      return f.match(pattern);
-    })) {
-      return;
-    }
+  if (stat.isDirectory()) {
+    files.mkdir_p(to, 0o755);
 
-    var fullFrom = files.pathJoin(from, f);
-    if (options.transformFilename) {
-      f = options.transformFilename(f);
-    }
-    var fullTo = files.pathJoin(to, f);
-    var stats = options.preserveSymlinks
-          ? files.lstat(fullFrom) : files.stat(fullFrom);
-    if (stats.isDirectory()) {
-      files.cp_r(fullFrom, fullTo, options);
-    } else if (stats.isSymbolicLink()) {
-      var linkText = files.readlink(fullFrom);
-      files.symlink(linkText, fullTo);
-    } else {
-      var absFullFrom = files.pathResolve(fullFrom);
-
-      // Create the file as readable and writable by everyone, and executable by
-      // everyone if the original file is executably by owner. (This mode will
-      // be modified by umask.) We don't copy the mode *directly* because this
-      // function is used by 'meteor create' which is copying from the read-only
-      // tools tree into a writable app.
-      var mode = (stats.mode & 0o100) ? 0o777 : 0o666;
-      if (!options.transformContents) {
-        copyFileHelper(fullFrom, fullTo, mode);
-      } else {
-        var contents = files.readFile(fullFrom);
-        contents = options.transformContents(contents, f);
-        files.writeFile(fullTo, contents, { mode: mode });
+    files.readdir(from).forEach(f => {
+      if (options.ignore &&
+          _.any(options.ignore,
+                pattern => f.match(pattern))) {
+        return;
       }
+
+      if (options.transformFilename) {
+        f = options.transformFilename(f);
+      }
+
+      files.cp_r(
+        files.pathJoin(from, f),
+        files.pathJoin(to, f),
+        options
+      );
+    })
+
+  } else if (stat.isSymbolicLink()) {
+    files.symlink(files.readlink(from), to);
+
+  } else {
+    // Create the file as readable and writable by everyone, and
+    // executable by everyone if the original file is executable by
+    // owner. (This mode will be modified by umask.) We don't copy the
+    // mode *directly* because this function is used by 'meteor create'
+    // which is copying from the read-only tools tree into a writable app.
+    const mode = (stat.mode & 0o100) ? 0o777 : 0o666;
+
+    if (options.transformContents) {
+      files.writeFile(to, options.transformContents(
+        files.readFile(from),
+        files.basename(from)
+      ), { mode });
+
+    } else {
+      copyFileHelper(from, to, mode);
     }
-  });
+  }
 };
 
 /**

commit de5325d4860c8dfc2c4266fd8407009e1098a02e

Fix options.transformFilename behavior in files.cp_r.
The options.transformFilename function is only supposed to transform
target file names, not the names of source files to be copied.

This behavior was broken by c5809a4a1c90a4878a6883639e2d1b6da0698aca
between Meteor 1.4.0.1 and 1.4.1. Thank goodness for `git bisect`!

The biggest symptom of this mistake was that `meteor create --package`
no longer created files based on the ~fs-name~.js and ~fs-name~-tests.js
template files.

This probably merits a 1.4.1.4 release in addition to 1.4.2.2.

tools/fs/files.js
@@ -479,12 +479,14 @@ files.cp_r = function(from, to, options = {}) {
         return;
       }
 
+      const fullFrom = files.pathJoin(from, f);
+
       if (options.transformFilename) {
         f = options.transformFilename(f);
       }
 
       files.cp_r(
-        files.pathJoin(from, f),
+        fullFrom,
         files.pathJoin(to, f),
         options
       );