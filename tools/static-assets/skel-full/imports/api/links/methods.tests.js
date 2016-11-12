76f85113f1528a74fd3eeb067fa3c8041b3813fa
David Burles <dburles@gmail.com>
2016-11-12 11:02:22 +1100

    remove validation test

--- tools/static-assets/skel-full/imports/api/links/methods.tests.js
+++ tools/static-assets/skel-full/imports/api/links/methods.tests.js
@@ -23,32 +23,5 @@ if (Meteor.isServer) {
 
       assert.equal(Links.find().count(), 1);
     });
-
-    it('insert link method validation', function () {
-      const addLink = Meteor.server.method_handlers['links.insert'];
-
-      let errors = 0;
-
-      // Check url is String
-      try {
-        addLink.apply({}, ['meteor.com', 2]);
-      } catch (e) {
-        errors += 1;
-      }
-      // Check title is String
-      try {
-        addLink.apply({}, [1, 'meteor.com']);
-      } catch (e) {
-        errors += 1;
-      }
-      // Check url is valid
-      try {
-        addLink.apply({}, ['meteor.com', 'meteor.com']);
-      } catch (e) {
-        errors += 1;
-      }
-
-      assert.equal(errors, 3);
-    });
   });
 }
