24b0ea9fdddafffcd15ef910cf0c26ddc0835a8e
Ben Newman <ben@meteor.com>
2016-11-14 21:05:57 -0500

    Bump compiler/linker/bundler salts to trigger fresh builds.

--- tools/isobuild/compiler-plugin.js
+++ tools/isobuild/compiler-plugin.js
@@ -62,7 +62,7 @@ import { isTestFilePath } from './test-files.js';
 // Cache the (slightly post-processed) results of linker.fullLink.
 const CACHE_SIZE = process.env.METEOR_LINKER_CACHE_SIZE || 1024*1024*100;
 const CACHE_DEBUG = !! process.env.METEOR_TEST_PRINT_LINKER_CACHE_DEBUG;
-const LINKER_CACHE_SALT = 12; // Increment this number to force relinking.
+const LINKER_CACHE_SALT = 13; // Increment this number to force relinking.
 const LINKER_CACHE = new LRU({
   max: CACHE_SIZE,
   // Cache is measured in bytes. We don't care about servePath.
