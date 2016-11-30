8bd9db71a7e35e09ab3beba2ba6a9bf0c304fff6
Jesse Rosenberger <github@jro.cc>
2016-11-30 03:43:12 +0200

    My own `meteor-tool` track to test meteor/meteor#7533
    This changes the publish tools and the track to use abernix:METEOR-TEST and an arbitrary beta release number.

--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,6 +1,7 @@
 Package.describe({
+  name: 'abernix:meteor-tool',
   summary: "The Meteor command-line tool",
-  version: '1.4.2_3'
+  version: '1.4.2-3-beta.59'
 });
 
 Package.includeTool();
--- scripts/admin/publish-meteor-tool-on-arch.sh
+++ scripts/admin/publish-meteor-tool-on-arch.sh
@@ -52,7 +52,7 @@ set -u
 if [ -d meteor ]; then
   rm -rf meteor
 fi
-git clone --recursive https://github.com/meteor/meteor.git
+git clone --recursive https://github.com/abernix/meteor.git
 cd meteor
 git fetch --tags
 END
