75ba505695f70162cdeb8e4f27558762cfe975d9
Jesse Rosenberger <git@jro.cc>
2016-12-13 17:02:01 +0200

    Upgrade `mongodb` npm package to version 2.2.15.

--- packages/npm-mongo/package.js
+++ packages/npm-mongo/package.js
@@ -3,12 +3,12 @@
 
 Package.describe({
   summary: "Wrapper around the mongo npm package",
-  version: '2.2.11_2',
+  version: '2.2.15_1',
   documentation: null
 });
 
 Npm.depends({
-  mongodb: "2.2.11"
+  mongodb: "2.2.15"
 });
 
 Package.onUse(function (api) {
