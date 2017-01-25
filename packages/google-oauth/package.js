b60750657e5b1e1fbeef7b1d2cd47f70d8ef790d
laosb <laosb@lasteam.net>
2017-01-25 12:43:25 +0800

    Fix according to @abernix 's review.

--- packages/google-oauth/package.js
+++ packages/google-oauth/package.js
@@ -8,10 +8,10 @@ Package.onUse(function(api) {
   api.use('oauth', ['client', 'server']);
   api.use('http', ['server']);
   api.use(['underscore', 'service-configuration'], ['client', 'server']);
-  api.use(['random', 'templating@1.2.13'], 'client');
-
-  api.export('Google');
+  api.use('random', 'client');
 
   api.addFiles('google_server.js', 'server');
   api.addFiles('google_client.js', 'client');
+
+  api.export('Google');
 });
