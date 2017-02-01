1fd3b44f926010d2a334fb0f2005709186c4af8d
Hugh Willson <hugh@octonary.com>
2017-02-01 09:21:00 -0500

    Removed beta.1 version suffix.

--- packages/meteor-developer-config-ui/package.js
+++ packages/meteor-developer-config-ui/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Blaze configuration templates for the Meteor developer accounts OAuth.',
-  version: '1.0.0-beta.1'
+  version: '1.0.0'
 });
 
 Package.onUse(function (api) {
