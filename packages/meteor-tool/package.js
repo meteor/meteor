21185dad10d3f3c7c72e1d4daf797df0700c3636
Ben Newman <ben@meteor.com>
2017-02-13 18:17:06 -0500

    Bump package versions for 1.4.3.1-rc.0 release.

--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "The Meteor command-line tool",
-  version: '1.4.3'
+  version: '1.4.3-1-rc.0'
 });
 
 Package.includeTool();
--- packages/modules/package.js
+++ packages/modules/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "modules",
-  version: "0.7.9",
+  version: "0.7.9-rc.0",
   summary: "CommonJS module system",
   documentation: "README.md"
 });
