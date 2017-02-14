5465ab8aeef16d33f1f4564496da44a7f3e45aae
Ben Newman <ben@meteor.com>
2017-02-13 19:19:54 -0500

    Bump package versions for 1.4.3.1-rc.1 release.
    @abernix Bumping the ecmascript version is apparently necessary if the
    babel-compiler package has changed, presumably because ecmascript defines
    a compiler plugin that uses babel-compiler. Unless the ecmascript version
    has been bumped, babel-compiler changes won't appear to have any effect.

--- packages/ecmascript/package.js
+++ packages/ecmascript/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ecmascript',
-  version: '0.6.2',
+  version: '0.6.3-rc.1',
   summary: 'Compiler plugin that supports ES2015+ in all .js files',
   documentation: 'README.md'
 });
