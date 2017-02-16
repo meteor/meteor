a1c4df3f579ce4625937959dd1c974184b0a5507
Ben Newman <ben@meteor.com>
2017-02-16 16:40:28 -0500

    Call api.use("ddp") in dynamic-import/package.js.
    Though I am still considering alternatives to DDP, the dynamic-import
    package should depend on the packages that it uses.

--- packages/dynamic-import/package.js
+++ packages/dynamic-import/package.js
@@ -9,6 +9,7 @@ Package.onUse(function (api) {
   api.use("modules");
   api.use("localstorage");
   api.use("promise");
+  api.use("ddp");
   api.use("ecmascript", "server");
   api.mainModule("client.js", "client");
   api.mainModule("server.js", "server");
