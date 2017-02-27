17c5d12f30ebdda04a0ceab4b997ca06bdc2ac0b
Hugh Willson <hugh@octonary.com>
2017-02-27 07:50:33 -0500

    Prevent accounts configuration UI warning from showing when using service-configuration.

--- packages/accounts-github/notice.js
+++ packages/accounts-github/notice.js
@@ -1,4 +1,6 @@
-if (Package['accounts-ui'] && !Package.hasOwnProperty('github-config-ui')) {
+if (Package['accounts-ui']
+    && !Package['service-configuration']
+    && !Package.hasOwnProperty('github-config-ui')) {
   console.warn(
     "Note: You're using accounts-ui and accounts-github,\n" +
     "but didn't install the configuration UI for the GitHub\n" +
--- packages/accounts-google/notice.js
+++ packages/accounts-google/notice.js
@@ -1,4 +1,6 @@
-if (Package['accounts-ui'] && !Package.hasOwnProperty('google-config-ui')) {
+if (Package['accounts-ui']
+    && !Package['service-configuration']
+    && !Package.hasOwnProperty('google-config-ui')) {
   console.warn(
     "Note: You're using accounts-ui and accounts-google,\n" +
     "but didn't install the configuration UI for the Google\n" +
