8fdea33cfb9a8efa6177df0d155761238c9d749a
Jesse Rosenberger <git@jro.cc>
2017-03-01 12:24:33 +0200

    Remove version constraint from minifier-css in standard-minifier-css.
    This restriction was preventing the standard-minifier-css from getting
    the latest updates to minifier-css.  Typically, when publishing
    a package outside of a release cycle, I believe we need to manually add
    this constraint since `meteor publish-release` normally takes care of it
    automatically, however in most cases, I don't believe this should be
    (permanently) committed.
    
    I may stand to be corrected, but will find out soon!

--- packages/standard-minifier-css/package.js
+++ packages/standard-minifier-css/package.js
@@ -8,7 +8,7 @@ Package.describe({
 Package.registerBuildPlugin({
   name: "minifyStdCSS",
   use: [
-    'minifier-css@1.2.14'
+    'minifier-css'
   ],
   npmDependencies: {
     "source-map": "0.5.6",
