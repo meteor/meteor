d611c91ce881059d253bfb4682f3cf7ba039c299
mutdmour <mutdmour@gmail.com>
2017-03-11 18:00:05 +0200

    style fix

--- packages/accounts-password/password_tests.js
+++ packages/accounts-password/password_tests.js
@@ -1474,7 +1474,7 @@ if (Meteor.isServer) (function () {
   );
 
   Tinytest.add(
-    'passwords - reset password doesn\t work if email changed after email sent',
+    "passwords - reset password doesn't work if email changed after email sent",
     function (test) {
       var username = Random.id();
       var email = username + '-intercept@example.com';
@@ -1688,31 +1688,31 @@ if (Meteor.isServer) (function () {
     function (test) {
       var email = test.id + '-intercept@example.com';
       var userId = Accounts.createUser({email: email, password: 'password'});
+      
       Accounts.sendEnrollmentEmail(userId, email);
       test.isTrue(!!Meteor.users.findOne(userId).services.password.reset);
 
       Accounts._expirePasswordEnrollTokens(new Date(), userId);
-
       test.isUndefined(Meteor.users.findOne(userId).services.password.reset);
     }
   )
 
   Tinytest.add(
-    'passwords - enroll tokens don\'t get cleaned up when reset tokens are cleaned up',
+    "passwords - enroll tokens don't get cleaned up when reset tokens are cleaned up",
     function (test) {
       var email = test.id + '-intercept@example.com';
       var userId = Accounts.createUser({email: email, password: 'password'});
+      
       Accounts.sendEnrollmentEmail(userId, email);
       test.isTrue(!!Meteor.users.findOne(userId).services.password.reset);
 
       Accounts._expirePasswordResetTokens(new Date(), userId);
-
       test.isTrue(Meteor.users.findOne(userId).services.password.reset);
     }
   )
 
   Tinytest.add(
-    'passwords - reset tokens don\'t get cleaned up when enroll tokens are cleaned up',
+    "passwords - reset tokens don't get cleaned up when enroll tokens are cleaned up",
     function (test) {
       var email = test.id + '-intercept@example.com';
       var userId = Accounts.createUser({email: email, password: 'password'});
