486ce486a7a97d5c67534165d48441af808c8eec
Jesse Rosenberger <git@jro.cc>
2017-03-13 17:39:20 +0200

    Ensure errors the minifier encounters are easy to understand.
    While Babili is much better about handling modern ECMAScript constructs
    than its UglifyJS predecessor, it may still be possible for an error to
    occur during minification.
    
    To account for that unlikely situation, and to preserve the code already
    written to assist in that situation, this implements an error re-throw
    in a similar fashion to that of 1a380f5532a95236eb066d44398d9a9c7b34b37f.

--- packages/standard-minifier-js/plugin/minify-js.js
+++ packages/standard-minifier-js/plugin/minify-js.js
@@ -23,13 +23,115 @@ meteorBabelMinifier.prototype.processFilesForBundle = function(files, options) {
     return;
   }
 
+  function maybeThrowMinifyErrorBySourceFile(error, file) {
+    var minifierErrorRegex = /^(.*?)\s?\((\d+):(\d+)\)$/;
+    var parseError = minifierErrorRegex.exec(error.message);
+
+    if (!parseError) {
+      // If we were unable to parse it, just let the usual error handling work.
+      return;
+    }
+
+    var lineErrorMessage = parseError[1];
+    var lineErrorLineNumber = parseError[2];
+
+    var parseErrorContentIndex = lineErrorLineNumber - 1;
+
+    // Unlikely, since we have a multi-line fixed header in this file.
+    if (parseErrorContentIndex < 0) {
+      return;
+    }
+
+    /*
+
+    What we're parsing looks like this:
+
+    /////////////////////////////////////////
+    //                                     //
+    // path/to/file.js                     //
+    //                                     //
+    /////////////////////////////////////////
+                                           // 1
+       var illegalECMAScript = true;       // 2
+                                           // 3
+    /////////////////////////////////////////
+
+    Btw, the above code is intentionally not newer ECMAScript so
+    we don't break ourselves.
+
+    */
+
+    var contents = file.getContentsAsString().split(/\n/);
+    var lineContent = contents[parseErrorContentIndex];
+
+    // Try to grab the line number, which sometimes doesn't exist on
+    // line, abnormally-long lines in a larger block.
+    var lineSrcLineParts = /^(.*?)(?:\s*\/\/ (\d+))?$/.exec(lineContent);
+
+    // The line didn't match at all?  Let's just not try.
+    if (!lineSrcLineParts) {
+      return;
+    }
+
+    var lineSrcLineContent = lineSrcLineParts[1];
+    var lineSrcLineNumber = lineSrcLineParts[2];
+
+    // Count backward from the failed line to find the filename.
+    for (var c = parseErrorContentIndex - 1; c >= 0; c--) {
+      var sourceLine = contents[c];
+
+      // If the line is a boatload of slashes, we're in the right place.
+      if (/^\/\/\/{6,}$/.test(sourceLine)) {
+
+        // If 4 lines back is the same exact line, we've found the framing.
+        if (contents[c - 4] === sourceLine) {
+
+          // So in that case, 2 lines back is the file path.
+          var parseErrorPath = contents[c - 2]
+            .substring(3)
+            .replace(/\s+\/\//, "");
+
+          var minError = new Error(
+            "Babili minification error " +
+            "within " + file.getPathInBundle() + ":\n" +
+            parseErrorPath +
+            (lineSrcLineNumber ? ", line " + lineSrcLineNumber : "") + "\n" +
+            "\n" +
+            lineErrorMessage + ":\n" +
+            "\n" +
+            lineSrcLineContent + "\n"
+          );
+
+          throw minError;
+        }
+      }
+    }
+  }
+
   var allJs = '';
   files.forEach(function (file) {
       // Don't reminify *.min.js.
       if (/\.min\.js$/.test(file.getPathInBundle())) {
         allJs += file.getContentsAsString();
       } else {
-        allJs += meteorBabelMinify(file.getContentsAsString()).code;
+        var minified;
+
+        try {
+          minified = meteorBabelMinify(file.getContentsAsString());
+
+          if (!(minified && typeof minified.code === "string")) {
+            throw new Error();
+          }
+        } catch (err) {
+          var filePath = file.getPathInBundle();
+
+          maybeThrowMinifyErrorBySourceFile(err, file);
+
+          err.message += " while minifying " + filePath;
+          throw err;
+        }
+
+        allJs += minified.code;
       }
       allJs += '\n\n';
 
