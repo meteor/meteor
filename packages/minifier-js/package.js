6e2948284334ca0325ca7642ff130297f958c864
Ben Newman <ben@meteor.com>
2017-03-25 11:29:30 -0400

    Use UglifyJS when possible, but fall back to Babel.minify.
    This gives us all the benefits of @sethmurphy18's great work without the
    additional performance hit of using Babili for everything.
    
    With this commit:
    
      % meteor create min-test
      % cd min-test
      % time meteor build ../min-build
      meteor build ../min-build  39.26s user 5.49s system 112% cpu 39.647 total
    
    Without this commit:
    
      meteor build ../min-build  59.28s user 5.23s system 107% cpu 1:00.08 total
    
    Keep in mind this is the best-case scenario for Babili (a very small app).
    Larger apps take much longer for Babili to minify.
    
    cc @abernix @glasser @sethmurphy18

--- packages/minifier-js/package.js
+++ packages/minifier-js/package.js
@@ -3,8 +3,12 @@ Package.describe({
   version: "2.0.0-rc.1"
 });
 
+Npm.depends({
+  "uglify-js": "2.8.16"
+});
+
 Package.onUse(function (api) {
   api.use('babel-compiler');
-  api.export(['meteorBabelMinify']);
+  api.export(['meteorJsMinify']);
   api.addFiles(['minifier.js'], 'server');
 });
