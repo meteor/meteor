a7806860988527e4e7e854f9e53dcd0eee2209ec
Ben Newman <ben@meteor.com>
2017-03-27 18:26:49 -0400

    Bump package versions for 1.4.4-rc.2 release.
    We (@abernix and I) have decided to abandon 1.4.3.3 in favor of 1.4.4,
    because the changes we wanted to include are slightly more significant
    than seems appropriate for a "patch" update.

--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "The Meteor command-line tool",
-  version: '1.4.3-3-rc.1'
+  version: '1.4.4-rc.2'
 });
 
 Package.includeTool();
--- packages/modules/package.js
+++ packages/modules/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "modules",
-  version: "0.8.1-rc.1",
+  version: "0.8.1-rc.2",
   summary: "CommonJS module system",
   documentation: "README.md"
 });
