49e67c6030ca08ff2f2f64db6a79208d09369534
Ben Newman <ben@meteor.com>
2017-03-30 16:31:20 -0400

    Support Google Sign-In in google-oauth package.
    Addresses #8253.

--- packages/google-oauth/package.js
+++ packages/google-oauth/package.js
@@ -3,7 +3,13 @@ Package.describe({
   version: "1.2.0"
 });
 
+Cordova.depends({
+  "cordova-plugin-googleplus": "5.1.1"
+});
+
 Package.onUse(function(api) {
+  api.use("modules");
+  api.use("promise");
   api.use('oauth2', ['client', 'server']);
   api.use('oauth', ['client', 'server']);
   api.use('http', ['server']);
@@ -12,6 +18,9 @@ Package.onUse(function(api) {
 
   api.addFiles('google_server.js', 'server');
   api.addFiles('google_client.js', 'client');
+  api.addFiles('google_sign-in.js', 'web.cordova');
+
+  api.mainModule('namespace.js');
 
   api.export('Google');
 });
