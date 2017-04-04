cba8c633402432f88abf6a6b47b03695097a8e2f
Jesse Rosenberger <git@jro.cc>
2017-04-04 19:40:02 +0300

    Update `node-gyp` and `node-pre-gyp` packages.
    * `node-gyp` - Adds support for Visual Studio 2017.
      - https://github.com/nodejs/node-gyp/blob/master/CHANGELOG.md#v360-2017-03-16
    * `node-pre-gyp` - Nothing notable.
      - https://github.com/mapbox/node-pre-gyp/blob/master/CHANGELOG.md#0634

--- scripts/dev-bundle-tool-package.js
+++ scripts/dev-bundle-tool-package.js
@@ -12,8 +12,8 @@ var packageJson = {
     // Explicit dependency because we are replacing it with a bundled version
     // and we want to make sure there are no dependencies on a higher version
     npm: "4.4.4",
-    "node-gyp": "3.5.0",
-    "node-pre-gyp": "0.6.33",
+    "node-gyp": "3.6.0",
+    "node-pre-gyp": "0.6.34",
     "meteor-babel": "0.19.1",
     "meteor-promise": "0.8.0",
     fibers: "1.0.15",
