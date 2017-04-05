d780b4de1b845424bc10f6f65ef586106346244c
mbrobbel <m1brobbel@gmail.com>
2017-04-05 23:40:15 +0200

    Update ecmascript-runtime to test Array polyfills.

--- packages/ecmascript-runtime/runtime-tests.js
+++ packages/ecmascript-runtime/runtime-tests.js
@@ -35,6 +35,27 @@ Tinytest.add("ecmascript-containers - Set", function (test) {
 });
 
 Tinytest.add("core-js - Array", function (test) {
+  test.equal(typeof Array.from, "function");
+  test.equal(typeof Array.of, "function");
+  test.equal(typeof Array.isArray, "function");
+  test.equal(typeof Array.prototype[Symbol.iterator], "function");
+  test.equal(typeof Array.prototype.copyWithin, "function");
+  test.equal(typeof Array.prototype.fill, "function");
+  test.equal(typeof Array.prototype.find, "function");
+  test.equal(typeof Array.prototype.findIndex, "function");
+  test.equal(typeof Array.prototype.slice, "function");
+  test.equal(typeof Array.prototype.join, "function");
+  test.equal(typeof Array.prototype.indexOf, "function");
+  test.equal(typeof Array.prototype.lastIndexOf, "function");
+  test.equal(typeof Array.prototype.every, "function");
+  test.equal(typeof Array.prototype.some, "function");
+  test.equal(typeof Array.prototype.forEach, "function");
+  test.equal(typeof Array.prototype.map, "function");
+  test.equal(typeof Array.prototype.filter, "function");
+  test.equal(typeof Array.prototype.reduce, "function");
+  test.equal(typeof Array.prototype.reduceRight, "function");
+  test.equal(typeof Array.prototype.sort, "function");
+
   test.equal(Array.from("123", Number), [1, 2, 3]);
   test.equal(Array.of(1, 3, 5), [1, 3, 5]);
   test.equal(
