523c7ce9d4d47442becc307817a35d6bb9800915
Jesse Rosenberger <git@jro.cc>
2017-04-06 06:54:37 +0300

    If `EXDEV` is encountered, resort to a delete and `cp_r`.
    This operation does not have to be done atomically and this should allow
    cross-device copies to be made if necessary.

--- tools/isobuild/builder.js
+++ tools/isobuild/builder.js
@@ -687,7 +687,16 @@ Previous builder: ${previousBuilder.outputPath}, this builder: ${outputPath}`
     // case of renameDirAlmostAtomically since that one is constructing files to
     // be checked in to version control, but here we could get away with it.
     if (this.buildPath !== this.outputPath) {
-      files.renameDirAlmostAtomically(this.buildPath, this.outputPath);
+      try {
+        files.rename(this.buildPath, this.outputPath);
+      } catch (e) {
+        if (e.code === "EXDEV") {
+          files.rm_recursive(this.outputPath);
+          files.cp_r(this.buildPath, this.outputPath);
+        } else {
+          throw e;
+        }
+      }
     }
   }
 
