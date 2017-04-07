fb2290960ce0eae6c80872decd5b845ad1ae2816
Ben Newman <ben@meteor.com>
2017-04-07 13:12:36 -0400

    Bump package versions for 1.5-beta.14 release.

--- packages/dynamic-import/package.js
+++ packages/dynamic-import/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "dynamic-import",
-  version: "0.1.0-beta.13",
+  version: "0.1.0-beta.14",
   summary: "Support for module.dynamicImport(id).then(namespace => ...)",
   documentation: "README.md"
 });
