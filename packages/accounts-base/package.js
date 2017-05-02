1ac0d4ca2f263b1b747628267f44c648eee3e4c9
Jesse Rosenberger <git@jro.cc>
2017-05-02 10:56:09 +0300

    Bump package versions for the official 1.4.4.2 release.

--- packages/accounts-base/package.js
+++ packages/accounts-base/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A user account system",
-  version: "1.2.17-rc.1"
+  version: "1.2.17"
 });
 
 Package.onUse(function (api) {
--- packages/tracker/package.js
+++ packages/tracker/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Dependency tracker to allow reactive callbacks",
-  version: '1.1.3-rc.1'
+  version: '1.1.3'
 });
 
 Package.onUse(function (api) {
