a89963fdbe5a55d0741cecae6bbb2fe52ca02063
Ben Newman <ben@meteor.com>
2017-05-04 16:23:07 -0400

    Bump package versions for 1.5-beta.19 release.

--- packages/babel-compiler/package.js
+++ packages/babel-compiler/package.js
@@ -6,7 +6,7 @@ Package.describe({
   // isn't possible because you can't publish a non-recommended
   // release with package versions that don't have a pre-release
   // identifier at the end (eg, -dev)
-  version: '6.19.0-beta.18'
+  version: '6.19.1-beta.19'
 });
 
 Npm.depends({
