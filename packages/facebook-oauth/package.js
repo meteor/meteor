8bd1005840a74edf4401c2622b5267a1110f2e1d
Ben Newman <ben@meteor.com>
2017-05-16 15:30:37 -0400

    Bump package versions for 1.5-rc.4 release.

--- packages/facebook-oauth/package.js
+++ packages/facebook-oauth/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Facebook OAuth flow",
-  version: "1.3.1-rc.3"
+  version: "1.3.1-rc.4"
 });
 
 Package.onUse(function(api) {
--- packages/modules-runtime/package.js
+++ packages/modules-runtime/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "modules-runtime",
-  version: "0.8.0-rc.3",
+  version: "0.8.0-rc.4",
   summary: "CommonJS module system",
   git: "https://github.com/benjamn/install",
   documentation: "README.md"
