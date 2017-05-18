meteor/meteor

commit b6694b2f5d1ba8f58ce20e56de68bef7487ca3ce

Expose dynamic modules via HTTP, but do not load them eagerly.
This allows fetching the compiled code of dynamic modules via HTTP,
without generating <script> tags for those resources in the intial
boilerplate HTML of the application.

The URL for a dynamic module should be formed by taking its absolute
module identifier, prepending "/dynamic" and appending "?hash=<version>".
Appropriate version hashes can be obtained from the tree exported by the
meteor/dynamic-import/dynamic-versions.js module, though the hashes are
used only for cache busting, so they could be anything at all.

A good place to do this fetching would be the meteorInstall.fetch
callback, as defined (for example) in meteor/dynamic-import/client.js.
That implementation still uses a WebSocket rather than HTTP, but this
commit will allow us to experiment with HTTP in the future.

Because the code returned for these dynamic modules is wrapped as an
anonymous function expression, you'll need to fetch them using an
XMLHttpRequest, the HTTP fetch() function, or some similar utility, rather
than using a <script> tag, because executing the unmodified code as JS
will likely throw a syntax error.

tools/isobuild/bundler.js
@@ -1598,20 +1598,20 @@ class ClientTarget extends Target {
       manifestItem.size = file.size();
       manifestItem.hash = file.hash();
 
-      if (! file.targetPath.startsWith("dynamic/")) {
-        writeFile(file, builder);
-        manifest.push(manifestItem);
+      writeFile(file, builder);
+      manifest.push(manifestItem);
 
-      } else if (manifestItem.sourceMapUrl) {
+      if (file.targetPath.startsWith("dynamic/") &&
+          manifestItem.sourceMapUrl) {
+        // If the file is a dynamic module, we don't embed its source map
+        // in the file itself (because base64-encoded data: URLs for
+        // source maps can be very large), but rather include a normal URL
+        // referring to the source map, so that it can be loaded from the
+        // web server when needed.
         writeFile(file, builder, {
           sourceMapUrl: manifestItem.sourceMapUrl,
         });
 
-        // If the file is dynamic, we don't need/want the file itself to
-        // be served by the web server, but we do want its source map (if
-        // defined) to be accessible via HTTP, so that we can refer to it
-        // by URL rather than embedding it as a very long data URL comment
-        // in the file.
         manifest.push({
           type: "json",
           path: manifestItem.sourceMap,
@@ -1620,9 +1620,6 @@ class ClientTarget extends Target {
           cacheable: manifestItem.cacheable,
           hash: manifestItem.hash,
         });
-
-      } else {
-        writeFile(file, builder);
       }
     });
 

packages/boilerplate-generator/boilerplate-generator.js
@@ -73,7 +73,10 @@ Boilerplate.prototype._generateBoilerplateFromManifestAndSource =
       if (item.type === 'css' && item.where === 'client') {
         boilerplateBaseData.css.push(itemObj);
       }
-      if (item.type === 'js' && item.where === 'client') {
+      if (item.type === 'js' && item.where === 'client' &&
+          // Dynamic JS modules should not be loaded eagerly in the
+          // initial HTML of the app.
+          ! item.path.startsWith('dynamic/')) {
         boilerplateBaseData.js.push(itemObj);
       }
       if (item.type === 'head') {

packages/boilerplate-generator/package.js
@@ -1,15 +1,15 @@
 Package.describe({
   summary: "Generates the boilerplate html from program's manifest",
-  version: '1.0.11'
+  version: '1.1.0'
 });
 
 Package.onUse(function (api) {
   api.use([
-    'underscore@1.0.9',
-    'spacebars-compiler@1.0.12',
-    'spacebars@1.0.12',
-    'htmljs@1.0.10',
-    'ui@1.0.11',
+    'underscore',
+    'spacebars-compiler',
+    'spacebars',
+    'htmljs',
+    'ui',
   ], 'server');
   api.addFiles(['boilerplate-generator.js'], 'server');
   api.export(['Boilerplate'], 'server');

commit 0fbb97432c9f5c9b4c5958e719ca32d32a3922f1

Fix ENOTEMPTY and EPERM errors on Windows.
Windows began suffering from cryptic ENOTEMPTY and EPERM errors between
1.5-beta.20 and 1.5-rc.0. After a tricky `git bisect` adventure, I tracked
the problem down to my commit b6694b2f5d1ba8f58ce20e56de68bef7487ca3ce,
which caused dynamic modules to be written more than once by the bundler.
Though I don't understand exactly why Windows complained in this way, I'm
glad that it did, because otherwise this mistake would merely have been a
performance problem, and might not have been noticed before the release.

tools/isobuild/bundler.js
@@ -1598,10 +1598,9 @@ class ClientTarget extends Target {
       manifestItem.size = file.size();
       manifestItem.hash = file.hash();
 
-      writeFile(file, builder);
-      manifest.push(manifestItem);
-
       if (! file.targetPath.startsWith("dynamic/")) {
+        writeFile(file, builder);
+        manifest.push(manifestItem);
         return;
       }
 
@@ -1610,12 +1609,20 @@ class ClientTarget extends Target {
       // prefixed with "dynamic/".
       manifestItem.type = "dynamic js";
 
-      if (manifestItem.sourceMapUrl) {
+      // Add the dynamic module to the manifest so that it can be
+      // requested via HTTP from the web server. Note, however, that we
+      // typically request dynamic modules via DDP, since we can compress
+      // the entire response more easily that way. We expose dynamic
+      // modules via HTTP here mostly to unlock future experimentation.
+      manifest.push(manifestItem);
+
+      if (manifestItem.sourceMap &&
+          manifestItem.sourceMapUrl) {
         // If the file is a dynamic module, we don't embed its source map
         // in the file itself (because base64-encoded data: URLs for
         // source maps can be very large), but rather include a normal URL
-        // referring to the source map, so that it can be loaded from the
-        // web server when needed.
+        // referring to the source map (as a comment), so that it can be
+        // loaded from the web server when needed.
         writeFile(file, builder, {
           sourceMapUrl: manifestItem.sourceMapUrl,
         });
@@ -1628,6 +1635,19 @@ class ClientTarget extends Target {
           cacheable: manifestItem.cacheable,
           hash: manifestItem.hash,
         });
+
+        // Now that we've written the module with a source map URL comment
+        // embedded in it, and also made sure the source map is exposed by
+        // the web server, we do not need to include the source map URL in
+        // the manifest, because then it would also be provided via the
+        // X-SourceMap HTTP header, redundantly.
+        delete manifestItem.sourceMap;
+        delete manifestItem.sourceMapUrl;
+
+      } else {
+        // If the dynamic module does not have a source map, just write it
+        // normally.
+        writeFile(file, builder);
       }
     });
 