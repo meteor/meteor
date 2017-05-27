122f0d2cb3ec3d3ceeedff6c9cb728362edc4b0c
Ben Newman <ben@meteor.com>
2017-05-27 12:28:54 -0400

    Bump package versions for 1.5-rc.8 release.

--- packages/facebook-oauth/package.js
+++ packages/facebook-oauth/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Facebook OAuth flow",
-  version: "1.3.1-rc.7"
+  version: "1.3.1-rc.8"
 });
 
 Package.onUse(function(api) {
--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "The Meteor command-line tool",
-  version: '1.5.0-rc.7'
+  version: '1.5.0-rc.8'
 });
 
 Package.includeTool();
