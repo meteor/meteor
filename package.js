006da22ca6404a14b1ee0fa8d03ae8ea2cb93b8c
David Burles <dburles@gmail.com>
2017-06-02 10:10:01 +1000

    1.0.1

--- package.js
+++ package.js
@@ -1,18 +1,19 @@
 Package.describe({
-  summary: "Define and run db migrations.",
-  version: "1.0.0",
-  name: "percolate:migrations",
-  git: "https://github.com/percolatestudio/meteor-migrations.git"
+  summary: 'Define and run db migrations.',
+  version: '1.0.1',
+  name: 'percolate:migrations',
+  git: 'https://github.com/percolatestudio/meteor-migrations.git',
 });
 
-Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.1.1');
+Package.on_use(function(api) {
+  api.versionsFrom('METEOR@1.5');
+  api.use('ecmascript');
   api.use(['underscore', 'check', 'mongo', 'logging'], 'server');
-  api.addFiles(['migrations_server.js'], "server");
+  api.addFiles(['migrations_server.js'], 'server');
   api.export('Migrations', 'server');
 });
 
-Package.on_test(function (api) {
+Package.on_test(function(api) {
   api.use(['percolate:migrations', 'tinytest']);
   api.addFiles('migrations_tests.js', ['server']);
 });
