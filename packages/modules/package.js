7472916236a5437ea03b026813d282e29fbc5f0f
Ben Newman <ben@meteor.com>
2017-06-01 20:46:17 -0400

    Remove underscore as a dependency from the modules package.

--- packages/modules/package.js
+++ packages/modules/package.js
@@ -10,7 +10,6 @@ Npm.depends({
 });
 
 Package.onUse(function(api) {
-  api.use("underscore");
   api.use("modules-runtime");
   api.mainModule("client.js", "client");
   api.mainModule("server.js", "server");
