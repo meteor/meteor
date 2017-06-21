9b4c16331381582856afd4242d7f5d414aa46d96
Ben Newman <ben@meteor.com>
2017-06-21 19:44:27 -0400

    Update npm to version 5.0.3.

--- scripts/build-dev-bundle-common.sh
+++ scripts/build-dev-bundle-common.sh
@@ -7,7 +7,7 @@ UNAME=$(uname)
 ARCH=$(uname -m)
 MONGO_VERSION=3.2.12
 NODE_VERSION=8.1.2
-NPM_VERSION=4.6.1
+NPM_VERSION=5.0.3
 
 if [ "$UNAME" == "Linux" ] ; then
     if [ "$ARCH" != "i686" -a "$ARCH" != "x86_64" ] ; then
