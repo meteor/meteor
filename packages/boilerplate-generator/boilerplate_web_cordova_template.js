cedbf0f22a422aee2729a3c4f1fad55366299b03
stevenhao <stevenhao@gmail.com>
2017-06-22 14:45:33 -0700

    add cordova template code

--- packages/boilerplate-generator/boilerplate_web_cordova_template.js
+++ packages/boilerplate-generator/boilerplate_web_cordova_template.js
@@ -2,82 +2,69 @@
 // Replicates the template defined in boilerplate_web.cordova.html
 // Arguments: root : { htmlAttributes, css : [{ url }], bundledJsCssUrlRewriteHook : Function, head, dynamicHead, body, dynamicBody, inlineScriptsAllowed, additionalStaticJs, meteorRuntimeConfig }
 
-function escapeAttr(attr) {
-  // XXX Technically, we only need to escape ['<', '"'] in attribute mode
-  // For simplicity, we fall back onto _.template's html escaping, and escape ['&', '<', '>', '"', "'"] regardless of context
-  // Most browsers seem to handle 'over-escaping' gracefully
-  return _.template('<%- attr %>')({ attr });
-}
-
-function escapeURL(url) {
-  // XXX Do we need to do anything here? It's possible that the URL may be escaped already
-  return url;
-}
-
 export default function(manifest) {
   const root = manifest;
   // XXX do we need to do some validation on the properties of root?
   return [].concat(
     [
-      '<html' +_.map(root.htmlAttributes, (value, key) =>
-        _.template(' <%= attrName %>="<%= attrValue %>"')({
-          attrName: key,
-          attrValue: escapeAttr(value)
-        })
-      ).join('') + '>',
-      '<head>'
+      '<html>',
+      '<head>',
+      '  <meta charset="utf-8">',
+      '  <meta name="format-detection" content="telephone=no">',
+      '  <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height">',
+      '  <meta name="msapplication-tap-highlight" content="no">',
+      '  <meta http-equiv="Content-Security-Policy" content="default-src * gap: data: blob: \'unsafe-inline\' \'unsafe-eval\' ws: wss:;">',
     ],
-
+    // We are explicitly not using bundledJsCssUrlRewriteHook: in cordova we serve assets up directly from disk, so rewriting the URL does not make sense
     _.map(root.css, ({url}) =>
-      _.template('  <link rel="stylesheet" type="text/css" class="__meteor-css__" href="<%= href %>">')({
-        href: escapeAttr(escapeURL(root.bundledJsCssUrlRewriteHook(url)))
+      _.template('  <link rel="stylesheet" type="text/css" class="__meteor-css__" href="<%- href %>">')({
+        href: url
       })
     ),
-
     [
+      '  <script type="text/javascript">',
+      _.template('    __meteor_runtime_config__ = JSON.parse(decodeURIComponent(<%= conf %>));')({
+        conf: root.meteorRuntimeConfig
+      }),
+      '    if (/Android/i.test(navigator.userAgent)) {',
+      // When Android app is emulated, it cannot connect to localhost,
+      // instead it should connect to 10.0.2.2
+      // (unless we\'re using an http proxy; then it works!)
+      '      if (!__meteor_runtime_config__.httpProxyPort) {',
+      '        __meteor_runtime_config__.ROOT_URL = (__meteor_runtime_config__.ROOT_URL || \'\').replace(/localhost/i, \'10.0.2.2\');',
+      '        __meteor_runtime_config__.DDP_DEFAULT_CONNECTION_URL = (__meteor_runtime_config__.DDP_DEFAULT_CONNECTION_URL || \'\').replace(/localhost/i, \'10.0.2.2\');',
+      '      }',
+      '    }',
+      '  </script>',
+      '',
+      '  <script type="text/javascript" src="/cordova.js"></script>'
+    ],
+    _.map(root.js, ({url}) =>
+      _.template('  <script type="text/javascript" src="<%- src %>"></script>')({
+        src: url
+      })
+    ),
+    _.map(root.additionalStaticJs, ({pathname, contents}) =>
+      _.template(inlineScriptsAllowed
+        ? '  <script type="text/javascript"><%= contents %></script>'
+        : '  <script type="text/javascript" src="<%- src %>"></script>'
+      )({
+        src: root.rootUrlPathPrefix + pathname,
+        contents: contents
+      })
+    ),
+    [
+      '',
       root.head,
-      root.dynamicHead,
       '</head>',
+      '',
       '<body>',
       root.body,
-      root.dynamicBody,
-      '',
-      _.template(root.inlineScriptsAllowed
-        ? '  <script type="text/javascript"><%= contents %></script>'
-        : '  <script type="text/javascript" src="<%= src %>"></script>'
-      )({
-        // maybe using another template here is overkill...
-        contents: _.template('__meteor_runtime_config__ = JSON.parse(decodeURIComponent(<%= conf %>))')({
-          conf: root.meteorRuntimeConfig // XXX why is this a URI?
-        }),
-        src: escapeAttr(escapeURL(root.rootUrlPathPrefix + '/meteor_runtime_config.js'))
-      }),
-      ''
-    ],
-
-    _.map(root.js, ({url}) =>
-      _.template('  <script type="text/javascript" src="<%= src %>"></script>')({
-        src: escapeAttr(escapeURL(root.bundledJsCssUrlRewriteHook(url)))
-      })
-    ),
-
-    _.map(root.additionalStaticJs, ({contents, pathname}) => (
-      _.template(root.inlineScriptsAllowed
-        ? '  <script><%= contents %></script>'
-        : '  <script type="text/javascript" src="<%= src %>"></script>'
-      )({
-        contents: contents,
-        src: escapeAttr(escapeURL(root.rootUrlPathPrefix + pathname))
-      })
-    )),
-
-    [
-      '', '',
       '</body>',
       '</html>'
     ],
 
-    ['', '<!-- Generated by boilerplate generator -->'] // to help distinguish old generator from new
+    ['', '<!-- Generated for cordova by boilerplate-generator -->']
   ).join('\n');
 }
 
