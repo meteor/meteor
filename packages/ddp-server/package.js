e0565d5d16c4aa8010c24699b1789c3917ef18c1
Ben Newman <ben@meteor.com>
2017-06-29 16:21:11 -0400

    Bump package versions for 1.5.1-rc.2 release.

--- packages/ddp-server/package.js
+++ packages/ddp-server/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Meteor's latency-compensated distributed data server",
-  version: '2.0.0-rc.1',
+  version: '2.0.0-rc.2',
   documentation: null
 });
 
--- packages/minifier-js/package.js
+++ packages/minifier-js/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "JavaScript minifier",
-  version: "2.1.1-rc.1"
+  version: "2.1.1-rc.2"
 });
 
 Npm.depends({
