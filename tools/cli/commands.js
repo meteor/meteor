ff0030e88a74bda5b200ac0917bd408b61ff4911
Mitar <mitar.github@tnode.com>
2017-07-08 00:52:13 -0700

    filePath is relative to appPath.

--- tools/cli/commands.js
+++ tools/cli/commands.js
@@ -661,7 +661,7 @@ main.registerCommand({
       // We do mind if there are non-hidden directories, because we don't want
       // to recursively check everything to do some crazy heuristic to see if
       // we should try to creat an app.
-      var stats = files.stat(filePath);
+      var stats = files.stat(files.pathJoin(appPath, filePath));
       if (stats.isDirectory()) {
         // Could contain code
         return true;
