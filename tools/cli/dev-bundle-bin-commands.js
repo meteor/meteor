8c92522f027f575e0dc28c24b93e2de88a881392
Ben Newman <ben@meteor.com>
2017-07-12 13:14:10 -0400

    Use const and let in tools/cli/dev-bundle-bin-commands.js.

--- tools/cli/dev-bundle-bin-commands.js
+++ tools/cli/dev-bundle-bin-commands.js
@@ -1,46 +1,48 @@
 // Note that this file is required before we install our Babel hooks in
 // ../tool-env/install-babel.js, so we can't use ES2015+ syntax here.
 
-var fs = require("fs");
-var path = require("path");
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
 
 // The dev_bundle/bin command has to come immediately after the meteor
 // command, as in `meteor npm` or `meteor node`, because we don't want to
 // require("./main.js") for these commands.
-var devBundleBinCommand = process.argv[2];
-var args = process.argv.slice(3);
+const devBundleBinCommand = process.argv[2];
+const args = process.argv.slice(3);
 
 // On Windows, try the .cmd and .exe extensions.
-var isWindows = process.platform === "win32";
-var extensions = isWindows ? [".cmd", ".exe"] : [""];
+const isWindows = process.platform === "win32";
+const extensions = isWindows ? [".cmd", ".exe"] : [""];
 
 function getChildProcess() {
   if (typeof devBundleBinCommand !== "string") {
     return Promise.resolve(null);
   }
 
-  var helpers = require("./dev-bundle-bin-helpers.js");
-  var getEnvOptions = {};
+  const helpers = require("./dev-bundle-bin-helpers.js");
+  const getEnvOptions = {};
 
   return Promise.all([
     helpers.getDevBundle(),
     helpers.getEnv(getEnvOptions)
   ]).then(function (devBundleAndEnv) {
-    var devBundleDir = devBundleAndEnv[0];
-    var env = devBundleAndEnv[1];
+    const devBundleDir = devBundleAndEnv[0];
+    const env = devBundleAndEnv[1];
 
     // Strip leading and/or trailing whitespace.
-    var name = devBundleBinCommand.replace(/^\s+|\s+$/g, "");
+    const name = devBundleBinCommand.replace(/^\s+|\s+$/g, "");
 
     if (! helpers.isValidCommand(name, devBundleDir)) {
       return null;
     }
 
-    var cmd = null;
+    let cmd = null;
 
     getEnvOptions.extraPaths.some(function (dir) {
       return extensions.some(function (ext) {
-        var candidate = path.join(dir, name + ext);
+        const candidate = path.join(dir, name + ext);
         try {
           if (fs.statSync(candidate).isFile()) {
             cmd = candidate;
@@ -56,7 +58,7 @@ function getChildProcess() {
       return null;
     }
 
-    var child = require("child_process").spawn(cmd, args, {
+    const child = require("child_process").spawn(cmd, args, {
       stdio: "inherit",
       env: env
     });
