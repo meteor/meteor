2c446101bb2ae61a4a3d60862fdc2e38492869e6
Ben Newman <ben@meteor.com>
2017-07-17 18:01:06 -0400

    Bump package versions for 1.5.2-beta.2 release.

--- packages/minimongo/package.js
+++ packages/minimongo/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Meteor's client-side datastore: a port of MongoDB to Javascript",
-  version: '1.3.0-beta152.1'
+  version: '1.3.0-beta152.2'
 });
 
 Package.onUse(function (api) {
