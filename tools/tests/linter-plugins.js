f59a2f7f3cef83f2f584b801accbaa958ab5eec2
Hugh Willson <hugh@octonary.com>
2017-07-21 08:19:44 -0400

    Increase lint wait time; run logs show we're close to the current timeout.

--- tools/tests/linter-plugins.js
+++ tools/tests/linter-plugins.js
@@ -109,7 +109,7 @@ selftest.define('linter plugins - linting package with `meteor lint`', () => {
   s.cd('myapp/packages/my-package');
 
   const run = s.run('lint');
-  run.waitSecs(10);
+  run.waitSecs(15);
 
   const messages = [
     /While linting files .* my-package .*Server/,
