bd5370dda9a683f377433570b61dca2aa5f0ef9d
Ben Newman <ben@meteor.com>
2017-07-22 09:16:15 -0400

    Fix mistaken usage of require(reify/lib/runtime).enable.
    Related fix in meteor-babel@0.24.4:
    https://github.com/meteor/babel/commit/786194734c693aef91dc1ee3b9e7166d8c54fff6

--- tools/tool-env/install-runtime.js
+++ tools/tool-env/install-runtime.js
@@ -1,16 +1,20 @@
+"use strict";
+
 // Install ES2015-complaint polyfills for Object, Array, String, Function,
 // Symbol, Map, Set, and Promise, patching the native implementations when
 // they are available.
 require("./install-promise.js");
 
-// Enable the module.{watch,export,...} runtime API needed by Reify.
-require("reify/lib/runtime").enable(module.constructor);
+const Module = module.constructor;
+const Mp = Module.prototype;
 
-var Mp = module.constructor.prototype;
-var moduleLoad = Mp.load;
+// Enable the module.{watch,export,...} runtime API needed by Reify.
+require("reify/lib/runtime").enable(Mp);
+
+const moduleLoad = Mp.load;
 Mp.load = function (filename) {
-  var result = moduleLoad.apply(this, arguments);
-  var runSetters = this.runSetters || this.runModuleSetters;
+  const result = moduleLoad.apply(this, arguments);
+  const runSetters = this.runSetters || this.runModuleSetters;
   if (typeof runSetters === "function") {
     // Make sure we call module.runSetters (or module.runModuleSetters, a
     // legacy synonym) whenever a module finishes loading.
