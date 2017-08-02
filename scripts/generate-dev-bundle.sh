a6e4ed5c3255fa3cc7e0dd19ff50ee26c9171a81
Jesse Rosenberger <git@jro.cc>
2017-08-02 12:53:31 +0300

    Allow building the dev bundle from Node.js source.
    This introduces two *additional* environment variables,
    `NODE_COMMIT_HASH` and/or `NODE_FROM_SRC`, which can be set
    either in the environment, or preferably from
    `build-dev-bundle-common.sh`.
    
    `NODE_FROM_SRC` is implied by `NODE_COMMIT_HASH`, but in either case, it
    can also be set explicitly to `debug` to build with the `--debug` flag.
    
    This was incredibly helpful for `git bisect`-ing a Node.js issue.

--- scripts/generate-dev-bundle.sh
+++ scripts/generate-dev-bundle.sh
@@ -18,11 +18,41 @@ cd "$DIR"
 
 S3_HOST="s3.amazonaws.com/com.meteor.jenkins"
 
+if [ ! -z ${NODE_FROM_SRC+x} ] || [ ! -z ${NODE_COMMIT_HASH+x} ]; then
+  if [ ! -z ${NODE_COMMIT_HASH+x} ]; then
+    ${NODE_FROM_SRC:=true}
+    echo "Building Node source from Git hash ${NODE_COMMIT_HASH}...";
+    NODE_URL="https://github.com/meteor/node/archive/${NODE_COMMIT_HASH}.tar.gz"
+  else
+    echo "Building Node source from ${NODE_VERSION} src tarball...";
+    NODE_URL="https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.tar.gz"
+  fi
+else
+  NODE_URL="https://nodejs.org/dist/v${NODE_VERSION}/${NODE_TGZ}"
+fi
+
 # Update these values after building the dev-bundle-node Jenkins project.
 # Also make sure to update NODE_VERSION in generate-dev-bundle.ps1.
-NODE_URL="https://nodejs.org/dist/v${NODE_VERSION}/${NODE_TGZ}"
-echo "Downloading Node from ${NODE_URL}"
-curl "${NODE_URL}" | tar zx --strip-components 1
+function downloadNode {
+  echo "Downloading Node from ${NODE_URL}"
+  curl -sL "${NODE_URL}" | tar zx --strip-components 1
+}
+
+if [ ! -z ${NODE_FROM_SRC+x} ]; then
+  mkdir node-src/ && cd node-src/
+  downloadNode
+  if [ "${NODE_FROM_SRC:-}" = "debug" ]; then
+    ./configure --debug --prefix "${DIR}" 2>&1 > /dev/null
+  else
+    ./configure --prefix "${DIR}" 2>&1 > /dev/null
+  fi
+  make -j4 2>&1 > /dev/null
+  make install 2>&1 > /dev/null
+  export npm_config_nodedir="${DIR}/node-src"
+  cd "$DIR"
+else
+  downloadNode
+fi
 
 # Download Mongo from mongodb.com
 MONGO_NAME="mongodb-${OS}-${ARCH}-${MONGO_VERSION}"
@@ -53,6 +83,14 @@ npm version
 # packages that these depend on, so watch out for new dependencies when
 # you update version numbers.
 
+function npmInstall {
+  if [ "${NODE_FROM_SRC:-}" = "debug" ]; then
+    npm install --debug
+  else
+    npm install
+  fi
+}
+
 # First, we install the modules that are dependencies of tools/server/boot.js:
 # the modules that users of 'meteor bundle' will also have to install. We save a
 # shrinkwrap file with it, too.  We do this in a separate place from
@@ -61,7 +99,7 @@ npm version
 mkdir "${DIR}/build/npm-server-install"
 cd "${DIR}/build/npm-server-install"
 node "${CHECKOUT_DIR}/scripts/dev-bundle-server-package.js" > package.json
-npm install
+npmInstall
 npm shrinkwrap
 
 mkdir -p "${DIR}/server-lib/node_modules"
@@ -89,20 +127,24 @@ shrink_fibers
 mkdir "${DIR}/build/npm-tool-install"
 cd "${DIR}/build/npm-tool-install"
 node "${CHECKOUT_DIR}/scripts/dev-bundle-tool-package.js" >package.json
-npm install
+npmInstall
 cp -R node_modules/* "${DIR}/lib/node_modules/"
 # Also include node_modules/.bin, so that `meteor npm` can make use of
 # commands like node-gyp and node-pre-gyp.
 cp -R node_modules/.bin "${DIR}/lib/node_modules/"
 
-# Make node-gyp install Node headers and libraries in $DIR/.node-gyp/.
-# https://github.com/nodejs/node-gyp/blob/4ee31329e0/lib/node-gyp.js#L52
-export HOME="$DIR"
-export USERPROFILE="$DIR"
-node "${DIR}/lib/node_modules/node-gyp/bin/node-gyp.js" install
-INCLUDE_PATH="${DIR}/.node-gyp/${NODE_VERSION}/include/node"
-echo "Contents of ${INCLUDE_PATH}:"
-ls -al "$INCLUDE_PATH"
+if [ -z ${NODE_FROM_SRC+x} ]; then
+  # Make node-gyp install Node headers and libraries in $DIR/.node-gyp/.
+  # https://github.com/nodejs/node-gyp/blob/4ee31329e0/lib/node-gyp.js#L52
+  export HOME="$DIR"
+  export USERPROFILE="$DIR"
+  node "${DIR}/lib/node_modules/node-gyp/bin/node-gyp.js" install
+  INCLUDE_PATH="${DIR}/.node-gyp/${NODE_VERSION}/include/node"
+  echo "Contents of ${INCLUDE_PATH}:"
+  ls -al "$INCLUDE_PATH"
+else
+  echo "Skipping node-gyp headers because we're building from source."
+fi
 
 cd "${DIR}/lib"
 
@@ -138,6 +180,14 @@ find . -path '*/esprima-fb/test' | xargs rm -rf
 cd "$DIR/lib/node_modules/fibers/bin"
 shrink_fibers
 
+if [ "${NODE_FROM_SRC:-}" = "debug" ]; then
+  # Hack for modules which explicitly `require()` the "Release" binding path.
+  sed -i -e 's|build/Release/|build/Debug/|g' \
+    $DIR/lib/node_modules/pathwatcher/lib/main.js \
+    $DIR/lib/node_modules/runas/lib/runas.js \
+    $DIR/lib/node_modules/kexec/index.js
+fi
+
 # Sanity check to see if we're not breaking anything by replacing npm
 INSTALLED_NPM_VERSION=$(cat "$DIR/lib/node_modules/npm/package.json" |
 xargs -0 node -e "console.log(JSON.parse(process.argv[1]).version)")
@@ -150,6 +200,7 @@ fi
 echo BUNDLING
 
 cd "$DIR"
+delete node-src/
 echo "${BUNDLE_VERSION}" > .bundle_version.txt
 rm -rf build CHANGELOG.md ChangeLog LICENSE README.md
 
