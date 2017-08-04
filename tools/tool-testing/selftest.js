23b6c78b2f93c5819cde51f70bd7f6868b459ca1
Jesse Rosenberger <git@jro.cc>
2017-08-04 16:59:31 +0300

    When an instantiated `Test` is cloned, it should maintain its prototype.

--- tools/tool-testing/selftest.js
+++ tools/tool-testing/selftest.js
@@ -1780,7 +1780,7 @@ var getFilteredTests = function (options) {
         return test;
       }
 
-      return _.extend({}, test, { tags: test.tags.concat(newTags) });
+      return _.extend(Object.create(Object.getPrototypeOf(test)), test, { tags: test.tags.concat(newTags) });
     });
   }
 
