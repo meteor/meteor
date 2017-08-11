56aea027234b213cc584f37c40aa04e8e0bfbd67
Ben Newman <ben@meteor.com>
2017-08-11 12:17:09 -0400

    Stop setting METEOR_WATCH_PRIORITIZE_CHANGED=false in self-tests.
    Assuming #9007 fixes the file watcher timing problems, it should no longer
    be necessary to disable the optimizations of #8866.

--- tools/tests/run.js
+++ tools/tests/run.js
@@ -22,8 +22,6 @@ selftest.define("run", function () {
   var s = new Sandbox({ fakeMongo: true });
   var run;
 
-  s.set("METEOR_WATCH_PRIORITIZE_CHANGED", "false");
-
   // Starting a run
   s.createApp("myapp", "standard-app");
   s.cd("myapp");
@@ -240,8 +238,6 @@ selftest.define("update during run", ["checkout", 'custom-warehouse'], function 
     fakeMongo: true
   });
   var run;
-
-  s.set("METEOR_WATCH_PRIORITIZE_CHANGED", "false");
 
   s.createApp("myapp", "packageless", { release: DEFAULT_RELEASE_TRACK + '@v1' });
   s.cd("myapp");
