b5cd99a818795504eeb8d8bc1e39f46f9f45f49f
Dan Rubins <dan@legalrobot.com>
2017-09-05 21:21:07 -0700

    wrap changes @ 80 char

--- packages/accounts-password/password_server.js
+++ packages/accounts-password/password_server.js
@@ -67,11 +67,18 @@ Accounts._checkPassword = function (user, password) {
 
   if (! bcryptCompare(password, user.services.password.bcrypt)) {
     result.error = handleError("Incorrect password", false);
-  } else if (user.services.password.bcrypt && Accounts._bcryptRounds > Number(user.services.password.bcrypt.substring(4, 6))) {
+  } else if (
+    user.services.password.bcrypt && 
+    Accounts._bcryptRounds > 
+      Number(user.services.password.bcrypt.substring(4, 6))
+  ) {
     // password checks out, but user bcrypt may need update
     Meteor.defer(() => {
       Meteor.users.update({ _id: user._id }, {
-        $set: { 'services.password.bcrypt': bcryptHash(password, Accounts._bcryptRounds) }
+        $set: { 
+          'services.password.bcrypt': 
+            bcryptHash(password, Accounts._bcryptRounds)
+        }
       });
     });
   }
