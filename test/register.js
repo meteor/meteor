720dbad903287a10caa157f0a2c8556347b05ef4
Ben Newman <ben@benjamn.com>
2017-09-06 21:16:33 -0400

    Run tests again with default options in Node 8+.

--- test/register.js
+++ test/register.js
@@ -1,12 +1,16 @@
 var path = require("path");
 var meteorBabelTestPath = __dirname;
 var meteorBabelPath = path.dirname(meteorBabelTestPath);
-var nodeMajorVersion = parseInt(process.versions.node);
-var babelOptions = require("../options").getDefaults({
-  nodeMajorVersion,
+var features = {
   react: true,
   jscript: true
-});
+};
+
+if (! process.env.IGNORE_NODE_MAJOR_VERSION) {
+  features.nodeMajorVersion = parseInt(process.versions.node);
+}
+
+var babelOptions = require("../options").getDefaults(features);
 
 require("../register")
   .setSourceMapRootPath(meteorBabelPath)
--- test/run.sh
+++ test/run.sh
@@ -6,9 +6,21 @@ TEST_DIR=$(pwd)
 BABEL_CACHE_DIR=${TEST_DIR}/.cache
 export BABEL_CACHE_DIR
 
-mocha \
-    --reporter spec \
-    --full-trace \
-    --require ../runtime.js \
-    --compilers js:${TEST_DIR}/register.js \
-    tests.js
+runTests() {
+    mocha \
+        --reporter spec \
+        --full-trace \
+        --require ../runtime.js \
+        --compilers js:${TEST_DIR}/register.js \
+        tests.js
+}
+
+runTests
+
+if [ $(node -p "parseInt(process.versions.node)") -ge "8" ]
+then
+    echo "Running tests again with default options..."
+    echo
+    export IGNORE_NODE_MAJOR_VERSION=1
+    runTests
+fi
