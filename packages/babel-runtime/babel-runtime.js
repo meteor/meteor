4f09556bae6089eda16278cfb86a95bd0ea06bce
Ben Newman <ben@meteor.com>
2017-09-14 18:47:38 -0400

    Warn in babel-runtime Meteor package if babel-runtime npm package outdated.

--- packages/babel-runtime/babel-runtime.js
+++ packages/babel-runtime/babel-runtime.js
@@ -10,13 +10,26 @@ exports.checkHelper = function checkHelper(id) {
 };
 
 try {
+  var babelRuntimeVersion = require("babel-runtime/package.json").version;
   var regeneratorRuntime = require("babel-runtime/regenerator");
 } catch (e) {
   throw new Error([
     "The babel-runtime npm package could not be found in your node_modules ",
     "directory. Please run the following command to install it:",
     "",
-    "  meteor npm install --save babel-runtime",
+    "  meteor npm install --save babel-runtime@next",
+    ""
+  ].join("\n"));
+}
+
+if (parseInt(babelRuntimeVersion, 10) < 7) {
+  throw new Error([
+    "The version of babel-runtime installed in your node_modules directory ",
+    "(" + babelRuntimeVersion + ") is out of date. Please upgrade it by running ",
+    "",
+    "  meteor npm install --save babel-runtime@next",
+    "",
+    "in your application directory.",
     ""
   ].join("\n"));
 }
