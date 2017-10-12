0454f23c8c29eede655b1aeb1582509cb021d2e3
Jesse Rosenberger <git@jro.cc>
2017-10-13 01:43:16 +0300

    Remove duplicate Node headers in Unix dev bundle.
    This was accomplished in 67b76abc7867eebd07df7dce2da2bae064a8c1ac by
    @benjamn, but a merge commit from devel to release-1.6 inadvertently
    removed half of it.  Prior to this commit, the new headers have been
    used, but the old headers were still in place and taking up room.

--- scripts/generate-dev-bundle.sh
+++ scripts/generate-dev-bundle.sh
@@ -61,20 +61,6 @@ rm -rf "${MONGO_NAME}"
 export PATH="$DIR/bin:$PATH"
 
 cd "$DIR/lib"
-
-# Make node-gyp install Node headers and libraries in $DIR/.node-gyp/.
-# https://github.com/nodejs/node-gyp/blob/4ee31329e0/lib/node-gyp.js#L52
-export HOME="$DIR"
-export USERPROFILE="$DIR"
-
-# Make sure the latest version of node-gyp is installed at the top level.
-npm install node-gyp
-
-node "${DIR}/lib/node_modules/node-gyp/bin/node-gyp.js" install
-INCLUDE_PATH="${DIR}/.node-gyp/${NODE_VERSION}/include/node"
-echo "Contents of ${INCLUDE_PATH}:"
-ls -al "$INCLUDE_PATH"
-
 # Overwrite the bundled version with the latest version of npm.
 npm install "npm@$NPM_VERSION"
 
