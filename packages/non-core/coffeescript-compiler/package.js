fd483c34e31c23693b9c3d0417a6cd430a55b7c0
Geoffrey Booth <webmaster@geoffreybooth.com>
2017-10-15 11:24:39 -0700

    Use CoffeeScript 2.0.1

--- packages/non-core/coffeescript-compiler/package.js
+++ packages/non-core/coffeescript-compiler/package.js
@@ -3,11 +3,11 @@ Package.describe({
   summary: 'Compiler for CoffeeScript code, supporting the coffeescript package',
   // This version of NPM `coffeescript` module, with _1, _2 etc.
   // If you change this, make sure to also update ../coffeescript/package.js to match.
-  version: '1.12.7_3'
+  version: '2.0.1_1'
 });
 
 Npm.depends({
-  'coffeescript': '1.12.7',
+  'coffeescript': '2.0.1',
   'source-map': '0.5.7'
 });
 
