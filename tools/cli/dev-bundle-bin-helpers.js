0fddc26a2e673a2ccd7fd64fb14030afa9c51868
Ben Newman <ben@meteor.com>
2017-10-23 19:37:44 -0400

    Adjust npm update notifier opt-out environment variable.
    https://github.com/zkat/npx/issues/98#issuecomment-316936135
    https://www.npmjs.com/package/update-notifier#user-settings

--- tools/cli/dev-bundle-bin-helpers.js
+++ tools/cli/dev-bundle-bin-helpers.js
@@ -78,7 +78,7 @@ exports.getEnv = function (options) {
     var env = Object.create(process.env);
 
     // Make sure notifications to update npm aren't presented to the user.
-    env.NPM_CONFIG_NO_UPDATE_NOTIFIER = true;
+    env.NO_UPDATE_NOTIFIER = true;
 
     // Make sure `meteor npm install --global ...` installs into
     // dev_bundle/lib/node_modules by default.
