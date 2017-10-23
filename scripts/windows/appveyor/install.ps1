696127f4b853419ecd1ecc287db8864a02512e3f
Jesse Rosenberger <git@jro.cc>
2017-10-23 16:38:31 -0700

    Throw an error when any external command fails during test preparation.
    Even with $ErrorActionPreference, PowerShell won't automatically fail
    when an external command fails with an error code.  This explicitly
    checks those exit codes and throws an error when that occurs.
    
    Hopefully prevents false successes like that shown in this AppVeyor
    test run: https://goo.gl/xxRsF9.

--- scripts/windows/appveyor/install.ps1
+++ scripts/windows/appveyor/install.ps1
@@ -11,11 +11,19 @@ Write-Host "Updating submodules recursively..." -ForegroundColor Magenta
 # Appveyor suggests -q flag for 'git submodule...' https://goo.gl/4TFAHm
 & git.exe -C "$dirCheckout" submodule -q update --init --recursive
 
+If ($LASTEXITCODE -ne 0) {
+  throw "Updating submodules failed."
+}
+
 Write-Host "Running 'meteor --get-ready'..." -ForegroundColor Magenta
 # By redirecting error to host, we avoid a shocking/false error color,
 # since --get-ready and --version can print (anything) to STDERR and
 # PowerShell will interpret that as something being terribly wrong.
 & "$meteorBat" --get-ready 2>&1 | Write-Host -ForegroundColor Green
+
+If ($LASTEXITCODE -ne 0) {
+  throw "Running .\meteor --get-ready failed."
+}
 
 # This should no longer be necessary with Meteor 1.6, which will
 # automatically install these dependencies when they're not found in the
@@ -26,3 +34,7 @@ Write-Host "Installing test npm dependencies..." `
 & "$meteorBat" npm install --prefix "${dirCheckout}\dev_bundle\lib" `
   phantomjs-prebuilt `
   browserstack-webdriver
+
+If ($LASTEXITCODE -ne 0) {
+  throw "Installing npm dependencies required for testing has failed."
+}
