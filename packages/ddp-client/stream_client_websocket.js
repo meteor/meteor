f1967de74f0687c68f95244478d078dbeebb47aa
Sashko Stubailo <sashko@meteor.com>
2017-11-07 06:35:31 -0800

    Remove sockjs

--- packages/ddp-client/stream_client_websocket.js
+++ packages/ddp-client/stream_client_websocket.js
@@ -129,40 +129,40 @@ _.extend(LivedataTest.ClientStream.prototype, {
       self.HEARTBEAT_TIMEOUT);
   },
 
-  _sockjsProtocolsWhitelist: function () {
-    // only allow polling protocols. no streaming.  streaming
-    // makes safari spin.
-    var protocolsWhitelist = [
-      'xdr-polling', 'xhr-polling', 'iframe-xhr-polling', 'jsonp-polling'];
+  // _sockjsProtocolsWhitelist: function () {
+  //   // only allow polling protocols. no streaming.  streaming
+  //   // makes safari spin.
+  //   var protocolsWhitelist = [
+  //     'xdr-polling', 'xhr-polling', 'iframe-xhr-polling', 'jsonp-polling'];
 
-    // iOS 4 and 5 and below crash when using websockets over certain
-    // proxies. this seems to be resolved with iOS 6. eg
-    // https://github.com/LearnBoost/socket.io/issues/193#issuecomment-7308865.
-    //
-    // iOS <4 doesn't support websockets at all so sockjs will just
-    // immediately fall back to http
-    var noWebsockets = navigator &&
-          /iPhone|iPad|iPod/.test(navigator.userAgent) &&
-          /OS 4_|OS 5_/.test(navigator.userAgent);
+  //   // iOS 4 and 5 and below crash when using websockets over certain
+  //   // proxies. this seems to be resolved with iOS 6. eg
+  //   // https://github.com/LearnBoost/socket.io/issues/193#issuecomment-7308865.
+  //   //
+  //   // iOS <4 doesn't support websockets at all so sockjs will just
+  //   // immediately fall back to http
+  //   var noWebsockets = navigator &&
+  //         /iPhone|iPad|iPod/.test(navigator.userAgent) &&
+  //         /OS 4_|OS 5_/.test(navigator.userAgent);
 
-    if (!noWebsockets)
-      protocolsWhitelist = ['websocket'].concat(protocolsWhitelist);
+  //   if (!noWebsockets)
+  //     protocolsWhitelist = ['websocket'].concat(protocolsWhitelist);
 
-    return protocolsWhitelist;
-  },
+  //   return protocolsWhitelist;
+  // },
 
   _launchConnection: function () {
     var self = this;
     self._cleanup(); // cleanup the old socket, if there was one.
 
-    var options = _.extend({
-      protocols_whitelist:self._sockjsProtocolsWhitelist()
-    }, self.options._sockjsOptions);
+    // var options = _.extend({
+    //   protocols_whitelist:self._sockjsProtocolsWhitelist()
+    // }, self.options._sockjsOptions);
 
-    // Convert raw URL to SockJS URL each time we open a connection, so that we
+    // Convert raw URL to Websocket URL each time we open a connection, so that we
     // can connect to random hostnames and get around browser per-host
     // connection limits.
-    self.socket = new SockJS(toSockjsUrl(self.rawUrl), undefined, options);
+    self.socket = new WebSocket(toWebsocketUrl(self.rawUrl));
     self.socket.onopen = function (data) {
       self._connected();
     };
