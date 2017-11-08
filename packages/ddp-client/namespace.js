2f099b476edc64a283575359739ab518146a2305
Sashko Stubailo <sashko@meteor.com>
2017-11-08 08:39:52 -0800

    Get rid of random_stream

--- packages/ddp-client/namespace.js
+++ packages/ddp-client/namespace.js
@@ -27,3 +27,11 @@ DDP.ConnectionError = Meteor.makeErrorType(
 
 DDP.ForcedReconnectError = Meteor.makeErrorType(
   "DDP.ForcedReconnectError", function () {});
+
+  // Returns the named sequence of pseudo-random values.
+// The scope will be DDP._CurrentMethodInvocation.get(), so the stream will produce
+// consistent values for method calls on the client and server.
+DDP.randomStream = function (name) {
+  var scope = DDP._CurrentMethodInvocation.get();
+  return DDPCommon.RandomStream.get(scope, name);
+};
