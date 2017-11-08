f1ebb2aedfe62a5182c25b6aedbf6cc3f51284a4
Sashko Stubailo <sashko@meteor.com>
2017-11-08 14:17:17 -0800

    Refactor some small stuff

--- packages/ddp-client/client/client.js
+++ packages/ddp-client/client/client.js
@@ -1,8 +1,9 @@
 import { DDP, LivedataTest } from "../common/namespace";
-import "./stream_client_sockjs";
 
+import "./stream_client_sockjs";
 import "../common/livedata_connection";
 
+// Initialize the default server connection and put it on Meteor.connection
 import "./client_convenience";
 
 export { DDP, LivedataTest };
