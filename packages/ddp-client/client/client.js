371d4990ac5d046be15369eb0b39690bf1420b52
Sashko Stubailo <sashko@meteor.com>
2017-11-08 16:49:01 -0800

    Get ClientStream off of LivedataTest

--- packages/ddp-client/client/client.js
+++ packages/ddp-client/client/client.js
@@ -1,7 +1,8 @@
 import { DDP, LivedataTest } from "../common/namespace";
 
-import "./stream_client_sockjs";
-import "../common/livedata_connection";
+if (false) {
+  import "./stream_client_sockjs";
+}
 
 // Initialize the default server connection and put it on Meteor.connection
 import "./client_convenience";
