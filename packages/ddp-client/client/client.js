caf91e7932a1892febe6d778b06fdb5860b0b914
Sashko Stubailo <sashko@meteor.com>
2017-11-09 13:37:45 -0800

    Add back import that isn't strictly required

--- packages/ddp-client/client/client.js
+++ packages/ddp-client/client/client.js
@@ -4,5 +4,7 @@ if (false) {
   import './stream_client_sockjs';
 }
 
+import '../common/livedata_connection';
+
 // Initialize the default server connection and put it on Meteor.connection
 import './client_convenience';
