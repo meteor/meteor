0dcc4f18e14eeab60ab1f83b64c7e3bd1785f058
Sashko Stubailo <sashko@meteor.com>
2017-11-09 08:23:34 -0800

    Use export from instead of import then export

--- packages/ddp-client/client/client.js
+++ packages/ddp-client/client/client.js
@@ -1,8 +1,7 @@
-import { DDP, LivedataTest } from '../common/namespace';
+export { DDP, LivedataTest } from '../common/namespace';
+
 import './stream_client_sockjs';
 
 import '../common/livedata_connection';
 
 import './client_convenience';
-
-export { DDP, LivedataTest };
--- packages/ddp-client/server/server.js
+++ packages/ddp-client/server/server.js
@@ -1,7 +1,5 @@
-import { DDP, LivedataTest } from '../common/namespace';
+export { DDP, LivedataTest } from '../common/namespace';
 
 import './stream_client_nodejs';
 
 import '../common/livedata_connection';
-
-export { DDP, LivedataTest };
