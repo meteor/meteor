3ba8af8519ba84b0202fe217733b703e6543c74d
Ben Newman <ben@meteor.com>
2017-11-15 13:21:17 -0500

    Remove no-longer-necessary ClientStream-related imports from ddp-client.

--- packages/ddp-client/client/client.js
+++ packages/ddp-client/client/client.js
@@ -1,11 +1,5 @@
 export { DDP } from '../common/namespace.js';
 
-if (false) {
-  // This is used inside livedata_connection, but this is what gets
-  // it included in the client bundle
-  import './stream_client_sockjs';
-}
-
 import '../common/livedata_connection';
 
 // Initialize the default server connection and put it on Meteor.connection
