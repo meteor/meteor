b81872b01a223f1f3b5c2c94be01072da32b4d09
Ben Newman <ben@meteor.com>
2017-11-17 13:20:00 -0500

    Remove weak dependency on browser-policy-common from dynamic-import.
    Even a weak dependency affects the load order of packages by forcing the
    depended-on package to load before the dependent package. Waiting until
    Meteor.startup to check for Package["browser-policy-common"] dynamically
    does not have this problem.

--- packages/dynamic-import/package.js
+++ packages/dynamic-import/package.js
@@ -9,9 +9,6 @@ Package.onUse(function (api) {
   // Do not allow this package to be used in pre-Meteor 1.5 apps.
   api.use("isobuild:dynamic-import@1.5.0");
 
-  // Modify browser policy only if browser-policy packages are used.
-  api.use("browser-policy-content", { weak: true });
-
   api.use("modules");
   api.use("promise");
   api.use("http");
