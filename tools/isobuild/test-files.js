e2fdadeebd3ee14e51493f2e5048ded6ec7cc433
James Burgess <jamesmillerburgess@gmail.com>
2017-11-17 11:55:13 +0400

    Revert "Add option to specify test file patterns to CLI"
    This reverts commit 72f65ebc9ee04e3b781a260512004aee3bf27db9.

--- tools/isobuild/test-files.js
+++ tools/isobuild/test-files.js
@@ -20,15 +20,11 @@ export const APP_TEST_FILENAME_REGEXPS = [
 
 // Given a path to a file in an app (relative to the app root
 // directory), is this file a test file?
-export function isTestFilePath(path, testMatch) {
+export function isTestFilePath(path) {
   const splitPath = path.split(pathSep);
 
-  // Use testMatch if it was provided, otherwise use the defaults
-  let patterns = [...TEST_FILENAME_REGEXPS, ...APP_TEST_FILENAME_REGEXPS];
-  if (testMatch) {
-    patterns = [new RegExp(testMatch)];
-  }
-
-  // Does the filename match one of the patterns?
-  return _.any(patterns, regexp => regexp.test(_.last(splitPath)));
+  // Does the filename match one of the test filename forms?
+  return _.any(
+    [...TEST_FILENAME_REGEXPS, ...APP_TEST_FILENAME_REGEXPS],
+    regexp => regexp.test(_.last(splitPath)));
 }
