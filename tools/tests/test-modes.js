2547957268a5cdd84f8841cadadb2e724c589e4a
James Burgess <jamesmillerburgess@gmail.com>
2017-11-22 19:21:49 +0300

    Fix meteor test file matching patterns (#9339)
    * Adjust test filename RegExps to match Meteor guide. Fixes #9332.
    * Adjusted help text for --drive-package on meteor test.
    * Add integration tests for `meteor test` eager file loading.
    * Fix typo in selftest.forbid comment.
    * Improve test file eager load integration test coverage and clarity.

--- tools/tests/test-modes.js
+++ tools/tests/test-modes.js
@@ -1,4 +1,6 @@
 var selftest = require('../tool-testing/selftest.js');
+import { isTestFilePath } from '../isobuild/test-files';
+const expectEqual = selftest.expectEqual;
 var Sandbox = selftest.Sandbox;
 
 selftest.define("'meteor test --port' accepts/rejects proper values", function () {
@@ -29,3 +31,80 @@ selftest.define("'meteor test --port' accepts/rejects proper values", function (
   run.match('App running at: http://[::]:3700/');
   run.stop();
 });
+
+selftest.define("'meteor test' eagerly loads correct files", () => {
+  // Unit tests for test file match regexps
+  expectEqual(isTestFilePath('/foo.test.js'), true);
+  expectEqual(isTestFilePath('/foo.tests.js'), true);
+  expectEqual(isTestFilePath('/foo.spec.js'), true);
+  expectEqual(isTestFilePath('/foo.specs.js'), true);
+  expectEqual(isTestFilePath('/foo.test.bar.js'), true);
+  expectEqual(isTestFilePath('/foo.tests.bar.js'), true);
+  expectEqual(isTestFilePath('/foo.spec.bar.js'), true);
+  expectEqual(isTestFilePath('/foo.specs.bar.js'), true);
+  expectEqual(isTestFilePath('/foo.app-test.js'), true);
+  expectEqual(isTestFilePath('/foo.app-tests.js'), true);
+  expectEqual(isTestFilePath('/foo.app-spec.js'), true);
+  expectEqual(isTestFilePath('/foo.app-specs.js'), true);
+  expectEqual(isTestFilePath('/foo.app-test.bar.js'), true);
+  expectEqual(isTestFilePath('/foo.app-tests.bar.js'), true);
+  expectEqual(isTestFilePath('/foo.app-spec.bar.js'), true);
+  expectEqual(isTestFilePath('/foo.app-specs.bar.js'), true);
+
+  // Regression tests for #9332
+  expectEqual(isTestFilePath('/foo.testify.js'), false);
+  expectEqual(isTestFilePath('/foo.retest.js'), false);
+  expectEqual(isTestFilePath('/foo.spectacular.js'), false);
+  expectEqual(isTestFilePath('/foo.respec.js'), false);
+  expectEqual(isTestFilePath('/foo.testify.bar.js'), false);
+  expectEqual(isTestFilePath('/foo.retest.bar.js'), false);
+  expectEqual(isTestFilePath('/foo.spectacular.bar.js'), false);
+  expectEqual(isTestFilePath('/foo.respec.bar.js'), false);
+  expectEqual(isTestFilePath('/foo.app-testify.js'), false);
+  expectEqual(isTestFilePath('/foo.reapp-test.js'), false);
+  expectEqual(isTestFilePath('/foo.app-spectacular.js'), false);
+  expectEqual(isTestFilePath('/foo.reapp-spec.js'), false);
+  expectEqual(isTestFilePath('/foo.app-testify.bar.js'), false);
+  expectEqual(isTestFilePath('/foo.reapp-test.bar.js'), false);
+  expectEqual(isTestFilePath('/foo.app-spectacular.bar.js'), false);
+  expectEqual(isTestFilePath('/foo.reapp-spec.bar.js'), false);
+
+  // Integration tests for test file eager loading with `meteor test` and 
+  // `meteor test --full-app`
+  const s = new Sandbox();
+  let run;
+
+  s.createApp('myapp', 'test-eagerly-load');
+  s.cd('myapp');
+  s.set('');
+
+  // `meteor` should load app files, but not test files or app-test files
+  run = s.run();
+  run.waitSecs(120);
+  run.match('index.js');
+  run.stop();
+  run.forbid('foo.test.js');
+  run.forbid('foo.app-test.js');
+
+  // `meteor test` should load test files, but not app files or app-test files
+  run = s.run('test', '--driver-package', 'tmeasday:acceptance-test-driver');
+  run.waitSecs(120);
+  run.match('foo.test.js');
+  run.stop();
+  run.forbid('index.js');
+  run.forbid('foo.app-test.js');
+
+  // `meteor test --full-app` should load both test files and app-test files,
+  // but not test files
+  run = s.run(
+    'test',
+    '--driver-package',
+    'tmeasday:acceptance-test-driver',
+    '--full-app',
+  );
+  run.waitSecs(120);
+  run.match('foo.app-test.js');
+  run.match('index.js');
+  run.stop();
+  run.forbid('foo.test.js');  
+})
