ff588b436c2cc9559c01a4cf0c84c5503c7177a4
Seba Kerckhof <seba.kerckhof@gmail.com>
2017-12-02 23:18:09 +0100

    Add support for $type aliases. (#9402)

--- packages/minimongo/common.js
+++ packages/minimongo/common.js
@@ -87,8 +87,41 @@ export const ELEMENT_OPERATORS = {
     // should *not* include it itself.
     dontIncludeLeafArrays: true,
     compileElementSelector(operand) {
-      if (typeof operand !== 'number') {
-        throw Error('$type needs a number');
+      if (typeof operand === 'string') {
+        const operandAliasMap = {
+          'double': 1,
+          'string': 2,
+          'object': 3,
+          'array': 4,
+          'binData': 5,
+          'undefined': 6,
+          'objectId': 7,
+          'bool': 8,
+          'date': 9,
+          'null': 10,
+          'regex': 11,
+          'dbPointer': 12,
+          'javascript': 13,
+          'symbol': 14,
+          'javascriptWithScope': 15,
+          'int': 16,
+          'timestamp': 17,
+          'long': 18,
+          'decimal': 19,
+          'minKey': -1,
+          'maxKey': 127,
+        };
+        if (!hasOwn.call(operandAliasMap, operand)) {
+          throw Error(`unknown string alias for $type: ${operand}`);
+        }
+        operand = operandAliasMap[operand];
+      } else if (typeof operand === 'number') {
+        if (operand === 0 || operand < -1
+          || (operand > 19 && operand !== 127)) {
+          throw Error(`Invalid numerical $type code: ${operand}`);
+        }
+      } else {
+        throw Error('argument to $type is not a number or a string');
       }
 
       return value => (
