meteor/meteor

commit 95d093a17ad37114af38f4aba3ecf5e56f80dc75

Use /__meteor__/bundle-visualizer/stats URL for bundle-visualizer stats.

packages/non-core/bundle-visualizer/common.js
@@ -1,6 +1,5 @@
-export const packageName = "bundle-visualizer";
 export const classPrefix = "meteorBundleVisualizer";
-export const methodNameStats = `_meteor/${packageName}/stats`;
+export const methodNameStats = "/__meteor__/bundle-visualizer/stats";
 export const typeBundle = "bundle";
 export const typePackage = "package";
 export const typeNodeModules = "node_modules";

packages/non-core/bundle-visualizer/client.js
@@ -19,7 +19,11 @@ function main(builder) {
   document.body.appendChild(mask);
   document.body.appendChild(container);
 
-  HTTP.call("GET", Meteor.absoluteUrl(methodNameStats), {
+  // Always match the protocol (http or https) and the domain:port of the
+  // current page.
+  const url = "//" + location.host + methodNameStats;
+
+  HTTP.call("GET", url, {
     params: {
       cacheBuster: Math.random().toString(36).slice(2)
     }

packages/non-core/bundle-visualizer/server.js
@@ -107,7 +107,7 @@ Meteor.startup(() => {
   }
 
   Package.webapp.WebApp.connectHandlers.use(
-    "/" + methodNameStats,
+    methodNameStats,
     statsMiddleware
   );
 });

commit 20fc65d2fed19f8090fdac6fffe746d09082ccf7

Go back to exporting packageName from bundle-visualizer/common.js.
This mistake was introduced by 95d093a17ad37114af38f4aba3ecf5e56f80dc75.

packages/non-core/bundle-visualizer/common.js
@@ -1,5 +1,6 @@
+export const packageName = "bundle-visualizer";
 export const classPrefix = "meteorBundleVisualizer";
-export const methodNameStats = "/__meteor__/bundle-visualizer/stats";
+export const methodNameStats = `/__meteor__/${packageName}/stats`;
 export const typeBundle = "bundle";
 export const typePackage = "package";
 export const typeNodeModules = "node_modules";