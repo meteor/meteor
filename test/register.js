e5fb801ccab6cc67c1b9cb2dcae32c70c1b01d13
Ben Newman <ben@benjamn.com>
2017-12-07 12:38:07 -0500

    Actually compile modern browser tests with modern options.

--- test/register.js
+++ test/register.js
@@ -8,10 +8,10 @@ var features = {
 
 if (! process.env.IGNORE_NODE_MAJOR_VERSION) {
   features.nodeMajorVersion = parseInt(process.versions.node);
+}
 
-  if (process.env.COMPILE_FOR_MODERN_BROWSERS) {
-    features.modernBrowsers = true;
-  }
+if (process.env.COMPILE_FOR_MODERN_BROWSERS) {
+  features.modernBrowsers = true;
 }
 
 var babelOptions = require("../options").getDefaults(features);
