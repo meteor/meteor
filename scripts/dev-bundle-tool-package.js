ac705ebd92be2fa8621a0d85f7f90edb8108f394
Ben Newman <ben@meteor.com>
2017-12-12 13:07:13 -0500

    Bump `reify` npm package to version 0.13.3.

--- scripts/dev-bundle-tool-package.js
+++ scripts/dev-bundle-tool-package.js
@@ -18,7 +18,7 @@ var packageJson = {
     "meteor-babel": "7.0.0-beta.34-1",
     "meteor-promise": "0.8.6",
     promise: "8.0.1",
-    reify: "0.13.1",
+    reify: "0.13.3",
     fibers: "2.0.0",
     // So that Babel can emit require("@babel/runtime/helpers/...") calls.
     "@babel/runtime": "7.0.0-beta.34",
