e46a08daaf9c52d4be483e79a5ecdf036db33c17
Ben Newman <ben@meteor.com>
2018-01-16 19:46:35 -0500

    Fix `meteor bundle`-related self-tests.

--- tools/tests/bundle.js
+++ tools/tests/bundle.js
@@ -3,31 +3,31 @@ var Sandbox = selftest.Sandbox;
 var files = require('../fs/files.js');
 import { execSync } from 'child_process';
 
-selftest.define("bundle", ["slow"], function () {
+selftest.define("bundle", function () {
   var s = new Sandbox();
   var run;
 
   s.createApp("myapp", "standard-app");
   s.cd("myapp");
-  run = s.run("bundle", "myapp.tgz");
+  run = s.run("bundle", "../myapp.tgz");
   run.waitSecs(60);
   run.expectExit(0);
 
-  var tarball = files.pathJoin(s.cwd, "myapp.tgz");
+  var tarball = files.pathJoin(s.cwd, "../myapp.tgz");
   selftest.expectEqual(files.exists(tarball), true);
 });
 
-selftest.define("bundle - verify sanitized asset names", ["slow"], function () {
+selftest.define("bundle - verify sanitized asset names", function () {
   const s = new Sandbox();
   let run;
 
   s.createApp("sanitized-app", "sanitized-app");
   s.cd("sanitized-app");
-  run = s.run("bundle", "sanitized-app.tgz");
+  run = s.run("bundle", "../sanitized-app.tgz");
   run.waitSecs(60);
   run.expectExit(0);
 
-  const tarball = files.pathJoin(s.cwd, "sanitized-app.tgz");
+  const tarball = files.pathJoin(s.cwd, "../sanitized-app.tgz");
   const sanitizedFilename = 'Meteor_:-@2x.png';
   selftest.expectTrue(
     execSync(`tar -tf ${tarball}`).toString().indexOf(sanitizedFilename) > -1
