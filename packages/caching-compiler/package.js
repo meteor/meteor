7678c46f302249c572a1330db44ec29d7b72492c
Ben Newman <ben@meteor.com>
2018-01-21 11:27:07 -0500

    Bump caching-compiler package version to republish.

--- packages/caching-compiler/package.js
+++ packages/caching-compiler/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'caching-compiler',
-  version: '1.1.9',
+  version: '1.1.10',
   summary: 'An easy way to make compiler plugins cache',
   documentation: 'README.md'
 });
