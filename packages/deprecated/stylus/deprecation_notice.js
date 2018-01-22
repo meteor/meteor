41161774d6485ccd955a8bb3ef4f2d32e43fd8ab
Ben Newman <ben@meteor.com>
2018-01-22 16:27:35 -0500

    Bump stylus patch version to 2.513.14 and update references.

--- packages/deprecated/stylus/deprecation_notice.js
+++ packages/deprecated/stylus/deprecation_notice.js
@@ -1,7 +1,7 @@
-console.warn(
-  "The `stylus` package has been deprecated.\n" +
-  "\n" +
-  "To continue using the last supported version \n" +
-  "of this package, pin your package version to \n" +
-  "2.513.13 (`meteor add stylus@=2.513.13`)."
-);
+console.warn([
+  "The `stylus` package has been deprecated.",
+  "",
+  "To continue using the last supported version",
+  "of this package, pin your package version to",
+  "2.513.14 (`meteor add stylus@=2.513.14`).",
+].join("\n"));
--- packages/deprecated/stylus/package.js
+++ packages/deprecated/stylus/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Expressive, dynamic, robust CSS',
-  version: "2.513.13"
+  version: "2.513.14"
 });
 
 Package.registerBuildPlugin({
