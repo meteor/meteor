d9d43a7efa67ba6d97b9285c8cafd1e70d9629ae
Ben Newman <ben@meteor.com>
2018-01-23 18:16:02 -0500

    Bump package versions for 1.6.2-beta.2 release.

--- packages/promise/package.js
+++ packages/promise/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "promise",
-  version: "0.11.0-beta162.1",
+  version: "0.11.0-beta162.2",
   summary: "ECMAScript 2015 Promise polyfill with Fiber support",
   git: "https://github.com/meteor/promise",
   documentation: "README.md"
--- packages/socket-stream-client/package.js
+++ packages/socket-stream-client/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "socket-stream-client",
-  version: "0.2.0-beta162.1",
+  version: "0.2.0-beta162.2",
   summary: "Provides the ClientStream abstraction used by ddp-client",
   documentation: "README.md"
 });
