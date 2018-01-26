9cc9ff7f499af468974ceb673b4709e37a7967a3
Ben Newman <ben@meteor.com>
2018-01-26 17:43:37 -0500

    Actually depend on es5-shim so that Cordova can import it.
    Another attempt to fix #9572. :crossed_fingers:
    
    cc @paulincai

--- packages/es5-shim/package.js
+++ packages/es5-shim/package.js
@@ -1,8 +1,12 @@
 Package.describe({
   name: "es5-shim",
-  version: "4.7.2",
+  version: "4.7.3",
   summary: "Shims and polyfills to improve ECMAScript 5 support",
   documentation: "README.md"
+});
+
+Npm.depends({
+  "es5-shim": "4.5.10"
 });
 
 Package.onUse(function(api) {
