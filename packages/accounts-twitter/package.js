55aeb8fb6e5cb6028c988f3b7ec51387e9026940
James Burgess <jamesmillerburgess@gmail.com>
2018-01-27 17:33:33 +0100

    Modernize accounts-twitter package
    - Bumped patch version number 1.4.1 -> 1.4.2
    - ES6 syntax and shorthand applied
    - Underscore removed as a dependency

--- packages/accounts-twitter/package.js
+++ packages/accounts-twitter/package.js
@@ -1,11 +1,10 @@
 Package.describe({
   summary: "Login service for Twitter accounts",
-  version: "1.4.1"
+  version: "1.4.2",
 });
 
-Package.onUse(function(api) {
+Package.onUse(api => {
   api.use('ecmascript');
-  api.use('underscore', ['server']);
   api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
