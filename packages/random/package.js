97222a9d6c1d0343c6a3053c68a17f9826b4074a
sebakerckhof <seba.kerckhof@gmail.com>
2018-01-30 14:59:30 +0100

    Modernize random

--- packages/random/package.js
+++ packages/random/package.js
@@ -1,17 +1,17 @@
 Package.describe({
-  summary: "Random number generator and utilities",
+  summary: 'Random number generator and utilities',
   version: '1.1.0'
 });
 
 Package.onUse(function (api) {
   api.use('ecmascript');
   api.export('Random');
-  api.addFiles('random.js');
+  api.mainModule('random.js');
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
   api.use('random');
   api.use('ecmascript');
   api.use('tinytest');
-  api.addFiles('random_tests.js', ['client', 'server']);
+  api.mainModule('random_tests.js');
 });
--- packages/random/random_tests.js
+++ packages/random/random_tests.js
@@ -1,3 +1,6 @@
+import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+
 Tinytest.add('random', function (test) {
   // Deterministic with a specified seed, which should generate the
   // same sequence in all environments.
@@ -7,24 +10,24 @@ Tinytest.add('random', function (test) {
   // algorithm being used and it starts generating a different
   // sequence for a seed, as long as the sequence is consistent for
   // a particular release.
-  var random = Random.createWithSeeds(0);
-  test.equal(random.id(), "cp9hWvhg8GSvuZ9os");
-  test.equal(random.id(), "3f3k6Xo7rrHCifQhR");
-  test.equal(random.id(), "shxDnjWWmnKPEoLhM");
-  test.equal(random.id(), "6QTjB8C5SEqhmz4ni");
+  const random = Random.createWithSeeds(0);
+  test.equal(random.id(), 'cp9hWvhg8GSvuZ9os');
+  test.equal(random.id(), '3f3k6Xo7rrHCifQhR');
+  test.equal(random.id(), 'shxDnjWWmnKPEoLhM');
+  test.equal(random.id(), '6QTjB8C5SEqhmz4ni');
 });
 
 // node crypto and window.crypto.getRandomValues() don't let us specify a seed,
 // but at least test that the output is in the right format.
 Tinytest.add('random - format', function (test) {
-  var idLen = 17;
+  const idLen = 17;
   test.equal(Random.id().length, idLen);
   test.equal(Random.id(29).length, 29);
-  var numDigits = 9;
-  var hexStr = Random.hexString(numDigits);
+  const numDigits = 9;
+  const hexStr = Random.hexString(numDigits);
   test.equal(hexStr.length, numDigits);
-  parseInt(hexStr, 16); // should not throw
-  var frac = Random.fraction();
+  Number.parseInt(hexStr, 16); // should not throw
+  const frac = Random.fraction();
   test.isTrue(frac < 1.0);
   test.isTrue(frac >= 0.0);
 
@@ -37,7 +40,7 @@ Tinytest.add('random - Alea is last resort', function (test) {
     test.isTrue(Random.alea === undefined);
   }
   if (Meteor.isClient) {
-    var useGetRandomValues = !!(typeof window !== "undefined" &&
+    const useGetRandomValues = !!(typeof window !== 'undefined' &&
         window.crypto && window.crypto.getRandomValues);
     test.equal(Random.alea === undefined, useGetRandomValues);
   }
