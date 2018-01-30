a076890927dbb580643d87ec3f940c2a99c67566
James Burgess <jamesmillerburgess@gmail.com>
2018-01-30 13:23:33 +0100

    Modernize appcache package

--- packages/appcache/package.js
+++ packages/appcache/package.js
@@ -1,23 +1,21 @@
 Package.describe({
   summary: "Enable the application cache in the browser",
-  version: "1.1.1"
+  version: "1.1.2",
 });
 
-Package.onUse(function (api) {
-  api.use('webapp', 'server');
+Package.onUse(api => {
+  api.use('ecmascript', ['client', 'server']);
+  api.use(['webapp', 'routepolicy'], 'server');
   api.use('reload', 'client');
-  api.use('routepolicy', 'server');
-  api.use('underscore', 'server');
   api.use('autoupdate', 'server', {weak: true});
-  api.addFiles('appcache-client.js', 'client');
-  api.addFiles('appcache-server.js', 'server');
+  api.mainModule('appcache-client.js', 'client');
+  api.mainModule('appcache-server.js', 'server');
 });
 
-Package.onTest(function (api) {
+Package.onTest(api => {
   api.use('tinytest');
   api.use('appcache');
   api.use('http', 'client');
-  api.use('underscore', 'client');
   api.use('webapp', 'server');
   api.addFiles('appcache_tests-server.js', 'server');
   api.addFiles('appcache_tests-client.js', 'client');
