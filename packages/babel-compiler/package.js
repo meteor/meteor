a5d976318632e89d0a589644c2d6838f55c3fb94
Ben Newman <ben@meteor.com>
2018-02-06 20:55:07 -0500

    Update meteor-babel to latest version.

--- packages/babel-compiler/package.js
+++ packages/babel-compiler/package.js
@@ -6,11 +6,11 @@ Package.describe({
   // isn't possible because you can't publish a non-recommended
   // release with package versions that don't have a pre-release
   // identifier at the end (eg, -dev)
-  version: '7.0.2'
+  version: '7.0.3'
 });
 
 Npm.depends({
-  'meteor-babel': '7.0.0-beta.38-2'
+  'meteor-babel': '7.0.0-beta.38-3'
 });
 
 Package.onUse(function (api) {
