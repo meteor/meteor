89b0e5919e3a65dcf99c54f43249d3d54c05c030
Geoffrey Booth <webmaster@geoffreybooth.com>
2018-02-12 22:36:30 -0800

    Bump CoffeeScript version to 2.2.1

--- packages/non-core/coffeescript-compiler/package.js
+++ packages/non-core/coffeescript-compiler/package.js
@@ -13,11 +13,11 @@ Package.describe({
   summary: 'Compiler for CoffeeScript code, supporting the coffeescript package',
   // This version of NPM `coffeescript` module, with _1, _2 etc.
   // If you change this, make sure to also update ../coffeescript/package.js to match.
-  version: '2.0.3_4'
+  version: '2.2.1_1'
 });
 
 Npm.depends({
-  'coffeescript': '2.0.3',
+  'coffeescript': '2.2.1',
   'source-map': '0.5.7'
 });
 
