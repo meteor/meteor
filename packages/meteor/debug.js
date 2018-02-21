7cd7a474dedb09d0f4db1983cfb39b65eb495a1e
pravdomil <pravdomil.toman@gmail.com>
2018-02-21 20:48:04 +0100

    jsdocs

--- packages/meteor/debug.js
+++ packages/meteor/debug.js
@@ -1,16 +1,17 @@
 let suppress = 0;
 
-// replacement for console.log. This is a temporary API. We should
-// provide a real logging API soon (possibly just a polyfill for
-// console?)
-//
-// NOTE: this is used on the server to print the warning about
-// having autopublish enabled when you probably meant to turn it
-// off. it's not really the proper use of something called
-// _debug. the intent is for this message to go to the terminal and
-// be very visible. if you change _debug to go someplace else, etc,
-// please fix the autopublish code to do something reasonable.
-//
+/**
+ * replacement for console.log. This is a temporary API. We should
+ * provide a real logging API soon (possibly just a polyfill for
+ * console?)
+ *
+ * NOTE: this is used on the server to print the warning about
+ * having autopublish enabled when you probably meant to turn it
+ * off. it's not really the proper use of something called
+ * _debug. the intent is for this message to go to the terminal and
+ * be very visible. if you change _debug to go someplace else, etc,
+ * please fix the autopublish code to do something reasonable.
+ */
 Meteor._debug = function(...arguments) {
   if (suppress) {
     suppress--;
