705480ded27db86769e476f062e83a45acb7c589
pravdomil <pravdomil.toman@gmail.com>
2018-02-21 20:41:00 +0100

    optimize

--- packages/meteor/debug.js
+++ packages/meteor/debug.js
@@ -16,43 +16,46 @@ Meteor._debug = function(/* arguments */) {
     suppress--;
     return;
   }
+  
   if (typeof console !== 'undefined' &&
-      typeof console.log !== 'undefined') {
-    if (arguments.length == 0) { // IE Companion breaks otherwise
-      // IE10 PP4 requires at least one argument
-      console.log('');
-    } else {
-      // IE doesn't have console.log.apply, it's not a real Object.
-      // http://stackoverflow.com/questions/5538972/console-log-apply-not-working-in-ie9
-      // http://patik.com/blog/complete-cross-browser-console-log/
-      if (typeof console.log.apply === 'function') {
-        // Most browsers
+    typeof console.log !== 'undefined') {
+    return;
+  }
 
-        // Chrome and Safari only hyperlink URLs to source files in first argument of
-        // console.log, so try to call it with one argument if possible.
-        // Approach taken here: If all arguments are strings, join them on space.
-        // See https://github.com/meteor/meteor/pull/732#issuecomment-13975991
-        var allArgumentsOfTypeString = true;
-        for (var i = 0; i < arguments.length; i++) {
-          if (typeof arguments[i] !== 'string') {
-            allArgumentsOfTypeString = false;
-          }
+  if (arguments.length == 0) { // IE Companion breaks otherwise
+    // IE10 PP4 requires at least one argument
+    console.log('');
+  } else {
+    // IE doesn't have console.log.apply, it's not a real Object.
+    // http://stackoverflow.com/questions/5538972/console-log-apply-not-working-in-ie9
+    // http://patik.com/blog/complete-cross-browser-console-log/
+    if (typeof console.log.apply === 'function') {
+      // Most browsers
+
+      // Chrome and Safari only hyperlink URLs to source files in first argument of
+      // console.log, so try to call it with one argument if possible.
+      // Approach taken here: If all arguments are strings, join them on space.
+      // See https://github.com/meteor/meteor/pull/732#issuecomment-13975991
+      var allArgumentsOfTypeString = true;
+      for (var i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] !== 'string') {
+          allArgumentsOfTypeString = false;
         }
-
-        if (allArgumentsOfTypeString) {
-          console.log.apply(console, [Array.prototype.join.call(arguments, ' ')]);
-        } else {
-          console.log.apply(console, arguments);
-        }
-
-      } else if (typeof Function.prototype.bind === 'function') {
-        // IE9
-        var log = Function.prototype.bind.call(console.log, console);
-        log.apply(console, arguments);
-      } else {
-        // IE8
-        Function.prototype.call.call(console.log, console, Array.prototype.slice.call(arguments));
       }
+
+      if (allArgumentsOfTypeString) {
+        console.log.apply(console, [Array.prototype.join.call(arguments, ' ')]);
+      } else {
+        console.log.apply(console, arguments);
+      }
+
+    } else if (typeof Function.prototype.bind === 'function') {
+      // IE9
+      var log = Function.prototype.bind.call(console.log, console);
+      log.apply(console, arguments);
+    } else {
+      // IE8
+      Function.prototype.call.call(console.log, console, Array.prototype.slice.call(arguments));
     }
   }
 };
