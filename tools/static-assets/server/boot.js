883bed60238534f1ef06d8a7f10acdc0cffd5d87
Michał Powaga <michalpowaga13@gmail.com>
2018-02-28 22:05:03 +0000

    Revert "Print nicer error message when missing meteor package is imported"
    This reverts commit 1574eb18fe400a5c99703afa204565484e72b1c1.

--- tools/static-assets/server/boot.js
+++ tools/static-assets/server/boot.js
@@ -285,19 +285,7 @@ var loadServerBundles = Profile("Load server bundles", function () {
           }
         }
 
-        try {
-          var resolved = require.resolve(name);
-        } catch (err) {
-          const nameParts = name.split("/");
-          // If it looks like `meteor/xxx`, the user may forgot to add the
-          // package before importing it.
-          if (nameParts.length === 2 && nameParts[0] === "meteor") {
-            const meteorAddTip = ". Try `meteor add " + nameParts[1] + "` " +
-              "as it looks like you tried to import it without adding " +
-              "to the project.";
-            throw new Error("Cannot find module " + JSON.stringify(name) + meteorAddTip);
-          }
-        }
+        var resolved = require.resolve(name);
         if (resolved === name && ! path.isAbsolute(resolved)) {
           // If require.resolve(id) === id and id is not an absolute
           // identifier, it must be a built-in module like fs or http.
