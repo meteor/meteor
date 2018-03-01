f7845e80b5f81b2f3fde2ca1f2fbce716b837732
zebra-lucky <zebra.lucky@gmail.com>
2018-03-01 03:21:56 +0200

    add simple test to mongo-decimal package

--- packages/mongo-decimal/package.js
+++ packages/mongo-decimal/package.js
@@ -13,3 +13,11 @@ Package.onUse(function (api) {
   api.mainModule('decimal.js');
   api.export('Decimal');
 });
+
+Package.onTest(function (api) {
+  api.use('mongo');
+  api.use('mongo-decimal');
+  api.use('insecure');
+  api.use(['tinytest']);
+  api.addFiles('decimal_tests.js', ['client', 'server']);
+});
