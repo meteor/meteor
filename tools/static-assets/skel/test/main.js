88ac202349f92568d5e3aa7accd2d25883e5ec68
Ben Newman <ben@meteor.com>
2018-03-03 12:56:45 -0500

    Simplify test/main.js for newly `meteor create`d apps.

--- tools/static-assets/skel/test/main.js
+++ tools/static-assets/skel/test/main.js
@@ -1,27 +1,20 @@
 import assert from "assert";
 
-export const testMessage = "Welcome to Meteor!";
-
-describe("~name~", () => {
-  it("package.json has correct name", () => {
-    const { name } = require("../package.json");
+describe("~name~", function () {
+  it("package.json has correct name", async function () {
+    const { name } = await import("../package.json");
     assert.strictEqual(name, "~name~");
   });
 
   if (Meteor.isClient) {
-    it("client is not server", () => {
+    it("client is not server", function () {
       assert.strictEqual(Meteor.isServer, false);
     });
   }
 
   if (Meteor.isServer) {
-    it("server is not client", () => {
+    it("server is not client", function () {
       assert.strictEqual(Meteor.isClient, false);
     });
   }
-
-  it("async/await and dynamic import()", async () => {
-    const tests = await import("./main.js");
-    assert.strictEqual(tests.testMessage, testMessage);
-  });
 });
