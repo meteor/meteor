ccc6294018f376faa7a57259f21da93a63e3f3da
Seba Kerckhof <seba.kerckhof@gmail.com>
2018-03-07 17:46:56 +0100

    Modernize route-policy (#9667)

--- packages/routepolicy/package.js
+++ packages/routepolicy/package.js
@@ -1,19 +1,18 @@
 Package.describe({
   summary: "route policy declarations",
-  version: '1.0.12'
+  version: '1.0.13'
 });
 
 Package.onUse(function (api) {
-  api.use('underscore', 'server');
+  api.use(['underscore', 'ecmascript'], 'server');
   // Resolve circular dependency with webapp. We can only use WebApp via
   // Package.webapp and only after initial load.
   api.use('webapp', 'server', {unordered: true});
   api.export('RoutePolicy', 'server');
-  api.export('RoutePolicyTest', 'server', {testOnly: true});
-  api.addFiles('routepolicy.js', 'server');
+  api.mainModule('main.js', 'server');
 });
 
 Package.onTest(function (api) {
-  api.use(['routepolicy', 'tinytest']);
-  api.addFiles(['routepolicy_tests.js'], 'server');
+  api.use(['routepolicy', 'tinytest', 'ecmascript']);
+  api.mainModule('routepolicy_tests.js', 'server');
 });
