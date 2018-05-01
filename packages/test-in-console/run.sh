d95a42df3c367323897957249d9ee430ce07a48c
Toine van Kampen <toine.vankampen@gmail.com>
2018-05-01 21:45:29 +1000

    Retain PhantomJS for Console Tests
    Add Phantom back in to retain the ability to run tests in legacy
    browser. By default its false.

--- packages/test-in-console/run.sh
+++ packages/test-in-console/run.sh
@@ -4,12 +4,17 @@ cd "`dirname "$0"`"
 cd ../..
 export METEOR_HOME=`pwd`
 
+export phantom=$phantom
+
+# only install dependencies if required
+if [ "$phantom" = true ]
+then
 # Just in case these packages haven't been installed elsewhere.
 ./meteor npm install -g phantomjs-prebuilt browserstack-webdriver
-
+else
 # Install locally
-./meteor npm install puppeteer@1.3.0
-
+./meteor npm install puppeteer
+fi
 
 export PATH=$METEOR_HOME:$PATH
 # synchronously get the dev bundle and NPM modules if they're not there.
@@ -21,7 +26,14 @@ exec 3< <(meteor test-packages --driver-package test-in-console -p 4096 --exclud
 EXEC_PID=$!
 
 sed '/test-in-console listening$/q' <&3
-node "$METEOR_HOME/packages/test-in-console/runner.js"
+
+if [ "$phantom" = true ]
+then
+./dev_bundle/bin/phantomjs "$METEOR_HOME/packages/test-in-console/phantomRunner.js"
+else
+node "$METEOR_HOME/packages/test-in-console/puppeteerRunner.js"
+fi
+
 STATUS=$?
 
 pkill -TERM -P $EXEC_PID
