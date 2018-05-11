06ea07d14b4526803a51ed1e0b4631471c88c7e4
Ben Newman <ben@meteor.com>
2018-05-10 20:16:50 -0400

    Provide isomorphic implementation of WHATWG fetch() API.

---
+++ packages/fetch/fetch-tests.js
@@ -0,0 +1,8 @@
+// Import Tinytest from the tinytest Meteor package.
+import { Tinytest } from "meteor/tinytest";
+
+// Write your tests here!
+// Here is an example.
+Tinytest.add('fetch - example', function (test) {
+  test.equal(typeof fetch, "function");
+});
