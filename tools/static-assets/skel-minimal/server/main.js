c88b8882755d25775e5d40dc8a4a7216efc20707
Ben Newman <ben@meteor.com>
2018-05-16 17:22:04 -0400

    Demonstrate basic server-side rendering in minimal starter app.
    Now that the meteor/server-render package is lazy on the client, it adds
    no weight to the client JavaScript bundle unless imported.

--- tools/static-assets/skel-minimal/server/main.js
+++ tools/static-assets/skel-minimal/server/main.js
@@ -1,1 +1,15 @@
-console.log(`Greetings from ${module.id}!`);
+import { Meteor } from "meteor/meteor";
+import { onPageLoad } from "meteor/server-render";
+
+Meteor.startup(() => {
+  // Code to run on server startup.
+  console.log(`Greetings from ${module.id}!`);
+});
+
+onPageLoad(sink => {
+  // Code to run on every request.
+  sink.renderIntoElementById(
+    "server-render-target",
+    `Server time: ${new Date}`
+  );
+});
