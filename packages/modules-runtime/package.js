445fd3714a9e85ffe40975185f550faf4dcde099
Ben Newman <ben@meteor.com>
2018-06-25 13:22:52 -0400

    Update `install` package used by modules-runtime to version 0.11.1.
    This enables using index.* modules with non-.js file extensions to make
    the containing directory importable (cc @justinanastos):
    https://github.com/benjamn/install/commit/c417044421ff3026fa6053663eaea018be7652e2
    
    Since this is a core package patch update, you can update to this version
    of the modules-runtime package by running
    
      meteor update modules-runtime
    
    This should give you modules-runtime@0.10.1.

--- packages/modules-runtime/package.js
+++ packages/modules-runtime/package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: "modules-runtime",
-  version: "0.10.0",
+  version: "0.10.1",
   summary: "CommonJS module system",
   git: "https://github.com/benjamn/install",
   documentation: "README.md"
 });
 
 Npm.depends({
-  install: "0.11.0"
+  install: "0.11.1"
 });
 
 Package.onUse(function(api) {
