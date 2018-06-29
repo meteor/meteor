2ced13a180eef36c7c717e451d571b46c521dec8
Ben Newman <ben@meteor.com>
2018-06-29 18:17:55 -0400

    Bump package versions for 1.7.1-beta.10 release.

--- packages/less/package.js
+++ packages/less/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'less',
-  version: '2.8.0-beta171.9',
+  version: '2.8.0-beta171.10',
   summary: 'Leaner CSS language',
   documentation: 'README.md'
 });
--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "The Meteor command-line tool",
-  version: '1.7.1-beta.9'
+  version: '1.7.1-beta.10'
 });
 
 Package.includeTool();
