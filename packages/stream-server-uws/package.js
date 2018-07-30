d8752215267c4f85b94f4dd49ac3488d532bda3a
Mrau Hu <mrauhu@yandex.ru>
2018-07-30 13:53:09 +0300

    Rearrange `Package.onUse()` list in `stream-server-uws/package.js`.

--- packages/stream-server-uws/package.js
+++ packages/stream-server-uws/package.js
@@ -1,15 +1,14 @@
 Package.describe({
   summary: "Meteor's DDP stream server based on uWS",
-  version: '0.5.0',
+  version: '0.5.1',
   documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
   api.use(['modules'], 'server');
-
   api.use(['webapp', 'routepolicy'], 'server');
+  // Update StreamServers global variable
   api.use(['stream-server'], 'server');
-
   api.imply(['stream-server'], 'server');
 
   api.export('StreamServers', 'server');
