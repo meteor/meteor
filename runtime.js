d2359630a19c851921d513feae71fd6fefbc3160
Ben Newman <ben@benjamn.com>
2018-08-04 12:47:15 -0400

    Implement Module.prototype.resolve for Reify's new module.link API.

--- runtime.js
+++ runtime.js
@@ -1,6 +1,14 @@
 require("meteor-babel-helpers");
 
 var Module = module.constructor;
+
+// The Reify runtime now requires a working implementation of
+// module.resolve, which should return a canonical absolute module
+// identifier string, like require.resolve(id).
+Module.prototype.resolve = function (id) {
+  return Module._resolveFilename(id, this);
+};
+
 require("reify/lib/runtime").enable(Module.prototype);
 
 require("meteor-promise").makeCompatible(
