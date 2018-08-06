699df7ed1997df2fe49dfe53e9601851556af998
James Miller Burgess <jamesmillerburgess@gmail.com>
2018-08-06 09:11:35 +0400

    Remove underscore from reactive-dict and session packages

--- packages/reactive-dict/package.js
+++ packages/reactive-dict/package.js
@@ -1,10 +1,10 @@
 Package.describe({
   summary: "Reactive dictionary",
-  version: '1.2.0'
+  version: '1.2.1'
 });
 
 Package.onUse(function (api) {
-  api.use(['underscore', 'tracker', 'ejson', 'ecmascript']);
+  api.use(['tracker', 'ejson', 'ecmascript']);
   // If we are loading mongo-livedata, let you store ObjectIDs in it.
   api.use('mongo', {weak: true});
   api.mainModule('migration.js');
@@ -15,7 +15,6 @@ Package.onTest(function (api) {
   api.use('tinytest');
   api.use('reactive-dict');
   api.use('tracker');
-  api.use('underscore');
   api.use('reload');
   api.addFiles('reactive-dict-tests.js');
 });
