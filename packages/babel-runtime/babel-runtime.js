1e34acadc06572557e7e781c0738758d017d2cf2
Jesse Rosenberger <git@jro.cc>
2018-08-31 16:19:45 +0300

    Enforce specific `@babel/runtime` versions on `babel-runtime@1.2.x`.
    Meteor 1.6.x through 1.7.0.x use the `babel-runtime@1.2.x` packages, and
    this change is only intended to affect those versions.
    
    This will no longer be necessary in Meteor 1.7.1, since we account for
    the changes to `@babel/runtime` through changes to the `meteor/babel`
    package, but this ensures that anyone who updates to `@babel/runtime@7` (the
    final, latest release), or anything newer, isn't surprised by their broken
    application.

--- packages/babel-runtime/babel-runtime.js
+++ packages/babel-runtime/babel-runtime.js
@@ -25,12 +25,14 @@ if (parseInt(babelRuntimeVersion, 10) < 6) {
     ""
   ].join("\n"));
 
-} else {
-  var match = /^7\.0\.0-(beta|rc)\./.exec(babelRuntimeVersion);
-  if (match &&
-      (match[1] === "rc" ||
-      (match[1] === "beta" &&
-        parseInt(babelRuntimeVersion.split(".").pop(), 10) > 55))) {
+} else if (parseInt(babelRuntimeVersion.split(".").shift()) >= 7) {
+  // If Babel 7, only allow -beta.55 and earlier.  The final release, all RCs
+  // and -beta.56 all suffere from the removal of the "builtins" helpers.
+  var acceptableBabelRuntimeVersion =
+    /^7\.0\.0-beta/.test(babelRuntimeVersion) &&
+    parseInt(babelRuntimeVersion.split(".").pop(), 10) <= 55;
+
+  if (! acceptableBabelRuntimeVersion) {
     console.warn([
       "The version of @babel/runtime installed in your node_modules directory ",
       "(" + babelRuntimeVersion + ") contains a breaking change which was introduced by ",
