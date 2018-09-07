c632912c4310ec57b1dafbfb17f835ad2db3c94e
Ben Newman <ben@meteor.com>
2018-09-07 14:31:48 -0400

    Make sure `meteor shell` commands are compiled for Node 8.

--- packages/ecmascript/ecmascript.js
+++ packages/ecmascript/ecmascript.js
@@ -1,6 +1,8 @@
 ECMAScript = {
   compileForShell(command, cacheOptions) {
-    const babelOptions = Babel.getDefaultOptions();
+    const babelOptions = Babel.getDefaultOptions({
+      nodeMajorVersion: parseInt(process.versions.node, 10)
+    });
     delete babelOptions.sourceMap;
     delete babelOptions.sourceMaps;
     babelOptions.ast = false;
