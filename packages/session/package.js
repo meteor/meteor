f8e59735f6595f59782153c6c82d2be4eb885c8a
Seba Kerckhof <seba.kerckhof@gmail.com>
2018-11-03 21:12:06 +0100

    Modernize session package (#10228)

--- packages/session/package.js
+++ packages/session/package.js
@@ -4,7 +4,7 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-  api.use(['reactive-dict', 'ejson'], 'client');
+  api.use(['ecmascript', 'reactive-dict', 'ejson'], 'client');
 
   // Session can work with or without reload, but if reload is present
   // it should load first so we can detect it at startup and populate
@@ -12,10 +12,11 @@ Package.onUse(function (api) {
   api.use('reload', 'client', {weak: true});
 
   api.export('Session', 'client');
-  api.addFiles('session.js', 'client');
+  api.mainModule('session.js', 'client');
 });
 
 Package.onTest(function (api) {
+  api.use('ecmascript');
   api.use('tinytest');
   api.use('session', 'client');
   api.use('tracker');
