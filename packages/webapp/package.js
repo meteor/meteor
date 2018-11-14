5e987902915f357575f942c316601f9fba5e0fda
Jesse Rosenberger <git@jro.cc>
2018-11-14 11:27:40 +0200

    Update `webapp`'s `useragent` npm to v2.3.0 to support HeadlessChrome.
    Previously, while the `useragent` package was able to parse the User-Agent
    for so-called "Headless Chrome" and generate a family of "HeadlessChrome",
    it was unable to parse out the individual portions of the version number
    (e.g. major, minor, patch).
    
    For example, the following User-Agent (herein referred to as `userAgentAbove`):
    
    ```
    Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/69.0.3497.100 Safari/537.36
    ```
    
    Previously resulted in:
    
    ```
    > require('useragent').lookup(userAgentAbove);
    
    {
      family: 'HeadlessChrome',
      major: '0',
      minor: '0',
      patch: '0',
      /* ... */
    }
    ```
    
    With the newer version of `useragent`, these are now properly extracted and
    set which will enable Meteor to treat Headless Chrome the same as Chrome in
    a follow-up commit.  Now:
    
    ```
    > require('useragent').lookup(userAgentAbove);
    
    {
      family: 'HeadlessChrome',
      major: '69',
      minor: '0',
      patch: '3497'
      /* ... */
    }
    ```

--- packages/webapp/package.js
+++ packages/webapp/package.js
@@ -12,7 +12,7 @@ Npm.depends({"basic-auth-connect": "1.0.0",
              send: "0.16.1",
              "stream-to-string": "1.1.0",
              "qs-middleware": "1.0.3",
-             useragent: "2.2.1"});
+             useragent: "2.3.0"});
 
 Npm.strip({
   multiparty: ["test/"],
