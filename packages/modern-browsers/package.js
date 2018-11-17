c0a35f0457a24fbfd854c554423155c0eb434553
Ben Newman <ben@meteor.com>
2018-11-17 15:27:35 -0500

    Bump package versions for 1.8.0.1-rc.2 release.

--- packages/modern-browsers/package.js
+++ packages/modern-browsers/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "modern-browsers",
-  version: "0.1.3-rc1801.1",
+  version: "0.1.3-rc1801.2",
   summary: "API for defining the boundary between modern and legacy " +
     "JavaScript clients",
   documentation: "README.md"
--- packages/session/package.js
+++ packages/session/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Session variable",
-  version: '1.2.0-rc1801.1'
+  version: '1.2.0-rc1801.2'
 });
 
 Package.onUse(function (api) {
