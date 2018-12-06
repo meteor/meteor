ad2d7153677905af785b4a25eb48ede64e1085f6
Ben Newman <ben@meteor.com>
2018-12-06 12:17:09 -0500

    Bump meteor-promise version to 0.8.7.
    Should help with #10359, as this version includes @VeselyT's commit
    https://github.com/ExentriqLtd/promise/commit/bbe4f0d20b70417950381aea112993c4cc8c1168

--- scripts/dev-bundle-server-package.js
+++ scripts/dev-bundle-server-package.js
@@ -8,9 +8,9 @@ var packageJson = {
   name: "meteor-dev-bundle",
   private: true,
   dependencies: {
-    "meteor-promise": "0.8.6",
+    "meteor-promise": "0.8.7",
     fibers: "2.0.0",
-    promise: "8.0.1",
+    promise: "8.0.2",
     // Not yet upgrading Underscore from 1.5.2 to 1.7.0 (which should be done
     // in the package too) because we should consider using lodash instead
     // (and there are backwards-incompatible changes either way).
