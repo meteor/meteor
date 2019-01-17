54911d5dbb9a1fa957c73663524b0fe9fe5568bc
Lam Hieu <lamhieu.vk@gmail.com>
2019-01-17 12:59:25 +0700

    Move helpers fn to utils file

--- packages/ejson/ejson.js
+++ packages/ejson/ejson.js
@@ -1,3 +1,14 @@
+import {
+  isFunction,
+  isObject,
+  keysOf,
+  lengthOf,
+  hasOwn,
+  convertMapToObject,
+  isArguments,
+  isInfOrNaN,
+} from './utils';
+
 /**
  * @namespace
  * @summary Namespace for EJSON functions
@@ -51,27 +62,6 @@ const EJSON = {};
  */
 
 const customTypes = new Map();
-
-const isFunction = (fn) => typeof fn === 'function';
-
-const isObject = (fn) => typeof fn === 'object';
-
-const keysOf = (obj) => Object.keys(obj);
-
-const lengthOf = (obj) => Object.keys(obj).length;
-
-const hasOwn = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop);
-
-const convertMapToObject = (map) => Array.from(map).reduce((acc, [key, value]) => {
-  // reassign to not create new object
-  acc[key] = value;
-  return acc;
-}, {});
-
-const isArguments = obj => obj != null && hasOwn(obj, 'callee');
-
-const isInfOrNaN =
-  obj => Number.isNaN(obj) || obj === Infinity || obj === -Infinity;
 
 // Add a custom type, using a method of your choice to get to and
 // from a basic JSON-able representation.  The factory argument
