23014dd884966dbf87fd03c979bea97271dc3432
Jesse Rosenberger <git@jro.cc>
2019-02-15 11:52:39 +0200

    Fix CHANGELOG generator to accommodate breaking/migration steps in `v.NEXT`. (#559)
    * Bump documentation CHANGELOG.md to v1.8.0.2
    
    * Test Netlify fix.  Empty commit.
    
    * Fix CHANGELOG generator to accommodate breaking/migration steps in `v.NEXT`.
    
    The Meteor docs use a RegExp to remove the `v.NEXT` section from the
    documentation, but the new `Breaking changes` and `Migration steps` blocks
    weren't being removed from the `v.NEXT` section, resulting in a weird look
    when I first bumped them to 1.8.0.2, as seen in this screenshot:
    
    https://c.jro.cc/722335ff832c
    
    This should fix that!
    
    * Test Netlify fix.  Empty commit.

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -9,6 +9,7 @@ var converter = new showdown.Converter({
 // Read the file given, strip of #vNEXT, render w/ markdown
 hexo.extend.tag.register('changelog', function(args) {
   var str = fs.readFileSync(args[0], 'utf8');
-  str = str.replace('## v.NEXT', '');
+  // Remove everything from `## v.NEXT` until the next H2 (released) heading.
+  str = str.replace(/^## v\.NEXT.*?^(?=##[^#])/ms, '');
   return converter.makeHtml(str);
 });
