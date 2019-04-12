3614e227300e69251ed9bba4eac1614e4490ce44
Ben Newman <ben@meteor.com>
2019-04-12 19:03:07 -0400

    Bump package versions for 1.9-alpha.0 release.

--- packages/context/package.js
+++ packages/context/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "context",
-  version: "0.4.0",
+  version: "0.4.0-alpha190.0",
   summary: "Manage contextual information without passing objects around",
   documentation: "README.md"
 });
