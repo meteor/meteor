9171b568a4b5fa2e2782754c7f729f41df907145
Ben Newman <ben@meteor.com>
2019-04-12 18:39:06 -0400

    Remove long-deprecated idle-gc dependency from modules test app.
    https://circleci.com/gh/meteor/meteor/32476

--- tools/tests/apps/modules/server/only.js
+++ tools/tests/apps/modules/server/only.js
@@ -11,11 +11,6 @@ assert.strictEqual(typeof winston.default, "object");
 assert.strictEqual(typeof winston.transports.Console, "function");
 assert.strictEqual(winstonNamespace.default, winston);
 
-// This package has a native .node module as the "main" property of its
-// package.json file. (#7947)
-import { start } from "idle-gc";
-assert.strictEqual(typeof start, "function");
-
 assert.strictEqual(Meteor.isServer, true);
 assert.strictEqual(Meteor.isClient, false);
 
