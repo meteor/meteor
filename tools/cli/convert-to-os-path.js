422a513c140a1571199fc9b54051823d3c7eecc8
Ben Newman <ben@meteor.com>
2019-04-22 14:05:07 -0400

    Stop using mini-files.js in non-Babel-compiled code.

---
+++ tools/cli/convert-to-os-path.js
@@ -0,0 +1,19 @@
+function toDosPath(path) {
+  if (path[0] === "/") {
+    if (! /^\/[A-Za-z](\/|$)/.test(path)) {
+      throw new Error("Surprising path: " + path);
+    }
+    // transform a previously windows path back
+    // "/C/something" to "c:/something"
+    path = path[1] + ":" + path.slice(2);
+  }
+  return path.split("/").join("\\");
+}
+
+function convertToOSPath(path) {
+  if (process.platform === "win32") {
+    return toDosPath(standardPath);
+  }
+  return standardPath;
+}
+exports.convertToOSPath = convertToOSPath;
