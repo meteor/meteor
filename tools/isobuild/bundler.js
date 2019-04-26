2d9310a7cd34c2ad4da0850f01cd5e147ac15482
Ben Newman <ben@meteor.com>
2019-04-26 15:49:18 -0400

    Convert programs/server/debug.js to TypeScript.
    Another tiny module (like tools/isobuild/css-modules.ts) that proves we
    can use TypeScript in programs/server/* code.

--- tools/isobuild/bundler.js
+++ tools/isobuild/bundler.js
@@ -2704,7 +2704,7 @@ class ServerTarget extends JsImageTarget {
     builder.transpile([
       "boot.js",
       "boot-utils.js",
-      "debug.js",
+      "debug.ts",
       "server-json.js",
       "mini-files.js",
       "npm-require.js",
