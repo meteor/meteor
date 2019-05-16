6d3386d77d749a9547af329585e6b79b3009184e
Ben Newman <ben@meteor.com>
2019-05-16 13:27:56 -0400

    Bump package versions for 1.8.2-beta.4 release.

--- packages/context/package.js
+++ packages/context/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "context",
-  version: "0.4.0-beta182.3",
+  version: "0.4.0-beta182.4",
   summary: "Manage contextual information without passing objects around",
   documentation: "README.md"
 });
--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "The Meteor command-line tool",
-  version: '1.8.2-beta.3'
+  version: '1.8.2-beta.4'
 });
 
 Package.includeTool();
