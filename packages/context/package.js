758d2ce7c8856a4609a7d4b4df33918e9517c0b0
Ben Newman <ben@meteor.com>
2019-06-23 19:39:28 -0400

    Bump package versions for 1.8.2-beta.7 release.

--- packages/context/package.js
+++ packages/context/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "context",
-  version: "0.4.0-beta182.6",
+  version: "0.4.0-beta182.7",
   summary: "Manage contextual information without passing objects around",
   documentation: "README.md"
 });
