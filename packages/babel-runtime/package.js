48276c3d3604a80518bdad69036db3ac25c6c7f5
Ben Newman <ben@meteor.com>
2019-06-25 14:28:38 -0400

    Bump package versions for 1.8.2-beta.10 release.

--- packages/babel-runtime/package.js
+++ packages/babel-runtime/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "babel-runtime",
   summary: "Runtime support for output of Babel transpiler",
-  version: '1.4.0-beta182.9',
+  version: '1.4.0-beta182.10',
   documentation: 'README.md'
 });
 
--- packages/modules/package.js
+++ packages/modules/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "modules",
-  version: "0.14.0-beta182.9",
+  version: "0.14.0-beta182.10",
   summary: "CommonJS module system",
   documentation: "README.md"
 });
