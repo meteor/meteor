d276ef7c30138b0362a271e14613cf0cbba88ede
Ben Newman <ben@meteor.com>
2019-07-30 13:16:26 -0400

    Bump package versions for 1.8.2-beta.16 release.

--- packages/babel-runtime/package.js
+++ packages/babel-runtime/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "babel-runtime",
   summary: "Runtime support for output of Babel transpiler",
-  version: '1.4.0-beta182.16',
+  version: '1.4.0-beta182.17',
   documentation: 'README.md'
 });
 
--- packages/context/package.js
+++ packages/context/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "context",
-  version: "0.4.0-beta182.16",
+  version: "0.4.0-beta182.17",
   summary: "Manage contextual information without passing objects around",
   documentation: "README.md"
 });
