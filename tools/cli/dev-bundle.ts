0d2906e95957ed52f3478cbbe060e2adf32bfb2d
James Miller Burgess <jamesmillerburgess@gmail.com>
2019-08-03 21:17:05 +0900

    Convert dev-bundle.js to dev-bundle.ts

--- tools/cli/dev-bundle.ts
+++ tools/cli/dev-bundle.ts
@@ -5,11 +5,11 @@
 // but that's unavoidable if we don't want to install Babel and load all
 // the rest of the code every time we run `meteor npm` or `meteor node`.
 
-var fs = require("fs");
-var path = require("path");
-var links = require("./dev-bundle-links.js");
-var rootDir = path.resolve(__dirname, "..", "..");
-var defaultDevBundlePromise =
+import fs from "fs";
+import path from "path";
+import links from "./dev-bundle-links.js";
+const rootDir = path.resolve(__dirname, "..", "..");
+const defaultDevBundlePromise =
   Promise.resolve(path.join(rootDir, "dev_bundle"));
 
 function getDevBundleDir() {
@@ -17,8 +17,8 @@ function getDevBundleDir() {
   // checkout, because it's always better to respect the .meteor/release
   // file of the current app, if possible.
 
-  var releaseFile = find(
-    process.cwd(),
+  const releaseFile = find(
+    (process as any).cwd(),
     makeStatTest("isFile"),
     ".meteor", "release"
   );
@@ -27,7 +27,7 @@ function getDevBundleDir() {
     return defaultDevBundlePromise;
   }
 
-  var localDir = path.join(path.dirname(releaseFile), "local");
+  const localDir = path.join(path.dirname(releaseFile), "local");
   if (! statOrNull(localDir, "isDirectory")) {
     try {
       fs.mkdirSync(localDir);
@@ -36,15 +36,15 @@ function getDevBundleDir() {
     }
   }
 
-  var devBundleLink = path.join(localDir, "dev_bundle");
-  var devBundleStat = statOrNull(devBundleLink);
+  const devBundleLink = path.join(localDir, "dev_bundle");
+  const devBundleStat = statOrNull(devBundleLink);
   if (devBundleStat) {
     return new Promise(function (resolve) {
       resolve(links.readLink(devBundleLink));
     });
   }
 
-  var release = fs.readFileSync(
+  const release = fs.readFileSync(
     releaseFile, "utf8"
   ).replace(/^\s+|\s+$/g, "");
 
@@ -64,16 +64,16 @@ function getDevBundleDir() {
   });
 }
 
-function getDevBundleForRelease(release) {
-  var parts = release.split("@");
+function getDevBundleForRelease(release: string) {
+  const parts = release.split("@");
   if (parts.length < 2) {
     return null;
   }
 
-  var track = parts[0];
-  var version = parts.slice(1).join("@");
+  const track = parts[0];
+  const version = parts.slice(1).join("@");
 
-  var packageMetadataDir = find(
+  const packageMetadataDir = find(
     rootDir,
     makeStatTest("isDirectory"),
     ".meteor", "package-metadata"
@@ -83,50 +83,50 @@ function getDevBundleForRelease(release) {
     return null;
   }
 
-  var meteorToolDir = path.resolve(
+  const meteorToolDir = path.resolve(
     packageMetadataDir,
     "..", "packages", "meteor-tool"
   );
 
-  var meteorToolStat = statOrNull(meteorToolDir, "isDirectory");
+  const meteorToolStat = statOrNull(meteorToolDir, "isDirectory");
   if (! meteorToolStat) {
     return null;
   }
 
-  var dbPath = path.join(
+  const dbPath = path.join(
     packageMetadataDir,
     "v2.0.1",
     "packages.data.db"
   );
 
-  var dbStat = statOrNull(dbPath, "isFile");
+  const dbStat = statOrNull(dbPath, "isFile");
   if (! dbStat) {
     return null;
   }
 
-  var sqlite3 = require("sqlite3");
-  var db = new sqlite3.Database(dbPath);
+  const sqlite3 = require("sqlite3");
+  const db = new sqlite3.Database(dbPath);
 
   return new Promise(function (resolve, reject) {
     db.get(
       "SELECT content FROM releaseVersions WHERE track=? AND version=?",
       [track, version],
-      function (error, data) {
+      function (error: Error, data) {
         error ? reject(error) : resolve(data);
       }
     );
 
   }).then(function (data) {
     if (data) {
-      var tool = JSON.parse(data.content).tool;
-      var devBundleDir = path.join(
+      const tool = JSON.parse(data.content).tool;
+      const devBundleDir = path.join(
         meteorToolDir,
         tool.split("@").slice(1).join("@"),
         "mt-" + getHostArch(),
         "dev_bundle"
       );
 
-      var devBundleStat = statOrNull(devBundleDir, "isDirectory");
+      const devBundleStat = statOrNull(devBundleDir, "isDirectory");
       if (devBundleStat) {
         return devBundleDir;
       }
@@ -140,9 +140,10 @@ function getDevBundleForRelease(release) {
   });
 }
 
-function statOrNull(path, statMethod) {
+function statOrNull(path: string, statMethod?: (() => void) | string) {
+  let stat;
   try {
-    var stat = fs.statSync(path);
+    stat = fs.statSync(path);
   } catch (e) {
     if (e.code !== "ENOENT") {
       throw e;
@@ -162,18 +163,17 @@ function statOrNull(path, statMethod) {
   return null;
 }
 
-function find(dir, predicate) {
-  var joinArgs = Array.prototype.slice.call(arguments, 2);
-  joinArgs.unshift(null);
+function find(dir: string, predicate: (input: string) => boolean, ...joinArgs: string[]) {
+  joinArgs.unshift('');
 
   while (true) {
     joinArgs[0] = dir;
-    var joined = path.join.apply(path, joinArgs);
+    const joined = path.join.apply(path, joinArgs);
     if (predicate(joined)) {
       return joined;
     }
 
-    var parentDir = path.dirname(dir);
+    const parentDir = path.dirname(dir);
     if (parentDir === dir) break;
     dir = parentDir;
   }
@@ -181,28 +181,28 @@ function find(dir, predicate) {
   return null;
 }
 
-function makeStatTest(method) {
-  return function (file) {
+function makeStatTest(method: string) {
+  return function (file: string) {
     return statOrNull(file, method);
   };
 }
 
 function getHostArch() {
-  if (process.platform === "win32") {
-    if (process.arch === "x64") {
+  if ((process as any).platform === "win32") {
+    if ((process as any).arch === "x64") {
       return "os.windows.x86_64";
     }
     return "os.windows.x86_32";
   }
 
-  if (process.platform === "linux") {
-    if (process.arch === "x64") {
+  if ((process as any).platform === "linux") {
+    if ((process as any).arch === "x64") {
       return "os.linux.x86_64";
     }
     return "os.linux.x86_32";
   }
 
-  if (process.platform === "darwin") {
+  if ((process as any).platform === "darwin") {
     return "os.osx.x86_64";
   }
 }
