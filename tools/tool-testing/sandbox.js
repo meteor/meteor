8567390e7e24e08053dcb0df1975619daec560e6
Ben Newman <ben@meteor.com>
2019-11-12 16:29:36 -0500

    Fix error stack trace parsing during Windows self-tests.

--- tools/tool-testing/sandbox.js
+++ tools/tool-testing/sandbox.js
@@ -368,7 +368,7 @@ export default class Sandbox {
     // Allow user to set TOOL_NODE_FLAGS for self-test app.
     if (process.env.TOOL_NODE_FLAGS && ! process.env.SELF_TEST_TOOL_NODE_FLAGS)
       console.log('Consider setting SELF_TEST_TOOL_NODE_FLAGS to configure ' +
-                  'self-test test applicaion spawns');
+                  'self-test test application spawns');
     env.TOOL_NODE_FLAGS = process.env.SELF_TEST_TOOL_NODE_FLAGS || '';
 
     return env;
