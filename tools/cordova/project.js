b3c1700fdff4f7b671c8ba11f062b2b50da13172
renanccastro <renanccastro@gmail.com>
2019-12-14 18:58:09 -0300

    Use Cordova plugin info for checking plugin installation.
    Avoid false missing error on git tarballs that were having their ids parsed
    like https://github.com/pathable/cordova-plugin-appsettings.git#6ffc8fa844468495d09c2e46f68aad06017cd744
    
    It's easier if the ID is just the plugin name like cordova-plugin-appsettings,
    which we can get from config.xml as PluginInfo does.

--- tools/cordova/project.js
+++ tools/cordova/project.js
@@ -737,7 +737,7 @@ perform cordova plugins reinstall`);
     // cordova-plugin-whitelist@1.3.2 => { 'cordova-plugin-whitelist': '1.3.2' }
     // com.cordova.plugin@file://.cordova-plugins/plugin => { 'com.cordova.plugin': 'file://.cordova-plugins/plugin' }
     // @scope/plugin@1.0.0 => { 'com.cordova.plugin': 'scope/plugin' }
-    const installed = this.listInstalledPluginVersions();
+    const installed = this.listInstalledPluginVersions(true);
     const installedPluginsNames = Object.keys(installed);
     const installedPluginsVersions = Object.values(installed);
     const missingPlugins = {};
