36c4dc7695540004d94efcb0df72571ad6135e75
filipenevola <filipemkv@gmail.com>
2019-12-16 09:04:07 -0400

    updates cordova-plugin-wkwebview-engine to 1.2.1

--- packages/webapp/package.js
+++ packages/webapp/package.js
@@ -21,7 +21,7 @@ Npm.strip({
 
 Cordova.depends({
   'cordova-plugin-whitelist': '1.3.4',
-  'cordova-plugin-wkwebview-engine': '1.1.4',
+  'cordova-plugin-wkwebview-engine': '1.2.1',
   'cordova-plugin-meteor-webapp': '1.7.1-beta.1'
 });
 
