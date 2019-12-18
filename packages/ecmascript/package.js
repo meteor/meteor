dc66b94080462ecd1b572c1c4c83c48d0354549a
Ben Newman <ben@meteor.com>
2019-12-18 11:48:32 -0500

    Update meteor-babel to version 7.7.5.

--- packages/ecmascript/package.js
+++ packages/ecmascript/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ecmascript',
-  version: '0.13.1',
+  version: '0.13.2',
   summary: 'Compiler plugin that supports ES2015+ in all .js files',
   documentation: 'README.md'
 });
