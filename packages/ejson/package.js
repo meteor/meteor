096641b084b70f4ab52f3ef4468728d164ec66d1
filipenevola <filipemkv@gmail.com>
2019-12-20 08:15:27 -0400

    Bump ejson package version to 1.1.1

--- packages/ejson/package.js
+++ packages/ejson/package.js
@@ -4,7 +4,6 @@ Package.describe({
 });
 
 Package.onUse(function onUse(api) {
-  api.versionsFrom('1.8.1');
   api.use(['ecmascript', 'base64']);
   api.mainModule('ejson.js');
   api.export('EJSON');
