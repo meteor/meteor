bb96f38d9b90ee2942d221b072a3a24d3766cee7
Ben Newman <ben@meteor.com>
2020-01-14 13:00:29 -0500

    Update meteor-babel and typescript to latest versions.
    Babel releases included in this update:
    - https://github.com/babel/babel/releases/tag/v7.7.6
    - https://github.com/babel/babel/releases/tag/v7.7.7
    - https://github.com/babel/babel/releases/tag/v7.8.0
    - https://github.com/babel/babel/releases/tag/v7.8.1
    - https://github.com/babel/babel/releases/tag/v7.8.2
    - https://github.com/babel/babel/releases/tag/v7.8.3
    
    Although some of these changes won't take effect until the next Meteor
    release, I intend to publish the babel-compiler, ecmascript, and
    typescript changes using Meteor 1.9, so that applications can update
    without waiting for Meteor 1.10.

--- packages/babel-compiler/package.js
+++ packages/babel-compiler/package.js
@@ -6,11 +6,11 @@ Package.describe({
   // isn't possible because you can't publish a non-recommended
   // release with package versions that don't have a pre-release
   // identifier at the end (eg, -dev)
-  version: '7.5.0'
+  version: '7.5.1'
 });
 
 Npm.depends({
-  'meteor-babel': '7.7.5',
+  'meteor-babel': '7.8.0',
   'json5': '2.1.1'
 });
 
