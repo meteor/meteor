96613f31039703d55f54e99d42a8eebc9da5ee86
Brian Mulhall <brian.e.mulhall@gmail.com>
2020-03-14 15:44:25 -0500

    updated the test cases to include a test for the keep_inifinty setting

--- packages/minifier-js/tests/minifier-tests.js
+++ packages/minifier-js/tests/minifier-tests.js
@@ -1,6 +1,5 @@
 
 Tinytest.add('minifier-js - verify simple JS minifications work', (test) => {
-
   let result = meteorJsMinify('function add(first,second){return first + second; }\n');
   test.equal(result.code, 'function add(n,d){return n+d}');
 
@@ -9,21 +8,24 @@ Tinytest.add('minifier-js - verify simple JS minifications work', (test) => {
 
   result = meteorJsMinify('function a(z) { let returnValue = z == 10; return returnValue; }\n');
   test.equal(result.code,'function a(n){let t;return 10==n}');
-
 });
 
 // properties -- default(true)
 Tinytest.add('minifier-js - verify properties setting', (test) => {
-
   let result = meteorJsMinify('const person = {};person["name"] = "brian";person["age"] = 100; function printName(person){console.log(person["name"])};printName(person);\n');
   test.equal(result.code, 'const person={};function printName(n){console.log(n.name)}person.name="brian",person.age=100,printName(person);');
-
 });
 
 // evaluate -- default(true)
 Tinytest.add('minifier-js - verify evaluate setting', (test) => {
   let result = meteorJsMinify('let a = 10 + 20 + 30;\n');
   test.equal(result.code, 'let a=60;');
+});
+
+// keep_infinity -- default(false)
+Tinytest.add('minifier-js - verify keep_infinity setting', (test) => {
+  let result = meteorJsMinify('let a = Infinity;\n');
+  test.equal(result.code, 'let a=1/0;');
 });
 
 // sequences -- default(true)
@@ -49,8 +51,7 @@ Tinytest.add('minifier-js - verify dead_code setting', (test) => {
 });
 
 //keep_numbers -- default(false)
-Tinytest.add('minifier-js - verify keep_numbers setting', (test) => {
-   
+Tinytest.add('minifier-js - verify keep_numbers setting', (test) => {   
   let result = meteorJsMinify('let number = 1_000_000_000_000;\n');
   test.equal(result.code, 'let number=1e12;');
   
@@ -61,19 +62,15 @@ Tinytest.add('minifier-js - verify keep_numbers setting', (test) => {
 
 // unused -- default(true)
 Tinytest.add('minifier-js - verify unused setting', (test) => {
-   
   let result = meteorJsMinify('function foo(){let name = "ron";let firstName = "roger";console.log(firstName)};function bar(){let name = "ron";let firstName = "roger";console.log(firstName)};foo()\n');
   test.equal(result.code, 'function foo(){let o="ron",r="roger";console.log("roger")}function bar(){let o="ron",r="roger";console.log("roger")}foo();');
-
 });
 
 // drop_debugger -- default(true)
 Tinytest.add('minifier-js - verify drop_debugger setting', (test) => {
-   
   let result = meteorJsMinify('let name = "meteor"; debugger; let age = 100;\n');
   test.equal(result.code, 'let name="meteor";debugger;let age=100;');
 
   result = meteorJsMinify('let name = "meteor";debugger;let age = 100;function printStatement(name) { console.log("hello QA developers " + name); debugger; };printStatement("brian");\n');
   test.equal(result.code, 'let name="meteor";debugger;let age=100;function printStatement(e){console.log("hello QA developers "+e);debugger}printStatement("brian");');
- 
 });
