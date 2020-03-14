dd822d2cdb0529a7d2ba0180aaab05ec5891f1a1
Brian Mulhall <brian.e.mulhall@gmail.com>
2020-03-14 15:45:36 -0500

    update terser from 4.4.0 to 4.6.6 and then bump up the package version from 2.6.0 to 2.6.1

--- packages/minifier-js/package.js
+++ packages/minifier-js/package.js
@@ -1,10 +1,10 @@
 Package.describe({
   summary: "JavaScript minifier",
-  version: "2.6.0"
+  version: "2.6.1"
 });
 
 Npm.depends({
-  terser: "4.4.0"
+  terser: "4.6.6"
 });
 
 Package.onUse(function (api) {
