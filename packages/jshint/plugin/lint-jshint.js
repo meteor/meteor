d34f20192424fdb25f2013f74930e4aa4f7aa2c2
Brian Mulhall <brian.e.mulhall@gmail.com>
2020-03-16 20:08:43 -0500

    updated the syntax used in the jshint package to use const/let and not var and to use the class syntax for class definitions

--- packages/jshint/plugin/lint-jshint.js
+++ packages/jshint/plugin/lint-jshint.js
@@ -1,116 +1,139 @@
-/* globals Npm, Plugin */
-
 var jshint = Npm.require('jshint').JSHINT;
 
 Plugin.registerLinter({
   extensions: ["js"],
   filenames: [".jshintrc"]
-}, function () {
-  var linter = new JsHintLinter();
-  return linter;
-});
+},
+  () => new JsHintLinter()
+);
 
-function JsHintLinter () {
-  // packageName -> { config (json),
-  //                  files: { [pathInPackage,arch] -> { hash, errors }}}
-  this._cacheByPackage = {};
-}
-
-var DEFAULT_CONFIG = JSON.stringify({
+const DEFAULT_CONFIG = JSON.stringify({
   undef: true,
   unused: true,
   node: true,
-  browser: true
+  browser: true,
 });
 
-JsHintLinter.prototype.processFilesForPackage = function (files, options) {
-  var self = this;
-  var globals = options.globals;
+class JsHintLinter {
 
-  // Assumes that this method gets called once per package.
-  var packageName = files[0].getPackageName();
-  if (! self._cacheByPackage.hasOwnProperty(packageName)) {
-    self._cacheByPackage[packageName] = {
-      configString: DEFAULT_CONFIG,
-      files: {}
-    };
+  constructor() {
+    // the cache has package names for keys and values that contain the configuration
+    // for the application or package you are processing
+    // packageName -> { config (json),
+    //                  files: { [pathInPackage,arch] -> { hash, errors }}}
+    this._cacheByPackage = {};
   }
-  var cache = self._cacheByPackage[packageName];
 
-  var configs = files.filter(function (file) {
-    return file.getBasename() === '.jshintrc';
-  });
-  if (configs.length > 1) {
-    configs[0].error({
-      message: "Found multiple .jshintrc files in package " + packageName +
-        ": " +
-        configs.map(function (c) { return c.getPathInPackage(); }).join(', ')
+  // this method gets called once per package and once for the app itself
+  processFilesForPackage(files, options) {
+
+    const globals = options.globals;
+
+    const packageName = files[0].getPackageName();
+
+    // populate the cache with an entry for this package
+    // if one doesn't already exist
+    if (!this._cacheByPackage.hasOwnProperty(packageName)) {
+      this._cacheByPackage[packageName] = {
+        configString: DEFAULT_CONFIG,
+        files: {}
+      };
+    }
+
+    // then access that cache entry
+    const cache = this._cacheByPackage[packageName];
+
+    // get the package config file
+    const configs = files.filter(function (file) {
+      return file.getBasename() === '.jshintrc';
     });
-    return;
-  }
 
-  if (configs.length) {
-    var newConfigString = configs[0].getContentsAsString();
-    if (cache.configString !== newConfigString) {
-      // Reset cache.
-      cache.files = {};
-      cache.configString = newConfigString;
-    }
-  } else {
-    if (cache.configString !== DEFAULT_CONFIG) {
-      // Reset cache.
-      cache.files = {};
-      cache.configString = DEFAULT_CONFIG;
-    }
-  }
-
-  try {
-    var config = JSON.parse(cache.configString);
-  } catch (err) {
-    // This should really not happen for DEFAULT_CONFIG :)
-    configs[0].error({
-      message: "Failed to parse " + configs[0].getPathInPackage() +
-        ": not valid JSON: " + err.message
-    });
-    return;
-  }
-
-  // JSHint has a particular format for defining globals. `false` means that the
-  // global is not allowed to be redefined. `true` means it is allowed to be
-  // redefined. Since the passed imports are probably not great for definition,
-  // mark them as false.
-  var predefinedGlobals = {};
-  globals.forEach(function (symbol) {
-    predefinedGlobals[symbol] = false;
-  });
-
-  files.forEach(function (file) {
-    if (file.getBasename() === '.jshintrc')
-      return;
-
-    // skip files we already linted
-    var cacheKey = JSON.stringify([file.getPathInPackage(), file.getArch()]);
-    if (cache.files.hasOwnProperty(cacheKey) &&
-        cache.files[cacheKey].hash === file.getSourceHash()) {
-      reportErrors(file, cache.files[cacheKey].errors);
-      return;
-    }
-
-    var errors = [];
-    if (! jshint(file.getContentsAsString(), config, predefinedGlobals)) {
-      errors = jshint.errors;
-      reportErrors(file, errors);
-    }
-    cache.files[cacheKey] = { hash: file.getSourceHash(), errors: errors };
-  });
-
-  function reportErrors(file, errors) {
-    errors.forEach(function (error) {
-      file.error({
-        message: error.reason,
-        line: error.line,
-        column: error.character
+    // check for multiple config files, which is an error condition
+    if (configs.length > 1) {
+      configs[0].error({
+        message: "Found multiple .jshintrc files in package " + packageName +
+          ": " +
+          configs.map(function (c) { return c.getPathInPackage(); }).join(', ')
       });
+      return;
+    }
+
+    if (configs.length) {
+      const newConfigString = configs[0].getContentsAsString();
+      if (cache.configString !== newConfigString) {
+        // Reset cache.
+        cache.files = {};
+        cache.configString = newConfigString;
+      }
+    } else {
+      if (cache.configString !== DEFAULT_CONFIG) {
+        // Reset cache.
+        cache.files = {};
+        cache.configString = DEFAULT_CONFIG;
+      }
+    }
+
+    // attempt to parse the read in configuration file
+    let config;
+    try {
+      config = JSON.parse(cache.configString);
+    } catch (err) {
+      // This should really not happen for DEFAULT_CONFIG :)
+      configs[0].error({
+        message: "Failed to parse " + configs[0].getPathInPackage() +
+          ": not valid JSON: " + err.message
+      });
+      return;
+    }
+
+    // JSHint has a particular format for defining globals. `false` means that the
+    // global is not allowed to be redefined. `true` means it is allowed to be
+    // redefined. So we mark them false so they are read-only.
+    const predefinedGlobals = {};
+    globals.forEach(function (symbol) {
+      predefinedGlobals[symbol] = false;
     });
+
+    // this is the loop where we process the files in the app or package
+    files.forEach(function (file) {
+
+      // skip the config file
+      if (file.getBasename() === '.jshintrc') return;
+
+      const cacheKey = JSON.stringify([file.getPathInPackage(), file.getArch()]);
+
+      // if the cache has an entry with that key and its hash is the same as the
+      // hash of the file (meaning the file hasn't changed at all since that last time
+      // the linter ran)
+      if (cache.files.hasOwnProperty(cacheKey) &&
+        cache.files[cacheKey].hash === file.getSourceHash()) {
+        // since the file hasn't changed we can report the errors found the last time
+        // the plugin ran
+        reportErrors(file, cache.files[cacheKey].errors);
+        return;
+      }
+
+      // this is the case where we need to process the file and report newly found errors
+      let errors = [];
+    
+      // this is where we actually use jshint
+      if (!jshint(file.getContentsAsString(), config, predefinedGlobals)) {
+        errors = jshint.errors;        
+        reportErrors(file, errors);
+      }
+
+      // add the newly processed file into the cache
+      cache.files[cacheKey] = { hash: file.getSourceHash(), errors: errors };
+    });
+
+    function reportErrors(file, errors) {
+      errors.forEach(function (error) {        
+        file.error({
+          message: error.reason,
+          line: error.line,
+          column: error.character,
+        });
+      });
+    }
   }
-};
+}
