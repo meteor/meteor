106062b2114fe6d56db7fc84345796d29ddb23ec
Joe Pea <joe@trusktr.io>
2020-04-15 22:59:43 -0700

    test Base64 the Meteor way while importing it from an NPM package

--- packages/base64/package.js
+++ packages/base64/package.js
@@ -1,19 +1,19 @@
 Package.describe({
   summary: "Base64 encoding and decoding",
-  version: '1.0.12',
+  version: "1.0.12",
 });
 
 Npm.depends({
-  '@trusktr/meteor-base64': '1.0.12',
+  "@trusktr/meteor-base64": "1.0.12",
 });
 
-Package.onUse(api => {
-  api.export('Base64');
-  api.use('ecmascript');
-  api.mainModule('base64.js');
+Package.onUse((api) => {
+  api.export("Base64");
+  api.use("ecmascript");
+  api.mainModule("base64.js");
 });
 
-Package.onTest(api => {
-  api.use(['ecmascript', 'tinytest', 'ejson']);
-  api.addFiles('base64_test.js', ['client', 'server']);
+Package.onTest((api) => {
+  api.use(["typescript", "tinytest", "ejson"]);
+  api.addFiles("base64_test.ts", ["client", "server"]);
 });
