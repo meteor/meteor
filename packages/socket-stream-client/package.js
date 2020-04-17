2f331252656a50ab408368568fed9804ea9d0176
filipenevola <filipenevola@gmail.com>
2020-04-17 13:46:07 -0400

    Bump socket-stream-client package version to 0.3.0

--- packages/socket-stream-client/package.js
+++ packages/socket-stream-client/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "socket-stream-client",
-  version: "0.2.3",
+  version: "0.3.0-beta1102.1",
   summary: "Provides the ClientStream abstraction used by ddp-client",
   documentation: "README.md"
 });
