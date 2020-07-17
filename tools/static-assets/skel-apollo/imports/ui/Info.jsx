e2dfd420160590a31c1998c6de090c9f80564174
Jan Dvorak <storyteller@freedombase.net>
2020-07-17 23:44:06 +0900

    Use Apollo Client v3
    Co-authored-by: Filipe Névola <filipe@quave.com.br>

--- tools/static-assets/skel-apollo/imports/ui/Info.jsx
+++ tools/static-assets/skel-apollo/imports/ui/Info.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/client';
 import { gql } from 'apollo-boost';
 
 const GET_LINKS = gql`
