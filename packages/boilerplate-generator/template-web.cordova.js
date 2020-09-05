18a30c267ad2e560c057be1ec5d3317c4c31541e
Bart Sturm <bart@tutormundi.com>
2020-09-05 17:05:05 -0300

    Allow android-webview-video-poster (fixes #8965)

--- packages/boilerplate-generator/template-web.cordova.js
+++ packages/boilerplate-generator/template-web.cordova.js
@@ -29,7 +29,7 @@ export const headTemplate = ({
     '  <meta name="format-detection" content="telephone=no">',
     '  <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, viewport-fit=cover">',
     '  <meta name="msapplication-tap-highlight" content="no">',
-    '  <meta http-equiv="Content-Security-Policy" content="default-src * gap: data: blob: \'unsafe-inline\' \'unsafe-eval\' ws: wss:;">',
+    '  <meta http-equiv="Content-Security-Policy" content="default-src * android-webview-video-poster: gap: data: blob: \'unsafe-inline\' \'unsafe-eval\' ws: wss:;">',
 
   (headSections.length === 1)
     ? [cssBundle, headSections[0]].join('\n')
