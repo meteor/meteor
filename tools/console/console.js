ae97171daf61975e4e21da861f073332907434c4
Jan Dvorak <storyteller@freedombase.net>
2020-09-06 17:07:59 +0200

    Start work on deprecation flag for packages

--- tools/console/console.js
+++ tools/console/console.js
@@ -1069,7 +1069,7 @@ class Console extends ConsoleBase {
   //        level with Console.LEVEL_INFO, Console.LEVEL_ERROR, etc.
   //      - ignoreWidth: ignore the width of the terminal, and go over the
   //        character limit instead of trailing off with '...'. Useful for
-  //        printing directories, for examle.
+  //        printing directories, for example.
   //      - indent: indent the entire table by a given number of spaces.
   printTwoColumns(rows, options) {
     options = options || Object.create(null);
