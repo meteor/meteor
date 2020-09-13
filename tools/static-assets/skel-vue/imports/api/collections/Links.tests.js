230662859884b3d517e72a6ce46a5f7113ed7955
Chris Visser <2496739+chris-visser@users.noreply.github.com>
2020-09-13 20:48:32 +0200

    Fixes #11153 to proper vue skel folder structure

--- tools/static-assets/skel-vue/imports/api/collections/Links.tests.js
+++ tools/static-assets/skel-vue/imports/api/collections/Links.tests.js
@@ -4,7 +4,7 @@
 
 import { Meteor } from 'meteor/meteor';
 import { assert } from 'chai';
-import Links from './links.js';
+import Links from './Links.js';
 
 if (Meteor.isServer) {
   describe('links collection', function () {
