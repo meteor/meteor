a27470bff373fa99b193aed20e240a354d31390f
filipenevola <filipenevola@gmail.com>
2020-09-30 15:17:48 -0400

    Allows developers to override the ROOT_URL in the redirect URL

--- packages/facebook-oauth/package.js
+++ packages/facebook-oauth/package.js
@@ -1,9 +1,11 @@
 Package.describe({
   summary: "Facebook OAuth flow",
-  version: "1.7.1"
+  version: "1.7.2"
 });
 
 Package.onUse(api => {
+  api.versionsFrom('1.11.1');
+
   api.use('ecmascript', ['client', 'server']);
   api.use('oauth2', ['client', 'server']);
   api.use('oauth', ['client', 'server']);
