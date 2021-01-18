0b3dd593eef0f29e0c2b0ac643a54bfb446ffb20
filipenevola <filipenevola@gmail.com>
2021-01-18 17:30:55 -0400

    Bumps versions to Meteor 2.0-rc.2

--- packages/modules-runtime-hot/package.js
+++ packages/modules-runtime-hot/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "modules-runtime-hot",
-  version: "0.13.0-rc200.1",
+  version: "0.13.0-rc200.2",
   summary: "Patches modules-runtime to support Hot Module Replacement",
   git: "https://github.com/benjamn/install",
   documentation: "README.md"
