8b9296e2de22b3195b4f2fa37cf5d7c222d0fd06
filipenevola <filipenevola@gmail.com>
2021-02-23 20:14:21 -0400

    bump to Node 14.16.0

--- scripts/dev-bundle-tool-package.js
+++ scripts/dev-bundle-tool-package.js
@@ -10,7 +10,7 @@ var packageJson = {
   dependencies: {
     // Explicit dependency because we are replacing it with a bundled version
     // and we want to make sure there are no dependencies on a higher version
-    npm: "6.14.8",
+    npm: "6.14.11",
     pacote: "https://github.com/meteor/pacote/tarball/a81b0324686e85d22c7688c47629d4009000e8b8",
     "node-gyp": "6.1.0",
     "node-pre-gyp": "0.15.0",
