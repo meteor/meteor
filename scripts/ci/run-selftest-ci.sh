a57ea935f46705849556ed788d91fc27ee301c31
filipenevola <filipenevola@gmail.com>
2021-02-24 08:49:15 -0400

    Create a script to run selftest in the new CI

---
+++ scripts/ci/run-selftest-ci.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+ulimit -c unlimited; # Set core dump size as Ubuntu 14.04 lacks prlimit.
+ulimit -a # Display all ulimit settings for transparency.
+
+pushd tools
+# Ensure that meteor/tools has no TypeScript errors.
+../meteor npx tsc --noEmit
+popd
+./meteor --get-ready
