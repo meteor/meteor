8a6a75290b497389eb3b020c451e363f7557d181
filipenevola <filipenevola@gmail.com>
2021-03-30 10:44:12 -0400

    Browserstack tests should run only on browserstack-check branch These tests are flaky and also we are going to run them only before releases in a specific internal server

--- scripts/admin/browserstack-check.sh
+++ scripts/admin/browserstack-check.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+# We need to set up properly Puppeteer and BrowserStack dependencies in the
+# machine first.
+#
+# We also need to setup s3cmd and its config to be able to read the file with
+# Browserstack key from S3. Only Meteor Software employees have access to this
+# credentials.
+#
+# This script is executed in our internal machine called Jenkins V3 before
+# at least every official release to be sure these tests listed below are
+# passing.
+
 cd ../..
 
 ./meteor self-test \
