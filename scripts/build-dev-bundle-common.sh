6467b0a4457628138f8b1a41e750e16dd9cd6439
Jan Dvorak <storyteller@freedombase.net>
2021-04-06 19:22:30 +0200

    Update npm version to fix security vulnerability

--- scripts/build-dev-bundle-common.sh
+++ scripts/build-dev-bundle-common.sh
@@ -8,7 +8,7 @@ ARCH=$(uname -m)
 NODE_VERSION=14.16.0
 MONGO_VERSION_64BIT=4.2.8
 MONGO_VERSION_32BIT=3.2.22
-NPM_VERSION=6.14.11
+NPM_VERSION=6.14.12
 
 # If we built Node from source on Jenkins, this is the build number.
 NODE_BUILD_NUMBER=
