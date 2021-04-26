6038bd5c591b80c13e264b2d180fd1adb0e65e0f
zodern <zodern@users.noreply.github.com>
2021-04-26 13:57:12 -0500

    Add cache for reify runtime compiler

--- tools/isobuild/bundler.js
+++ tools/isobuild/bundler.js
@@ -212,7 +212,7 @@ exports._mainJsContents = [
   "",
   "process.argv.splice(2, 0, 'program.json');",
   "process.chdir(require('path').join(__dirname, 'programs', 'server'));",
-  'require("./programs/server/runtime.js");',
+  'require("./programs/server/runtime.js")({ cachePath: process.env.METEOR_REIFY_CACHE_DIR });',
   "require('./programs/server/boot.js');",
 ].join("\n");
 
