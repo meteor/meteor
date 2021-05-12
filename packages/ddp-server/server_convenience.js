a12be38a13573605bc10d2a2e08fa5af2ffda68d
Michael Vogt <mtpfeif@gmail.com>
2021-05-12 13:12:51 -0500

    Update diff-sequence@1.1.2 | Convert DiffSequence.makeChangedFields to utilize Map instead of Object

--- packages/ddp-server/server_convenience.js
+++ packages/ddp-server/server_convenience.js
@@ -1,11 +1,9 @@
-import ServerClass from "./server";
-
 if (process.env.DDP_DEFAULT_CONNECTION_URL) {
   __meteor_runtime_config__.DDP_DEFAULT_CONNECTION_URL =
     process.env.DDP_DEFAULT_CONNECTION_URL;
 }
 
-Meteor.server = new ServerClass();
+Meteor.server = new Server;
 
 Meteor.refresh = function (notification) {
   DDPServer._InvalidationCrossbar.fire(notification);
@@ -13,9 +11,9 @@ Meteor.refresh = function (notification) {
 
 // Proxy the public methods of Meteor.server so they can
 // be called directly on Meteor.
-['publish', 'methods', 'call', 'apply', 'onConnection', 'onMessage'].forEach(
+_.each(['publish', 'methods', 'call', 'apply', 'onConnection', 'onMessage'],
        function (name) {
-         Meteor[name] = () => Meteor.server[name]
+         Meteor[name] = _.bind(Meteor.server[name], Meteor.server);
        });
 
 // Meteor.server used to be called Meteor.default_server. Provide
