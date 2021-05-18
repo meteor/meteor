9bf7e4eaf4637961d89f1da4a343c740cfc384a4
Michael Vogt <mtpfeif@gmail.com>
2021-05-17 22:21:30 -0500

    testing bug

--- packages/id-map/id-map.js
+++ packages/id-map/id-map.js
@@ -11,22 +11,22 @@ export class IdMap {
 // set of methods that overlap between the two.)
 
   get(id) {
-    const key = this._idStringify(id);
+    var key = this._idStringify(id);
     return this._map.get(key);
   }
 
   set(id, value) {
-    const key = this._idStringify(id);
+    var key = this._idStringify(id);
     this._map.set(key, value);
   }
 
   remove(id) {
-    const key = this._idStringify(id);
+    var key = this._idStringify(id);
     this._map.delete(key);
   }
 
   has(id) {
-    const key = this._idStringify(id);
+    var key = this._idStringify(id);
     return this._map.has(key);
   }
 
@@ -41,8 +41,8 @@ export class IdMap {
   // Iterates over the items in the map. Return `false` to break the loop.
   forEach(iterator) {
     // don't use _.each, because we can't break out of it.
-    for (let [key, value] of this._map){
-      const breakIfFalse = iterator.call(
+    for (var [key, value] of this._map){
+      var breakIfFalse = iterator.call(
         null,
         value,
         this._idParse(key)
@@ -58,7 +58,7 @@ export class IdMap {
   }
 
   setDefault(id, def) {
-    const key = this._idStringify(id);
+    var key = this._idStringify(id);
     if (this._map.has(key)) {
       return this._map.get(key);
     }
@@ -69,7 +69,7 @@ export class IdMap {
   // Assumes that values are EJSON-cloneable, and that we don't need to clone
   // IDs (ie, that nobody is going to mutate an ObjectId).
   clone() {
-    const clone = new IdMap(this._idStringify, this._idParse);
+    var clone = new IdMap(this._idStringify, this._idParse);
     // copy directly to avoid stringify/parse overhead
     this._map.forEach(function(value, key){
       clone._map.set(key, EJSON.clone(value));
