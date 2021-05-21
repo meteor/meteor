cbd654e2b0532c26e8fc4d9b2af784817da65cd0
Jan Dvorak <storyteller@freedombase.net>
2021-05-21 15:55:28 +0200

    beta230.1 -> beta230.0

--- packages/minimongo/package.js
+++ packages/minimongo/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Meteor's client-side datastore: a port of MongoDB to Javascript",
-  version: '1.7.0-beta230.1'
+  version: '1.7.0-beta230.0'
 });
 
 Package.onUse(api => {
--- packages/oauth/package.js
+++ packages/oauth/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Common code for OAuth-based services",
-  version: "2.0.0-beta230.1"
+  version: "2.0.0-beta230.0"
 });
 
 Package.onUse(api => {
