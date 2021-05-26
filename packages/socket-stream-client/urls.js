ae1de6d4be93f30e7272968b242e2b6922394602
Jan Dvorak <storyteller@freedombase.net>
2021-05-26 21:24:27 +0200

    Add Meteor import in socket-stream-client/urls

--- packages/socket-stream-client/urls.js
+++ packages/socket-stream-client/urls.js
@@ -1,3 +1,5 @@
+import { Meteor } from "meteor/meteor";
+
 // @param url {String} URL to Meteor app, eg:
 //   "/" or "madewith.meteor.com" or "https://foo.meteor.com"
 //   or "ddp+sockjs://ddp--****-foo.meteor.com/sockjs"
