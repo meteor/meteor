97c8e7ddb154303e7e7e376c58a1cf6577889ad7
Jan Dvorak <storyteller@freedombase.net>
2021-06-14 19:57:16 +0200

    Published observe-sequence@1.0.17 && 1.0.18

--- packages/observe-sequence/package.js
+++ packages/observe-sequence/package.js
@@ -1,14 +1,14 @@
 Package.describe({
   summary: "Observe changes to various sequence types such as arrays, cursors and objects",
-  version: "1.0.16"
+  version: "1.0.18"
 });
 
 Package.onUse(function (api) {
-  api.use('tracker@1.1.0');
-  api.use('mongo-id@1.0.5');  // for idStringify
-  api.use('diff-sequence@1.0.6');
-  api.use('underscore@1.0.9');
-  api.use('random@1.0.10');
+  api.use('tracker@1.2.0');
+  api.use('mongo-id@1.0.8');  // for idStringify
+  api.use('diff-sequence@1.1.1');
+  api.use('underscore@1.0.10');
+  api.use('random@1.2.0');
   api.export('ObserveSequence');
   api.addFiles(['observe_sequence.js']);
 });
