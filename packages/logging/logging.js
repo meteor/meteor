0d0709c126fba688e35792f0fd9899a4c7850e6e
Jan Dvorak <storyteller@freedombase.net>
2021-07-20 15:54:09 +0200

    Move relevant code from Meteor._debug to logging package

--- packages/logging/logging.js
+++ packages/logging/logging.js
@@ -80,10 +80,18 @@ const logInBrowser = obj => {
   if ((typeof console !== 'undefined') && console[level]) {
     console[level](str);
   } else {
-    // XXX Uses of Meteor._debug should probably be replaced by Log.debug or
-    //     Log.info, and we should have another name for "do your best to
-    //     call call console.log".
-    Meteor._debug(str);
+    // IE doesn't have console.log.apply, it's not a real Object.
+    // http://stackoverflow.com/questions/5538972/console-log-apply-not-working-in-ie9
+    // http://patik.com/blog/complete-cross-browser-console-log/
+    if (typeof console.log.apply === "function") {
+      // Most browsers
+      console.log.apply(console, [str]);
+
+    } else if (typeof Function.prototype.bind === "function") {
+      // IE9
+      const log = Function.prototype.bind.call(console.log, console);
+      log.apply(console, [str]);
+    }
   }
 };
 
