95fc1115579d7cd7fd10fcc174fc198e609edcc9
Jan Dvorak <storyteller@freedombase.net>
2021-07-29 10:15:31 +0200

    Add @since to customTransport

--- packages/email/email.js
+++ packages/email/email.js
@@ -167,6 +167,7 @@ Email.hookSend = function (f) {
 /**
  * @summary Overrides sending function with your own.
  * @locus Server
+ * @since 2.2
  * @param f {function} function that will receive options from the send function and under `settings` will
  * include the package settings from Meteor.settings.packages.email for your custom transport to access.
  */
