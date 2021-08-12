c6abe1f4559d70bf211d8a2b00f2a0fd5802ab1c
Jan Dvorak <storyteller@freedombase.net>
2021-08-12 10:13:28 +0200

    2.2.3-rc.2

--- packages/typescript/package.js
+++ packages/typescript/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "typescript",
-  version: "4.3.5-rc223.1",
+  version: "4.3.5-rc223.2",
   summary: "Compiler plugin that compiles TypeScript and ECMAScript in .ts and .tsx files",
   documentation: "README.md"
 });
