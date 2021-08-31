3e8547092519637ba4e8cf366f4ef78a8dd90429
Jan Dvorak <jan@meteor.com>
2021-08-31 15:45:25 +0200

    1.0.3
    Improved locking & drop use of removed API.

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   summary: 'Define and run db migrations.',
-  version: '1.0.2',
+  version: '1.0.3',
   name: 'percolate:migrations',
   git: 'https://github.com/percolatestudio/meteor-migrations.git',
 });
 
-Package.on_use(function(api) {
+Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.5');
   api.use('ecmascript');
   api.use(['underscore', 'check', 'mongo', 'logging'], 'server');
@@ -13,7 +13,7 @@ Package.on_use(function(api) {
   api.export('Migrations', 'server');
 });
 
-Package.on_test(function(api) {
+Package.onTest(function(api) {
   api.use('ecmascript');
   api.use(['percolate:migrations', 'tinytest']);
   api.addFiles('migrations_tests.js', ['server']);
