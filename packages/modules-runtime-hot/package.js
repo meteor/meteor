1cb8deb9f30be22085bf575705d6ef57d7668bd5
Jan Dvorak <storyteller@freedombase.net>
2021-10-01 17:35:48 +0200

    Meteor 2.5-beta.1

--- packages/modules-runtime-hot/package.js
+++ packages/modules-runtime-hot/package.js
@@ -1,24 +1,22 @@
 Package.describe({
-  name: "modules-runtime-hot",
-  version: "0.14.0-beta250.0",
-  summary: "Patches modules-runtime to support Hot Module Replacement",
-  git: "https://github.com/benjamn/install",
-  documentation: "README.md"
+  name: 'modules-runtime-hot',
+  version: '0.14.0-beta250.1',
+  summary: 'Patches modules-runtime to support Hot Module Replacement',
+  git: 'https://github.com/benjamn/install',
+  documentation: 'README.md',
 });
 
-Package.onUse(function (api) {
-  api.addFiles("installer.js", [
-    "client",
-  ], {
-    bare: true
+Package.onUse(function(api) {
+  api.addFiles('installer.js', ['client'], {
+    bare: true,
   });
 
-  api.addFiles("modern.js", "modern");
-  api.addFiles("legacy.js", "legacy");
-  api.export("meteorInstall", "client");
+  api.addFiles('modern.js', 'modern');
+  api.addFiles('legacy.js', 'legacy');
+  api.export('meteorInstall', 'client');
 });
 
-Package.onTest(function (api) {
-  api.use("tinytest");
-  api.use("modules"); // Test modules-runtime via modules.
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('modules'); // Test modules-runtime via modules.
 });
