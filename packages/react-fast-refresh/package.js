3a89cd28ad6fd18a045cae62a3d711979870b481
Filipe Névola <filipe@quave.com.br>
2021-12-21 17:16:24 -0400

    Updates History.md

--- packages/react-fast-refresh/package.js
+++ packages/react-fast-refresh/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'react-fast-refresh',
-  version: '0.2.1',
+  version: '0.2.2',
   summary: 'Automatically update React components with HMR',
   documentation: 'README.md',
   devOnly: true,
