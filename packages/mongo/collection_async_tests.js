222110f2b749296981a2ea1504040e70d1e47a4f
Filipe Névola <filipe@quave.com.br>
2022-01-03 15:46:23 -0400

    Starting to add new tests for Mongo without fiber

--- packages/mongo/collection_async_tests.js
+++ packages/mongo/collection_async_tests.js
@@ -1,44 +1,34 @@
 Tinytest.only(
   'async collection - create Mongo.Collection and check the name',
-  function (test) {
+  function(test) {
     const collection = Mongo.Collection.create('myAsyncCollection');
-    test.equal(
-      collection._name,
-      'myAsyncCollection'
-    )
+    test.equal(collection._name, 'myAsyncCollection');
   }
 );
 
 Tinytest.only(
   'async collection - create sync Mongo.Collection and try to use async insert',
-  function (test) {
+  function(test) {
     const collection = new Mongo.Collection('myAsyncCollection');
-
 
     test.throws(
       collection.insertAsync({
-        name: 'test'
-      }),
-    )
+        name: 'test',
+      })
+    );
   }
 );
 
+Tinytest.only('async collection - reusing instances when we have the same name', function(
+  test
+) {
+  const collection = new Mongo.Collection('myCollection');
+  const collAsync = Mongo.Collection.create('myCollection');
+  test.equal(collection._name, 'myCollection');
+  test.equal(collAsync._name, 'myCollection');
+  test.equal(new Mongo.Collection('myCollection2')._name, 'myCollection2');
+  test.equal(new Mongo.Collection('myCollection2')._name, 'myCollection2');
 
-Tinytest.only(
-  'async collection - create collections with same name',
-  function (test) {
-    const collection = new Mongo.Collection('myCollection');
-    const collAsync = Mongo.Collection.create('myCollection');
-    test.ok(
-      collection
-    )
-    test.ok(
-      collAsync
-    )
-    new Mongo.Collection('myCollection2');
-    new Mongo.Collection('myCollection2');
-
-    Mongo.Collection.create('myCollection3');
-    Mongo.Collection.create('myCollection3');
-  }
-);
+  test.equal(Mongo.Collection.create('myCollection3')._name, 'myCollection3');
+  test.equal(Mongo.Collection.create('myCollection3')._name, 'myCollection3');
+});
