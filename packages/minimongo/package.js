c4d4ea2a09b0c2697ae4e817a2b160e2bd547e5e
Renan Castro <renanccastro@gmail.com>
2022-01-07 13:40:27 -0300

    Improve primary detection mechanism to be compatible with older mongodb instances Generate 2.6 beta 2

--- packages/minimongo/package.js
+++ packages/minimongo/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Meteor's client-side datastore: a port of MongoDB to Javascript",
-  version: '1.8.0-beta260.1'
+  version: '1.8.0-beta260.2'
 });
 
 Package.onUse(api => {
