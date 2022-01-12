f3c38b8cb0c4e1a69a787667913a06cb0862f8ec
Renan Castro <renanccastro@gmail.com>
2022-01-12 15:18:40 -0300

    New beta: 2.6-beta.3

--- packages/minimongo/package.js
+++ packages/minimongo/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Meteor's client-side datastore: a port of MongoDB to Javascript",
-  version: '1.8.0-beta260.2'
+  version: '1.8.0-beta260.3'
 });
 
 Package.onUse(api => {
