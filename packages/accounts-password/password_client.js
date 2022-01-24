bedc94719520376fb545852c24943f5b5355ed6d
denihs <denilsonh2014@gmail.com>
2022-01-24 14:51:47 -0400

    Changes requested on code review
    - loginWithPasswordAnd2faCode on the right package

--- packages/accounts-password/password_client.js
+++ packages/accounts-password/password_client.js
@@ -8,7 +8,7 @@ const reportError = (error, callback) => {
 };
 
 
-const internalLoginWithPassword = ({ selector, password, code, callback }) => {
+Accounts._internalLoginWithPassword = ({ selector, password, code, callback }) => {
   if (typeof selector === 'string')
     if (!selector.includes('@')) selector = { username: selector };
     else selector = { email: selector };
@@ -56,32 +56,7 @@ const internalLoginWithPassword = ({ selector, password, code, callback }) => {
  * @importFromPackage meteor
  */
 Meteor.loginWithPassword = (selector, password, callback) => {
-  return internalLoginWithPassword({ selector, password, callback });
-};
-
-/**
- * @summary Log the user in with a password and token.
- * @locus Client
- * @param {Object | String} selector
- *   Either a string interpreted as a username or an email; or an object with a
- *   single key: `email`, `username` or `id`. Username or email match in a case
- *   insensitive manner.
- * @param {String} password The user's password.
- * @param {String} token Token provide by the user's authenticator app.
- * @param {Function} [callback] Optional callback.
- *   Called with no arguments on success, or with a single `Error` argument
- *   on failure.
- * @importFromPackage meteor
- */
-
-Meteor.loginWithPasswordAnd2faCode = (selector, password, code, callback) => {
-  if (code == null || typeof code !== 'string' || !code) {
-    throw new Meteor.Error(
-      400,
-      'token is required to use loginWithPasswordAnd2faCode and must be a string'
-    );
-  }
-  return internalLoginWithPassword({ selector, password, code, callback });
+  return Accounts._internalLoginWithPassword({ selector, password, callback });
 };
 
 Accounts._hashPassword = password => ({
