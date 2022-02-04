832efd1baadf19d0e1015e70a956ed24b4b1fbce
Renan Castro <renanccastro@gmail.com>
2022-02-04 08:55:21 -0300

    Fix comment phrasing on oplog converter

--- packages/mongo/oplog_v2_converter_tests.js
+++ packages/mongo/oplog_v2_converter_tests.js
@@ -53,7 +53,7 @@ Tinytest.add('oplog - v2/v1 conversion', function(test) {
   //set a new nested field inside an object
   const entry51 =  { "$v" : 2, "diff" : { "u" : { "count" : 1 }, "i" : { "nested" : { "state" : {  } } } } };
   // the correct format for this test, inspecting the mongodb oplog, should be "nested" : { "state" : {  } } }
-  // but this is not a case in which we cant flatten the object without collateral, so we are considering
+  // but this is a case in which we can flatten the object without collateral, so we are considering
   // "nested.state" : {  } to be valid too
   test.equal(
     JSON.stringify(oplogV2V1Converter(entry51)),
