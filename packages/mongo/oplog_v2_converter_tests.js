b23c22de234f2c9997b3b02bd819a964beb742cf
Renan Castro <renanccastro@gmail.com>
2022-02-04 08:07:31 -0300

    Fix flatten object issue when the object is empty on oplog converter - fix #11884

--- packages/mongo/oplog_v2_converter_tests.js
+++ packages/mongo/oplog_v2_converter_tests.js
@@ -1,6 +1,6 @@
 import { oplogV2V1Converter } from './oplog_v2_converter';
 
-Tinytest.add('oplog - v2/v1 conversion', function(test) {
+Tinytest.only('oplog - v2/v1 conversion', function(test) {
   const entry1 = {
     $v: 2,
     diff: { scustom: { sEJSON$value: { u: { EJSONtail: 'd' } } } },
@@ -48,6 +48,16 @@ Tinytest.add('oplog - v2/v1 conversion', function(test) {
   test.equal(
     JSON.stringify(oplogV2V1Converter(entry5)),
     JSON.stringify({ $v: 2, $set: { 'a.b': 2 } })
+  );
+
+  //set a new nested field inside an object
+  const entry51 =  { "$v" : 2, "diff" : { "u" : { "count" : 1 }, "i" : { "nested" : { "state" : {  } } } } };
+  // the correct format for this test, inspecting the mongodb oplog, should be "nested" : { "state" : {  } } }
+  // but this is not a case in which we cant flatten the object without collateral, so we are considering
+  // "nested.state" : {  } to be valid too
+  test.equal(
+    JSON.stringify(oplogV2V1Converter(entry51)),
+    JSON.stringify( { "$v" : 2, "$set" : { "nested.state": {  }, "count" : 1 } })
   );
 
   //set an existing nested field inside an object
