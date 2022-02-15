5893114701a9b6a7c530b4c9b6a0b473f9df75bf
zodern <zodern@users.noreply.github.com>
2022-02-15 15:48:59 -0600

    Don't log to restart when outside of meteor tool

--- packages/hot-module-replacement/server.js
+++ packages/hot-module-replacement/server.js
@@ -1,5 +1,8 @@
-if (!process.env.METEOR_HMR_SECRET) {
-  console.log('Restart Meteor to enable hot module replacement.');
-} else {
+// METEOR_PARENT_PID 
+if (process.env.METEOR_HMR_SECRET) {
   __meteor_runtime_config__._hmrSecret = process.env.METEOR_HMR_SECRET;
+} else if (process.env.METEOR_PARENT_PID) {
+  // if METEOR_PARENT_PID isn't set, then the app isn't being run by the meteor
+  // tool and restarting won't enable HRM.
+  console.log('Restart Meteor to enable hot module replacement.');
 }
