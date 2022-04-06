19c25d586864208fa3b4a7d4e6a7ce8069b3a5f7
denihs <denilsonh2014@gmail.com>
2022-04-06 11:59:59 -0400

    Removing custom css

--- tools/static-assets/skel-tailwind/imports/ui/App.jsx
+++ tools/static-assets/skel-tailwind/imports/ui/App.jsx
@@ -3,7 +3,7 @@ import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
 
 export const App = () => (
-  <div>
+  <div className="p-2.5">
     <h1 className="text-3xl">Welcome to Meteor!</h1>
     <Hello/>
     <Info/>
