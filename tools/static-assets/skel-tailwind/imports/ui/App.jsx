4b21cfff2c471b1711a06e3e725223b9393ca1b8
denihs <denilsonh2014@gmail.com>
2022-04-07 14:59:18 -0400

    Stylishing the skeleton with tailwind

--- tools/static-assets/skel-tailwind/imports/ui/App.jsx
+++ tools/static-assets/skel-tailwind/imports/ui/App.jsx
@@ -3,8 +3,7 @@ import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
 
 export const App = () => (
-  <div className="p-2.5">
-    <h1 className="text-3xl">Welcome to Meteor!</h1>
+  <div className="max-w-3xl min-h-screen mx-auto sm:pt-10">
     <Hello/>
     <Info/>
   </div>
