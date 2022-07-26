fc727d52ce6d5eb897b49f7502dfd6629c72b312
denihs <denilsonh2014@gmail.com>
2022-07-25 20:01:34 -0400

    - bumping versions

--- packages/babel-compiler/package.js
+++ packages/babel-compiler/package.js
@@ -1,15 +1,16 @@
 Package.describe({
   name: "babel-compiler",
   summary: "Parser/transpiler for ECMAScript 2015+ syntax",
-  version: '7.9.0'
+  version: '7.9.1'
 });
 
 Npm.depends({
-  '@meteorjs/babel': '7.16.0-beta.1',
+  '@meteorjs/babel': '7.16.0-beta.4',
   'json5': '2.1.1'
 });
 
 Package.onUse(function (api) {
+  api.versionsFrom("2.7.4-beta.2")
   api.use('ecmascript-runtime', 'server');
   api.use('modern-browsers');
 
