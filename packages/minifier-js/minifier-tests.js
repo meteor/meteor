4bd7c8af0e7de69ce0260e5942aace73065a7bb5
eduwr <eduwric@gmail.com>
2022-07-27 22:55:24 -0300

    Test meteorJsMinifyAsync method

--- packages/minifier-js/minifier-tests.js
+++ packages/minifier-js/minifier-tests.js
@@ -27,3 +27,39 @@ Tinytest.add('minifier-js - verify tersers error object has the fields we use fo
     test.isNotUndefined(err.col);
   }
 });
+
+Tinytest.addAsync('Async: minifier-js - verify how terser handles an empty string', async (test, done ) => {
+  let result = await meteorJsMinifyAsync('');
+  test.equal(result.code, '');
+  test.equal(result.minifier, 'terser');
+  done();
+});
+
+Tinytest.addAsync('Async: minifier-js - verify terser is able to minify valid javascript', async (test, done) => {
+  let result = await meteorJsMinifyAsync('function add(first,second){return first + second; }\n');
+  test.equal(result.code, 'function add(n,d){return n+d}');
+  test.equal(result.minifier, 'terser');
+  done();
+});
+
+Tinytest.addAsync('Async: minifier-js - verify error handling is done as expected', async (test, done) => {
+  test.throws(() => meteorJsMinifyAsync('let name = {;\n'), undefined );
+  done();
+});
+
+Tinytest.addAsync('Async: minifier-js - verify tersers error object has the fields we use for reporting errors to users', async (test, done) => {
+  let result;
+  try {
+    result = await meteorJsMinifyAsync('let name = {;\n');
+    console.log("result", result);
+  }
+  catch (err) {
+    console.log("err", err);
+    test.isNotUndefined(err.name);
+    test.isNotUndefined(err.message);
+    test.isNotUndefined(err.filename);
+    test.isNotUndefined(err.line);
+    test.isNotUndefined(err.col);
+  }
+  done();
+});
