9f062036b9ae6e85cf2411085b089a180068b528
denihs <denilsonh2014@gmail.com>
2022-08-05 11:07:21 -0400

    Fixing group 9 tests

--- tools/tests/source-maps.js
+++ tools/tests/source-maps.js
@@ -17,7 +17,7 @@ selftest.define("source maps from checkout", ['checkout'], function () {
     throw new Error();
   } catch (e) {
     var index = (process.platform === 'win32') ? 2 : 1;
-    selftest.expectEqual(e.stack.split(":")[index], "18");
+    selftest.expectEqual(e.stack.split(":")[index], "17");
   }
 });
 
