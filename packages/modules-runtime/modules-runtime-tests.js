995ef9677e4aa4dc1ec6883a2e664f21bcaf2402
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-08-15 09:22:51 -0300

    wip(tests): started adding tests

--- packages/modules-runtime/modules-runtime-tests.js
+++ packages/modules-runtime/modules-runtime-tests.js
@@ -3,3 +3,9 @@ Tinytest.add('modules', function (test) {
   var require = meteorInstall();
   test.equal(typeof require, "function");
 });
+
+Tinytest.add('modules - error', function (test) {
+
+  const require = meteorInstall('non_existent_module');
+  test.throws(require(), /Cannot find package "meteor". Try "meteor add meteor"./);
+});
