968eb71800ee0830f54cf315d166e9fceb15ce9b
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-08-18 15:37:54 -0300

    tests(module-erros): added tests

--- packages/modules-runtime/modules-runtime-tests.js
+++ packages/modules-runtime/modules-runtime-tests.js
@@ -1,11 +1,22 @@
 Tinytest.add('modules', function (test) {
-  test.equal(typeof meteorInstall, "function");
+  test.equal(typeof meteorInstall, 'function');
   var require = meteorInstall();
-  test.equal(typeof require, "function");
+  test.equal(typeof require, 'function');
 });
 
-Tinytest.add('modules - error', function (test) {
+Tinytest.add('modules - meteor/ - error', function (test) {
+  const require = meteorInstall();
+  test.throws(require('meteor/foo'), /Cannot find package "meteor". Try "meteor add meteor"./);
+});
 
-  const require = meteorInstall('non_existent_module');
-  test.throws(require(), /Cannot find package "meteor". Try "meteor add meteor"./);
+Tinytest.add('modules - client calling server', function (test) {
+  const require = meteorInstall();
+  test.throws(require('./../server/main.js'), `Unable to import on the client a module from a server directory: './../server/main.js'
+       (cross-boundary import) see: https://guide.meteor.com/structure.html#special-directories`);
+});
+
+Tinytest.add('modules - server - error', function (test) {
+  const require = meteorInstall();
+  test.throws(require('./../client/main.js'), `Unable to import on the server a module from a client directory: './../client/main.js'
+       (cross-boundary import) see: https://guide.meteor.com/structure.html#special-directories`);
 });
