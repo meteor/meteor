ce179fa55ceb814578a1212d568169e7638c98d5
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-08-18 22:13:55 -0300

    chore(modules.fallback): all goes to varifyErrors

--- packages/modules-runtime/modern.js
+++ packages/modules-runtime/modern.js
@@ -1,5 +1,3 @@
-import { verifyErrors } from './verifyErrors';
-
 meteorInstall = makeInstaller({
   // On the client, make package resolution prefer the "browser" field of
   // package.json over the "module" field over the "main" field.
@@ -7,7 +5,6 @@ meteorInstall = makeInstaller({
   mainFields: ['browser', 'module', 'main'],
 
   fallback: function (id, parentId, error) {
-    verifyErrors(id);
-    throw error;
+    verifyErrors(id, parentId, error);
   }
 });
