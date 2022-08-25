550e752cb3b4032c27ff97fb1f9c76b50dfe0b2e
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-08-25 15:37:42 -0300

    tests(runtime-errors): adjusted the  ci

--- packages/modules-runtime/modules-runtime-tests.js
+++ packages/modules-runtime/modules-runtime-tests.js
@@ -5,22 +5,30 @@ Tinytest.add('modules', function (test) {
 });
 
 Tinytest.add('modules.throwStandardError', function (test) {
-  const require = meteorInstall();
-  test.throws(require('meteor/foo'), /Cannot find package "meteor". Try "meteor add meteor"./);
+  var require = meteorInstall();
+  test.throws(() => {
+    require('meteor/foo')
+  }, 'Cannot find package "foo". Try "meteor add foo".');
 });
 
 if (Meteor.isClient) {
   Tinytest.add('modules.throwClientError', function (test) {
-    const require = meteorInstall();
-    test.throws(require('./../server/main.js'), 'Unable to import on the client a module from a server directory: ../server/main.js \n (cross-boundary import) see: https://guide.meteor.com/structure.html#special-directories`'
+    var require = meteorInstall();
+    test.throws(() => {
+        require('./../server/main.js')
+      },
+      'Unable to import on the server a module from a client directory: "./../server/main.js" \n' +
+      ' (cross-boundary import) see: https://guide.meteor.com/structure.html#special-directories'
     );
   });
 }
 
 if (Meteor.isServer) {
   Tinytest.add('modules.throwServerError', function (test) {
-    const require = meteorInstall();
-    test.throws(require('./../client/main.js'), 'Unable to import on the server a module from a client directory: ../client/main.js \n (cross-boundary import) see: https://guide.meteor.com/structure.html#special-directories`'
+    var require = meteorInstall();
+    test.throws(() => {
+        require('./../client/main.js')
+      }, "Cannot find module './../client/main.js'"
     );
   });
 }
