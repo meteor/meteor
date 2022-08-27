1712c4c5aeb9dfdd1c01a7cbc087f9f3b9a459a0
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-08-27 14:52:35 -0300

    fix(errors): cross-boud. message

--- packages/modules-runtime/errors/cannotImportFrom.js
+++ packages/modules-runtime/errors/cannotImportFrom.js
@@ -24,7 +24,7 @@ cannotImport = function (id) {
   var fromClient =
     function () {
       return new Error(
-        'Unable to import on the client a module from a server directory: "' + id + '" \n (cross-boundary import) see: https://guide.meteor.com/structure.html#special-directories'
+        'Unable to import on the server a module from a client directory: "' + id + '" \n (cross-boundary import) see: https://guide.meteor.com/structure.html#special-directories'
       );
     };
 
@@ -32,7 +32,7 @@ cannotImport = function (id) {
   var fromServer =
     function () {
       return new Error(
-        'Unable to import on the server a module from a client directory: "' + id + '" \n (cross-boundary import) see: https://guide.meteor.com/structure.html#special-directories'
+        'Unable to import on the client a module from a server directory: "' + id + '" \n (cross-boundary import) see: https://guide.meteor.com/structure.html#special-directories'
       );
     };
 
