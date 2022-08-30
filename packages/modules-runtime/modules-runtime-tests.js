0255d06728474352fe1105dd2a186d47cf7d466b
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-08-30 16:17:40 -0300

    tests(module-runtime): adjusted test organization

--- packages/modules-runtime/modules-runtime-tests.js
+++ packages/modules-runtime/modules-runtime-tests.js
@@ -4,24 +4,24 @@ Tinytest.add('modules', function (test) {
   test.equal(typeof require, 'function');
 });
 
-Tinytest.add('modules.throwStandardError', function (test) {
+Tinytest.add('errors - standard', function (test) {
   var require = meteorInstall();
   test.throws(() => {
     require('meteor/foo');
   }, 'Cannot find package "foo". Try "meteor add foo".');
 });
 
-if (Meteor.isClient) {
-  Tinytest.add('modules.throwClientError', function (test) {
+
+
+if (Meteor.isServer) {
+  Tinytest.add('server - throwClientError', function (test) {
     var require = meteorInstall();
     test.throws(() => {
         require('./../server/main.js');
-      },
-      'Unable to import on the client a module from a server directory: "./../server/main.js" \n' +
-      ' (cross-boundary import) see: https://guide.meteor.com/structure.html#special-directories'
+      }, "Cannot find module './../server/main.js'"
     );
   });
-  Tinytest.add('modules.throwServerError', function (test) {
+  Tinytest.add('server - throwServerError', function (test) {
     var require = meteorInstall();
     test.throws(() => {
         require('./../client/main.js');
@@ -32,20 +32,22 @@ if (Meteor.isClient) {
   });
 }
 
-if (Meteor.isServer) {
-  Tinytest.add('modules.throwClientError', function (test) {
+if (Meteor.isClient) {
+  Tinytest.add('client - throwClientError', function (test) {
     var require = meteorInstall();
     test.throws(() => {
         require('./../server/main.js');
-      }, "Cannot find module './../server/main.js'"
+      },
+      'Unable to import on the client a module from a server directory: "./../server/main.js" \n' +
+      ' (cross-boundary import) see: https://guide.meteor.com/structure.html#special-directories'
     );
   });
-  Tinytest.add('modules.throwServerError', function (test) {
+  Tinytest.add('client - throwServerError', function (test) {
     var require = meteorInstall();
     test.throws(() => {
         require('./../client/main.js');
-      },"Cannot find module './../client/main.js'"
-    );
+      }, "Cannot find module './../client/main.js'");
   });
 }
+
 
