a385903a9f9890498d01e5bd9436af6bfa8c0e8b
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-08-30 16:17:01 -0300

    chore: added extra validations

--- packages/modules-runtime/verifyErrors.js
+++ packages/modules-runtime/verifyErrors.js
@@ -6,15 +6,33 @@
  * @param err {Error}
  */
 verifyErrors = function (id, parentId,err)  {
+
   if (id && id.startsWith('meteor/')) {
     throw cannotFindMeteorPackage(id);
   }
-  if (cannotImport(id).from('client')) {
-    throw cannotImport(id).fromClient();
+
+  if(!(id.startsWith('.') || id.startsWith('/'))) {
+    throw err;
   }
-  if (cannotImport(id).from('server')) {
-    throw cannotImport(id).fromServer();
+
+  if (id.endsWith('client') || id.endsWith('server')) {
+    // We don't know for sure what client wants to do so throw standard error
+    throw err;
   }
+
+  if (imports(id).from('node_modules')) {
+    // Problem with node modules
+    throw err;
+  }
+
+  // custom errors
+  if (Meteor.isServer && imports(id).from('client')) {
+    throw imports(id).fromClientError();
+  }
+  if (Meteor.isClient && imports(id).from('server')) {
+    throw imports(id).fromServerError();
+  }
+
   if (err) {
     throw err;
   }
