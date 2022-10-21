22403ce8c43831ce68eec4c163ccd660b0d58945
harryadel <harryadelb@gmail.com>
2022-10-21 12:28:59 +0200

    [webapp-hashing] Remove api.use('server')

--- packages/webapp-hashing/package.js
+++ packages/webapp-hashing/package.js
@@ -5,7 +5,6 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.use('ecmascript');
-  api.use('server');
   api.addFiles('webapp-hashing.js', 'server');
   api.export('WebAppHashing');
 });
