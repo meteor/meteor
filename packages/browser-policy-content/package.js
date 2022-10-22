be751161f76798845bea37805e1a5b9734e0a37e
harryadel <harryadelb@gmail.com>
2022-10-22 09:26:36 +0200

    [browser-policy-content] Remove underscore

--- packages/browser-policy-content/package.js
+++ packages/browser-policy-content/package.js
@@ -3,9 +3,15 @@ Package.describe({
   version: "1.1.1"
 });
 
+Npm.depends({
+  'lodash.isempty': '4.4.0',
+  'lodash.has': '4.5.2',
+  'lodash.union': '4.6.0'
+});
+
 Package.onUse(function (api) {
   api.use("modules");
-  api.use(["underscore", "browser-policy-common", "webapp"], "server");
+  api.use(["browser-policy-common", "webapp"], "server");
   api.imply(["browser-policy-common"], "server");
   api.mainModule("browser-policy-content.js", "server");
 });
