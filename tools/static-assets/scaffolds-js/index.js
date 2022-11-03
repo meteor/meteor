cfe20a240a7a042127dfb6b76336af75f7e60c89
Gabriel Grubba <Grubba27@hotmail.com>
2022-11-03 18:56:13 -0300

    feat: cli index.js

---
+++ tools/static-assets/scaffolds-js/index.js
@@ -0,0 +1,15 @@
+import { $$UpperName$$Collection } from './collection';
+import {
+  save$$UpperName$$,
+  update$$UpperName$$,
+  remove$$UpperName$$,
+  find$$UpperName$$ById
+} from './methods';
+
+export {
+  $$UpperName$$Collection,
+  save$$UpperName$$,
+  update$$UpperName$$,
+  remove$$UpperName$$,
+  find$$UpperName$$ById
+}
