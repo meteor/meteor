f2750750ff1cd09764a3297996cd33110c7ac62f
Gabriel Grubba <Grubba27@hotmail.com>
2022-11-03 18:56:31 -0300

    feat: cli collection.ts

---
+++ tools/static-assets/scaffolds-ts/collection.ts
@@ -0,0 +1,9 @@
+import { Mongo } from 'meteor/mongo';
+
+export type $$UpperName$$ = {
+  _id?: string;
+  name: string;
+  createdAt: Date;
+}
+
+export const $$UpperName$$Collection = new Mongo.Collection('$$name$$');
