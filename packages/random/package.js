a43b9649782ef7145d1a73b133880da791d0588d
Gabriel Grubba <Grubba27@hotmail.com>
2022-11-04 11:50:14 -0300

    Meteor version to 2.8.1-beta.1 :comet:

--- packages/random/package.js
+++ packages/random/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Random number generator and utilities',
-  version: '1.2.0-beta.1',
+  version: '1.2.0',
 });
 
 Package.onUse(function (api) {
--- packages/tracker/package.js
+++ packages/tracker/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Dependency tracker to allow reactive callbacks",
-  version: "1.2.0-beta.1"
+  version: "1.2.0"
 });
 
 Package.onUse(function (api) {
