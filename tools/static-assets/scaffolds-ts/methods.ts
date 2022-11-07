6235fba792745456d94b9da9d975c8f08c63c7b1
Gabriel Grubba <Grubba27@hotmail.com>
2022-11-07 15:15:28 -0300

    fix: adressed comments in methods.ts in scaffolds-ts

--- tools/static-assets/scaffolds-ts/methods.ts
+++ tools/static-assets/scaffolds-ts/methods.ts
@@ -1,25 +1,25 @@
-import { Meteor } from 'meteor/meteor'
-import { $$UpperName$$ , $$UpperName$$Collection } from './collection';
+import { Meteor } from 'meteor/meteor';
+import { $$UpperName$$Type, $$UpperName$$Collection } from './collection';
 
-export const save$$UpperName$$ = async (data: $$UpperName$$) => {
-  return await $$UpperName$$Collection.insertAsync({ ...data });
+export function create$$UpperName$$(data: $$UpperName$$Type) {
+  return $$UpperName$$Collection.insertAsync({ ...data });
 }
 
-export const update$$UpperName$$ = async (_id: string, data: Partial<$$UpperName$$>) => {
-  return await $$UpperName$$Collection.updateAsync(_id, { ...data });
+export function update$$UpperName$$(_id: string, data: Partial<$$UpperName$$Type>) {
+  return $$UpperName$$Collection.updateAsync(_id, { ...data });
 }
 
-export const remove$$UpperName$$ = async (_id: string) => {
-  return await $$UpperName$$Collection.removeAsync(_id);
+export function remove$$UpperName$$(_id: string) {
+  return $$UpperName$$Collection.removeAsync(_id);
 }
 
-export const find$$UpperName$$ById = async (_id: string) => {
-  return await $$UpperName$$Collection.findOneAsync(_id);
+export function find$$UpperName$$ById(_id: string) {
+  return $$UpperName$$Collection.findOneAsync(_id);
 }
 
 Meteor.methods({
-  save$$UpperName$$,
-  update$$UpperName$$,
-  remove$$UpperName$$,
-  find$$UpperName$$ById
+  '$$UpperName$$.create': create$$UpperName$$,
+  '$$UpperName$$.update': update$$UpperName$$,
+  '$$UpperName$$.remove': remove$$UpperName$$,
+  '$$UpperName$$.find': find$$UpperName$$ById
 });
