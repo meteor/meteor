d2e6d1fb2444b010add7b4296cf7a8215a5c839f
Gabriel Grubba <Grubba27@hotmail.com>
2022-11-08 17:24:42 -0300

    feat: Colors for Meteor CLI

---
+++ tools/console/colors.ts
@@ -0,0 +1,14 @@
+type Color = (text: string) => string;
+const yellow: Color = (text) => `\x1b[33m${ text }\x1b[0m`;
+const red: Color = (text) => `\x1b[31m${ text }\x1b[0m`;
+const purple: Color = (text) => `\x1b[35m${ text }\x1b[0m`;
+const green: Color = (text) => `\x1b[32m${ text }\x1b[0m`;
+const blue: Color = (text) => `\x1b[34m${ text }\x1b[0m`;
+
+export {
+  yellow,
+  red,
+  purple,
+  green,
+  blue,
+};
