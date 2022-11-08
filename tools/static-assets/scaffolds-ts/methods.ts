a4274cac405692d32c828a3e17b535b7623b2ccb
Gabriel Grubba <Grubba27@hotmail.com>
2022-11-08 17:26:25 -0300

    chore: changed methods.ts to PascalName

--- tools/static-assets/scaffolds-ts/methods.ts
+++ tools/static-assets/scaffolds-ts/methods.ts
@@ -1,26 +1,30 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
-import { $$UpperName$$, $$UpperName$$Collection } from './collection';
+import { check } from 'meteor/check';
+import { $$PascalName$$, $$PascalName$$Collection } from './collection';
 
-export function create(data: $$UpperName$$) {
-  return $$UpperName$$Collection.insertAsync({ ...data });
+export function create(data: $$PascalName$$) {
+  return $$PascalName$$Collection.insertAsync({ ...data });
 }
 
-export function update(_id: string, data: Mongo.Modifier<$$UpperName$$>) {
-  return $$UpperName$$Collection.updateAsync(_id, { ...data });
+export function update(_id: string, data: Mongo.Modifier<$$PascalName$$>) {
+  check(_id, String);
+  return $$PascalName$$Collection.updateAsync(_id, { ...data });
 }
 
 export function remove(_id: string) {
-  return $$UpperName$$Collection.removeAsync(_id);
+  check(_id, String);
+  return $$PascalName$$Collection.removeAsync(_id);
 }
 
 export function findById(_id: string) {
-  return $$UpperName$$Collection.findOneAsync(_id);
+  check(_id, String);
+  return $$PascalName$$Collection.findOneAsync(_id);
 }
 
 Meteor.methods({
-  '$$UpperName$$.create': create,
-  '$$UpperName$$.update': update,
-  '$$UpperName$$.remove': remove,
-  '$$UpperName$$.find': findById
+  '$$PascalName$$.create': create,
+  '$$PascalName$$.update': update,
+  '$$PascalName$$.remove': remove,
+  '$$PascalName$$.find': findById
 });
