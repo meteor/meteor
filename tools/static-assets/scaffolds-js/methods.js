0253cc0bb2223536dcbaeba050e183caaeab7da8
Gabriel Grubba <Grubba27@hotmail.com>
2022-11-08 09:54:55 -0300

    fix: updated names in methods.js

--- tools/static-assets/scaffolds-js/methods.js
+++ tools/static-assets/scaffolds-js/methods.js
@@ -1,25 +1,25 @@
 import { Meteor } from 'meteor/meteor';
 import { $$UpperName$$Collection } from './collection';
 
-export function create$$UpperName$$(data) {
+export function create(data) {
   return $$UpperName$$Collection.insertAsync({ ...data });
 }
 
-export function update$$UpperName$$(_id, data) {
+export function update(_id, data) {
   return $$UpperName$$Collection.updateAsync(_id, { ...data });
 }
 
-export function remove$$UpperName$$(_id) {
+export function remove(_id) {
   return $$UpperName$$Collection.removeAsync(_id);
 }
 
-export function find$$UpperName$$ById(_id) {
+export function findById(_id) {
   return $$UpperName$$Collection.findOneAsync(_id);
 }
 
 Meteor.methods({
-  '$$UpperName$$.create': create$$UpperName$$,
-  '$$UpperName$$.update': update$$UpperName$$,
-  '$$UpperName$$.remove': remove$$UpperName$$,
-  '$$UpperName$$.find': find$$UpperName$$ById
+  '$$UpperName$$.create': create,
+  '$$UpperName$$.update': update,
+  '$$UpperName$$.remove': remove,
+  '$$UpperName$$.find': findById
 });
