13c36773b3c1ce6429486a2cd0b6358a0d474ec1
Gabriel Grubba <Grubba27@hotmail.com>
2022-11-09 11:30:54 -0300

    fix: adjusted publications names

--- tools/static-assets/scaffolds-ts/publications.ts
+++ tools/static-assets/scaffolds-ts/publications.ts
@@ -1,10 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import { $$PascalName$$Collection } from './collection';
 
-Meteor.publish('$$PascalName$$sByLoggedUser', function publishTasksByUserId(this) {
+Meteor.publish('$$PascalName$$sByLoggedUser', function publish$$PascalName$$sByUserId(this) {
   return $$PascalName$$Collection.find({ userId: this.userId });
 });
 
-Meteor.publish('all$$PascalName$$s', function publishTasks() {
+Meteor.publish('all$$PascalName$$s', function publish$$PascalName$$s() {
   return $$PascalName$$Collection.find({});
 });
