8b26690d8001e6133704f5a3e578e3c0717d826a
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-11-14 14:02:46 -0300

    fix: addressed fred comments on publication.ts scaffold

--- tools/static-assets/scaffolds-ts/publications.ts
+++ tools/static-assets/scaffolds-ts/publications.ts
@@ -1,9 +1,6 @@
 import { Meteor, Subscription } from 'meteor/meteor';
 import { $$PascalName$$Collection } from './collection';
 
-Meteor.publish('$$PascalName$$sByLoggedUser', function publish$$PascalName$$sByUserId(this: Subscription) {
-  return $$PascalName$$Collection.find({ userId: this.userId });
-});
 
 Meteor.publish('all$$PascalName$$s', function publish$$PascalName$$s() {
   return $$PascalName$$Collection.find({});
