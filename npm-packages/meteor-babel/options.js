9d8fb1e0238d7833a6edb55f98677b8154f81e9c
Gabriel Grubba <Grubba27@hotmail.com>
2022-12-06 10:46:45 -0300

    tests: added flag and tested

--- npm-packages/meteor-babel/options.js
+++ npm-packages/meteor-babel/options.js
@@ -80,11 +80,14 @@ exports.getDefaults = function getDefaults(features) {
 
 function maybeAddReactPlugins(features, options) {
   if (features && features.react) {
-    options.presets.push(
-      [require("@babel/preset-react"), {
-        runtime: "automatic"
-      }]
-    );
+    // get flag passed in args BABEL_TESTING
+    const reactBabel =
+      process.env.BABEL_TESTING
+        ? require("@babel/preset-react")
+        : [require("@babel/preset-react"), {
+          runtime: "automatic"
+        }];
+    options.presets.push(reactBabel);
     options.plugins.push(
       [require("@babel/plugin-proposal-class-properties"), {
         loose: true
