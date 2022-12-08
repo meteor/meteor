2333bc35a3acd208715a59b9fb2390b921db8a4b
Gabriel Grubba <Grubba27@hotmail.com>
2022-12-07 21:45:58 -0300

    tests: made setup async

--- packages/accounts-base/accounts_tests_setup.js
+++ packages/accounts-base/accounts_tests_setup.js
@@ -1,25 +1,25 @@
-const getTokenFromSecret = ({ selector, secret: secretParam }) => {
+const getTokenFromSecret = async ({ selector, secret: secretParam }) => {
   let secret = secretParam;
 
   if (!secret) {
     const { services: { twoFactorAuthentication } = {} } =
-      Meteor.users.findOne(selector) || {};
+      await Meteor.users.findOne(selector) || {};
     if (!twoFactorAuthentication) {
       throw new Meteor.Error(500, 'twoFactorAuthentication not set.');
     }
     secret = twoFactorAuthentication.secret;
   }
-  const { token } = Accounts._generate2faToken(secret);
+  const { token } =  Accounts._generate2faToken(secret);
 
   return token;
 };
 
 Meteor.methods({
-  removeAccountsTestUser(username) {
-    Meteor.users.remove({ username });
+  async removeAccountsTestUser(username) {
+    await Meteor.users.remove({ username });
   },
-  forceEnableUser2fa(selector, secret) {
-    Meteor.users.update(
+  async forceEnableUser2fa(selector, secret) {
+   await Meteor.users.update(
       selector,
       {
         $set: {
