e6537550d15fbc8eec3ab3d20f2df3631f8dd39a
Gabriel Grubba <Grubba27@hotmail.com>
2023-01-25 17:05:56 -0300

    tests: updated match str

--- tools/tests/mongo.js
+++ tools/tests/mongo.js
@@ -35,7 +35,7 @@ function testMeteorMongo(appDir) {
   // Make sure we match the DB version that's printed as part of the
   // non-quiet shell startup text, so that we don't confuse it with the
   // output of the db.version() command below.
-  mongoRun.match(/MongoDB server version: 5\.\d+\.\d+/);
+  mongoRun.match(/MongoDB server version: v5\.\d+\.\d+/);
 
   // Make sure the shell does not display the banner about Mongo's free
   // monitoring service.
@@ -43,7 +43,7 @@ function testMeteorMongo(appDir) {
 
   // Note: when mongo shell's input is not a tty, there is no prompt.
   mongoRun.write('db.version()\n');
-  mongoRun.match(/5\.\d+\.\d+/);
+  mongoRun.match(/v5\.\d+\.\d+/);
   mongoRun.stop();
 
   run.stop();
