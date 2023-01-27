0559229370b4ddadeed587361775b22b4e09889f
Joshua Maserow <joshua@softwarecreations.co.za>
2023-01-27 11:32:01 +0200

    'package.js Increased package version to 1.9.0 to reflect new feature release.'

--- packages/standard-minifier-css/package.js
+++ packages/standard-minifier-css/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'standard-minifier-css',
-  version: '1.8.4',
+  version: '1.9.0',
   summary: 'Standard css minifier used with Meteor apps by default.',
   documentation: 'README.md'
 });
