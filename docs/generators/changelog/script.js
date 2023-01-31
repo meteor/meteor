9736aa46941c4130d7b675c80634e57e783cef99
Gabriel Grubba <Grubba27@hotmail.com>
2023-01-31 15:48:58 -0300

    feat: script to generate changelog

---
+++ docs/generators/changelog/script.js
@@ -0,0 +1,24 @@
+const _fs = require('fs');
+const fs = _fs.promises;
+const main = async () => {
+  try {
+    console.log('started concatenating files');
+    const files = await fs.readdir('./generators/changelog/docs', 'utf8');
+    const filesStream = files
+      .map(file => {
+        console.log(`reading file: ${ file }`);
+        return fs.readFile(`./generators/changelog/docs/${ file }`, 'utf8')
+      })
+      .reverse();
+    console.log('Giving some touches to the files');
+
+    const filesContent = await Promise.all(filesStream);
+    await fs.writeFile('./history.md', filesContent);
+    console.log('Finished :)');
+
+  }catch (e) {
+    console.log(e);
+  }
+
+}
+main().then(_ => _);
