d2042ddfd8b62aa2e0d9887f60f46c3ca834c17c
Evan Broder <evan@ebroder.net>
2023-02-08 16:05:36 -0800

    Add missing types from typescript skeleton
    The project's tsconfig.json has noImplicitAny: true, so we shouldn't
    allow these to be implicitly any.

--- tools/static-assets/skel-typescript/server/main.ts
+++ tools/static-assets/skel-typescript/server/main.ts
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
+import { Link, LinksCollection } from '/imports/api/links';
 
-async function insertLink({ title, url }) {
+async function insertLink({ title, url }: Pick<Link, 'title' | 'url'>) {
   await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
 }
 
