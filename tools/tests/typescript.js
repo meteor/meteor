ce75b877e725a9854469a3bd79ee5d7a2ae76777
Gabriel Grubba <Grubba27@hotmail.com>
2023-02-23 19:07:30 -0300

    trying something

--- tools/tests/typescript.js
+++ tools/tests/typescript.js
@@ -5,14 +5,19 @@ selftest.define("typescript template works", function () {
   const s = new Sandbox;
 
   let run = s.run("create", "--typescript", "typescript");
+
   run.waitSecs(60);
   run.match("Created a new Meteor app in 'typescript'.");
   run.match("To run your new app");
 
   s.cd("typescript");
+
+  run = s.run("npm", "install");
+  run.expectExit(0);
+
   run = s.run("lint");
   run.waitSecs(60);
-  run.match("[zodern:types] Exiting \"meteor lint\" early")
+  run.match("[zodern:types] Exiting \"meteor lint\" early");
   run.expectExit(0);
 
   run = s.run("npx", "tsc");
