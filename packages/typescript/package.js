3fa377af6524c9b99cf86b398172a6fd325abf46
Gabriel Grubba <Grubba27@hotmail.com>
2023-03-02 16:13:46 -0300

    Meteor version to 2.11.0 :comet:

--- packages/typescript/package.js
+++ packages/typescript/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'typescript',
-  version: '4.9.4-rc2110.1',
+  version: '4.9.4',
   summary:
     'Compiler plugin that compiles TypeScript and ECMAScript in .ts and .tsx files',
   documentation: 'README.md',
