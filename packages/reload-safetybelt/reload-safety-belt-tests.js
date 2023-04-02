a19a751d29c99de37f6fa02fd2f367e2ff50b534
Harry Adel <harryadelb@gmail.com>
2023-04-02 18:24:28 +0200

    Update reload-safety-belt-tests.js

--- packages/reload-safetybelt/reload-safety-belt-tests.js
+++ packages/reload-safetybelt/reload-safety-belt-tests.js
@@ -1,7 +1,7 @@
 var script = Assets.getText("safetybelt.js");
 
 Tinytest.add("reload-safetybelt - safety belt is added", function (test) {
-  test.isTrue(WebAppInternals.additionalStaticJs.some(
+  test.isTrue(Object.keys(WebAppInternals.additionalStaticJs).some(
     function (js, pathname) {
       return js === script;
     }
