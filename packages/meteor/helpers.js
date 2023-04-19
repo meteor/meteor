67afd31e377c0947c4cd7985f724b4215d7be5c4
matheusccastro <camargodecastromatheus@gmail.com>
2023-04-19 20:51:54 -0300

    Use es5 compatible syntax

--- packages/meteor/helpers.js
+++ packages/meteor/helpers.js
@@ -163,17 +163,17 @@ Meteor.wrapAsync = function (fn, context) {
   };
 };
 
-Meteor.wrapFn = (fn) => {
+Meteor.wrapFn = function (fn) {
   if (!fn || typeof fn !== 'function') {
     throw new Meteor.Error("Expected to receive function to wrap");
-  };
+  }
 
   if (Meteor.isClient) {
     return fn;
   }
 
-  return function(...args) {
-    const ret = fn.apply(this, args);
+  return function() {
+    var ret = fn.apply(this, arguments);
     if (ret && typeof ret.then === 'function') {
       return Promise.await(ret);
     }
