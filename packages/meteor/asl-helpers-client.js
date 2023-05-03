d7d8154593474ca383e534d3d1a40d345ac354c4
Gabriel Grubba <Grubba27@hotmail.com>
2023-05-03 17:16:50 -0300

    chore: updated syntax for Meteor._isPromise

--- packages/meteor/asl-helpers-client.js
+++ packages/meteor/asl-helpers-client.js
@@ -1,5 +1,5 @@
 Meteor.isFibersDisabled = true;
 
-Meteor._isPromise = (r) => {
-    return r && typeof r.then === 'function';
+Meteor._isPromise = function (r) {
+  return r && typeof r.then === "function";
 };
