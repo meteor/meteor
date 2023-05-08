8a7e0a9c20443caa5db693663acb789d463eb49b
Gabriel Grubba <Grubba27@hotmail.com>
2023-05-08 19:34:54 -0300

    typo in build tarbalss

--- tools/cli/commands-packages.js
+++ tools/cli/commands-packages.js
@@ -2717,7 +2717,7 @@ main.registerCommand({
   var packageMap =
         packageMapModule.PackageMap.fromReleaseVersion(releaseRecord);
 
-  for (const osArche in osArches) {
+  for (const osArch in osArches) {
     var tmpdir = files.mkdtemp();
     Console.info("Building tarball for " + osArch);
 
