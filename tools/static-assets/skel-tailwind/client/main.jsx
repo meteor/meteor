9958ccec53aef1b07162917e550deb40c0f40aec
Frederico Maia <fredmaiaarantes@gmail.com>
2023-07-30 09:39:16 -0300

    Updated Tailwind and Chakra-UI skeletons to use React 18

--- tools/static-assets/skel-tailwind/client/main.jsx
+++ tools/static-assets/skel-tailwind/client/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from '/imports/ui/App';
 
 Meteor.startup(() => {
-  render(<App/>, document.getElementById('react-target'));
+  const container = document.getElementById('react-target');
+  const root = createRoot(container);
+  root.render(<App />);
 });
