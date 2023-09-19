d056a26f0277b8e559cf32fc26be37b3638914cf
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2023-09-19 15:30:30 -0300

    testing thing

--- packages/fetch/tests/main.js
+++ packages/fetch/tests/main.js
@@ -4,18 +4,15 @@ Tinytest.add("fetch - sanity", function (test) {
   test.equal(typeof fetch, "function");
 });
 
-Tinytest.addAsync("fetch - asset", function (test) {
-  return fetch(
-    Meteor.absoluteUrl("/packages/local-test_fetch/tests/asset.json", {
-      replaceLocalhost: true, // https://docs.meteor.com/api/core.html#Meteor-absoluteUrl
-    })
-  )
-    .then((res) => {
+Tinytest.addAsync("fetch - asset", async function (test) {
+  const url = Meteor.absoluteUrl("/packages/local-test_fetch/tests/asset.json")
+  const {
+    word,
+    times
+  } = await fetch(url).then((res) => {
       if (!res.ok) throw res;
       return res.json();
-    })
-    .then((json) => {
-      test.equal(json.word, "oyez");
-      test.equal(json.times, 3);
-    });
+    }).catch(console.error)
+      test.equal(word, "oyez");
+    test.equal(times, 3);
 });
