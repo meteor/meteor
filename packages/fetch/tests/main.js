67956ee99a63af97fd45adc490819b6f5825c77b
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2023-09-19 15:12:53 -0300

    ci: solved one more issue

--- packages/fetch/tests/main.js
+++ packages/fetch/tests/main.js
@@ -6,12 +6,16 @@ Tinytest.add("fetch - sanity", function (test) {
 
 Tinytest.addAsync("fetch - asset", function (test) {
   return fetch(
-    Meteor.absoluteUrl("/packages/local-test_fetch/tests/asset.json")
-  ).then(res => {
-    if (! res.ok) throw res;
-    return res.json();
-  }).then(json => {
-    test.equal(json.word, "oyez");
-    test.equal(json.times, 3);
-  });
+    Meteor.absoluteUrl("/packages/local-test_fetch/tests/asset.json", {
+      replaceLocalhost: true, // https://docs.meteor.com/api/core.html#Meteor-absoluteUrl
+    })
+  )
+    .then((res) => {
+      if (!res.ok) throw res;
+      return res.json();
+    })
+    .then((json) => {
+      test.equal(json.word, "oyez");
+      test.equal(json.times, 3);
+    });
 });
