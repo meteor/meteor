484f812ce3aa982e355e093942615f9b8e776804
Jan Dvorak <storytellercz@proton.me>
2023-09-20 18:08:13 +0200

    Update node-gyp and node-pre-gyp to their latest versions
    Also update changelog for them.

--- scripts/dev-bundle-tool-package.js
+++ scripts/dev-bundle-tool-package.js
@@ -11,8 +11,8 @@ var packageJson = {
     // Explicit dependency because we are replacing it with a bundled version
     // and we want to make sure there are no dependencies on a higher version
     npm: "9.6.7",
-    "node-gyp": "8.0.0",
-    "node-pre-gyp": "0.15.0",
+    "node-gyp": "9.4.0",
+    "@mapbox/node-pre-gyp": "1.0.11",
     typescript: "4.9.4",
     "@meteorjs/babel": "7.19.0-beta.3",
     // Keep the versions of these packages consistent with the versions
