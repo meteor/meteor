c047887fc4818bfe23fdd703367a7c99a27f6b6f
harryadel <harryadelb@gmail.com>
2023-10-30 14:28:16 +0200

    [mongo] Format code

--- packages/mongo/mongo_livedata_tests.js
+++ packages/mongo/mongo_livedata_tests.js
@@ -2425,7 +2425,8 @@ function functionChain2Upsert (test, expect, coll, index) {
   }));
 }
 
-Object.entries( {collectionInsert: collectionInsert,
+Object.entries({
+  collectionInsert: collectionInsert,
   collectionUpsert: collectionUpsert,
   functionCallsInsert: functionCallsInsert,
   functionCallsUpsert: functionCallsUpsert,
@@ -2433,47 +2434,54 @@ Object.entries( {collectionInsert: collectionInsert,
   functionCalls3Insert: functionCalls3Inserts,
   functionChainInsert: functionChainInsert,
   functionChain2Insert: functionChain2Insert,
-  functionChain2Upsert: functionChain2Upsert}).forEach(function ([name, fn]) {
-    [1, 3].forEach(function (repetitions) {
-      [1, 3].forEach( function (collectionCount) {
-        ['STRING', 'MONGO'].forEach(function (idGeneration) {
+  functionChain2Upsert: functionChain2Upsert
+}).forEach(function([name, fn]) {
+  [1, 3].forEach(function(repetitions) {
+      [1, 3].forEach(function(collectionCount) {
+          ['STRING', 'MONGO'].forEach(function(idGeneration) {
 
-testAsyncMulti('mongo-livedata - consistent _id generation ' + name + ', ' + repetitions + ' repetitions on ' + collectionCount + ' collections, idGeneration=' + idGeneration, [ function (test, expect) {
-var collectionOptions = { idGeneration: idGeneration };
+              testAsyncMulti('mongo-livedata - consistent _id generation ' + name + ', ' + repetitions + ' repetitions on ' + collectionCount + ' collections, idGeneration=' + idGeneration, [function(test, expect) {
+                  var collectionOptions = {
+                      idGeneration: idGeneration
+                  };
 
-var cleanups = this.cleanups = [];
-this.collections = times(collectionCount, function () {
-var collectionName = "consistentid_" + Random.id();
-if (Meteor.isClient) {
- Meteor.call('createInsecureCollection', collectionName, collectionOptions);
- Meteor.subscribe('c-' + collectionName, expect());
- cleanups.push(function (expect) { Meteor.call('dropInsecureCollection', collectionName, expect(function () {})); });
-}
+                  var cleanups = this.cleanups = [];
+                  this.collections = times(collectionCount, function() {
+                      var collectionName = "consistentid_" + Random.id();
+                      if (Meteor.isClient) {
+                          Meteor.call('createInsecureCollection', collectionName, collectionOptions);
+                          Meteor.subscribe('c-' + collectionName, expect());
+                          cleanups.push(function(expect) {
+                              Meteor.call('dropInsecureCollection', collectionName, expect(function() {}));
+                          });
+                      }
 
-var collection = new Mongo.Collection(collectionName, collectionOptions);
-if (Meteor.isServer) {
- cleanups.push(function () { collection._dropCollection(); });
-}
-COLLECTIONS[collectionName] = collection;
-return collection;
-});
-}, function (test, expect) {
-// now run the actual test
-for (var i = 0; i < repetitions; i++) {
-for (var j = 0; j < collectionCount; j++) {
- fn(test, expect, this.collections[j], i);
-}
-}
-}, function (test, expect) {
-// Run any registered cleanup functions (e.g. to drop collections)
-this.cleanups.forEach(function(cleanup) {
-cleanup(expect);
-});
-}]);
+                      var collection = new Mongo.Collection(collectionName, collectionOptions);
+                      if (Meteor.isServer) {
+                          cleanups.push(function() {
+                              collection._dropCollection();
+                          });
+                      }
+                      COLLECTIONS[collectionName] = collection;
+                      return collection;
+                  });
+              }, function(test, expect) {
+                  // now run the actual test
+                  for (var i = 0; i < repetitions; i++) {
+                      for (var j = 0; j < collectionCount; j++) {
+                          fn(test, expect, this.collections[j], i);
+                      }
+                  }
+              }, function(test, expect) {
+                  // Run any registered cleanup functions (e.g. to drop collections)
+                  this.cleanups.forEach(function(cleanup) {
+                      cleanup(expect);
+                  });
+              }]);
 
-});
-});
-});
+          });
+      });
+  });
 });
 
 
