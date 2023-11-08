8dd8c04ac76cf2e7f738b4bcf55a7a9d79ce4d36
filipenevola <filipe@quave.com.br>
2023-11-08 16:28:47 -0500

    2.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+## 2.0.0 (2023-11-8)
+
+- Moves away from `var` usage
+- Upgrades code to Meteor 3
+- Upgrades tests to Meteor 3 (async tests)
+
+## 1.1.1 - percolate:migrations
+
+- Initial [version](https://github.com/percolatestudio/meteor-migrations).
--- package.js
+++ package.js
@@ -8,8 +8,11 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@3.0-alpha.17');
 
-  api.use('ecmascript');
-  api.use(['check', 'mongo', 'logging'], 'server');
+  api.use('ecmascript@0.16.7||0.16.8-alpha300.17');
+  api.use(
+      ["check@1.3.2||1.3.3-alpha300.17", "mongo@1.0.0||2.0.0||2.0.0-alpha300.17", "logging@1.3.2||1.3.3-alpha300.17"],
+      "server",
+  );
 
   api.mainModule('migrations_server.js', 'server');
 
