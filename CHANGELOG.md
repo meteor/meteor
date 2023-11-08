150ad19bc700385467ad03bc078ba71c20845110
filipenevola <filipe@quave.com.br>
2023-11-08 16:21:38 -0500

    2.0.2 - Meteor 3 release issues, I had to run `chmod +rw .npm/package` for it to work.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,16 @@
 # Changelog
 
+## 2.0.2 (2023-11-8)
+
+- Meteor 3 release issues, I had to run `chmod +rw .npm/package` for it to work.
+
+## 2.0.1 (2023-11-8)
+
+This release failed but the content is present in the next.
+
 ## 2.0.0 (2023-11-8)
+
+This release failed but the content is present in the next.
 
 - Migrates `later` to `@breejs/later@4.1.0`
 - Moves away from `underscore`
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary:
     "Allows you to define and run scheduled jobs across multiple servers.",
-  version: "2.0.0",
+  version: "2.0.2",
   name: "quave:synced-cron",
   git: "https://github.com/quavedev/meteor-synced-cron.git",
 });
@@ -12,7 +12,7 @@ Package.onUse(function (api) {
   api.versionsFrom("METEOR@3.0-alpha.17");
 
   api.use(
-    ["check", "mongo@1.0.0||2.0.0||2.0.0-alpha300.17", "logging"],
+    ["check@1.3.2||1.3.3-alpha300.17", "mongo@1.0.0||2.0.0||2.0.0-alpha300.17", "logging@1.3.2||1.3.3-alpha300.17"],
     "server",
   );
 
