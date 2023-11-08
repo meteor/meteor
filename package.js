bc678df2f5c66c932c311d8b76dff891386c3f3c
filipenevola <filipe@quave.com.br>
2023-11-08 16:29:02 -0500

    Adds ecmascript

--- package.js
+++ package.js
@@ -11,6 +11,8 @@ Npm.depends({ "@breejs/later": "4.1.0" });
 Package.onUse(function (api) {
   api.versionsFrom("METEOR@3.0-alpha.17");
 
+  api.use('ecmascript@0.16.7||0.16.8-alpha300.17');
+  
   api.use(
     ["check@1.3.2||1.3.3-alpha300.17", "mongo@1.0.0||2.0.0||2.0.0-alpha300.17", "logging@1.3.2||1.3.3-alpha300.17"],
     "server",
