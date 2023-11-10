7eab0ab65f427d912fe4eeaf073b75b47ecfe0ff
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2023-11-10 10:44:43 -0300

    bump babel-compiler,ecmascript,typescript to beta

--- packages/babel-compiler/package.js
+++ packages/babel-compiler/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "babel-compiler",
   summary: "Parser/transpiler for ECMAScript 2015+ syntax",
-  version: '7.10.5',
+  version: '7.10.5-beta2140.0',
 });
 
 Npm.depends({
