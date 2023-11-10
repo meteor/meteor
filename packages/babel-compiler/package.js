2b16a73e512012cd9c3c60a98cdb06a1e5dcec4c
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2023-11-10 10:45:33 -0300

    Meteor version to 2.14-beta.1 :comet:

--- packages/babel-compiler/package.js
+++ packages/babel-compiler/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "babel-compiler",
   summary: "Parser/transpiler for ECMAScript 2015+ syntax",
-  version: '7.10.5-beta2140.0',
+  version: '7.10.5-beta2140.1',
 });
 
 Npm.depends({
