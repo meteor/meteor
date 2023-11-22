f9e8532d83073ff958e49f10c59cc3c3508b7f21
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2023-11-22 10:24:51 -0300

    bumped packages

--- packages/deprecated/meteor-platform/package.js
+++ packages/deprecated/meteor-platform/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "(Deprecated) Include a standard set of Meteor packages in your app",
-  version: '1.2.6',
+  version: '1.2.7-beta2140.3',
   deprecated: true,
   documentation: 'README.md'
 });
