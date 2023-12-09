e0e716d4930b688c03035680ca3dbe709f417361
filipenevola <filipe@quave.com.br>
2023-12-09 18:12:53 -0400

    Adds a way to disable source map: DISABLE_SOURCE_MAP

--- tools/static-assets/server/boot.js
+++ tools/static-assets/server/boot.js
@@ -182,6 +182,8 @@ function tryLoadSourceMap() {
   }
 }
 
+console.log('process.env.DISABLE_SOURCE_MAP', process.env.DISABLE_SOURCE_MAP);
+
 // Read all the source maps into memory once.
 if (!process.env.DISABLE_SOURCE_MAP) {
   tryLoadSourceMap();
