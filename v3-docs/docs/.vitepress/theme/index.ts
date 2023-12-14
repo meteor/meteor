13e1b05ec0718503fb6395ec2d0611d8761ad918
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2023-12-14 14:04:47 -0300

    update index.ts

--- v3-docs/docs/.vitepress/theme/index.ts
+++ v3-docs/docs/.vitepress/theme/index.ts
@@ -1,15 +1,15 @@
 // .vitepress/theme/index.ts
-import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
-import ApiBox from '../../components/ApiBox.vue'
+import type { Theme } from "vitepress";
+import DefaultTheme from "vitepress/theme";
+import ApiBox from "../../components/ApiBox.vue";
+import Layout from "./Layout.vue";
+import "./theme.css";
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     // register your custom global components
-    app.component(
-      'ApiBox',
-      ApiBox,
-    )
-  }
-} satisfies Theme
+    app.component("ApiBox", ApiBox);
+  },
+  Layout,
+} satisfies Theme;
