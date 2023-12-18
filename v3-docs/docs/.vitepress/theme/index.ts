4f9e497c56edba73a56334ff3db55793078952d1
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2023-12-18 17:28:06 -0300

    Add apimap

--- v3-docs/docs/.vitepress/theme/index.ts
+++ v3-docs/docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import ApiBox from "../../components/ApiBox.vue";
+import ApiMap from "../../components/ApiMap.vue";
 import Layout from "./Layout.vue";
 import "./theme.css";
 
@@ -10,6 +11,7 @@ export default {
   enhanceApp({ app }) {
     // register your custom global components
     app.component("ApiBox", ApiBox);
+    app.component("ApiMap", ApiMap);
   },
   Layout,
 } satisfies Theme;
---
+++ v3-docs/docs/components/ApiMap.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { useData } from "vitepress";
+import { ref, computed, watchEffect } from 'vue'
+import names from "../data/names.json";
+import { createMap, filterMap } from "./scripts/map-maker.js";
+
+const apiList = createMap(names)
+const listRef = ref(apiList);
+const search = ref("");
+
+watchEffect(() => {
+  listRef.value = filterMap(search.value, apiList);
+})
+
+</script>
+
+<template>
+  <main>
+    <label for="search">Search</label>
+    <br />
+    <input class="search" type="text" v-model="search" />
+    <div v-for="(list, api, index) in listRef" :key="index">
+      <h2>
+        {{ api }}
+      </h2>
+      <div v-for="(links, key) in list" :key="key">
+        <h5 v-for="(link) in links" :key="link">
+          <a :href="'api/' + api + '#' + link.replace('#', '-').replace('.', '-')">
+            {{ link }}
+          </a>
+        </h5>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.search {
+  width: 100%;
+  height: 2rem;
+  border-radius: 0.5rem;
+  border: 1px solid #ccc;
+  padding: 0.5rem;
+  margin-bottom: 1rem;
+}
+</style>
+
