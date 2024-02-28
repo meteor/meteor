42d47cec48ec62a69565b9a56a111ab99f833947
Nacho Codoñer <igcogi@gmail.com>
2024-02-28 14:55:31 +0100

    adjust package.js for migrations package

--- packages/meteor-migrations/package.js
+++ packages/meteor-migrations/package.js
@@ -1,18 +1,12 @@
 Package.describe({
-  summary: 'Define and run db migrations.',
+  name: 'migrations',
   version: '2.0.2',
-  name: 'quave:migrations',
-  git: 'https://github.com/quavedev/meteor-migrations.git',
+  summary: 'Define and run db migrations.',
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@3.0-alpha.17');
-
-  api.use('ecmascript@0.16.7||0.16.8-alpha300.17');
-  api.use(
-      ["check@1.3.2||1.3.3-alpha300.17", "mongo@1.0.0||2.0.0||2.0.0-alpha300.17", "logging@1.3.2||1.3.3-alpha300.17"],
-      "server",
-  );
+  api.use('ecmascript');
+  api.use(['check', 'mongo', 'logging'], 'server');
 
   api.mainModule('migrations_server.js', 'server');
 
@@ -21,7 +15,8 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('ecmascript');
-  api.use(['percolate:migrations', 'tinytest']);
+  api.use(['check', 'mongo', 'logging'], 'server');
+  api.use(['tinytest']);
 
   api.addFiles('migrations_tests.js', ['server']);
 });
