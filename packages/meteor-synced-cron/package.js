add18c2726827216b7b807ec9f2315f867c31a9d
Nacho Codoñer <igcogi@gmail.com>
2024-02-29 16:37:24 +0100

    adapt synced-cron package to run properly on meteor core

--- packages/meteor-synced-cron/package.js
+++ packages/meteor-synced-cron/package.js
@@ -1,20 +1,17 @@
 Package.describe({
+  name: "synced-cron",
   summary:
     "Allows you to define and run scheduled jobs across multiple servers.",
-  version: "2.0.2",
-  name: "quave:synced-cron",
-  git: "https://github.com/quavedev/meteor-synced-cron.git",
+  version: "2.0.2"
 });
 
 Npm.depends({ "@breejs/later": "4.1.0" });
 
 Package.onUse(function (api) {
-  api.versionsFrom("METEOR@3.0-alpha.17");
-
-  api.use('ecmascript@0.16.7||0.16.8-alpha300.17');
+  api.use('ecmascript');
   
   api.use(
-    ["check@1.3.2||1.3.3-alpha300.17", "mongo@1.0.0||2.0.0||2.0.0-alpha300.17", "logging@1.3.2||1.3.3-alpha300.17"],
+    ["check", "mongo", "logging"],
     "server",
   );
 
