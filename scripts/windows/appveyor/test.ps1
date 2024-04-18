0c02a531f437d7a1b9f521d74571a3507193726e
Nacho Codoñer <igcogi@gmail.com>
2024-04-18 17:42:56 +0200

    Revert "disable junit to see if it blocks the success on CI"
    This reverts commit c18dd666a81909d2ea60af5faec01ad8a515c0f5.

--- scripts/windows/appveyor/test.ps1
+++ scripts/windows/appveyor/test.ps1
@@ -1,4 +1,7 @@
-﻿$tests = @(
+﻿# For now, we only have one script.
+$jUnit = Join-Path $env:TEMP 'self-test-junit-0.xml'
+
+$tests = @(
   '^assets'
   '^autoupdate'
   '^dynamic import.*development'
@@ -10,6 +13,7 @@ Write-Host "Excluded: $env:SELF_TEST_EXCLUDE" -ForegroundColor Yellow
 
 .\meteor.bat self-test `
   --retries 2 `
+  --junit "$jUnit" `
   --exclude "$env:SELF_TEST_EXCLUDE" `
   "$tests" `
   '2>&1'
