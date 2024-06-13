d8522b9b76e48234c597e8af1b40982624e3b203
Nacho Codoñer <igcogi@gmail.com>
2024-06-13 15:34:01 +0200

    fix how export the proposals

--- npm-packages/babel-preset-meteor/proposals.js
+++ npm-packages/babel-preset-meteor/proposals.js
@@ -1,21 +1,23 @@
-exports.plugins = [
-  require("@babel/plugin-syntax-nullish-coalescing-operator"),
-  require("@babel/plugin-transform-nullish-coalescing-operator"),
+module.exports = {
+  plugins: [
+    require("@babel/plugin-syntax-nullish-coalescing-operator"),
+    require("@babel/plugin-transform-nullish-coalescing-operator"),
 
-  require("@babel/plugin-syntax-optional-chaining"),
-  require("@babel/plugin-transform-optional-chaining"),
+    require("@babel/plugin-syntax-optional-chaining"),
+    require("@babel/plugin-transform-optional-chaining"),
 
-  require("@babel/plugin-syntax-optional-catch-binding"),
-  require("@babel/plugin-transform-optional-catch-binding"),
+    require("@babel/plugin-syntax-optional-catch-binding"),
+    require("@babel/plugin-transform-optional-catch-binding"),
 
-  require("@babel/plugin-syntax-class-properties"),
-  require("@babel/plugin-transform-class-properties"),
+    require("@babel/plugin-syntax-class-properties"),
+    require("@babel/plugin-transform-class-properties"),
 
-  require("@babel/plugin-syntax-async-generators"),
-  require("@babel/plugin-transform-async-generator-functions"),
+    require("@babel/plugin-syntax-async-generators"),
+    require("@babel/plugin-transform-async-generator-functions"),
 
-  require("@babel/plugin-syntax-object-rest-spread"),
-  require("@babel/plugin-transform-object-rest-spread"),
+    require("@babel/plugin-syntax-object-rest-spread"),
+    require("@babel/plugin-transform-object-rest-spread"),
 
-  require("@babel/plugin-transform-logical-assignment-operators")
-];
+    require("@babel/plugin-transform-logical-assignment-operators")
+  ]
+};
