31abbaa63e21bfcaf54679b0446f1ede826fe171
Saïd <20957603+ayewo@users.noreply.github.com>
2024-06-23 08:53:46 +0100

    fix: use native tar.exe (based on bsdtar) to speed up npm install on Windows 10 and newer

--- npm-packages/meteor-installer/install.js
+++ npm-packages/meteor-installer/install.js
@@ -199,8 +199,15 @@ function download() {
     }
 
     if (isWindows()) {
-      decompress();
-      return;
+      const isWindows10OrLater = fs.existsSync(path.resolve('C:/Windows/System32', 'tar.exe'));
+      if (isWindows10OrLater) {
+        // tar works exactly the same as it's bsdtar counterpart on UNIX so continue
+        console.log(`Native binary for tar is available on this version of Windows.`);
+        console.log(`Switching to the native tar.exe binary on Windows.`);
+      } else {
+        decompress();
+        return;
+      }
     }
 
     fs.writeFileSync(startedPath, 'Meteor install started');
