c6b8d6b3ad0cd65b686f37f989ce085de6e34b9e
Nacho Codoñer <igcogi@gmail.com>
2024-07-09 14:05:26 +0200

    tweak permissions of npm-shrinkwrap.json file to properly install deps later

--- tools/cli/commands.js
+++ tools/cli/commands.js
@@ -1533,6 +1533,11 @@ https://guide.meteor.com/cordova.html#submitting-android
   }
 
   await files.rm_recursive(buildDir);
+
+  const npmShrinkwrapFilePath = files.pathJoin(bundlePath, 'programs/server/npm-shrinkwrap.json');
+  if (files.exists(npmShrinkwrapFilePath)) {
+    files.chmod(npmShrinkwrapFilePath, 0o755);
+  }
 };
 
 ///////////////////////////////////////////////////////////////////////////////
