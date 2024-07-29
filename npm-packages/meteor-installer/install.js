cad895c5d131e122f9b2f8dcc583da5326579a18
Jordan Tucker <jordanbtucker@gmail.com>
2024-07-29 11:08:46 -0500

    style(installer): fix comma

--- npm-packages/meteor-installer/install.js
+++ npm-packages/meteor-installer/install.js
@@ -303,7 +303,7 @@ async function setupExecPath() {
   if (isWindows()) {
     // set for the current session and beyond
     child_process.execSync(
-      `powershell -c "$path = (Get-Item 'HKCU:\\Environment').GetValue('Path', '', 'DoNotExpandEnvironmentNames'); [Environment]::SetEnvironmentVariable('PATH', \\"${meteorPath};$path\\", 'User');"`
+      `powershell -c "$path = (Get-Item 'HKCU:\\Environment').GetValue('Path', '', 'DoNotExpandEnvironmentNames'); [Environment]::SetEnvironmentVariable('PATH', \\"${meteorPath};$path\\", 'User');"`,
     );
     return;
   }
