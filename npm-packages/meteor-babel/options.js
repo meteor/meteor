cd5b55bcd806c56205ae36b59f43410c59e06c58
Jan Dvorak <storytellercz@proton.me>
2024-08-01 11:52:35 +0200

    Revert "Re-add #12327"
    This reverts commit e9214a3d9b4b6204cad0c5ee70f4103f3f2b1ce1.

--- npm-packages/meteor-babel/options.js
+++ npm-packages/meteor-babel/options.js
@@ -84,11 +84,7 @@ exports.getDefaults = function getDefaults(features) {
 
 function maybeAddReactPlugins(features, options) {
   if (features && features.react) {
-    options.presets.push(
-      [require("@babel/preset-react"), {
-        runtime: "automatic"
-      }]
-    );
+    options.presets.push(require("@babel/preset-react"));
     options.plugins.push(
       [require("@babel/plugin-proposal-class-properties"), {
         loose: true
