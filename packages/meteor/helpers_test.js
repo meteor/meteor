meteor/meteor

commit 87f8697941fddd0145ebb9680f8a8b456a66fe87

Adjust syntax to be es5

packages/meteor/helpers.js
@@ -80,11 +80,15 @@ Meteor._delete = function (obj /*, arguments */) {
  * @param errorFirst - If the callback follows the errorFirst style
  * @returns {function(...[*]): Promise<unknown>}
  */
-Meteor.promisify = function (fn, context, errorFirst = true) {
-  return function (...fnArgs) {
-    return new Promise((resolve, reject) => {
-      const callback = Meteor.bindEnvironment((error, result) => {
-        let _error = error, _result = result;
+Meteor.promisify = function (fn, context, errorFirst) {
+  if (errorFirst === undefined) {
+    errorFirst = true;
+  }
+
+  return function () {
+    return new Promise(function (resolve, reject) {
+      var callback = Meteor.bindEnvironment(function (error, result) {
+        var _error = error, _result = result;
         if (!errorFirst) {
           _error = result;
           _result = error;
@@ -97,7 +101,10 @@ Meteor.promisify = function (fn, context, errorFirst = true) {
         resolve(_result);
       });
 
-      const filteredArgs = [...fnArgs, callback].filter(i => i !== undefined);
+      var filteredArgs = Array.prototype.slice.call(arguments)
+        .filter(function (i) { return i !== undefined; });
+      filteredArgs.push(callback);
+
       return fn.apply(context || this, filteredArgs);
     });
   };

packages/babel-compiler/babel-compiler.js
@@ -80,7 +80,7 @@ BCp.processOneFileForTarget = function (inputFile, source) {
       // compilation, give it the following file extension: .es5.js
       ! excludedFileExtensionPattern.test(inputFilePath)) {
 
-    const features = { ...this.extraFeatures };
+    const features = Object.assign({}, this.extraFeatures);
     const arch = inputFile.getArch();
 
     if (arch.startsWith("os.")) {
@@ -122,7 +122,7 @@ BCp.processOneFileForTarget = function (inputFile, source) {
     this.inferExtraBabelOptions(
       inputFile,
       babelOptions,
-      cacheOptions.cacheDeps,
+      cacheOptions.cacheDeps
     );
 
     babelOptions.sourceMaps = true;

packages/babel-compiler/babel.js
@@ -26,7 +26,7 @@ Babel = {
     return getMeteorBabel().compile(
       source,
       babelOptions || getDefaultOptions(),
-      cacheOptions,
+      cacheOptions
     );
   },
 

packages/browser-policy-content/browser-policy-content.js
@@ -50,7 +50,7 @@ var keywords = {
 // If false, we set the X-Content-Type-Options header to 'nosniff'.
 var contentSniffingAllowed = false;
 
-const BrowserPolicy = require("meteor/browser-policy-common").BrowserPolicy;
+var BrowserPolicy = require("meteor/browser-policy-common").BrowserPolicy;
 BrowserPolicy.content = {};
 
 var parseCsp = function (csp) {
@@ -260,12 +260,12 @@ var resources = [
   { methodResource: "Frame", directive: "frame-src" },
   { methodResource: "FrameAncestors", directive: "frame-ancestors" }
 ];
-_.each(resources,  function (resource) {
-  var directive = resource.directive; 
-  var methodResource = resource.methodResource; 
+_.each(resources, function (resource) {
+  var directive = resource.directive;
+  var methodResource = resource.methodResource;
   var allowMethodName = "allow" + methodResource + "Origin";
   var disallowMethodName = "disallow" + methodResource;
-  var allowDataMethodName = "allow" + methodResource + "DataUrl"; 
+  var allowDataMethodName = "allow" + methodResource + "DataUrl";
   var allowBlobMethodName = "allow" + methodResource + "BlobUrl";
   var allowSelfMethodName = "allow" + methodResource + "SameOrigin";
 

packages/browser-policy-framing/browser-policy-framing.js
@@ -9,7 +9,7 @@
 var defaultXFrameOptions = "SAMEORIGIN";
 var xFrameOptions = defaultXFrameOptions;
 
-const BrowserPolicy = require("meteor/browser-policy-common").BrowserPolicy;
+var BrowserPolicy = require("meteor/browser-policy-common").BrowserPolicy;
 BrowserPolicy.framing = {};
 
 Object.assign(BrowserPolicy.framing, {

packages/context/package.js
@@ -10,7 +10,7 @@ Npm.depends({
 });
 
 Package.onUse(function(api) {
-  api.use("modules");
+  api.use("ecmascript");
   api.addFiles("server.js", "server");
   api.mainModule("context.js");
 });

packages/dev-error-overlay/package.js
@@ -7,7 +7,7 @@ Package.describe({
 
 Package.onUse(function (api) {
   api.use([
-    'modules'
+    'ecmascript'
   ]);
   api.export('DevErrorOverlay', 'client');
   api.addFiles('client.js', 'modern');

packages/dynamic-import/dynamic-versions.js
@@ -3,7 +3,7 @@
 // modules, for use in client.js and cache.js.
 var versions = __DYNAMIC_VERSIONS__;
 
-const METEOR_PREFIX = '/node_modules/meteor/';
+var METEOR_PREFIX = '/node_modules/meteor/';
 
 exports.get = function (id) {
   var tree = versions;

commit 08258246aec317ffe15eb35c8d4c7bee558127fc

Fix Meteor.promisify (2.x) (#13291)
* Fix Meteor.promisify and add a test for it (#13284)

This fixes a regression introduced in commit 87f8697941.

Co-authored-by: Nacho Codoñer <igcogi@gmail.com>

* fix syntax check

---------

Co-authored-by: Oleksandr Chekhovskyi <oleksandr.chekhovskyi@favro.com>
Co-authored-by: Leonardo Venturini <leovenbag@gmail.com>

packages/meteor/helpers.js
@@ -86,6 +86,10 @@ Meteor.promisify = function (fn, context, errorFirst) {
   }
 
   return function () {
+    var self = this;
+    var filteredArgs = Array.prototype.slice.call(arguments)
+      .filter(function (i) { return i !== undefined; });
+
     return new Promise(function (resolve, reject) {
       var callback = Meteor.bindEnvironment(function (error, result) {
         var _error = error, _result = result;
@@ -101,11 +105,9 @@ Meteor.promisify = function (fn, context, errorFirst) {
         resolve(_result);
       });
 
-      var filteredArgs = Array.prototype.slice.call(arguments)
-        .filter(function (i) { return i !== undefined; });
       filteredArgs.push(callback);
 
-      return fn.apply(context || this, filteredArgs);
+      return fn.apply(context || self, filteredArgs);
     });
   };
 };

packages/meteor/helpers_test.js
@@ -100,3 +100,29 @@ Tinytest.add("environment - startup", function (test) {
   });
   test.isTrue(called);
 });
+
+Tinytest.addAsync("environment - promisify", function (test, done) {
+  function TestClass(value) {
+    this.value = value;
+  }
+
+  TestClass.prototype.method = function (arg1, arg2, callback) {
+    var value = this.value;
+    setTimeout(function () {
+      callback(null, arg1 + arg2 + value);
+    }, 0);
+  };
+
+  TestClass.prototype.methodAsync = Meteor.promisify(TestClass.prototype.method);
+
+  var instance = new TestClass(5);
+  var asyncMethodWithContext = Meteor.promisify(instance.method, instance);
+
+  Promise.all([
+    instance.methodAsync(1, 2),
+    asyncMethodWithContext(2, 3),
+  ]).then(function (results) {
+    test.equal(results, [8, 10]);
+    done();
+  });
+});