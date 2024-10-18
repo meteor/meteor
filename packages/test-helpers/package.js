24d7a8dc3e053ecf24bba9bca95e59f0f40d1d73
Leonardo Venturini <leovenbag@gmail.com>
2024-10-18 09:51:17 -0400

    fix test-helpers

--- packages/test-helpers/package.js
+++ packages/test-helpers/package.js
@@ -3,12 +3,6 @@ Package.describe({
   version: "2.0.1",
 });
 
-Npm.depends({
-  'lodash.isequal': '4.5.0',
-  'lodash.isempty': '4.4.0',
-  'lodash.isstring': '4.0.1'
-});
-
 Package.onUse(function (api) {
   api.use([
     "ecmascript",
