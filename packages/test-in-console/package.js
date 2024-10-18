bb4ca37afde8ce149599718aa19cd01298e7a521
Leonardo Venturini <leovenbag@gmail.com>
2024-10-18 09:56:25 -0400

    cannot use it here

--- packages/test-in-console/package.js
+++ packages/test-in-console/package.js
@@ -4,8 +4,6 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('3.0.1');
-
   api.use(['tinytest', 'random', 'ejson', 'check', 'ecmascript']);
   api.use('fetch', 'server');
 
