6b06fdcb8c432ee15c6c365c2456c378a62f0622
Nacho Codoñer <igcogi@gmail.com>
2024-10-23 15:57:06 +0200

    log further

--- scripts/generate-dev-bundle.ps1
+++ scripts/generate-dev-bundle.ps1
@@ -185,7 +185,7 @@ Function Add-NodeAndNpm {
 
   # Let's install the npm version we really want.
   Write-Host "Installing npm@${NPM_VERSION}..." -ForegroundColor Magenta
-  & "$tempNpmCmd" install --prefix="$dirLib" --no-bin-links --save `
+  & "$tempNpmCmd" install --prefix="$dirLib" --no-bin-links --save --loglevel verbose `
     --cache="$dirNpmCache" --nodedir="$dirTempNode" npm@${NPM_VERSION} |
       Write-Debug
 
