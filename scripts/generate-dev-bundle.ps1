eec31ff5d3e73532fd87645c6cd3ab2524877aff
Leonardo Venturini <leovenbag@gmail.com>
2024-10-23 13:50:03 -0400

    try this

--- scripts/generate-dev-bundle.ps1
+++ scripts/generate-dev-bundle.ps1
@@ -189,7 +189,7 @@ Function Add-NodeAndNpm {
   # Let's install the npm version we really want.
   Write-Host "Installing npm@${NPM_VERSION}..." -ForegroundColor Magenta
 
-  & "$tempNpmCmd" --version | Write-Host
+  & "$tempNpmCmd" --version 2>&1 | Write-Host
 
   Write-Host (Get-Location)
   & "$tempNpmCmd" install --prefix="$dirLib" --no-bin-links --save `
