cfc20050d24f70bd0667a26a60ba5a8a8d15495c
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-06 19:03:13 +0300

    Add note about purpose of entry-meteor.js files.

---
+++ tools/static-assets/skel-vue/server/entry-meteor.js
@@ -0,0 +1,8 @@
+// Entrypoint for the Meteor server
+// Generally, this file can be left empty. Vite will add imports for
+// lazy-loaded Meteor packages to this file to ensure they are included in your
+// final production bundle.
+
+// This can also be a good place to put code that you don't want Vite to
+// process, for example, if you run into a compatability issue or need to use
+// nested imports.
