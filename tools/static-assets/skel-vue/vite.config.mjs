a6442546e68490c90400cf1cc2061869bcbb3ebb
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-06 19:19:46 +0300

    Use .mjs file extension for Vite config to avoid CJS interop exceptions

--- tools/static-assets/skel-vue/vite.config.mjs
+++ tools/static-assets/skel-vue/vite.config.mjs
@@ -1,5 +1,5 @@
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from 'tailwindcss';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import { meteor } from 'meteor-vite/plugin';
 
