b575286226c74fe336a80b2892dd8395a4e63787
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-06 19:12:45 +0300

    Upgrade Tailwind to v4 in Vue skeleton

--- tools/static-assets/skel-vue/vite.config.js
+++ tools/static-assets/skel-vue/vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import tailwindcss from 'tailwindcss';
+import { defineConfig } from 'vite';
 import { meteor } from 'meteor-vite/plugin';
 
 export default defineConfig({
   plugins: [
     vue(),
+    tailwindcss(),
     meteor({
       clientEntry: 'client/main.js',
       serverEntry: 'server/main.js',
