7cf7f98e1c80bb1e26ce48eb75e4fcf05b970116
italo jose <italo.i@live.com>
2025-08-18 20:10:51 -0300

    Add debug logging for error handling in geoPolygonSchema test

--- packages/mongo/tests/mongo_livedata_tests.js
+++ packages/mongo/tests/mongo_livedata_tests.js
@@ -4607,6 +4607,7 @@ Tinytest.addAsync('mongo-livedata - publish with geoPolygonSchema does not throw
           });
         });
       } catch (e) {
+        console.log('ERRO-DEBUG]', e);
         error = e;
       }
       test.isNull(error, 'shouldnt throw error for valid viewport');
