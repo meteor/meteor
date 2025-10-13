90f73c5b63cd2ebabd15bc06f781a3c3c0aca85a
rishu685 <rishabh45628@gmail.com>
2025-10-13 23:54:13 +0530

    refactor: Remove duplicated storeOAuthError function
    - Extract shared storeOAuthError function to oauth_utils.js
    - Update end_of_popup_response.js and end_of_redirect_response.js to use shared utility
    - Update HTML templates to load shared utility first
    - Eliminates code duplication while maintaining functionality

--- packages/oauth/end_of_popup_response.js
+++ packages/oauth/end_of_popup_response.js
@@ -1,15 +1,6 @@
 // NOTE: This file is added to the client as asset and hence ecmascript package has no effect here.
 (function() {
 
-  function storeOAuthError(storage, config) {
-    if (config.error) {
-      storage[config.storagePrefix + "error"] = config.error;
-      if (config.error_description) {
-        storage[config.storagePrefix + "error_description"] = config.error_description;
-      }
-    }
-  }
-
   var config = JSON.parse(document.getElementById("config").innerHTML);
 
   if (config.setCredentialToken) {
@@ -34,11 +25,11 @@
     }
   }
 
-  storeOAuthError(localStorage, config);
+  OAuthUtils.storeOAuthError(localStorage, config);
   
   if (config.error && window.opener && window.opener.Package &&
       window.opener.Package.oauth && window.opener.Package.oauth.OAuth._handleCredentialSecret) {
-    storeOAuthError(window.opener.localStorage, config);
+    OAuthUtils.storeOAuthError(window.opener.localStorage, config);
   }
 
   if (! config.isCordova) {
--- packages/oauth/end_of_redirect_response.js
+++ packages/oauth/end_of_redirect_response.js
@@ -1,15 +1,6 @@
 // NOTE: This file is added to the client as asset and hence ecmascript package has no effect here.
 (function() {
 
-  function storeOAuthError(storage, config) {
-    if (config.error) {
-      storage[config.storagePrefix + "error"] = config.error;
-      if (config.error_description) {
-        storage[config.storagePrefix + "error_description"] = config.error_description;
-      }
-    }
-  }
-
   var config = JSON.parse(document.getElementById("config").innerHTML);
 
   if (config.setCredentialToken) {
@@ -17,7 +8,7 @@
       config.credentialSecret;
   }
 
-  storeOAuthError(localStorage, config);
+  OAuthUtils.storeOAuthError(localStorage, config);
 
   window.location =
     config.redirectUrl
