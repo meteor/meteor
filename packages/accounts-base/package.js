ff63ebe111e93b1a294026c0b9e3beb75d51ff8d
italo jose <italo.i@live.com>
2026-02-11 14:34:08 -0300

    release: prepare v3.5.0-beta
    - bump bundle and installer: meteor BUNDLE_VERSION -> 22.22.0.4, meteor-installer -> 3.5.0-beta
    - bump package versions: accounts-base, ddp-server, mongo, mongo-decimal, oauth1, service-configuration, session
    - update package references (.versions) and modern-tests apps to use new ddp-server/mongo versions
    - mark packages/non-core/blaze subproject as dirty
    - add v3.5.0 changelog (v3-docs)

--- packages/accounts-base/package.js
+++ packages/accounts-base/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A user account system",
-  version: "3.2.0",
+  version: "3.2.1-beta350",
 });
 
 Package.onUse((api) => {
--- packages/oauth1/package.js
+++ packages/oauth1/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Common code for OAuth1-based login services",
-  version: '1.5.2',
+  version: '1.5.3-beta350',
 });
 
 Package.onUse(api => {
