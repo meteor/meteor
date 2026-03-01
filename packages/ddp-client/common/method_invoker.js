08560e45ad9c6c25d340fb81dfd952a17ba6e33a
Harry Adel <harryadelb@gmail.com>
2026-03-02 01:55:42 +0200

    Defer method cleanup and reconnect sends until async callbacks settle

--- packages/ddp-client/common/method_invoker.js
+++ packages/ddp-client/common/method_invoker.js
@@ -49,14 +49,24 @@ export class MethodInvoker {
     if (this._methodResult && this._dataVisible) {
       // Call the callback. (This won't throw: the callback was wrapped with
       // bindEnvironment.)
-      this._callback(this._methodResult[0], this._methodResult[1]);
+      const result = this._callback(this._methodResult[0], this._methodResult[1]);
 
-      // Forget about this method.
-      delete this._connection._methodInvokers[this.methodId];
+      const finish = () => {
+        // Forget about this method.
+        delete this._connection._methodInvokers[this.methodId];
 
-      // Let the connection know that this method is finished, so it can try to
-      // move on to the next block of methods.
-      this._connection._outstandingMethodFinished();
+        // Let the connection know that this method is finished, so it can try to
+        // move on to the next block of methods.
+        this._connection._outstandingMethodFinished();
+      };
+
+      // If the callback returned a Promise (async callback), defer cleanup
+      // until the Promise settles. Otherwise, run cleanup immediately.
+      if (result && typeof result.then === 'function') {
+        result.then(finish, finish);
+      } else {
+        finish();
+      }
     }
   }
   // Call with the result of the method from the server. Only may be called
