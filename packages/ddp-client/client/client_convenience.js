65fe9aba381f7bbb673be81e8edfaa03586a8dce
alexta <terpyakov@gmail.com>
2026-03-04 01:34:05 +0300

    ddp-client: use browser host for default DDP connection

--- packages/ddp-client/client/client_convenience.js
+++ packages/ddp-client/client/client_convenience.js
@@ -2,27 +2,28 @@ import { DDP } from '../common/namespace.js';
 import { Meteor } from 'meteor/meteor';
 import { loadAsyncStubHelpers } from "./queue_stub_helpers";
 
+const getDDPUrl = () => {
+  const absoluteUrl = Meteor.absoluteUrl();
+  const protocol = absoluteUrl.split('//')[0] || window.location.protocol;
+  const runtimeConfig = typeof __meteor_runtime_config__ !== 'undefined'
+    ? __meteor_runtime_config__
+    : Object.create(null);
+
+  if (runtimeConfig.DDP_DEFAULT_CONNECTION_URL) {
+    return runtimeConfig.DDP_DEFAULT_CONNECTION_URL;
+  }
+
+  return `${protocol}//${window.location.host}/`;
+};
+
 // Meteor.refresh can be called on the client (if you're in common code) but it
 // only has an effect on the server.
 Meteor.refresh = () => {};
 
-// By default, try to connect back to the same endpoint as the page
-// was served from.
-//
-// XXX We should be doing this a different way. Right now we don't
-// include ROOT_URL_PATH_PREFIX when computing ddpUrl. (We don't
-// include it on the server when computing
-// DDP_DEFAULT_CONNECTION_URL, and we don't include it in our
-// default, '/'.) We get by with this because DDP.connect then
-// forces the URL passed to it to be interpreted relative to the
-// app's deploy path, even if it is absolute. Instead, we should
-// make DDP_DEFAULT_CONNECTION_URL, if set, include the path prefix;
-// make the default ddpUrl be '' rather that '/'; and make
-// _translateUrl in stream_client_common.js not force absolute paths
-// to be treated like relative paths. See also
-// stream_client_common.js #RationalizingRelativeDDPURLs
-const runtimeConfig = typeof __meteor_runtime_config__ !== 'undefined' ? __meteor_runtime_config__ : Object.create(null);
-const ddpUrl = runtimeConfig.DDP_DEFAULT_CONNECTION_URL || '/';
+// By default, connect to the current browser host so mirrored domains
+// establish their websocket connection against the same host users loaded.
+// Keep the protocol from Meteor.absoluteUrl() to preserve force-ssl behavior.
+const ddpUrl = getDDPUrl() || '/';
 
 const retry = new Retry();
 
