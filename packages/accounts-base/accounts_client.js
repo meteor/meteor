a25a233c39d01d7d4bb80a23f6eae4d51382f0a4
Michael Vogt <mtpfeif@gmail.com>
2026-03-06 14:20:27 -0600

    refactor(accounts-base): DRY link callback registration methods

--- packages/accounts-base/accounts_client.js
+++ packages/accounts-base/accounts_client.js
@@ -710,6 +710,16 @@ export class AccountsClient extends AccountsCommon {
     attemptToMatchHash(this, this.savedHash, defaultSuccessHandler);
   };
 
+  _registerLinkCallback(type, callback) {
+    if (this._accountsCallbacks[type]) {
+      Meteor._debug(
+        `Accounts callback for "${type}" was registered more than once. ` +
+        "Only one callback added will be executed."
+      );
+    }
+    this._accountsCallbacks[type] = callback;
+  };
+
   /**
    * @summary Register a function to call when a reset password link is clicked
    * in an email sent by
@@ -728,12 +738,7 @@ export class AccountsClient extends AccountsCommon {
    * @locus Client
    */
   onResetPasswordLink(callback) {
-    if (this._accountsCallbacks["reset-password"]) {
-      Meteor._debug("Accounts.onResetPasswordLink was called more than once. " +
-        "Only one callback added will be executed.");
-    }
-
-    this._accountsCallbacks["reset-password"] = callback;
+    this._registerLinkCallback("reset-password", callback);
   };
 
   /**
@@ -755,12 +760,7 @@ export class AccountsClient extends AccountsCommon {
    * @locus Client
    */
   onEmailVerificationLink(callback) {
-    if (this._accountsCallbacks["verify-email"]) {
-      Meteor._debug("Accounts.onEmailVerificationLink was called more than once. " +
-        "Only one callback added will be executed.");
-    }
-
-    this._accountsCallbacks["verify-email"] = callback;
+    this._registerLinkCallback("verify-email", callback);
   };
 
   /**
@@ -782,12 +782,7 @@ export class AccountsClient extends AccountsCommon {
    * @locus Client
    */
   onEnrollmentLink(callback) {
-    if (this._accountsCallbacks["enroll-account"]) {
-      Meteor._debug("Accounts.onEnrollmentLink was called more than once. " +
-        "Only one callback added will be executed.");
-    }
-
-    this._accountsCallbacks["enroll-account"] = callback;
+    this._registerLinkCallback("enroll-account", callback);
   };
 
 }
