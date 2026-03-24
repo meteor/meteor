04281498a023e9ff89e040d99d489557f858ef94
italo jose <italo.i@live.com>
2026-03-24 11:05:14 -0300

    refactor: use `URL` from the npm `url` package and improve `METEOR_MODERN` environment variable parsing.

--- packages/url/bc/url_server.js
+++ packages/url/bc/url_server.js
@@ -1,7 +1,8 @@
 var common = require("./url_common.js");
+var { URL } = Npm.require('url');
 
 exports._constructUrl = function (urlString, query, params) {
-  var url_parts = new globalThis.URL(urlString);
+  var url_parts = new URL(urlString);
   return common.buildUrl(
     url_parts.protocol + "//" + url_parts.host + url_parts.pathname,
     url_parts.search,
