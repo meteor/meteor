c4a9c5e099b6fb8b5990c9ecc349ef88f65eac0f
dupontbertrand <dupontbertrand59@gmail.com>
2026-04-01 23:50:29 +0200

    Add Slack OAuth packages (slack-oauth, accounts-slack, slack-config-ui)

---
+++ packages/slack-oauth/package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  summary: 'Slack OAuth flow',
+  version: '1.0.0',
+});
+
+Package.onUse(api => {
+  api.use('ecmascript', ['client', 'server']);
+  api.use('oauth2', ['client', 'server']);
+  api.use('oauth', ['client', 'server']);
+  api.use('fetch', 'server');
+  api.use('random', 'client');
+  api.use('accounts-base', ['client', 'server']);
+  api.use('service-configuration', ['client', 'server']);
+
+  api.addFiles('slack_client.js', 'client');
+  api.addFiles('slack_server.js', 'server');
+
+  api.export('Slack');
+});
+
+Package.onTest(function(api) {
+  api.use('slack-oauth');
+  api.use(['tinytest', 'ecmascript', 'test-helpers', 'oauth', 'oauth2', 'service-configuration']);
+  api.addFiles('slack-oauth_tests.js');
+});
