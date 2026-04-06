c8bd7f827d1f2402cb1faf66b51039361919849e
italo jose <italo.i@live.com>
2026-04-06 18:24:37 -0300

    types(tracker): replace Function types with specific callback signatures
    - onInvalidate: (computation: Computation) => void
    - onStop: (computation: Computation) => void
    - afterFlush: () => void
    - onError: (error: Error) => void
    - firstRunPromise: Promise<void> instead of Promise<unknown>

--- packages/tracker/tracker.d.ts
+++ packages/tracker/tracker.d.ts
@@ -19,7 +19,7 @@ export namespace Tracker {
     /**
      * Forces autorun blocks to be executed in synchronous-looking order by storing the value autorun promise thus making it awaitable.
      */
-    firstRunPromise: Promise<unknown>
+    firstRunPromise: Promise<void>;
     /**
      * Invalidates this computation so that it will be rerun.
      */
@@ -33,12 +33,12 @@ export namespace Tracker {
      * future invalidations unless `onInvalidate` is called again after the computation becomes valid again.
      * @param callback Function to be called on invalidation. Receives one argument, the computation that was invalidated.
      */
-    onInvalidate(callback: Function): void;
+    onInvalidate(callback: (computation: Computation) => void): void;
     /**
      * Registers `callback` to run when this computation is stopped, or runs it immediately if the computation is already stopped.  The callback is run after any `onInvalidate` callbacks.
      * @param callback Function to be called on stop. Receives one argument, the computation that was stopped.
      */
-    onStop(callback: Function): void;
+    onStop(callback: (computation: Computation) => void): void;
     /**
      * Prevents this computation from rerunning.
      */
@@ -93,7 +93,7 @@ export namespace Tracker {
    * once and not on subsequent flushes unless `afterFlush` is called again.
    * @param callback A function to call at flush time.
    */
-  function afterFlush(callback: Function): void;
+  function afterFlush(callback: () => void): void;
 
   /**
    * Run a function now and rerun it later whenever its dependencies
@@ -109,7 +109,7 @@ export namespace Tracker {
        * happens in the Computation. The only argument it receives is the Error
        * thrown. Defaults to the error being logged to the console.
        */
-      onError?: Function | undefined;
+      onError?: ((error: Error) => void) | undefined;
     }
   ): Computation;
 
@@ -138,5 +138,5 @@ export namespace Tracker {
    * Registers a new `onInvalidate` callback on the current computation (which must exist), to be called immediately when the current computation is invalidated or stopped.
    * @param callback A callback function that will be invoked as `func(c)`, where `c` is the computation on which the callback is registered.
    */
-  function onInvalidate(callback: Function): void;
+  function onInvalidate(callback: (computation: Computation) => void): void;
 }
