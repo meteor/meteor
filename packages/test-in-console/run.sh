0dec6fb6b543f617c3d1f2b0666831be16c8725b
Gabriel Grubba <gabriel@discourse.org>
2026-04-14 09:55:39 -0300

    DEV: remove line

--- packages/test-in-console/run.sh
+++ packages/test-in-console/run.sh
@@ -4,13 +4,6 @@
 # ./packages/test-in-console/run.sh
 # or for a specific package
 # ./packages/test-in-console/run.sh "mongo"
-# or to run tests in parallel (default 4 concurrent package groups)
-# ./packages/test-in-console/run.sh --parallel
-# ./packages/test-in-console/run.sh --parallel=8
-# ./packages/test-in-console/run.sh "mongo" --parallel
-# packages listed in --serial-packages run one-at-a-time after all other packages
-# (defaults to "mongo" when --parallel is used)
-# ./packages/test-in-console/run.sh --parallel --serial-packages="mongo,allow-deny"
 
 cd $(dirname $0)/../..
 export METEOR_HOME=`pwd`
@@ -28,51 +21,9 @@ export PATH=$METEOR_HOME:$PATH
 export URL='http://127.0.0.1:4096/'
 export METEOR_PACKAGE_DIRS='packages/deprecated'
 
-# Parse arguments: separate the package filter (first positional arg) from flags.
-PACKAGE_FILTER=""
-PARALLEL_FLAG=""
-SERIAL_PACKAGES_FLAG=""
-PORT="${TEST_PORT:-4096}"
-for arg in "$@"; do
-  case "$arg" in
-    --parallel|--parallel=*)
-      PARALLEL_FLAG="$arg"
-      ;;
-    --port=*)
-      PORT="${arg#--port=}"
-      ;;
-    --serial-packages=*)
-      SERIAL_PACKAGES_FLAG="$arg"
-      ;;
-    *)
-      # Treat the first non-flag argument as the package filter.
-      if [[ -z "$PACKAGE_FILTER" ]]; then
-        PACKAGE_FILTER="$arg"
-      fi
-      ;;
-  esac
-done
-
-export URL="http://127.0.0.1:$PORT/"
-MONGO_PORT=$(($PORT + 1))
-
-# Kill all processes related to a test run on the given port (meteor tools,
-# server app, mongod). Sending SIGTERM first then SIGKILL for mongod.
-cleanup() {
-  pkill -TERM -f "test-packages.*-p $PORT" 2>/dev/null
-  pkill -TERM -f "mongod.*--port $MONGO_PORT" 2>/dev/null
-  sleep 1
-  pkill -9 -f "mongod.*--port $MONGO_PORT" 2>/dev/null
-  pkill -TERM -P $EXEC_PID 2>/dev/null
-}
-
-EXCLUDE_FLAG=""
-if [[ -n "${TEST_PACKAGES_EXCLUDE:-}" ]]; then
-  EXCLUDE_FLAG="--exclude $TEST_PACKAGES_EXCLUDE"
-fi
-exec 3< <(./meteor test-packages --driver-package test-in-console -p "$PORT" $EXCLUDE_FLAG $PARALLEL_FLAG $SERIAL_PACKAGES_FLAG $PACKAGE_FILTER)
+exec 3< <(./meteor test-packages --driver-package test-in-console -p 4096 --exclude ${TEST_PACKAGES_EXCLUDE:-''} $1)
 EXEC_PID=$!
-trap "cleanup; exit 1" SIGINT SIGTERM
+trap "pkill -TERM -P $EXEC_PID; exit 1" SIGINT
 
 sed '/test-in-console listening$/q' <&3
 
@@ -89,5 +40,5 @@ node --trace-warnings "$METEOR_HOME/packages/test-in-console/puppeteer_runner.js
 
 STATUS=$?
 
-cleanup
+pkill -TERM -P $EXEC_PID
 exit $STATUS
