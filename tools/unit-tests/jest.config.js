9124e8d994f7bc07728ab22d6b96514181269ef9
italo jose <italo.i@live.com>
2026-04-14 14:44:56 -0300

    chore: add scripts/check-type-test-coverage to jest test path ignore patterns

--- tools/unit-tests/jest.config.js
+++ tools/unit-tests/jest.config.js
@@ -14,6 +14,7 @@ module.exports = {
     "<rootDir>/tools/tests/",
     "<rootDir>/packages/",
     "<rootDir>/.github/",
+    "<rootDir>/scripts/check-type-test-coverage/",
   ],
   modulePathIgnorePatterns: [
     "<rootDir>/tools/modern-tests/",
