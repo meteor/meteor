ac69ac402c3a958e946df5431a1afee1f78bf6f8
Harry Adel <harryadelb@gmail.com>
2026-07-08 20:10:44 +0300

    Update packages/mongo/tests/hooks/upsert.js
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- packages/mongo/tests/hooks/upsert.js
+++ packages/mongo/tests/hooks/upsert.js
@@ -121,7 +121,7 @@ if (Meteor.isServer) {
       test.equal(counts.before.insert, 0, "before.insert should be 0");
       test.equal(counts.before.update, 0, "before.update should be 0");
       test.equal(counts.before.remove, 0, "before.remove should be 0");
-      test.equal(counts.before.upsert, 2, "before.insert should be 2");
+      test.equal(counts.before.upsert, 2, "before.upsert should be 2");
       test.equal(counts.after.insert, 1, "after.insert should be 1");
       test.equal(counts.after.update, 1, "after.update should be 1");
       test.equal(counts.after.remove, 0, "after.remove should be 0");
