4e9626e966c020e5c3e609b08a8da85c5f1e67fe
npt-1707 <npthanh132@gmail.com>
2026-08-17 05:36:54 +0800

    packages/deprecated/bootstrap/js/bootstrap.js: Fix XSS in Modal

--- packages/deprecated/bootstrap/js/bootstrap.js
+++ packages/deprecated/bootstrap/js/bootstrap.js
@@ -1030,7 +1030,10 @@
   $(document).on('click.modal.data-api', '[data-toggle="modal"]', function (e) {
     var $this = $(this)
       , href = $this.attr('href')
-      , $target = $($this.attr('data-target') || (href && href.replace(/.*(?=#[^\s]+$)/, ''))) //strip for ie7
+    var target  = $this.attr('data-target') ||
+      (href && href.replace(/.*(?=#[^\s]+$)/, '')) // strip for ie7
+
+    var $target = $(document).find(target)
       , option = $target.data('modal') ? 'toggle' : $.extend({ remote:!/#/.test(href) && href }, $target.data(), $this.data())
 
     e.preventDefault()
