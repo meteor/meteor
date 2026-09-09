25684bece82ce72831c8af81168433ee045f25e0
Nacho Codoñer <igcogi@gmail.com>
2026-09-09 16:50:23 +0200

    Keep Rspack proxy unit tests alongside the package

--- tools/unit-tests/jest.config.js
+++ tools/unit-tests/jest.config.js
@@ -7,13 +7,14 @@ module.exports = {
   testMatch: [
     "<rootDir>/tools/**/*.test.js",
     "<rootDir>/scripts/**/*.test.js",
+    // Opt package unit tests in explicitly; other packages use Tinytest.
+    "<rootDir>/packages/rspack/tests/**/*.test.js",
   ],
   testPathIgnorePatterns: [
     "/node_modules/",
     "<rootDir>/tools/e2e-tests/",
     "<rootDir>/tools/native-tests/",
     "<rootDir>/tools/tests/",
-    "<rootDir>/packages/",
     "<rootDir>/.github/",
   ],
   modulePathIgnorePatterns: [
