fd576c74c5109ffe268aea476bbf7b3bf7304159
Nacho Codoñer <igcogi@gmail.com>
2026-09-21 14:31:29 +0200

    Handle IPC shutdown races during app restarts

--- packages/inter-process-messaging/package.js
+++ packages/inter-process-messaging/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "inter-process-messaging",
-  version: '0.1.3',
+  version: '0.1.4-beta360.0',
   summary: "Support for sending messages from the build process to the server process",
   documentation: "README.md"
 });
